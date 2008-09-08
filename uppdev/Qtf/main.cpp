56a6c1aa8b0172b7adc01c3cba2753a2e7e16c4b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2008-09-08 17:57:47 +0000

    git-svn-id: svn://ultimatepp.org/upp/trunk@408 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/Qtf/main.cpp
+++ uppdev/Qtf/main.cpp
@@ -4,11 +4,11 @@ using namespace Upp;
 
 GUI_APP_MAIN
 {
-	String r_title = "This is [MY TITLE!]";
+	String r_title = "[t This] is [MY TITLE!]";
 
 	String qtf;
 	qtf << " && [*= " << DeQtf(r_title)
-	    << "&& [_= Analysis Report]&";
+	    << "&& [_= Analysis [t Report]]&";
 	
 	Report r;
 	r.Header("[A2> Page $$P");
@@ -24,7 +24,7 @@ void Foo()
 	dw.DrawRect(50, 50, 50, 50, Yellow);
 	QtfRichObject pict(CreateDrawingObject(dw.GetResult(), Size(500, 500), Size(500, 500)));
 	String qtf;
-	qtf << "[A6 This is some drawing in QTF: " << pict << "&";
+	qtf << "[A6 This is some [t drawing] in QTF: " << pict << "&";
 	qtf << "[C3*@B And now in the table: {{1:1 " << pict << "::H100 Another cell " << pict << "}}";
 	qtf << "[A6ph Some text ajdsfh akjds fakjs fjkash dfalsd fhal fakhjds fkahjds fka fkajhs dfka&]";
 	qtf << "[A6pd Some text ajdsfh akjds fakjs fjkash dfalsd fhal fakhjds fkahjds fka fkajhs dfka&]";
