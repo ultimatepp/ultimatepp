5b719a0180f670572ee10af7f87684a1ecb1f438
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2008-12-26 19:32:31 +0000

    SDraw Xft/FreeType character rendering in SDraw
    git-svn-id: svn://ultimatepp.org/upp/trunk@719 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/SDrawTest/main.cpp
+++ uppdev/SDrawTest/main.cpp
@@ -230,96 +230,6 @@ void PaintLion(SDraw *sw, Draw *w)
 		w->DrawPolygon(p, color);
 }
 
-inline double fx_to_dbl(const FIXED& p) {
-	return double(p.value) + double(p.fract) * (1.0 / 65536.0);
-}
-
-void RenderCharPath(const char* gbuf,
-                    unsigned total_size,
-                    SDraw& sw)
-{
-	bool flip_y = true;
-    const char* cur_glyph = gbuf;
-    const char* end_glyph = gbuf + total_size;
-    double x, y;
-    
-    while(cur_glyph < end_glyph)
-    {
-        const TTPOLYGONHEADER* th = (TTPOLYGONHEADER*)cur_glyph;
-        
-        const char* end_poly = cur_glyph + th->cb;
-        const char* cur_poly = cur_glyph + sizeof(TTPOLYGONHEADER);
-
-        x = fx_to_dbl(th->pfxStart.x);
-        y = fx_to_dbl(th->pfxStart.y);
-        if(flip_y) y = -y;
-        sw.MoveTo(x, y);
-
-        while(cur_poly < end_poly)
-        {
-            const TTPOLYCURVE* pc = (const TTPOLYCURVE*)cur_poly;
-            
-            if (pc->wType == TT_PRIM_LINE)
-            {
-                int i;
-                for (i = 0; i < pc->cpfx; i++)
-                {
-                    x = fx_to_dbl(pc->apfx[i].x);
-                    y = fx_to_dbl(pc->apfx[i].y);
-                    if(flip_y) y = -y;
-                    sw.LineTo(x, y);
-                }
-            }
-            
-            if (pc->wType == TT_PRIM_QSPLINE)
-            {
-                int u;
-                for (u = 0; u < pc->cpfx - 1; u++)  // Walk through points in spline
-                {
-                    POINTFX pnt_b = pc->apfx[u];    // B is always the current point
-                    POINTFX pnt_c = pc->apfx[u+1];
-                    
-                    if (u < pc->cpfx - 2)           // If not on last spline, compute C
-                    {
-                        // midpoint (x,y)
-                        *(int*)&pnt_c.x = (*(int*)&pnt_b.x + *(int*)&pnt_c.x) / 2;
-                        *(int*)&pnt_c.y = (*(int*)&pnt_b.y + *(int*)&pnt_c.y) / 2;
-                    }
-                    
-                    double x2, y2;
-                    x  = fx_to_dbl(pnt_b.x);
-                    y  = fx_to_dbl(pnt_b.y);
-                    x2 = fx_to_dbl(pnt_c.x);
-                    y2 = fx_to_dbl(pnt_c.y);
-                    if(flip_y) { y = -y; y2 = -y2; }
-                    sw.Quadratic(x, y, x2, y2);
-                }
-            }
-            cur_poly += sizeof(WORD) * 2 + sizeof(POINTFX) * pc->cpfx;
-        }
-        cur_glyph += th->cb;
-    }
-}
-
-void RenderCharacter(SDraw& sw, int ch, Font fnt)
-{
-	ScreenDraw w;
-	w.SetFont(fnt);
-	GLYPHMETRICS gm;
-	MAT2 m_matrix;
-    memset(&m_matrix, 0, sizeof(m_matrix));
-    m_matrix.eM11.value = 1;
-    m_matrix.eM22.value = 1;
-	int gsz = GetGlyphOutlineW(w.GetHandle(), ch, GGO_NATIVE, &gm, 0, NULL, &m_matrix);
-	if(gsz < 0)
-		return;
-	Buffer<char> gb(gsz);
-	gsz = GetGlyphOutlineW(w.GetHandle(), ch, GGO_NATIVE, &gm, gsz, gb, &m_matrix);
-	if(gsz < 0)
-		return;
-	RenderCharPath(gb, gsz, sw);
-}
-
 struct App : TopWindow {
 	Point p;
 
@@ -335,8 +245,8 @@ struct App : TopWindow {
 		SDraw agd(ib);
 		
 	//	agd.Scale(p.x / 100.0, p.y / 100.0);
-//		agd.Rotate(p.y / 100.00);
-//		agd.Translate(500, 300);
+		agd.Rotate(p.y / 100.00);
+		agd.Translate(500, 300);
 //		agd.Scale(1.4);
 //		agd.MoveTo(100, 100).LineTo(200.5, 100).LineTo(200, 200).LineTo(0, 300).Stroke(Blue(), 10);
 /*
