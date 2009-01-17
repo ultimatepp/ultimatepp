4377b9f6449600c8563d0878b0fac86c2c8c90ce
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-01-17 11:58:39 +0000

    Improved PainterExamples
    git-svn-id: svn://ultimatepp.org/upp/trunk@778 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/PainterExamples/RadialFocus.cpp
+++ examples/PainterExamples/RadialFocus.cpp
@@ -3,8 +3,7 @@
 void RadialFocus(Painter& sw)
 {
 	sw.Circle(400.5, 400.5, 200)
-	  .ColorStop(0.7, LtBlue())
-	  .Fill(300, 300, White(), 400.5, 400.5, 200, Black())
+	  .Fill(300, 300, White(), 400.5, 400.5, 200, LtBlue())
 	;
 }
 
