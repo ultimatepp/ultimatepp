8445ecc7903389803f1ed3b2055df36286568d0b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-02-13 15:15:06 +0000

    TheIDE: IconDes keys now definable
    git-svn-id: svn://ultimatepp.org/upp/trunk@861 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/IconDes/Bar.cpp
+++ uppsrc/IconDes/Bar.cpp
@@ -2,6 +2,11 @@
 
 NAMESPACE_UPP
 
+#define KEYNAMESPACE IconDesKeys
+#define KEYGROUPNAME "Icon designer"
+#define KEYFILE      <IconDes/IconDes.key>
+#include             <CtrlLib/key_source.h>
+
 void IconDes::SetPen(int _pen)
 {
 	pen = _pen;
@@ -92,92 +97,80 @@ void IconDes::EditBar(Bar& bar)
 
 void IconDes::SettingBar(Bar& bar)
 {
+	using namespace IconDesKeys;
 	Slot *c = IsCurrent() ? &Current() : NULL;
-	bar.Add(c, "Zoom out", IconDesImg::ZoomMinus(), THISBACK(ZoomOut))
-		.Enable(magnify > 1)
-		.Key(K_SHIFT_CTRL_SUBRACT);
-	bar.Add(c, "Zoom in",  IconDesImg::ZoomPlus(), THISBACK(ZoomIn))
-		.Enable(magnify < 27)
-		.Key(K_SHIFT_CTRL_ADD);
-	bar.Add("Paste transparent/opaque",
+	bar.Add(c, AK_ZOOM_IN, IconDesImg::ZoomMinus(), THISBACK(ZoomOut))
+		.Enable(magnify > 1);
+	bar.Add(c, AK_ZOOM_OUT,  IconDesImg::ZoomPlus(), THISBACK(ZoomIn))
+		.Enable(magnify < 27);
+	bar.Add(AK_PASTE_MODE,
 	        paste_opaque ? IconDesImg::PasteOpaque()
 	                     : IconDesImg::PasteTransparent(),
-	        THISBACK(TogglePaste))
-	   .Key(K_ALT_T);
+	        THISBACK(TogglePaste));
 }
 
 void IconDes::SelectBar(Bar& bar)
 {
+	using namespace IconDesKeys;
 	Slot *c = IsCurrent() ? &Current() : NULL;
-	bar.Add(c, "Select", IconDesImg::Select(), THISBACK(Select))
-	   .Check(doselection)
-	   .Key(K_SPACE);
-	bar.Add(c, "Invert selection", IconDesImg::InvertSelect(), THISBACK(InvertSelect))
-	   .Key(K_CTRL_I);
-	bar.Add(c, "Cancel selection", IconDesImg::CancelSelect(), THISBACK(CancelSelect))
-	   .Key(K_CTRL_E);
-	bar.Add(c, "Select and move rectangle", IconDesImg::SelectRect(), THISBACK(SelectRect))
-	   .Check(selectrect)
-	   .Key(K_CTRL_R);
-	bar.Add(c, "Move", IconDesImg::Move(), THISBACK(Move))
-	   .Check(IsPasting())
-	   .Key(K_CTRL_M);
+	bar.Add(c, AK_SELECT, IconDesImg::Select(), THISBACK(Select))
+	   .Check(doselection);
+	bar.Add(c, AK_INVERT_SEL, IconDesImg::InvertSelect(), THISBACK(InvertSelect));
+	bar.Add(c, AK_CANCEL_SEL, IconDesImg::CancelSelect(), THISBACK(CancelSelect));
+	bar.Add(c, AK_SELECT_MOVE, IconDesImg::SelectRect(), THISBACK(SelectRect))
+	   .Check(selectrect);
+	bar.Add(c, AK_MOVE, IconDesImg::Move(), THISBACK(Move))
+	   .Check(IsPasting());
 }
 
 void IconDes::ImageBar(Bar& bar)
 {
+	using namespace IconDesKeys;
 	Slot *c = IsCurrent() ? &Current() : NULL;
 	if(c && c->supersampling)
 		bar.Add(c, "Smooth resize 3x down", IconDesImg::ResizeDown(), THISBACK(ResizeDown))
-		   .Key(K_TAB);
+		   .Key(AK_SUPERSAMPLING);
 	else
 		bar.Add(c && c->image.GetLength() < 256 * 256, "Resize 3x up (supersampling)",
 	    	    IconDesImg::ResizeUp(), THISBACK(ResizeUp))
-	       .Key(K_TAB);
+	       .Key(AK_SUPERSAMPLING);
 	bar.Separator();
-	bar.Add(c, "Set color", IconDesImg::SetColor(), THISBACK(SetColor));
-	bar.Add(c, "Empty", IconDesImg::Delete(), THISBACK(DoDelete))
-	   .Key(K_DELETE);
-	bar.Add(c, "Interpolate", IconDesImg::Interpolate(), THISBACK(Interpolate));
-	bar.Add(c, "Horizontal mirror", IconDesImg::MirrorX(), THISBACK(MirrorX));
-	bar.Add(c, "Vertical mirror", IconDesImg::MirrorY(), THISBACK(MirrorY));
-	bar.Add(c, "Horizontal symmetry", IconDesImg::SymmX(), THISBACK(SymmX));
-	bar.Add(c, "Vertical symmetry", IconDesImg::SymmY(), THISBACK(SymmY));
-	bar.Add(c, "Rotate", IconDesImg::Rotate(), THISBACK(Rotate));
-	bar.Add(c, "Free Rotate", IconDesImg::FreeRotate(), THISBACK(FreeRotate));
-	bar.Add(c, "Smooth Rescale", IconDesImg::Rescale(), THISBACK(SmoothRescale));
-	bar.Add(c, "Blur/Sharpen", IconDesImg::BlurSharpen(), THISBACK(BlurSharpen));
-	bar.Add(c, "Colorize", IconDesImg::Colorize(), THISBACK(Colorize));
-	bar.Add(c, "Chroma", IconDesImg::Chroma(), THISBACK(Chroma));
-	bar.Add(c, "Contrast", IconDesImg::Contrast(), THISBACK(Contrast));
-	bar.Add(c, "Alpha", IconDesImg::AlphaI(), THISBACK(Alpha));
-	bar.Add(c, "Colors", IconDesImg::Colors(), THISBACK(Colors));
+	bar.Add(c, AK_SETCOLOR, IconDesImg::SetColor(), THISBACK(SetColor));
+	bar.Add(c, AK_EMPTY, IconDesImg::Delete(), THISBACK(DoDelete));
+	bar.Add(c, AK_INTERPOLATE, IconDesImg::Interpolate(), THISBACK(Interpolate));
+	bar.Add(c, AK_HMIRROR, IconDesImg::MirrorX(), THISBACK(MirrorX));
+	bar.Add(c, AK_VMIRROR, IconDesImg::MirrorY(), THISBACK(MirrorY));
+	bar.Add(c, AK_HSYM, IconDesImg::SymmX(), THISBACK(SymmX));
+	bar.Add(c, AK_VSYM, IconDesImg::SymmY(), THISBACK(SymmY));
+	bar.Add(c, AK_ROTATE, IconDesImg::Rotate(), THISBACK(Rotate));
+	bar.Add(c, AK_FREE_ROTATE, IconDesImg::FreeRotate(), THISBACK(FreeRotate));
+	bar.Add(c, AK_RESCALE, IconDesImg::Rescale(), THISBACK(SmoothRescale));
+	bar.Add(c, AK_BLUR, IconDesImg::BlurSharpen(), THISBACK(BlurSharpen));
+	bar.Add(c, AK_COLORIZE, IconDesImg::Colorize(), THISBACK(Colorize));
+	bar.Add(c, AK_CHROMA, IconDesImg::Chroma(), THISBACK(Chroma));
+	bar.Add(c, AK_CONTRAST, IconDesImg::Contrast(), THISBACK(Contrast));
+	bar.Add(c, AK_ALPHA, IconDesImg::AlphaI(), THISBACK(Alpha));
+	bar.Add(c, AK_COLORS, IconDesImg::Colors(), THISBACK(Colors));
 }
 
 void IconDes::DrawBar(Bar& bar)
 {
+	using namespace IconDesKeys;
 	bool notpasting = !IsPasting();
-	bar.Add("FreeHand", IconDesImg::FreeHand(), THISBACK1(SetTool, &IconDes::FreehandTool))
-	   .Check(tool == &IconDes::FreehandTool && notpasting)
-	   .Key(K_H);
-	bar.Add("Lines", IconDesImg::Lines(), THISBACK1(SetTool, &IconDes::LineTool))
-	   .Check(tool == &IconDes::LineTool && notpasting)
-	   .Key(K_L);
-	bar.Add("Ellipses", IconDesImg::Circles(), THISBACK1(SetTool, &IconDes::EllipseTool))
-	   .Check(tool == &IconDes::EllipseTool && notpasting)
-	   .Key(K_E);
-	bar.Add("Empty ellipses", IconDesImg::EmptyCircles(), THISBACK1(SetTool, &IconDes::EmptyEllipseTool))
-	   .Check(tool == &IconDes::EmptyEllipseTool && notpasting)
-	   .Key(K_W);
-	bar.Add("Rectangles", IconDesImg::Rects(), THISBACK1(SetTool, &IconDes::RectTool))
-	   .Check(tool == &IconDes::RectTool && notpasting)
-	   .Key(K_R);
-	bar.Add("Empty rectangles", IconDesImg::EmptyRects(), THISBACK1(SetTool, &IconDes::EmptyRectTool))
-	   .Check(tool == &IconDes::EmptyRectTool && notpasting && !selectrect)
-	   .Key(K_T);
-	bar.Add("Hotspots", IconDesImg::HotSpot(), THISBACK1(SetTool, &IconDes::HotSpotTool))
-	   .Check(tool == &IconDes::HotSpotTool)
-	   .Key(K_H);
+	bar.Add(AK_FREEHAND, IconDesImg::FreeHand(), THISBACK1(SetTool, &IconDes::FreehandTool))
+	   .Check(tool == &IconDes::FreehandTool && notpasting);
+	bar.Add(AK_LINES, IconDesImg::Lines(), THISBACK1(SetTool, &IconDes::LineTool))
+	   .Check(tool == &IconDes::LineTool && notpasting);
+	bar.Add(AK_ELLIPSES, IconDesImg::Circles(), THISBACK1(SetTool, &IconDes::EllipseTool))
+	   .Check(tool == &IconDes::EllipseTool && notpasting);
+	bar.Add(AK_EMPTY_ELLIPSES, IconDesImg::EmptyCircles(), THISBACK1(SetTool, &IconDes::EmptyEllipseTool))
+	   .Check(tool == &IconDes::EmptyEllipseTool && notpasting);
+	bar.Add(AK_RECTANGLES, IconDesImg::Rects(), THISBACK1(SetTool, &IconDes::RectTool))
+	   .Check(tool == &IconDes::RectTool && notpasting);
+	bar.Add(AK_EMPTY_RECTANGLES, IconDesImg::EmptyRects(), THISBACK1(SetTool, &IconDes::EmptyRectTool))
+	   .Check(tool == &IconDes::EmptyRectTool && notpasting && !selectrect);
+	bar.Add(AK_HOTSPOTS, IconDesImg::HotSpot(), THISBACK1(SetTool, &IconDes::HotSpotTool))
+	   .Check(tool == &IconDes::HotSpotTool);
 	bar.Separator();
 	for(int i = 1; i <= 6; i++)
 		bar.Add("Pen " + AsString(i), IconDesImg::Get(IconDesImg::I_Pen1 + i - 1), THISBACK1(SetPen, i))
