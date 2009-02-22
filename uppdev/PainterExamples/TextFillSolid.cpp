d2b33579307cde03a7781b4c962cf86b8b060bcd
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-02-22 17:47:02 +0000

    PainterExamples: Various fonts example
    git-svn-id: svn://ultimatepp.org/upp/trunk@889 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/PainterExamples/TextFillSolid.cpp
+++ uppdev/PainterExamples/TextFillSolid.cpp
@@ -10,21 +10,24 @@ const char *text = "Quick Brown Fox Jumps Over The Lazy Dog!";
 
 void SomeTexts(Painter& sw)
 {
-	double y = 20;
 	int face[] = {
 		Font::ARIAL, Font::ROMAN, Font::COURIER
 	};
 	int height[] = {
-		10, 15, 20
+		10, 16, 20
 	};
-	for(int h = 0; h < __countof(height); h++)
-		for(int italic = 0; italic < 2; italic++)
-			for(int bold = 0; bold < 2; bold++)
-				for(int c = 0; c < __countof(face); c++) {
-					sw.Text(20, y, text, Font().Face(face[c]).Height(height[h]).Italic(italic).Bold(bold))
-					  .Fill(Black());
-					y += height[h] + 2;
-				}
+	sw.Rectangle(600, 0, 1000, 1000).Fill(Blue());
+	for(int x = 20; x < 1000; x += 600) {
+		double y = 20;
+		for(int h = 0; h < __countof(height); h++)
+			for(int italic = 0; italic < 2; italic++)
+				for(int bold = 0; bold < 2; bold++)
+					for(int c = 0; c < __countof(face); c++) {
+						sw.Text(x, y, text, Font().Face(face[c]).Height(height[h]).Italic(italic).Bold(bold))
+						  .Fill(x == 20 ? Black() : White());
+						y += height[h] + 2;
+					}
+	}
 }
 
 INITBLOCK {
