6a0a81bf743e3d117c9ec85e5c50130d11aec3cc
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-02-26 11:03:36 +0000

    Developing ODBC connector
    git-svn-id: svn://ultimatepp.org/upp/trunk@907 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/ODBC/ODBC.h
@@ -0,0 +1,55 @@
+#ifndef _ODBC_ODBC_h
+#define _ODBC_ODBC_h
+
+#include <Sql/Sql.h>
+#include <sql.h>
+#include <sqlext.h>
+
+NAMESPACE_UPP
+
+bool   ODBCPerformScript(const String& text, StatementExecutor& executor, Gate2<int, int> progress_canceled = false);
+
+class ODBCSession : public SqlSession {
+public:
+	virtual void           Begin();
+	virtual void           Commit();
+	virtual void           Rollback();
+
+	virtual String         Savepoint();
+	virtual void           RollbackTo(const String& savepoint);
+
+	virtual bool           IsOpen() const;
+
+	virtual Vector<String> EnumUsers();
+	virtual Vector<String> EnumDatabases();
+	virtual Vector<String> EnumTables(String database);
+	virtual Vector<String> EnumViews(String database);
+	virtual Vector<String> EnumSequences(String database);
+	virtual Vector<String> EnumPrimaryKeys(String database, String table);
+	virtual String         EnumRowID(String database, String table);
+
+	virtual RunScript      GetRunScript() const                   { return &ODBCPerformScript; }
+
+protected:
+	virtual SqlConnection *CreateConnection();
+
+private:
+	friend class ODBCConnection;
+	HENV                  henv;
+	HDBC                  hdbc;
+	Link<ODBCConnection>  clink;
+	String                user;
+
+	bool   IsOk(SQLRETURN ret)                  { return SQL_SUCCEEDED(ret); }
+
+public:
+	bool Connect(const char *cs);
+	void Close();
+
+	ODBCSession();
+	~ODBCSession();
+};
+
+END_UPP_NAMESPACE
+
+#endif
