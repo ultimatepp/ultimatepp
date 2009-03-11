6bf9814adb34e679be11250a924bde5870865fc0
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-03-11 21:06:10 +0000

    Added Image
    git-svn-id: svn://ultimatepp.org/upp/trunk@945 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ bazaar/PainterSvg/PainterSvg.h
@@ -0,0 +1,13 @@
+#ifndef _PainterSvg_PainterSvg_h
+#define _PainterSvg_PainterSvg_h
+
+#include <Painter/Painter.h>
+
+NAMESPACE_UPP
+
+void ParseSVG(Painter& p, const char *svg, const char *folder = '\0');
+
+END_UPP_NAMESPACE
+
+#endif
+
