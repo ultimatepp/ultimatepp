2dedb84abb49a169d5296111615d013a1b361c27
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-03-11 12:46:55 +0000

    ConditionVariable test
    git-svn-id: svn://ultimatepp.org/upp/trunk@938 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/odbctst/odbctst.cpp
+++ upptst/odbctst/odbctst.cpp
@@ -5,11 +5,14 @@ using namespace Upp;
 CONSOLE_APP_MAIN
 {
 	ODBCSession session;
-	session.Connect("DRIVER=SQL Server Native Client 10.0;SERVER=QUAD\\SQLEXPRESS;DATABASE=Holly;UID=test;PWD=koblih;");
+	session.LogErrors();
+	session.SetTrace();
+	if(!session.Connect("DRIVER=SQL Server Native Client 10.0;SERVER=QUAD\\SQLEXPRESS;DATABASE=Holly;UID=test;PWD=koblih;"))
+		return;
 	LOG("Connected!");
 	SQL = session;
 	Sql sql;
-	sql.Execute("select ID, TEXT, SINCE, TILL from SUBJECT_ATTR where SINCE > ?", Date(2009, 2, 20));
+	sql.Execute("select AID, TEXT, SINCE, TILL from SUBJECT_ATTR where SINCE > ?", Date(2009, 2, 20));
 //	sql.Execute("select ID, TEXT, SINCE, TILL from SUBJECT_ATTR where ID > ?", 1700);
 	while(sql.Fetch()) {
 		for(int i = 0; i < sql.GetColumns(); i++) {
