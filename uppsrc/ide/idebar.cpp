7c59fc90df2ccea9854878479176cfb68a9e313b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-03-13 08:30:40 +0000

    EncodeHTML enhancements by cbpporter
    git-svn-id: svn://ultimatepp.org/upp/trunk@955 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/idebar.cpp
+++ uppsrc/ide/idebar.cpp
@@ -342,10 +342,10 @@ void Ide::FilePropertiesMenu(Bar& menu)
 		.Help("File properties stored in package");
 	menu.Add(IsActiveFile(), AK_SAVEENCODING, THISBACK(ChangeCharset))
 	    .Help("Convert actual file to different encoding");
-	menu.AddMenu(IsActiveFile(), AK_DIFF, IdeImg::Diff(), THISBACK(Diff))
+	menu.AddMenu(IsActiveFile() && !IsFolder(editfile), AK_DIFF, IdeImg::Diff(), THISBACK(Diff))
 	    .Help("Show differences between the project and arbitrary files");
 	if(IsSvnDir(GetFileFolder(editfile)))
-		menu.AddMenu(IsActiveFile(), AK_SVNDIFF, IdeImg::Diff(), THISBACK(SvnHistory))
+		menu.AddMenu(IsActiveFile() && !IsFolder(editfile), AK_SVNDIFF, IdeImg::Diff(), THISBACK(SvnHistory))
 		    .Help("Show svn history of file");
 }
 
