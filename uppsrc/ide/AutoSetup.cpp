313764418d70e6e1d387f2e998956b3980b9cd52
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-03-15 14:46:47 +0000

    TheIDE Win32 SDK autosetup fix
    git-svn-id: svn://ultimatepp.org/upp/trunk@961 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/AutoSetup.cpp
+++ uppsrc/ide/AutoSetup.cpp
@@ -83,7 +83,7 @@ void AutoSetup()
 	}
 	dlg.visualcppmethod8x64 <<= "MSC8x64";
 	String  sdk9 = NormalizePathNN(GetWinRegString("InstallationFolder",
-	                                               "Software\\Microsoft\\Microsoft SDKs\\Windows\\v6.0a",
+	                                               "Software\\Microsoft\\Microsoft SDKs\\Windows\\v6.1",
 	                                                HKEY_CURRENT_USER));
 	String bin9;
 	if(!IsNull(sdk9) && FileExists(AppendFileName(sdk9, "VC\\Bin\\cl.exe")))
