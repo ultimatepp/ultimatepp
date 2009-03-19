b5472a2b5e6a389f745541e98074c96ac81c5d78
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-03-19 07:43:46 +0000

    PainterExamples: RoundedRectangle
    git-svn-id: svn://ultimatepp.org/upp/trunk@983 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ examples/PainterExamples/Shapes.cpp
@@ -0,0 +1,12 @@
+#include "Examples.h"
+
+#include "Examples.h"
+
+void RoundedRect(Painter& sw)
+{
+	sw.RoundedRectangle(100, 100, 400, 200, 20).Stroke(2, Black());
+}
+
+INITBLOCK {
+	RegisterExample("RoundedRectangle", RoundedRect);
+}
