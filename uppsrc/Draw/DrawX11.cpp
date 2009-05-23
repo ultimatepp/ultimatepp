2a9ebfbd49ffab7cbbda4b24ac259c2058d7d785
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-05-23 08:41:39 +0000

    DrawX11 fix
    git-svn-id: svn://ultimatepp.org/upp/trunk@1220 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Draw/DrawX11.cpp
+++ uppsrc/Draw/DrawX11.cpp
@@ -162,7 +162,7 @@ void InitX11Draw(XDisplay *display)
 		fflush(stdout);
 		XError();
 	}
-	int Xscreenno = DefaultScreen(Xdisplay);
+	Xscreenno = DefaultScreen(Xdisplay);
 	Xroot = RootWindow(Xdisplay, Xscreenno);
 	Xscreen = ScreenOfDisplay(Xdisplay, Xscreenno);
 	Xcolormap = DefaultColormap(Xdisplay, Xscreenno);
