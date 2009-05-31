fe9618711bb237060988ed92b718a6823fa5dad1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-05-31 10:41:18 +0000

    theide: Highlighting of .sql files
    git-svn-id: svn://ultimatepp.org/upp/trunk@1252 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CodeEditor/Highlight.cpp
+++ uppsrc/CodeEditor/Highlight.cpp
@@ -390,14 +390,91 @@ void CodeEditor::InitKeywords()
 			"COLUMN", "COLUMN_ARRAY", "ATTRIBUTE", "INLINE_ATTRIBUTE",
 			NULL
 		};
+		static const char *sql[] = {
+			"ACCESSIBLE", "ADD", "ALL",
+			"ALTER", "ANALYZE", "AND",
+			"AS", "ASC", "ASENSITIVE",
+			"BEFORE", "BETWEEN", "BIGINT",
+			"BINARY", "BLOB", "BOTH", "BEGIN", "END",
+			"BY", "CALL", "CASCADE",
+			"CASE", "CHANGE", "CHAR",
+			"CHARACTER", "CHECK", "COLLATE",
+			"COLUMN", "CONDITION", "CONSTRAINT",
+			"CONTINUE", "CONVERT", "CREATE",
+			"CROSS", "CURRENT_DATE", "CURRENT_TIME",
+			"CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR",
+			"DATABASE", "DATABASES", "DAY_HOUR",
+			"DAY_MICROSECOND", "DAY_MINUTE", "DAY_SECOND",
+			"DEC", "DECIMAL", "DECLARE",
+			"DEFAULT", "DELAYED", "DELETE",
+			"DESC", "DESCRIBE", "DETERMINISTIC",
+			"DISTINCT", "DISTINCTROW", "DIV",
+			"DOUBLE", "DROP", "DUAL",
+			"EACH", "ELSE", "ELSEIF",
+			"ENCLOSED", "ESCAPED", "EXISTS",
+			"EXIT", "EXPLAIN", "FALSE",
+			"FETCH", "FLOAT", "FLOAT4",
+			"FLOAT8", "FOR", "FORCE",
+			"FOREIGN", "FROM", "FULLTEXT",
+			"GRANT", "GROUP", "HAVING",
+			"HIGH_PRIORITY", "HOUR_MICROSECOND", "HOUR_MINUTE",
+			"HOUR_SECOND", "IF", "IGNORE",
+			"IN", "INDEX", "INFILE",
+			"INNER", "INOUT", "INSENSITIVE",
+			"INSERT", "INT", "INT1",
+			"INT2", "INT3", "INT4",
+			"INT8", "INTEGER", "INTERVAL",
+			"INTO", "IS", "ITERATE",
+			"JOIN", "KEY", "KEYS",
+			"KILL", "LEADING", "LEAVE",
+			"LEFT", "LIKE", "LIMIT",
+			"LINEAR", "LINES", "LOAD",
+			"LOCALTIME", "LOCALTIMESTAMP", "LOCK",
+			"LONG", "LONGBLOB", "LONGTEXT",
+			"LOOP", "LOW_PRIORITY", "MASTER_SSL_VERIFY_SERVER_CERT",
+			"MATCH", "MEDIUMBLOB", "MEDIUMINT",
+			"MEDIUMTEXT", "MIDDLEINT", "MINUTE_MICROSECOND",
+			"MINUTE_SECOND", "MOD", "MODIFIES",
+			"NATURAL", "NOT", "NO_WRITE_TO_BINLOG",
+			"NULL", "NUMERIC", "ON",
+			"OPTIMIZE", "OPTION", "OPTIONALLY",
+			"OR", "ORDER", "OUT",
+			"OUTER", "OUTFILE", "PRECISION",
+			"PRIMARY", "PROCEDURE", "PURGE",
+			"RANGE", "READ", "READS",
+			"READ_WRITE", "REAL", "REFERENCES",
+			"REGEXP", "RELEASE", "RENAME",
+			"REPEAT", "REPLACE", "REQUIRE",
+			"RESTRICT", "RETURN", "REVOKE",
+			"RIGHT", "RLIKE", "SCHEMA",
+			"SCHEMAS", "SECOND_MICROSECOND", "SELECT",
+			"SENSITIVE", "SEPARATOR", "SET",
+			"SHOW", "SMALLINT", "SPATIAL",
+			"SPECIFIC", "SQL", "SQLEXCEPTION",
+			"SQLSTATE", "SQLWARNING", "SQL_BIG_RESULT",
+			"SQL_CALC_FOUND_ROWS", "SQL_SMALL_RESULT", "SSL",
+			"STARTING", "STRAIGHT_JOIN", "TABLE",
+			"TERMINATED", "THEN", "TINYBLOB",
+			"TINYINT", "TINYTEXT", "TO",
+			"TRAILING", "TRIGGER", "TRUE",
+			"UNDO", "UNION", "UNIQUE",
+			"UNLOCK", "UNSIGNED", "UPDATE",
+			"USAGE", "USE", "USING",
+			"UTC_DATE", "UTC_TIME", "UTC_TIMESTAMP",
+			"VALUES", "VARBINARY", "VARCHAR",
+			"VARCHARACTER", "VARYING", "WHEN",
+			"WHERE", "WHILE", "WITH",
+			"WRITE", "XOR", "YEAR_MONTH",
+			"ZEROFILL",
+		};
 		static const char *javan[] = {
 			NULL
 		};
 		static const char **kw[HIGHLIGHT_COUNT] = {
-			cpp, usc, java, tfile, usc, sch
+			cpp, usc, java, tfile, usc, sch, sql
 		};
 		static const char **nm[HIGHLIGHT_COUNT] = {
-			upp, usclib, javan, tlng, usclib, javan
+			upp, usclib, javan, tlng, usclib, javan, javan
 		};
 		const char **q = NULL;
 		for(int i = 0; i < HIGHLIGHT_COUNT; i++) {
@@ -619,6 +696,8 @@ void CodeEditor::HighlightLine(int line, Vector<LineEdit::Highlight>& hl, int po
 			while(iscid(*q) && q < e)
 				id.Cat(*q++);
 			String iid = id;
+			if(highlight == HIGHLIGHT_SQL)
+				iid = ToUpper(iid);
 			int uq = kw_upp.Find(iid);
 			int nq;
 			hls.Put(int(q - p), (nq = keyword[highlight].Find(iid)) >= 0 ? hl_style[INK_KEYWORD] :
