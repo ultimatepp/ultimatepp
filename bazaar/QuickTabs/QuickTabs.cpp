69a93caf9e7f1938faccbadde530bed6342a9432
mrjt <mrjt@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-06-16 08:56:55 +0000

    QuickTabs compile error fix
    git-svn-id: svn://ultimatepp.org/upp/trunk@1315 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/QuickTabs/QuickTabs.cpp
+++ bazaar/QuickTabs/QuickTabs.cpp
@@ -7,21 +7,21 @@ QuickTabs::QuickTabs()
 	GroupSeparators(true);
 	FileTabs::FileIcons(false, false, true);
 	
-	FileTabs::WhenCloseAll <<= THISBACK(OnCloseAll);
+	FileTabs::WhenCloseAll = THISBACK(OnCloseAll);
 }
 
 void QuickTabs::ContextMenu(Bar& bar)
 {
-	bar.Add("Settings", THISBACK(OnSettingsMenu));
+	bar.Add("Settings", THISBACK(SettingsMenu));
 	FileTabs::ContextMenu(bar);
 }
 
 void QuickTabs::SettingsMenu(Bar &bar)
 {
-	bar.Add("Stacking", IsStacking() ? TabBarImg::CHK() : Image(), THISBACK1(ToggleStacking));
-	bar.Add("Sort by group", IsGroupSort() ? TabBarImg::CHK() : Image(), THISBACK1(ToggleGroupSort));
-	bar.Add("Group separators", HasGroupSeparators() ? TabBarImg::CHK() : Image(), THISBACK1(ToggleGroupSeparators));
-	bar.Add("Show inactive", IsShowInactive ? TabBarImg::CHK() : Image(), THISBACK1(ToggleInactive));
+	bar.Add("Stacking", IsStacking() ? TabBarImg::CHK() : Image(), THISBACK(ToggleStacking));
+	bar.Add("Sort by group", IsGroupSort() ? TabBarImg::CHK() : Image(), THISBACK(ToggleGroupSort));
+	bar.Add("Group separators", HasGroupSeparators() ? TabBarImg::CHK() : Image(), THISBACK(ToggleGroupSeparators));
+	bar.Add("Show inactive", IsShowInactive() ? TabBarImg::CHK() : Image(), THISBACK(ToggleInactive));
 }
 
 void QuickTabs::Add(const char *name, bool make_active)
@@ -31,7 +31,7 @@ void QuickTabs::Add(const char *name, bool make_active)
 
 bool QuickTabs::FindSetFile(const String& fn)
 {
-	int n = FindKey(fn.ToWString())
+	int n = FindKey(fn.ToWString());
 	if (n >= 0) SetCursor(n);
 	return n >= 0;
 }
@@ -40,7 +40,7 @@ void QuickTabs::SetAddFile(const String& fn)
 {
 	if (IsNull(fn)) return;
 	if (!FindSetFile(fn))
-		FileTabs::AddFile(tn.ToWString());
+		FileTabs::AddFile(fn.ToWString());
 }
 
 void QuickTabs::RenameFile(const String& fn, const String& nn)
