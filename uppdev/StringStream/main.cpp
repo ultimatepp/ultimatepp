fabb787bde041d0248b8095713179568c78011fa
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-08-02 10:11:30 +0000

    Syncing uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@1464 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/StringStream/main.cpp
+++ uppdev/StringStream/main.cpp
@@ -1,15 +1,9 @@
-#include "sstrm.h"
+#include "Core/Core.h"
+
+using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
 	StringStream ss;
-	ss.Put('*');
-	ss.Put("ahoj kamarade jak se mas");
-	ss.Seek(0);
-	DUMP(ss.GetLine());
-	LOGHEXDUMP(ss.GetResult(), ss.GetResult().GetCount());
-	ss.Seek(0);
-	DUMP(ss.GetLine());
-	ss.Put("Another one");
-	LOGHEXDUMP(ss.GetResult(), ss.GetResult().GetCount());
+	ss.Put("xxx", 3);
 }
