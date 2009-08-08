d89a0903b9961928b79d102b72163e5c05d28b0e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-08-08 21:52:08 +0000

    reference: Minor improvement of ConsoleDraw
    git-svn-id: svn://ultimatepp.org/upp/trunk@1479 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/ConsoleDraw/main.cpp
+++ reference/ConsoleDraw/main.cpp
@@ -5,23 +5,18 @@
 
 using namespace Upp;
 
-void DoDraw(Draw& w)
-{
-	w.DrawRect(0, 0, 600, 200, White());
-	w.DrawText(0, 0, "Hello world!", Arial(100), Black());
-}
-
 CONSOLE_APP_MAIN {
 	{
 		ImagePainter w(600, 100);
-		DoDraw(w);
+		w.DrawRect(0, 0, 700, 250, White());
+		w.DrawText(0, 0, "Hello world!", Arial(100), Black());
 		PNGEncoder().SaveFile(GetHomeDirFile("hello.png"), w);
 	}
 	{
 		RichText txt = ParseQTF("[A4 Just a [*/ little] test!");
-		ImagePainter w(1000, 200);
-		w.DrawRect(0, 0, 1000, 200, White());
-		txt.Paint(w, 0, 0, 1000);
+		ImagePainter w(1500, 200);
+		w.DrawRect(0, 0, 1500, 200, White());
+		txt.Paint(w, 0, 0, 1500);
 		PNGEncoder().SaveFile(GetHomeDirFile("richtext.png"), w);	
 		
 		PdfDraw pdf;
