73ed7a4fa5a02b6732bdb930c2cf6b11b30a1cf6
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-08-24 12:27:21 +0000

    syncing uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@1527 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/Prompt/main.cpp
+++ uppdev/Prompt/main.cpp
@@ -5,7 +5,13 @@ using namespace Upp;
 GUI_APP_MAIN
 {
 //	CtrlImg::Set(CtrlImg::I_information, Win32Icon(IDI_INFORMATION));
-	PromptOK("[* \1test\1][A Arial][R Roman][* xxx]Test 自由的百科全书，可以由用户编辑");
+	PromptOK(
+		"hello hello hello hello hello hello hello hello hello hello hello hello hello &"
+		"hello hello hello hello hello hello hello hello hello hello hello hello hello &"
+		"hello hello hello hello hello hello hello hello hello hello hello hello hello &"
+		"hello hello hello hello hello hello hello hello hello hello hello hello hello &"
+		"hello hello hello hello hello hello hello hello hello hello hello hello hello &"
+	);
 //	CtrlImg::Reset();
 //	PromptOK("Test");
 }
