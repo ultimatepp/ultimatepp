6a5626dfe5e452d85920a61feacb56adc3bbb82d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-09-22 09:04:04 +0000

    reference: AttrText
    git-svn-id: svn://ultimatepp.org/upp/trunk@1580 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/AttrText/main.cpp
@@ -0,0 +1,18 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+GUI_APP_MAIN
+{
+	DropList dl;
+	TopWindow win;
+	win.SetRect(0, 0, 400, 50);
+	win.Add(dl.HSizePos(4, 4).TopPos(10, Ctrl::STDSIZE));
+	dl.Add(0, AttrText("This is bold").SetFont(StdFont().Bold()));
+	dl.Add(1, AttrText("This is red").Ink(Red()));
+	dl.Add(2, AttrText("This has icon").SetImage(CtrlImg::print()));
+	dl.Add(3, AttrText("This is centered").Center());
+	dl.SetLineCy(max(18, GetStdFontCy()));
+	dl <<= 0;
+	win.Run();
+}
