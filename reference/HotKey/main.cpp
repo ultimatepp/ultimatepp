0351e4aa2b2dd89c08e28fab35c9a212e5613937
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-09-24 09:41:56 +0000

    reference: Hotkey Ctrl::RegisterSystemHotKey demontration
    git-svn-id: svn://ultimatepp.org/upp/trunk@1588 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/HotKey/main.cpp
@@ -0,0 +1,23 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+void HotKey1()
+{
+	PromptOK("HotKey1");
+}
+
+void HotKey2()
+{
+	PromptOK("HotKey2");
+}
+
+GUI_APP_MAIN
+{
+	TopWindow win;
+	int id = Ctrl::RegisterSystemHotKey(K_ALT|K_CTRL|K_F11, callback(HotKey1));
+	Ctrl::RegisterSystemHotKey(K_ALT|K_CTRL|K_SHIFT|K_A, callback(HotKey2));
+	win.Run();
+	Ctrl::UnregisterSystemHotKey(id);
+	win.Run();
+}
