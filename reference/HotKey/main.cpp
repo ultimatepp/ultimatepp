0158b37cd1fbfb121efffb58aa9c76724a400e32
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-09-25 08:09:43 +0000

    reference: Hotkey minor fix
    git-svn-id: svn://ultimatepp.org/upp/trunk@1591 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/HotKey/main.cpp
+++ reference/HotKey/main.cpp
@@ -15,7 +15,7 @@ void HotKey2()
 GUI_APP_MAIN
 {
 	TopWindow win;
-	int id = Ctrl::RegisterSystemHotKey(K_ALT|K_CTRL|K_F11, callback(HotKey1));
+	int id = Ctrl::RegisterSystemHotKey(K_CTRL|K_F1, callback(HotKey1));
 	Ctrl::RegisterSystemHotKey(K_ALT|K_CTRL|K_SHIFT|K_A, callback(HotKey2));
 	win.Run();
 	Ctrl::UnregisterSystemHotKey(id);
