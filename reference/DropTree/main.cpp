89e2137cb2e68828c8d18a02bb1652c47bd9d0bf
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-10-02 12:51:36 +0000

    reference: DropTree example
    git-svn-id: svn://ultimatepp.org/upp/trunk@1602 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/DropTree/main.cpp
@@ -0,0 +1,21 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+GUI_APP_MAIN
+{
+	TopWindow win;
+	DropTree dt;
+	
+	dt->SetRoot(CtrlImg::Home(), "Home");
+	for(int i = 0; i < 10; i++) {
+		int id = dt->Add(0, CtrlImg::Computer(), "Computer " + AsString(i + 1));
+		for(int j = 0; j < i; j++)
+			dt->Add(id, CtrlImg::File(), "File " + AsString(j + 1));
+	}
+
+	dt.AutoResize();
+	
+	win.Add(dt.HSizePos().TopPos(0, Ctrl::STDSIZE));
+	win.Run();
+}
