cf582bd6281a5acd98c34286fb1c2dc970a5b104
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-11-02 16:54:23 +0000

    reference: SelectFile example
    git-svn-id: svn://ultimatepp.org/upp/trunk@1675 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/SelectFile/main.cpp
@@ -0,0 +1,22 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+GUI_APP_MAIN
+{
+	PromptOK(DeQtf(SelectFileOpen("Text files\t*.txt\nAll files\t*.*")));
+
+	PromptOK(DeQtf(SelectFileSaveAs("Text files\t*.txt\nAll files\t*.*")));
+	
+	SelectFileIn in("Text files\t*.txt");
+	if(in) {
+		int n = 0;
+		while(!in.IsEof()) {
+			in.GetLine();
+			n++;
+		}
+		PromptOK("There is " + AsString(n) + " lines in the file.");
+	}
+	
+	PromptOK("[{1} \1" + SelectLoadFile("Text files\t*.txt"));
+}
