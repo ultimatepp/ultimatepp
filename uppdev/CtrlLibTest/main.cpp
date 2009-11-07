46b40c284e489e6e37ed95dac7af2bcc8fcf8ed1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-11-07 07:10:52 +0000

    Syncing uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@1681 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/CtrlLibTest/main.cpp
+++ uppdev/CtrlLibTest/main.cpp
@@ -1,31 +1,15 @@
 #include "CtrlLibTest.h"
 
-ChildWnd::ChildWnd()
-{
-	CtrlLayout(*this, "Child Window");
-	btn1.Hide();
-	btn2.Hide();		
-}
+#define IMAGECLASS MyIm
+#define IMAGEFILE <CtrlLibTest/CtrlLibTest.iml>
+#include <Draw/iml_source.h>
 
 CtrlLibTest::CtrlLibTest()
 {
-	CtrlLayout(*this, "Main Window");
-	btn1 <<= THISBACK(OnPush1);
-	btn2 <<= THISBACK(OnPush2);
-}
-
-void CtrlLibTest::OnPush1()
-{
-	wnd.Open(this);
-	wnd.TopMost();
-}
-
-void CtrlLibTest::OnPush2()
-{
-	PromptOK("I'm invisible!");
+	CtrlLayout(*this, "Window title");
 }
 
 GUI_APP_MAIN
 {
-	CtrlLibTest().Run();
+	CtrlLibTest().Icon(MyIm::icon).Run();
 }
