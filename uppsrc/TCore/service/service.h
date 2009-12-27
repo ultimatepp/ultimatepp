23bfb66bd25f00e1fef934c991aa385973c2a550
rylek <rylek@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2009-12-27 07:51:08 +0000

    A simple Win32 service protocol support module
    git-svn-id: svn://ultimatepp.org/upp/trunk@1845 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/TCore/service/service.h
@@ -0,0 +1,25 @@
+#ifndef _TCore_service_service_h_
+#define _TCore_service_service_h_
+
+#include <Web/Web.h>
+
+#ifdef PLATFORM_WIN32
+
+NAMESPACE_UPP
+
+extern volatile bool service_stopped;
+extern volatile bool service_paused;
+
+extern bool InitService(); // implemented by main program
+extern void RunService(); // implemented by main program
+
+void InstallConsoleCtrlHandler();
+bool InstallService(String name, String display_name, String& cmdline, String& status);
+bool UninstallService(String name, String& status);
+void RunServiceDispatcher(String service_name);
+
+END_UPP_NAMESPACE
+
+#endif
+
+#endif
