bd392f0fc1084721ee69d8bfeeb48cb365d82110
bytefield <bytefield@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-01-24 23:28:42 +0000

    bazaar: Notify removed, it's functionaliy is in TrayIcon now
    git-svn-id: svn://ultimatepp.org/upp/trunk@1942 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/Notify/Notify.h
+++
@@ -1,9 +0,0 @@
-#ifndef _Notify_Notify_h_
-#define _Notify_Notify_h_
-
-void NotifyInfo(const char * title, const char * message, int timeout = 10);
-void NotifyWarning(const char * title, const char * message, int timeout = 10);
-void NotifyError(const char * title, const char * message, int timeout = 10);
-
-
-#endif
