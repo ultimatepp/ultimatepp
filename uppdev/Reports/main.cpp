ec6ed6959b0b470c4f7f40a69a88229ede94eaf4
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-01-28 23:19:40 +0000

    syncing uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@1976 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/Reports/main.cpp
+++ uppdev/Reports/main.cpp
@@ -2,33 +2,10 @@
 
 using namespace Upp;
 
-struct TableRowTracer : RichTextLayoutTracer {
-	VectorMap<int, int> pgr;
-
-	virtual void TableRow(const Rect& page, PageY y, int, const RichTable&) {
-		pgr.GetAdd(y.page, 0)++;
-	}	
-};
-
 GUI_APP_MAIN
 {
 	Report r;
-	r.Footer("[R1 ");
-	TableRowTracer t;
-	r.SetRichTextLayoutTracer(t);
-	String qtf = "&[R This is a demonstration of [* RichTextLayoutTracer]&&&{{1 Table";
-	for(int i = 0; i < 100; i++)
-		qtf << ":: " << i;
-	qtf << "}}";
-	r.Put(qtf);
+	r << "@@iml:400*400`CtrlImg:exclamation`";
 	
-	Report rr;
-	for(int i = 0; i < r.GetCount(); i++) {
-		if(i)
-			rr.NewPage();
-		rr.Footer("[R1 There is " + AsString(t.pgr.Get(i, 0)) + " table rows on this page");
-		rr.DrawDrawing(r.GetPageSize(), r[i]);
-	}
-	
-	Perform(rr);
+	Perform(r);
 }
