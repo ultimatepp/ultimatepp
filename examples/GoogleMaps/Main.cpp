1f0a9f03d896aee0c73c3c3be32a1a455122904e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-02-09 14:50:46 +0000

    examples: GoogleMaps
    git-svn-id: svn://ultimatepp.org/upp/trunk@2037 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ examples/GoogleMaps/Main.cpp
@@ -0,0 +1,11 @@
+#include "GoogleMaps.h"
+
+GUI_APP_MAIN {
+	// Following key was obtained for U++ project, please get your owen key if you want to use
+	// GoogleMaps API here (it is free): http://code.google.com/intl/cs/apis/maps/signup.html
+	SetGoogleMapsKey("ABQIAAAAXHmSFgmVIbMZDJ5RhfPINBSBetkRueiarolywVmVT7jJWZRGvBQFIvrtty50ivBLt4YApjpEVXW8Hw");
+	
+	Pointf p(0, 51.477222);
+	if(MapDlg(p))
+		Exclamation("Marker positions " + FormatGPS(p));
+}
