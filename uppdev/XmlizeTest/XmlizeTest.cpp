6e8c1c6f91c29ece059a22817938a82071400715
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-03-10 07:40:36 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@2204 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/XmlizeTest/XmlizeTest.cpp
+++ uppdev/XmlizeTest/XmlizeTest.cpp
@@ -11,10 +11,29 @@ struct MyVector : public Vector<T> {
 	}
 };
 
-CONSOLE_APP_MAIN
+void CONSOLE_APP_MAIN0()
 {
 	MyVector<int> x;
 	x.Add(10);
 	DDUMP(StoreAsXML(x, "test"));
 	LOG("XXX");
+}
+
+#include <Core/Core.h>
+using namespace Upp;
+
+struct A
+{
+	void Xmlize(XmlIO &xml) { xml ("v",v); } //<-- exception on 2nd call
+
+	Value v;
+};
+
+CONSOLE_APP_MAIN
+{
+	A a;
+	a.v = "test";
+	StoreAsXMLFile(a, "XmlizeTest", "xmlizeTtest");
+	a.v = 0;
+	LoadFromXMLFile(a, "xmlizeTtest");
 }
