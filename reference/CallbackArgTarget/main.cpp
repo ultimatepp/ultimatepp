0ad5abaee25b7ff6ef96d05d87032888ffa062b9
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-04-16 11:06:30 +0000

    *reference: CallbackArgTarget fix
    git-svn-id: svn://ultimatepp.org/upp/trunk@2323 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/CallbackArgTarget/main.cpp
+++ reference/CallbackArgTarget/main.cpp
@@ -2,15 +2,21 @@
 
 using namespace Upp;
 
+struct MyApp : TopWindow {
+	virtual void RightDown(Point p, dword keyflags) {
+		CallbackArgTarget<int> result;
+		MenuBar menu;
+		for(int i = 0; i < 10; i++)
+			menu.Add(AsString(i), result[i]);
+		menu.Execute();
+		if(IsNull(result))
+			PromptOK("Menu was cancelled");
+		else
+			PromptOK("You have selected " + AsString((int)result));
+	}
+};
+
 GUI_APP_MAIN
 {
-	CallbackArgTarget<int> result;
-	MenuBar menu;
-	for(int i = 0; i < 10; i++)
-		menu.Add(AsString(i), result[i]);
-	menu.Execute();
-	if(IsNull(result))
-		PromptOK("Menu was cancelled");
-	else
-		PromptOK("You have selected " + AsString((int)result));
+	MyApp().Run();
 }
