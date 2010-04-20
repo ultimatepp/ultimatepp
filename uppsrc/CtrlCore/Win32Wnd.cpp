459ba47aa3af3343dd22998e087e545918776a14
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-04-20 16:14:25 +0000

    .LOGs
    git-svn-id: svn://ultimatepp.org/upp/trunk@2328 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/Win32Wnd.cpp
+++ uppsrc/CtrlCore/Win32Wnd.cpp
@@ -8,7 +8,7 @@ NAMESPACE_UPP
 #define LOGTIMING 0
 
 #ifdef _DEBUG
-#define LOGMESSAGES 1
+#define LOGMESSAGES 0
 #endif
 
 #define ELOGW(x)  // RLOG(GetSysTime() << ": " << x) // Only activate in MT!
