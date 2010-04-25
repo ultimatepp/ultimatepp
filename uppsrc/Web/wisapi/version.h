a65eba5954e60c8e18dd516b06a90f904b423d4a
rylek <rylek@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-04-25 19:37:14 +0000

    .Web: fixed POST data handling in ISAPI extension library
    git-svn-id: svn://ultimatepp.org/upp/trunk@2338 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Web/wisapi/version.h
+++ uppsrc/Web/wisapi/version.h
@@ -1,3 +1,3 @@
-#define WISAPI_VERSION   "6.r15"
-#define WISAPI_DATE      Date(2009, 5, 12)
-#define WISAPI_COPYRIGHT "Copyright 1999-2009 Tomas Rylek"
+#define WISAPI_VERSION   "6.r16"
+#define WISAPI_DATE      Date(2010, 4, 25)
+#define WISAPI_COPYRIGHT "Copyright 1999-2010 Tomas Rylek"
