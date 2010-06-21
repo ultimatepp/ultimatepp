73812107c9c2949174fe97ea8dd512470f09317c
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-06-21 22:13:24 +0000

    Reference/RasterMultiPage: Included support for subimage Rect and disposal method
    git-svn-id: svn://ultimatepp.org/upp/trunk@2494 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/RasterMultiPage/main.cpp
+++ reference/RasterMultiPage/main.cpp
@@ -28,12 +28,23 @@ void RasterMultiPage::Browse()
 		return;
 	}
 	images.Clear();
-	delays.Clear();
+	delays.Clear();	
+	Size sz = raster->GetSize();
+	ImageDraw iw(sz); 
 	for (int i = 0; i < raster->GetPageCount(); ++i) {
 		raster->SeekPage(i);
-		images.Add(raster->GetImage());
-		int delay = raster->GetPageDelay(i);
-		delays.Add(delay <= 100 ? 100 : delay);
+		Rect r = raster->GetPageRect(i);
+		switch (raster->GetPageDisposal(i)) {
+		case 1:	iw.DrawRect(r, White());
+				break;
+		case 2: 
+		case 4:	iw.DrawRect(sz, White());
+				break;
+		}
+		iw.DrawImage(r.left, r.top, raster->GetImage(0, 0, r.right-r.left, r.bottom-r.top));
+		images.Add(iw);
+		int msdelay = 10*raster->GetPageDelay(i);
+		delays.Add(msdelay <= 100 ? 100 : msdelay);
 	}
 	numberPages = FormatInt(raster->GetPageCount());
 	openedPage = "0";
@@ -46,14 +57,15 @@ void RasterMultiPage::Play()
 	if (images.GetCount() <= 1)
 		return;
 	TimeStop t;
-	for (unsigned ms = 0; t.Elapsed() < 4000; ++ind) {
+	for (unsigned ms = 0; t.Elapsed() < 5000; ++ind) {
 		if (ind >= images.GetCount())
 			ind = 0;	
-		image.SetImage(images[ind]);	
+		image.SetImage(images[ind]);		
 		openedPage = FormatInt(ind);
+		
 		Refresh();
 		Ctrl::ProcessEvents();
-
+		
 		ms += delays[ind];
 		while (t.Elapsed() < ms)
 			Sleep(10);
