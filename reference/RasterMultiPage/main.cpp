5100728dbd43406a1a7d606e6254db79759bf42f
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-08-24 20:40:15 +0000

    AnimatedClip: Added RasterPlayer demo
    git-svn-id: svn://ultimatepp.org/upp/trunk@2635 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/RasterMultiPage/main.cpp
+++ reference/RasterMultiPage/main.cpp
@@ -1,36 +1,29 @@
-#include "RasterMultiPage.h"
+#include <CtrlLib/CtrlLib.h>
+#include <RasterPlayer/RasterPlayer.h>
 
-Image GetRect(const Image& orig, const Rect &r) 
-{
-	if(r.IsEmpty())
-		return Image();
-	ImageBuffer ib(r.GetSize());
-	for(int y = r.top; y < r.bottom; y++) {
-		const RGBA *s = orig[y] + r.left;
-		const RGBA *e = orig[y] + r.right;
-		RGBA *t = ib[y - r.top];
-		while(s < e) {
-			*t = *s;
-			t++;
-			s++;
-		}
-	}
-	return ib;
-}
+using namespace Upp;
 
-RasterMultiPage::RasterMultiPage()
-{
-	CtrlLayout(*this, "Multi page raster demo");
+#include "AnimatedClip.h"
+#include "AnimatedClip.brc"
+
+AnimatedClip::AnimatedClip() {
+	CtrlLayout(*this, "RasterPlayer demo");
 	
 	browse   <<= THISBACK(Browse);
 	play 	 <<= THISBACK(Play);
+	stop 	 <<= THISBACK(Stop);
 	openNext <<= THISBACK(OpenNext);
-
+	
+	clip.WhenShown = THISBACK(OnShown);
+	arrows.LoadBuffer(String(animatedArrow, animatedArrow_length));
+	arrows.Play();
+	earth.LoadBuffer(String(animatedEarth, animatedEarth_length));
+	earth.Play();
+		
 	Sizeable().Zoomable();
 }
 
-void RasterMultiPage::Browse()
-{
+void AnimatedClip::Browse() {
 	FileSel fs;
 	
 	fs.PreSelect(fileName);
@@ -39,79 +32,31 @@ void RasterMultiPage::Browse()
 	if (fs.ExecuteOpen("Choose animation file"))
 		fileName <<= ~fs;
 
-	FileIn in(fileName.GetData().ToString());
-	One<StreamRaster> raster = StreamRaster::OpenAny(in);
-	if(!raster) {
+	if (!clip.Load(fileName)) {
 		Exclamation("Invalid input");
 		return;
 	}
-	images.Clear();
-	delays.Clear();	
-	Size sz = raster->GetSize();
-	ImageDraw iw(sz); 
-	Image previous;
-	Rect r;
-	for (int i = 0; i < raster->GetPageCount(); ++i) {
-		if (previous) {
-			iw.DrawImage(r, previous);
-			previous = Null;
-		}
-		raster->SeekPage(i);
-		r = raster->GetPageRect(i);
-		switch (raster->GetPageDisposal(i)) {
-		case 1:	iw.DrawRect(r, White());
-				break;
-		case 2: iw.DrawRect(sz, White());
-				break;
-		case 4:	if (i > 0) 
-					previous = ::GetRect(images[i-1], r);
-				iw.DrawRect(sz, White());
-				break;
-		}
-		iw.DrawImage(r.left, r.top, raster->GetImage(0, 0, r.right-r.left, r.bottom-r.top));
-		images.Add(iw);
-		int msdelay = 10*raster->GetPageDelay(i);
-		delays.Add(msdelay <= 100 ? 100 : msdelay);
-	}
-	numberPages = FormatInt(raster->GetPageCount());
-	openedPage = "0";
-	image.SetImage(images[0]);
-	ind = 0;
+	numberPages = FormatInt(clip.GetPageCount());
+	openedPage = FormatInt(clip.GetPage());
 }
 
-void RasterMultiPage::Play()
-{
-	if (images.GetCount() <= 1)
-		return;
-	TimeStop t;
-	for (unsigned ms = 0; t.Elapsed() < 5000; ++ind) {
-		if (ind >= images.GetCount())
-			ind = 0;	
-		image.SetImage(images[ind]);		
-		openedPage = FormatInt(ind);
-		
-		Refresh();
-		Ctrl::ProcessEvents();
-		
-		ms += delays[ind];
-		while (t.Elapsed() < ms)
-			Sleep(10);
-	}
+void AnimatedClip::Play() {
+	clip.Play();
 }
 
-void RasterMultiPage::OpenNext()
-{
-	if (images.GetCount() <= 1)
-		return;
-	ind++;
-	if (ind >= images.GetCount())
-		ind = 0;
-	image.SetImage(images[ind]);	
-	openedPage = FormatInt(ind);
+void AnimatedClip::Stop() {
+	clip.Stop();
+}
+
+void AnimatedClip::OpenNext() {
+	clip.NextFrame();
 }
 	
-GUI_APP_MAIN
-{
-	RasterMultiPage().Run();
+void AnimatedClip::OnShown() {
+	openedPage = FormatInt(clip.GetPage());
+}
+
+GUI_APP_MAIN {
+	AnimatedClip().Run();
 }
 
