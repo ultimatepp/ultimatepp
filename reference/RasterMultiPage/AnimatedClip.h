479e9b6143cd600e564bc1489b8b6003b1803d47
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-08-24 20:40:15 +0000

    AnimatedClip: Added RasterPlayer demo
    git-svn-id: svn://ultimatepp.org/upp/trunk@2635 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/RasterMultiPage/AnimatedClip.h
@@ -0,0 +1,21 @@
+#ifndef _AnimatedClipDemo_AnimatedClipDemo_h
+#define _AnimatedClipDemo_AnimatedClipDemo_h
+
+#define LAYOUTFILE <AnimatedClip/AnimatedClip.lay>
+#include <CtrlCore/lay.h>
+
+class AnimatedClip : public WithAnimatedClipLayout<TopWindow> {
+public:
+	typedef AnimatedClip CLASSNAME;
+	AnimatedClip();
+	
+	void Browse();
+	void Play();
+	void Stop();	
+	void OpenNext();
+	
+	void OnShown();
+};
+
+#endif
+
