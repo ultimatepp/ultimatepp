0c1f806daba80ef0c113465a3b42bf8305f15a33
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2010-11-22 18:31:22 +0000

    Rollback of 'black corners issue'
    git-svn-id: svn://ultimatepp.org/upp/trunk@2861 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/Win32Wnd.cpp
+++ uppsrc/CtrlCore/Win32Wnd.cpp
@@ -474,6 +474,59 @@ void Ctrl::Create0(Ctrl::CreateBox *cr)
 			top->hwnd = CreateWindowExW(cr->exstyle,
 			                            cr->savebits ? cr->dropshadow ? L"UPP-CLASS-SB-DS-W" : L"UPP-CLASS-SB-W"
 			                                         : cr->dropshadow ? L"UPP-CLASS-DS-W"    : L"UPP-CLASS-W",
+			                            L"", cr->style, r.left, r.top, r.Width(), r.Height(),
+			                            cr->parent, NULL, hInstance, this);
+		else
+			top->hwnd = CreateWindowW(L"UPP-CLASS-W",
+			                          L"", WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+			                          cr->parent, NULL, hInstance, this);
+#else
+	if(IsWinNT() && (!cr->parent || IsWindowUnicode(cr->parent)))
+		top->hwnd = CreateWindowExW(cr->exstyle,
+		                            cr->savebits ? cr->dropshadow ? L"UPP-CLASS-SB-DS-W" : L"UPP-CLASS-SB-W"
+		                                         : cr->dropshadow ? L"UPP-CLASS-DS-W"    : L"UPP-CLASS-W",
+		                            L"", cr->style, r.left, r.top, r.Width(), r.Height(),
+		                            cr->parent, NULL, hInstance, this);
+	else
+		top->hwnd = CreateWindowEx(cr->exstyle,
+		                           cr->savebits ? cr->dropshadow ? "UPP-CLASS-SB-DS-A" : "UPP-CLASS-SB-A"
+		                                        : cr->dropshadow ? "UPP-CLASS-DS-A"    : "UPP-CLASS-A",
+		                           "", cr->style, r.left, r.top, r.Width(), r.Height(),
+		                           cr->parent, NULL, hInstance, this);
+#endif
+
+	inloop = false;
+
+	ASSERT(top->hwnd);
+
+	::ShowWindow(top->hwnd, visible ? cr->show : SW_HIDE);
+//	::UpdateWindow(hwnd);
+	StateH(OPEN);
+	LLOG(EndIndent << "//Ctrl::Create in " <<UPP::Name(this));
+	RegisterDragDrop(top->hwnd, (LPDROPTARGET) (top->dndtgt = NewUDropTarget(this)));
+	CancelMode();
+	RefreshLayoutDeep();
+}
+
+/* Fix to avoid black corners temorarily artifact, unfortunately it seems to introduce ugly artifact in TS
+void Ctrl::Create0(Ctrl::CreateBox *cr)
+{
+	GuiLock __;
+	ASSERT(IsMainThread());
+	LLOG("Ctrl::Create(parent = " << (void *)parent << ") in " <<UPP::Name(this) << BeginIndent);
+	ASSERT(!IsChild() && !IsOpen());
+	Rect r = GetRect();
+	AdjustWindowRectEx(r, cr->style, FALSE, cr->exstyle);
+	isopen = true;
+	top = new Top;
+	ASSERT(!cr->parent || IsWindow(cr->parent));
+	if(!IsWinXP())
+		cr->dropshadow = false;
+#ifdef PLATFORM_WINCE
+		if(parent)
+			top->hwnd = CreateWindowExW(cr->exstyle,
+			                            cr->savebits ? cr->dropshadow ? L"UPP-CLASS-SB-DS-W" : L"UPP-CLASS-SB-W"
+			                                         : cr->dropshadow ? L"UPP-CLASS-DS-W"    : L"UPP-CLASS-W",
 			                            L"", cr->style, 0, 0, 0, 0,
 			                            cr->parent, NULL, hInstance, this);
 		else
@@ -508,6 +561,7 @@ void Ctrl::Create0(Ctrl::CreateBox *cr)
 	CancelMode();
 	RefreshLayoutDeep();
 }
+*/
 
 void ReleaseUDropTarget(UDropTarget *dt);
 
