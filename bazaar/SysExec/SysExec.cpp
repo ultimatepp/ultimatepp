bacd190f2e29587311ee1edee82a07de11927719
micio <micio@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-01-18 00:10:40 +0000

    Bazaar/SysExec : Added SysStartAdmin() for windows OS
    git-svn-id: svn://ultimatepp.org/upp/trunk@3038 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SysExec/SysExec.cpp
+++ bazaar/SysExec/SysExec.cpp
@@ -64,6 +64,7 @@ static int execvpe(const char *file, char * const *argv, char * const *envp)
 #endif
 #else
 #include <process.h>
+#include <Shellapi.h>
 #endif
 
 NAMESPACE_UPP
@@ -372,15 +373,70 @@ intptr_t SysStart(String const &command, String const &args)
 // executes an external command as Admin user, passing a command line to it without waiting for its termination
 // it WILL prompt for user intervention on secure systems (linux - Vista+ OSes)
 // return true on success, false otherwise
-bool SysStartAdmin(String const &command, String const &args, const VectorMap<String, String> &Environ);
+bool SysStartAdmin(String const &command, String const &args, const VectorMap<String, String> &Environ)
 {
 #ifdef PLATFORM_POSIX
 
+	// we use sudo to start an admin process
+
 #else
 
-	// as ShellExecuteEx doesn't take 
+	// we use ShellExecuteEx to start an Admin process
+
+	// as ShellExecuteEx doesn't take an environment argument, but inherits
+	// calling process one, we have to modify directly the environment before calling
+
+	// copy original environment, for that we can use upp :-)
+	// don't know if Environment() funcitons returns a cached one, so
+	// to be sure we copy it
+	VectorMap<String, String> prevEnv(Environment(), 1);
+	
+	// wipe original environment with windows api
+	for(int i = 0; i < prevEnv.GetCount(); i++)
+		SetEnvironmentVariable(prevEnv.GetKey(i), NULL);
+	
+	// insert new environment variables
+	for(int i = 0; i < Environ.GetCount(); i++)
+		SetEnvironmentVariable(Environ.GetKey(i), Environ[i]);
+	
+	// now we can use ShellExecute to raise process level
+	SHELLEXECUTEINFO info =
+	{
+		sizeof(SHELLEXECUTEINFO),		// cbsize
+		SEE_MASK_NOASYNC,				// fMask
+		0,								// hwnd
+		"runas",						// lpVerb
+		"C:\\Windows\\Notepad.exe",		// lpFile
+		0,								// lpParameters
+		0,								// lpDirectory
+		SW_SHOW,						// nShow
+		0,								// hHinstApp -- result handle or error code
+	/* REST AS DEFAULT -- NOT NEEDED
+		LPVOID    lpIDList;
+		LPCTSTR   lpClass;
+		HKEY      hkeyClass;
+		DWORD     dwHotKey;
+		union {	HANDLE hIcon; HANDLE hMonitor; } DUMMYUNIONNAME;
+		HANDLE    hProcess;
+	*/
+	};
+	BOOL res = ShellExecuteEx(&info);
+	
+	// restore the environment
+	for(int i = 0; i < prevEnv.GetCount(); i++)
+		SetEnvironmentVariable(Environ.GetKey(i), NULL);
+	for(int i = 0; i < Environ.GetCount(); i++)
+		SetEnvironmentVariable(prevEnv.GetKey(i), prevEnv[i]);
 
 #endif
+
+	return res;
+}
+
+bool SysStartAdmin(String const &command, String const &args)
+{
+	return SysStartAdmin(command, args, Environment());
+	
 }
 
 END_UPP_NAMESPACE
