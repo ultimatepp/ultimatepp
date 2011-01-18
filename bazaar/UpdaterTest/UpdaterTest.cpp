1a027114a6be889c94c757b6f6aba810fac792a5
micio <micio@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-01-18 00:15:09 +0000

    Bazaar/Updatertest : cleaning up tests
    git-svn-id: svn://ultimatepp.org/upp/trunk@3039 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/UpdaterTest/UpdaterTest.cpp
+++ bazaar/UpdaterTest/UpdaterTest.cpp
@@ -2,55 +2,6 @@
 
 GUI_APP_MAIN
 {
-/*
-typedef struct _SHELLEXECUTEINFO {
-  DWORD     cbSize;
-  ULONG     fMask;
-  HWND      hwnd;
-  LPCTSTR   lpVerb;
-  LPCTSTR   lpFile;
-  LPCTSTR   lpParameters;
-  LPCTSTR   lpDirectory;
-  int       nShow;
-  HINSTANCE hInstApp;
-  LPVOID    lpIDList;
-  LPCTSTR   lpClass;
-  HKEY      hkeyClass;
-  DWORD     dwHotKey;
-  union {
-    HANDLE hIcon;
-    HANDLE hMonitor;
-  } DUMMYUNIONNAME;
-  HANDLE    hProcess;
-} SHELLEXECUTEINFO, *LPSHELLEXECUTEINFO;
-*/
-/*
-::ShellExecute(0,							// owner window
-               "runas",						// operation
-               "C:\\Windows\\Notepad.exe",	// file
-               0, // params					// 
-               0, // directory
-               SW_SHOWNORMAL);
-*/
-SHELLEXECUTEINFO info =
-{
-	sizeof(SHELLEXECUTEINFO),
-	SEE_MASK_NOASYNC,// 0,//SEE_MASK_ASYNCOK,
-	0,
-	"runas",
-	"C:\\Windows\\Notepad.exe",
-	0, // parameters
-	0, // directory
-	SW_SHOW,
-	0, // hHinsApp -- result handle or error code
-	// REST AS DEFAULT -- NOT NEEDED
-};
-
-BOOL res = ShellExecuteEx(&info);
-PromptOK("DONE, result is" + String(res ? "TRUE" : "FALSE"));
-               return;
-
-
 	Updater updater;
 	updater
 		.SetMaxVersion(1.6)
