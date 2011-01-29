8010060a2179c6ce5f1c2e6e8bd99bd1b6cf190a
micio <micio@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-01-29 14:07:51 +0000

    Bazaar/ProductVersion : A class holding application versions
    git-svn-id: svn://ultimatepp.org/upp/trunk@3120 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ bazaar/ProductVersion/ProductVersion.cpp
@@ -0,0 +1,269 @@
+#include "ProductVersion.h"
+
+NAMESPACE_UPP
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// CLASS ProductVersion
+// Small handy class to hold product versions, composed my Major, Minor and devel numbers
+// hancles comparaison ant to/from string conversions
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// constructor
+ProductVersion::ProductVersion(int _major, int _minor, int _devel)
+{
+	major = _major;
+	minor = _minor;
+	devel = _devel;
+}
+
+ProductVersion::ProductVersion(int _major, int _minor)
+{
+	major = _major;
+	minor = _minor;
+	devel = 0;
+}
+
+ProductVersion::ProductVersion(ProductVersion const &v)
+{
+	major = v.major;
+	minor = v.minor;
+	devel = v.devel;
+}
+
+ProductVersion::ProductVersion(String const &s)
+{
+	Vector<String>sv = Split(s, '.', false);
+	if(sv.GetCount() > 0)
+		major = ScanInt(sv[0]);
+	else
+		major = 0;
+	if(sv.GetCount() > 1)
+		minor = ScanInt(sv[1]);
+	else
+		minor = 0;
+	if(sv.GetCount() > 2)
+		devel = ScanInt(sv[2]);
+	else
+		devel = 0;
+}
+
+// comparing
+bool ProductVersion::operator <(ProductVersion const &v2) const
+{
+	if(major < v2.major)
+		return true;
+	else if(major > v2.major)
+		return false;
+	else if(minor < v2.minor)
+		return true;
+	else if(minor > v2.minor)
+		return false;
+	else if(devel < v2.devel)
+		return true;
+	else if(devel > v2.devel)
+		return false;
+	else
+		return false;
+}
+
+bool ProductVersion::operator <=(ProductVersion const &v2) const
+{
+	if(major < v2.major)
+		return true;
+	else if(major > v2.major)
+		return false;
+	else if(minor < v2.minor)
+		return true;
+	else if(minor > v2.minor)
+		return false;
+	else if(devel < v2.devel)
+		return true;
+	else if(devel > v2.devel)
+		return false;
+	else
+		return true;
+}
+
+bool ProductVersion::operator >(ProductVersion const &v2) const
+{
+	return !operator<=(v2);
+}
+
+bool ProductVersion::operator >=(ProductVersion const &v2) const
+{
+	return !operator<(v2);
+}
+
+bool ProductVersion::operator ==(ProductVersion const &v2) const
+{
+	return major == v2.major && minor == v2.minor && devel == v2.devel;
+}
+
+// assignement
+ProductVersion &ProductVersion::operator=(ProductVersion const &v)
+{
+	major = v.major;
+	minor = v.minor;
+	devel = v.devel;
+	return *this;
+}
+
+ProductVersion &ProductVersion::operator=(String const &s)
+{
+	Vector<String>sv = Split(s, '.', false);
+	if(sv.GetCount() > 0)
+		major = ScanInt(sv[0]);
+	else
+		major = 0;
+	if(sv.GetCount() > 1)
+		minor = ScanInt(sv[1]);
+	else
+		minor = 0;
+	if(sv.GetCount() > 2)
+		devel = ScanInt(sv[2]);
+	else
+		devel = 0;
+	return *this;
+}
+
+// conversion to string
+String ProductVersion::ToString(void) const
+{
+	String s = Format("%d.%d", major, minor);
+	if(devel != 0)
+		s += Format(".%d", devel);
+	return s;
+}
+
+ProductVersion::operator String(void) const
+{
+	return ToString();
+}
+
+// check if version is a devel one
+bool ProductVersion::IsDevel(void) const
+{
+	return devel != 0;
+}
+
+// check for 'empty' version
+bool ProductVersion::operator!(void)
+{
+	return !operator bool();
+}
+
+ProductVersion::operator bool(void)
+{
+	return major != 0 || minor != 0 || devel != 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// CLASS ProductVersions
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// constructors
+ProductVersions::ProductVersions()
+{
+}
+
+ProductVersions::ProductVersions(ProductVersions pick_ &vs) : Vector<ProductVersion>(vs)
+{
+}
+
+ProductVersions::ProductVersions(ProductVersions const &vs, int) : Vector<ProductVersion>(vs, 1)
+{
+}
+
+ProductVersions::ProductVersions(String s)
+{
+	// strip eventually contained \r characters
+	int i;
+	while( (i = s.Find('\r')) >= 0)
+		s.Remove(i);
+	
+	// split string on newlines
+	Vector<String>sv = Split(s, '\n');
+	
+	// read all versions from vector
+	for(i = 0; i < sv.GetCount(); i++)
+		Add(sv[i]);
+}
+
+// assignement
+ProductVersions &ProductVersions::operator=(pick_ ProductVersions &vs)
+{
+	Vector<ProductVersion>::operator=(vs);
+	return *this;
+}
+
+// load/store to/from string
+// one version per line, format MMMM.mmmm.dddddd
+String ProductVersions::ToString(void) const
+{
+	String s;
+	for(int i = 0; i < GetCount(); i++)
+		s += operator[](i).ToString() + "\n";
+	return s;
+}
+
+ProductVersions::operator String(void) const
+{
+	return ToString();
+}
+
+// find next available version -- including or not devel versions
+bool ProductVersions::HasNew(ProductVersion const &v, bool devel) const
+{
+	for(int i = 0; i < GetCount(); i++)
+	{
+		if(!devel && operator[](i).IsDevel())
+			continue;
+		if(operator[](i) > v)
+			return true;
+	}
+	return false;
+}
+
+bool ProductVersions::HasNew(ProductVersion const &v, ProductVersion const &maxv, bool devel) const
+{
+	for(int i = 0; i < GetCount(); i++)
+	{
+		if(!devel && operator[](i).IsDevel())
+			continue;
+		if(operator[](i) > maxv)
+			continue;
+		if(operator[](i) > v)
+			return true;
+	}
+	return false;
+}
+
+ProductVersion ProductVersions::FindMax(ProductVersion const &v, bool devel) const
+{
+	ProductVersion maxV(v);
+	for(int i = 0; i < GetCount(); i++)
+	{
+		if(!devel && operator[](i).IsDevel())
+			continue;
+		if(operator[](i) > maxV)
+			maxV = operator[](i);
+	}
+	return maxV;
+}
+
+ProductVersion ProductVersions::FindMax(ProductVersion const &v, ProductVersion const &maxv, bool devel) const
+{
+	ProductVersion maxV(v);
+	for(int i = 0; i < GetCount(); i++)
+	{
+		if(!devel && operator[](i).IsDevel())
+			continue;
+		if(operator[](i) > maxv)
+			continue;
+		if(operator[](i) > maxV)
+			maxV = operator[](i);
+	}
+	return maxV;
+}
+
+END_UPP_NAMESPACE
---
+++ bazaar/ProductVersion/ProductVersion.h
@@ -0,0 +1,88 @@
+#ifndef _ProductVersion_ProductVersion_h
+#define _ProductVersion_ProductVersion_h
+
+#include <Core/Core.h>
+
+NAMESPACE_UPP
+
+// Application version handling - holds a version
+// with subversion and devel field
+// brings som compare functions and String conversion
+class ProductVersion : public Moveable<ProductVersion>
+{
+	private:
+	
+		int major;
+		int minor;
+		int devel;
+	
+	protected:
+	
+	public:
+	
+		// constructor
+		ProductVersion() { major = minor = devel = 0; }
+		ProductVersion(int major, int minor, int devel);
+		ProductVersion(int major, int minor);
+		ProductVersion(ProductVersion const &v);
+		ProductVersion(String const &s);
+		
+		// clearing
+		ProductVersion &Clear() { major = minor = devel = 0; return *this; }
+		
+		// comparing
+		bool operator <(ProductVersion const &v2) const;
+		bool operator <=(ProductVersion const &v2) const;
+		bool operator >(ProductVersion const &v2) const;
+		bool operator >=(ProductVersion const &v2) const;
+		bool operator ==(ProductVersion const &v2) const;
+		
+		// assignement
+		ProductVersion &operator=(ProductVersion const &v);
+		ProductVersion &operator=(String const &s);
+		
+		// conversion to string
+		String ToString(void) const;
+		operator String(void) const;
+		
+		// check if version is a devel one
+		bool IsDevel(void) const;
+		
+		// check for 'empty' version
+		bool operator!(void);
+		operator bool(void);
+};
+
+class ProductVersions : public Vector<ProductVersion>
+{
+	private:
+	
+	protected:
+	
+	public:
+	
+		// constructors
+		ProductVersions();
+		ProductVersions(ProductVersions pick_ &vs);
+		ProductVersions(ProductVersions const &vs, int);
+		ProductVersions(String s);
+		
+		// assignement
+		ProductVersions &operator=(pick_ ProductVersions &vs);
+		
+		// load/store to/from string
+		// one version per line, format MMMM.mmmm.dddddd
+		String ToString(void) const;
+		operator String(void) const;
+		
+		// find next available version -- including or not devel versions
+		bool HasNew(ProductVersion const &v, bool devel) const;
+		bool HasNew(ProductVersion const &v, ProductVersion const &maxv, bool devel) const;
+		ProductVersion FindMax(ProductVersion const &v, bool devel) const;
+		ProductVersion FindMax(ProductVersion const &v, ProductVersion const &maxv, bool devel) const;
+};
+
+END_UPP_NAMESPACE
+
+#endif
+
