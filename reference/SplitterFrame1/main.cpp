7f63606499548f4b2e1148a0e47c9ae61192f93c
sergeynikitin <sergeynikitin@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-02-24 00:02:38 +0000

    .reference/SplitterFrame : one more example with offten usable situation
    git-svn-id: svn://ultimatepp.org/upp/trunk@3236 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/SplitterFrame1/main.cpp
@@ -0,0 +1,32 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+#define LAYOUTFILE <SplitterFrame1/StandardLayout.lay>
+#include <CtrlCore/lay.h>
+
+struct MyApp : WithStandardLayout<TopWindow> {
+	SplitterFrame sf;
+	ArrayCtrl list,list1;
+	typedef MyApp CLASSNAME;
+	TopWindow app;
+
+	MyApp() {
+		CtrlLayoutOKCancel(*this, "Test SplitterFrame");
+		AddFrame(sf.Left(list, 200));
+		list.AddColumn("List");
+		for(int i = 0; i < 100; i++)
+			list.Add(FormatIntRoman(i, true));
+		AddFrame(InsetFrame());
+		sf.SizeMin(200).MinSize(100);
+		Sizeable();
+		
+	}
+};
+
+GUI_APP_MAIN
+{
+	MyApp().Run();
+}
+
+
+
