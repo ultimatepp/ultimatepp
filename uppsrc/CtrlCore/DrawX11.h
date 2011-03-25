e4a6dec5fb13a49a82bf4026c593dc9b8d89ab21
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-03-25 20:00:06 +0000

    .ide: fixed to compile in win32
    git-svn-id: svn://ultimatepp.org/upp/trunk@3311 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/DrawX11.h
+++ uppsrc/CtrlCore/DrawX11.h
@@ -174,6 +174,9 @@ public:
 	SystemDraw(Drawable dw, GC gc, XftDraw *xftdraw, const Vector<Rect>& clip);
 };
 
+Image X11Cursor(int c);
+void *CursorX11(const Image& img);
+
 END_UPP_NAMESPACE
 
 #endif
