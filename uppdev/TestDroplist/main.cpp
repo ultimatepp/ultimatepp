c673eb2140e6bae54ef79a1a0e32287d1c99815a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-04-17 20:05:18 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@3349 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/TestDroplist/main.cpp
+++ uppdev/TestDroplist/main.cpp
@@ -7,8 +7,10 @@ GUI_APP_MAIN
 	DropList dl;
 	for(int i = 0; i < 100; i++)
 		dl.Add(i);
+	dl <<= 20;
+	dl.SetReadOnly();
 	TopWindow w;
-	w.Add(dl.LeftPos(10, 50).TopPos(0, 20));
+	w.Add(dl.LeftPos(10, 150).TopPos(0, 20));
 	dl.DropWidth(120);
 	dl.Tip("Tip!");
 	w.Run();
