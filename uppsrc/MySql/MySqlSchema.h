6350d66fbf236f164b2543fbeecef4ab9b5b7d1f
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-05-04 18:00:08 +0000

    *MySql: SQLDEFAULT added
    git-svn-id: svn://ultimatepp.org/upp/trunk@3402 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/MySql/MySqlSchema.h
+++ uppsrc/MySql/MySqlSchema.h
@@ -40,6 +40,7 @@
 #define AUTO_INCREMENT             INLINE_ATTRIBUTE("auto_increment")
 #define KEY                        INLINE_ATTRIBUTE("key")
 #define NOT_NULL                   INLINE_ATTRIBUTE("not null")
+#define SQLDEFAULT(v)              INLINE_ATTRIBUTE("default " #v)
 
 #define INDEX                      ATTRIBUTE("alter table @t add index IDX_@x (@c);", \
                                              "alter table @t drop index IDX_@x;")
@@ -55,9 +56,7 @@
 
 #define INNODB                     TABLE_SUFFIX(" type=InnoDB")
 
-
 #include <Sql/sch_model.h>
-
 
 #undef INT
 #undef INT_ARRAY
@@ -91,6 +90,7 @@
 #undef AUTO_INCREMENT
 #undef KEY
 #undef NOT_NULL
+#undef SQLDEFAULT
 
 #undef INDEX
 
