53d1777d22b2d3562767bf788abed327a790de56
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-06-10 06:52:13 +0000

    .developing rainbow
    git-svn-id: svn://ultimatepp.org/upp/trunk@3506 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/CtrlCore.h
+++ uppsrc/CtrlCore/CtrlCore.h
@@ -31,52 +31,6 @@ struct GuiLock {
 bool ScreenInPaletteMode();
 Size GetScreenSize();
 
-class BackDraw : public SystemDraw {
-public:
-	virtual bool  IsPaintingOp(const Rect& r) const;
-
-protected:
-	GUIPLATFORM_BACKDRAW_DECLS
-	Size    size;
-	Draw   *painting;
-	Point   painting_offset;
-
-public:
-	void  Put(SystemDraw& w, int x, int y);
-	void  Put(SystemDraw& w, Point p)                  { Put(w, p.x, p.y); }
-
-	void Create(SystemDraw& w, int cx, int cy);
-	void Create(SystemDraw& w, Size sz)                { Create(w, sz.cx, sz.cy); }
-	void Destroy();
-
-	void SetPaintingDraw(Draw& w, Point off)           { painting = &w; painting_offset = off; }
-
-	BackDraw();
-	~BackDraw();
-};
-
-class ImageDraw : public SystemDraw {
-	Size    size;
-
-	GUIPLATFORM_IMAGEDRAW_DECLS
-
-	bool    has_alpha;
-
-	void Init();
-	Image Get(bool pm) const;
-
-public:
-	Draw& Alpha();                       
-
-	operator Image() const;
-	
-	Image GetStraight() const;
-	
-	ImageDraw(Size sz);
-	ImageDraw(int cx, int cy);
-	~ImageDraw();
-};
-
 typedef ImageDraw SystemImageDraw;
 
 void DrawDragRect(SystemDraw& w, const Rect& rect1, const Rect& rect2, const Rect& clip, int n,
