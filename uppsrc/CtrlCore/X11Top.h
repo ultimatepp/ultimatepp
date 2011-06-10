777a725efcad8c1a7328381eee07ee1e80ea1d5d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-06-10 08:44:50 +0000

    .developing rainbow
    git-svn-id: svn://ultimatepp.org/upp/trunk@3507 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/CtrlCore/X11Top.h
@@ -0,0 +1,18 @@
+protected:
+	virtual void    EventProc(XWindow& w, XEvent *event);
+private:
+	XSizeHints *size_hints;
+	XWMHints   *wm_hints;
+	XClassHint *class_hint;
+	Size        xminsize, xmaxsize;
+	bool        topmost;
+
+	void          CenterRect(Ctrl *owner);
+	void          DefSyncTitle();
+	void          EndIgnoreTakeFocus();
+
+	void          SyncState();
+	void          SyncState0();
+
+	Image         invert;
+	WString       title2;
