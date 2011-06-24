c9354953695f8e237d1ca5893132dfc5632203d2
kohait <kohait@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-06-24 08:05:23 +0000

    bazaar: SDL: linkage fixes
    git-svn-id: svn://ultimatepp.org/upp/trunk@3554 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SDL/SDLWrapper.h
+++ bazaar/SDL/SDLWrapper.h
@@ -1,17 +1,17 @@
 #ifndef _SDL_SDLWrapper_h_
 #define _SDL_SDLWrapper_h_
 
-#if defined(_WIN32)
+#ifdef COMPILER_MSC
 	#include <SDL.h> 
 	#include <SDL_thread.h> 
 	#include <SDL_syswm.h>
-#else
+#endif
+
+#ifdef COMPILER_GCC
 	#include <SDL/SDL.h>
 	#include <SDL/SDL_thread.h>
 	#include <SDL/SDL_syswm.h>
 #endif
-
-#undef main
 
 #define INT64_C(val) val##LL
 #define UINT64_C(val) val##ULL 
