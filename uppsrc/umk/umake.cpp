c1a98b419d6e57030de5e61d855ae214741abe38
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-08-06 13:27:14 +0000

    umk: improved verbose mode
    git-svn-id: svn://ultimatepp.org/upp/trunk@3746 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/umk/umake.cpp
+++ uppsrc/umk/umake.cpp
@@ -46,6 +46,7 @@ CONSOLE_APP_MAIN
 	Ide ide;
 	TheIde(&ide);
 	ide.console.SetSlots(CPU_Cores());
+	ide.console.console = true;
 	bool clset = false;
 	const Vector<String>& arg = CommandLine();
 	if(arg.GetCount() >= 2 && IsAlpha(arg[0][0]) && IsAlpha(arg[1][0])) {
@@ -53,26 +54,37 @@ CONSOLE_APP_MAIN
 		for(int i = 3; i < arg.GetCount(); i++)
 			if(arg[i][0] == '-') {
 				String x = arg[i];
-				for(int i = 1; i < x.GetCount(); i++)
+				for(int i = 1; i < x.GetCount(); i++) {
 					if(x[i] == 'l')
 						SilentMode = true;
+					if(x[i] == 'v')
+						ide.console.verbosebuild = true;
+				}						
 			}
-		if(!FileExists(GetUmkFile(arg[0] + ".var"))) {
+		String v = GetUmkFile(arg[0] + ".var");
+		if(!FileExists(v)) {
 			Vector<String> h = SplitDirs(arg[0]);
 			for(int i = 0; i < h.GetCount(); i++)
 				h[i] = GetFullPath(h[i]);
-			SetVar("UPP", Join(h, ";"), false);
+			String x = Join(h, ";");
+			SetVar("UPP", x, false);
+			PutVerbose("Inline assembly: " + x);
 			String outdir = ConfigFile("_out");
 			RealizeDirectory(outdir);
 			SetVar("OUTPUT", outdir, false);
 		}
-		else
+		else {
+			PutVerbose("Assembly file: " + v);
 			if(!LoadVars(arg[0])) {
 				Puts("Invalid assembly\n");
 				SetExitCode(2);
 				return;
 			}
-		if(!FileExists(SourcePath(arg[1], GetFileTitle(arg[1]) + ".upp"))) {
+		}
+		PutVerbose("Output directory: " + GetVar("OUTPUT"));;
+		v = SourcePath(arg[1], GetFileTitle(arg[1]) + ".upp");
+		PutVerbose("Main package: " + v);
+		if(!FileExists(v)) {
 			Puts("Package does not exist\n");
 			SetExitCode(2);
 			return;
@@ -107,7 +119,6 @@ CONSOLE_APP_MAIN
 			ide.debug.linkmode = ide.release.linkmode = 0;
 			ide.release.createmap = ide.debug.createmap = false;
 			ide.targetmode = 0;
-			ide.console.console = true;
 			bool clean = false;
 			bool makefile = false;
 			int  exporting = 0;
