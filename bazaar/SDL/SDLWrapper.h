aa1e6a49dc9321c3d230da9c67f7a2927e6d7f63
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-08-25 21:03:25 +0000

    *SDL: Fixed includes.
    git-svn-id: svn://ultimatepp.org/upp/trunk@3779 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SDL/SDLWrapper.h
+++ bazaar/SDL/SDLWrapper.h
@@ -1,17 +1,11 @@
 #ifndef _SDL_SDLWrapper_h_
 #define _SDL_SDLWrapper_h_
 
-#ifdef COMPILER_MSC
+
 	#include <SDL.h> 
 	#include <SDL_thread.h> 
 	#include <SDL_syswm.h>
-#endif
 
-#ifdef COMPILER_GCC
-	#include <SDL/SDL.h>
-	#include <SDL/SDL_thread.h>
-	#include <SDL/SDL_syswm.h>
-#endif
 
 #define INT64_C(val) val##LL
 #define UINT64_C(val) val##ULL 
