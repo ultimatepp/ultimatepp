c5856c73ce616350e619f154c2680cc0d08faf9e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-09-14 09:09:17 +0000

    *RichText: IsPainting fix, Web: HttpClient put fix
    git-svn-id: svn://ultimatepp.org/upp/trunk@3862 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/RichText/TxtPaint.cpp
+++ uppsrc/RichText/TxtPaint.cpp
@@ -125,7 +125,7 @@ bool IsPainting(PageDraw& pw, Zoom z, const Rect& page, PageY top, PageY bottom)
 	int t = top.y;
 	for(int pi = top.page; pi <= bottom.page; pi++)
 		if(pw.Page(pi).IsPainting(Rect(z * page.left, z * (pi == top.page ? top.y : page.top),
-		                               z * page.right, z * (pi == bottom.page ? bottom.y : page.bottom))));
+		                               z * page.right, z * (pi == bottom.page ? bottom.y : page.bottom))))
 			return true;
 	return false;
 }
