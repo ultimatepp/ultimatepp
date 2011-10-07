672c48815e72ba02d56fd3c178f766aeb2be5ae3
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-10-07 06:54:13 +0000

    *ide: fixed GCC compilation issue
    git-svn-id: svn://ultimatepp.org/upp/trunk@3958 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/UppWspc.cpp
+++ uppsrc/ide/UppWspc.cpp
@@ -23,7 +23,7 @@ Font WorkspaceWork::ListFont()
 
 void WorkspaceWork::SetErrorFiles(const Vector<String>& files)
 {
-	errorfiles <<= files;
+	errorfiles <<= Index<String>(files, 1);
 	int i = filelist.GetCursor();
 	int s = filelist.GetSbPos();
 	SaveLoadPackage();
