0ff08484789151a4f6f4e2864446883f0d12f3f6
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-11-06 12:30:18 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@4137 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ benchmarks/JSON/JSON.h
@@ -0,0 +1,23 @@
+#ifndef _Json_Json_h_
+#define _Json_Json_h_
+
+#include <Core/Core.h>
+
+using namespace Upp;
+/*
+struct JsonMaker {
+	String text;
+	String indent;
+
+	JsonString& operator()(const String& key, const Value& value);
+	JsonString& operator<<(const Value& value);
+	JsonString& Object();
+	JsonString& EndObject();
+	JsonString& Array();
+	JsonString& EndArray();
+	
+	JsonString(bool pretty);
+};
+*/
+
+#endif
