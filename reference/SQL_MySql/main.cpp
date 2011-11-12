a9e34387148290b7bb6d3b82696e22c950e94a17
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-11-12 17:52:30 +0000

    .reference: replace SQL_MySql example (thanks dolik.rce)
    git-svn-id: svn://ultimatepp.org/upp/trunk@4165 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/SQL_MySql/main.cpp
+++ reference/SQL_MySql/main.cpp
@@ -2,17 +2,46 @@
 
 using namespace Upp;
 
+#define SCHEMADIALECT <MySql/MySqlSchema.h>
+#define MODEL <schema/schema.sch>
+#include <Sql/sch_header.h>
+#include <Sql/sch_source.h>
+#include <Sql/sch_schema.h>
+
 CONSOLE_APP_MAIN
 {
 	MySqlSession session;
-	// substitute your 'username' and 'password' here:
-	if(!session.Connect("username", "password", "mysql", "localhost")) {
-		printf("Can't connect with MySql\n");
-		return;
+	// edit the connection parameters if necessary
+	if(session.Connect("root", NULL, "test")) {
+		Cout() << "Connected\n";
+		SQL = session;
+		
+		SqlSchema sch(MY_SQL);
+		All_Tables(sch);
+		// create the table if necessary
+		SqlPerformScript(sch.Upgrade());
+		SqlPerformScript(sch.Attributes());
+		SQL.ClearError();
+		
+		try {
+			// insert some random data
+			SQL & Insert(TEST_TABLE)(value, Uuid::Create().ToString());
+			// fetch some data
+			Sql sql;
+			SQL * Select(ID, VALUE).From(TEST_TABLE)
+			                       .OrderBy(Descending(id))
+			                       .Limit(5);
+			while(SQL.Fetch())
+				Cout() << AsString(sql[0]) << ": " << AsString(sql[VALUE]) << "\n";
+		}
+		catch(SqlExc &ex) {
+			Cerr() << "ERROR: " << ex << "\n";
+			SetExitCode(1);
+		}
 	}
-	Sql sql(session);
-	sql.Execute("use mysql");
-	sql.Execute("show tables");
-	while(sql.Fetch())
-		Cout() << (String)sql[0] << '\n';
+	else {
+		Cerr() <<"ERROR: Unable to connect to database\n";
+		SetExitCode(1);
+	}
+	SetExitCode(0);
 }
