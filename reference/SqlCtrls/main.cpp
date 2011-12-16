f636078db82971ba8a617a0dc17e0d66b8d872d4
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2011-12-16 17:02:02 +0000

    reference/SqlCtrls
    git-svn-id: svn://ultimatepp.org/upp/trunk@4310 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/SqlCtrls/main.cpp
@@ -0,0 +1,110 @@
+#include "SqlCtrls.h"
+
+#ifdef _DEBUG
+#include <Sql/sch_schema.h>
+#endif
+
+#include <Sql/sch_source.h>
+
+struct PersonDlg : public WithPersonLayout<TopWindow> {
+	typedef PersonDlg CLASSNAME;
+
+	SqlCtrls ctrls;
+
+	PersonDlg();
+};
+
+PersonDlg::PersonDlg()
+{
+	ctrls
+		(NAME, name)
+		(LASTNAME, lastname)
+		(AGE, age)
+	;
+	CtrlLayoutOKCancel(*this, "Person");
+}
+
+void PersonsDlg::Create()
+{
+	PersonDlg dlg;
+	dlg.Title("New person");
+	if(dlg.Execute() != IDOK)
+		return;
+	SQL * dlg.ctrls.Insert(PERSON);
+	int id = SQL.GetInsertedId();
+	list.ReQuery();
+	list.FindSetCursor(id);
+}
+
+void PersonsDlg::Edit()
+{
+	int id = list.GetKey();
+	if(IsNull(id))
+		return;
+	PersonDlg dlg;
+	if(!dlg.ctrls.Load(PERSON, ID == id))
+		return;
+	if(dlg.Execute() != IDOK)
+		return;
+	SQL * dlg.ctrls.Update(PERSON).Where(ID == id);
+	list.ReQuery();
+	list.FindSetCursor(id);
+}
+
+void PersonsDlg::Remove()
+{
+	int id = list.GetKey();
+	if(IsNull(id) || !PromptYesNo("Delete row?"))
+		return;
+	SQL * SqlDelete(PERSON).Where(ID == id);
+	list.ReQuery();
+}
+
+PersonsDlg::PersonsDlg()
+{
+	CtrlLayout(*this, "Persons");
+
+	create <<= THISBACK(Create);
+	edit <<= THISBACK(Edit);
+	remove <<= THISBACK(Remove);
+	
+	list.SetTable(PERSON);
+	list.AddKey(ID);
+	list.AddColumn(NAME, "Name");
+	list.AddColumn(LASTNAME, "Last Name");
+	list.AddColumn(AGE, "Age");
+	list.WhenLeftDouble = THISBACK(Edit);
+	list.SetOrderBy(AGE, NAME);
+	list.Query();
+}
+
+GUI_APP_MAIN
+{
+	Sqlite3Session sqlite3;
+	if(!sqlite3.Open(GetHomeDirFile("persons.db"))) {
+		LOG("Can't create or open database file\n");
+		return;
+	}
+	sqlite3.SetTrace();
+
+	SQL = sqlite3;
+
+	// Update the schema to match the schema described in "simple.sch"
+	#ifdef _DEBUG
+	SqlSchema sch(SQLITE3);
+	StdStatementExecutor se(sqlite3);
+	All_Tables(sch);
+	if(sch.ScriptChanged(SqlSchema::UPGRADE))
+		Sqlite3PerformScript(sch.Upgrade(),se);
+	if(sch.ScriptChanged(SqlSchema::ATTRIBUTES)) {
+		Sqlite3PerformScript(sch.Attributes(),se);
+	}
+	if(sch.ScriptChanged(SqlSchema::CONFIG)) {
+		Sqlite3PerformScript(sch.ConfigDrop(),se);
+		Sqlite3PerformScript(sch.Config(),se);
+	}
+	sch.SaveNormal();
+	#endif
+
+	PersonsDlg().Run();
+}
