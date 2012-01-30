807ab2e26e2c40eca6172c09a854a95bc3fbe774
dolik <dolik@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-01-30 18:50:41 +0000

    .lpbuild2: packaging development (#RM 244)
    git-svn-id: svn://ultimatepp.org/upp/trunk@4499 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/lpbuild2/localbuild.sh
+++ uppbox/lpbuild2/localbuild.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
-set -e
+
 # This scripts is meant to create deb packages for stable releases. It will best
 # work with ubuntu, but it should work on debian and even other distros, as long
 # as pbuilder, debootstrap and devscripts packages are available.
 
-# set desired architectures and distributions
-archs="i386 amd64"
-dists="karmic lucid maverick natty oneiric squeeze wheezy sid"
+# This script expects configuration file as parameter (see e.g. nightly.cfg)
 
-# prepare source packages
-stable=1
-noupload=1
-scriptpath="$(cd ${0%/*} 2>/dev/null || true; echo $PWD/${0##*/})"
-. "$(dirname $scriptpath)/lpbuild.sh"
+[ ! "$1" ] && echo "Usage: $0 file.cfg" > /dev/stderr && exit 1
+set -e
 
-for dist in $dists; do
-  for arch in $archs; do
+# read the variables from config file
+. "$1"
+
+# create the source packages
+DISTS="$LOCAL_DISTS"
+TMP="$LOCAL_TMP"
+. "$RES_DIR/dsccreate.sh"
+
+for dist in $LOCAL_DISTS; do
+  for arch in $ARCHS; do
     # create the chroot if it doesn't exist yet
-    if [ ! -f "/var/cache/pbuilder/$NAME-base.tgz" ]; then
-      sudo DIST=$dist ARCH=$arch pbuilder --create --configfile "$scriptdir/pbuilderrc" --distribution $dist --architecture $arch
+    if [ ! -f "/var/cache/pbuilder/$dist-$arch-base.tgz" ]; then
+      sudo DIST=$dist ARCH=$arch pbuilder --create --configfile "$RES_DIR/pbuilderrc" --distribution $dist --architecture $arch
     fi;
     # build the binary packages
-    sudo DIST=$dist ARCH=$arch pbuilder --build --configfile "$scriptdir/pbuilderrc" --distribution $dist --architecture $arch "$TMP/upp_${version}-0~${dist}0.dsc"
+    sudo DIST=$dist ARCH=$arch pbuilder --build --configfile "$RES_DIR/pbuilderrc" --distribution $dist --architecture $arch "$TMP/upp_${VERSION}-0~${dist}0.dsc"
   done
 done
-
-echo "Done!"
