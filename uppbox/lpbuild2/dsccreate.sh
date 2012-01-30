770e49e4248db278e7bf7ca424a6752979e9af9b
dolik <dolik@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-01-30 18:50:41 +0000

    .lpbuild2: packaging development (#RM 244)
    git-svn-id: svn://ultimatepp.org/upp/trunk@4499 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppbox/lpbuild2/dsccreate.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# This script takes some predefined variables and creates .dsc packages
+
+# which version to build
+VERSION="$(echo $SRC | sed 's/.*upp-x11-src-\([0-9]*\)[.]tar[.]gz/\1/')"
+
+# copy resources
+tmp="$TMP/upp-$VERSION"
+mkdir -p "$tmp"
+cp -r "$RES_DIR/debian" "$tmp/debian"
+for f in Makefile mkfile copyright theide.1 umk.1 theide.desktop; do
+    cp "$RES_DIR/$f" "$tmp/$f"
+done
+
+# link the original tarball
+ln -s "$SRC" "$TMP/upp_$VERSION.orig.tar.gz"
+
+# unpack the original tarball
+tar -xzf "$TMP/upp_$VERSION.orig.tar.gz" -C "$tmp"
+mv "$tmp/upp-x11-src-$VERSION"/{bazaar,examples,reference,tutorial,uppsrc} "$tmp/"
+rm -r "$tmp/upp-x11-src-$VERSION"
+
+# tweak watch file, if we build stable release
+[ "$STABLE" = "yes" ] && sed -i '/ultimatepp.org/s/^/#/;/sf.net/s/^#//;' "$tmp/debian/watch"
+
+workdir="$(pwd)"
+cd "$tmp"
+
+for dist in $DISTS; do
+    # create changelog
+    cat > "debian/changelog" << EOF
+upp ($VERSION-0~${dist}0) $dist; urgency=low
+
+  * Updated to SVN VERSION $VERSION
+  * See http://www.ultimatepp.org/redmine/projects/upp/repository for complete
+    and detailed list of changes.
+
+ -- $MAINTAINER  `date -R`
+EOF
+    # store changes done so far
+    [ "$skiporig" ] || dpkg-source --commit . packaging packaging.patch
+    # build source packages
+    dpkg-buildpackage -S $BUILDPKGOPTS $skiporig
+    skiporig="-sd"
+done
+
+cd "$workdir"
--- uppbox/lpbuild2/lpbuild.sh
+++ uppbox/lpbuild2/lpbuild.sh
@@ -1,68 +1,19 @@
 #!/bin/sh
 
-# PRODUCTION CONFIGURATION
-GPG_PROG="/home/upp/bin/gpg-extpass"   # what program to use for signing (here a wrapper that supplies passphrase)
-GPG_KEY="0CE4194D"                     # gpg key fingerprint to use
-TMP="~/lpbuild.tmp"                    # path for temporary files
-SRCDIR="~/upp.tmp/u"                   # where to look for source tarball
+# Launchpad.net ubuntu packages builds
+# This script expects configuration file as parameter (see e.g. nightly.cfg)
 
-## DEBUG CONFIGURATION
-#GPG_PROG="/usr/bin/gpg-extpass"
-#GPG_KEY="13A907FC"
-#TMP="/tmp/upp"
-#SRCDIR="/home/h"
+[ ! "$1" ] && echo "Usage: $0 file.cfg" > /dev/stderr && exit 1
 set -e
 
-version="$(ls $SRCDIR/upp-x11-src-*.tar.gz | sed '$s/.*upp-x11-src-\([0-9]*\)[.]tar[.]gz/\1/')"
-tmp="$TMP/upp-$version"
+# read the variables from config file
+. "$1"
 
-if [ ! "$version" ]; then
-    echo "Source tarball not found in $SRC!"
-    exit 1
-elif [ -d "$tmp" ]; then
-    echo "Version $version was already built once!"
-    echo " (If you are trying to build it again on purpose,"
-    echo "  delete directory '$tmp' manually)"
-    exit 1
-fi
+# create the source packages
+DISTS="$LP_DISTS"
+TMP="$LP_TMP"
+. "$RES_DIR/dsccreate.sh"
 
-[ -z "$dists" ] && dists="karmic lucid maverick natty oneiric"
-scriptpath="$(cd ${0%/*} 2>/dev/null || true; echo $PWD/${0##*/})"
-scriptdir="$(dirname $scriptpath)"
-src="$SRCDIR/upp-x11-src-$version.tar.gz"
-
-rm -rf "$TMP"
-mkdir -p "$tmp"
-
-cp -r "$scriptdir/debian" "$tmp/debian"
-for f in Makefile mkfile copyright theide.1 umk.1 theide.desktop; do
-    cp "$scriptdir/$f" "$tmp/$f"
+for dist in $LP_DISTS; do
+  dput $DPUT_OPTS "upp-$dist" "$TMP/upp_${VERSION}-0~${dist}0_source.changes"
 done
-[ "$stable" = 1 ] && sed -i '/code.google/s/^/#/;/sf.net/s/^#//;' "$tmp/debian/watch"
-ln -s "$src" "$TMP/upp_$version.orig.tar.gz"
-
-cd $tmp
-tar -xzf "$TMP/upp_$version.orig.tar.gz"
-mv "$tmp/upp-x11-src-$version"/{bazaar,examples,reference,tutorial,uppsrc} "$tmp/"
-rm -r "$tmp/upp-x11-src-$version"
-
-set +e
-for dist in $dists; do
-    distver=$version-0~$dist"0"
-
-    cat > "debian/changelog" << EOF
-upp ($distver) $dist; urgency=low
-
-  * Updated to SVN version $version
-  * See http://www.ultimatepp.org/redmine/projects/upp/repository for complete
-    and detailed list of changes.
-
- -- Jan Dolinar <dolik.rce@seznam.cz>  `date -R`
-EOF
-
-    [ "$skiporig" ] || dpkg-source --commit . packaging packaging.patch
-    dpkg-buildpackage -d -S -k$GPG_KEY -m'Jan Dolinar <dolik.rce@seznam.cz>' -sgpg -p$GPG_PROG $skiporig
-    [ "$noupload" = 1 ] || dput -c "$scriptdir/dput.cfg" "upp-$dist" "../upp_"$distver"_source.changes"
-    skiporig="-sd"
-done
-set -e
