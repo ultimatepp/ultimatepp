eb223120f10928441b8117462d4a62947159d71c
dolik <dolik@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-03-22 11:55:29 +0000

    .lpbuild2: fix parallel builds
    git-svn-id: svn://ultimatepp.org/upp/trunk@4712 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/lpbuild2/localbuild.sh
+++ uppbox/lpbuild2/localbuild.sh
@@ -24,8 +24,13 @@ for dist in $LOCAL_DISTS; do
     # create the chroot if it doesn't exist yet
     if [ ! -f "/var/cache/pbuilder/$dist-$arch-base.tgz" ]; then
       sudo DIST=$dist ARCH=$arch pbuilder --create --configfile "$RES_DIR/pbuilderrc" --distribution $dist --architecture $arch
+      #import key into newly created chroot
+      echo "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $GPG_KEY" > "$TMP/keyscript.sh"
+      chmod a+x "$TMP/keyscript.sh"
+      sudo DIST=$dist ARCH=$arch pbuilder --execute --save-after-exec --configfile "$RES_DIR/pbuilderrc" --distribution $dist --architecture $arch -- "$TMP/keyscript.sh"
+      rm -f "$TMP/keyscript.sh"
     fi;
     # build the binary packages
-    sudo DIST=$dist ARCH=$arch pbuilder --build --configfile "$RES_DIR/pbuilderrc" --distribution $dist --architecture $arch "$TMP/upp_${VERSION}-0~${dist}0.dsc"
+    sudo DIST=$dist ARCH=$arch pbuilder --build --configfile "$RES_DIR/pbuilderrc" --distribution $dist --architecture $arch --debbuildopts "-j$JOBS " "$TMP/upp_${VERSION}-0~${dist}0.dsc"
   done
 done
