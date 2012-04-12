2b648923811d29cd2eff5fb0541e946a82a42046
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-04-12 07:01:40 +0000

    Core: Http improvements
    git-svn-id: svn://ultimatepp.org/upp/trunk@4777 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Web.h
+++ uppsrc/Core/Web.h
@@ -331,7 +331,7 @@ public:
 	HttpRequest&  StdHeaders(bool sh)                     { std_headers = sh; return *this; }
 	HttpRequest&  NoStdHeaders()                          { return StdHeaders(false); }
 	HttpRequest&  Accept(const String& a)                 { accept = a; return *this; }
-	HttpRequest&  Agent(const String& a)                  { agent = a; return *this; }
+	HttpRequest&  UserAgent(const String& a)              { agent = a; return *this; }
 	HttpRequest&  ContentType(const String& a)            { contenttype = a; return *this; }
 
 	HttpRequest&  Proxy(const String& host, int port)     { proxy_host = host; proxy_port = port; return *this; }
