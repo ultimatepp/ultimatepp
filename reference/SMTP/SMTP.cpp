f2ed74f8b917ba8ffaeb94b6cc366dd69d5d85c6
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-04-22 13:52:55 +0000

    reference: SMTP
    git-svn-id: svn://ultimatepp.org/upp/trunk@4844 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/SMTP/SMTP.cpp
@@ -0,0 +1,33 @@
+#include <Core/SMTP/SMTP.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+//	Smtp::Trace();
+	for(;;) {
+		Cout() << "Send email to: ";
+		String to = ReadStdIn();
+		if(IsNull(to))
+			break;
+		Cout() << "Your full email address: ";
+		String user = ReadStdIn();
+		Cout() << "Your Gmail password: ";
+		String pwd = ReadStdIn();
+		Smtp mail;
+		mail.Host("smtp.gmail.com")
+		    .Port(465)
+		    .SSL();
+		mail
+			.Auth(user, pwd)
+			.To(to)
+		    .Subject("Test message")
+		    .Text("Hello world!")
+		    .Attach("somedata.txt", "This is a test of String attachement")
+		    .AttachFile(GetDataFile("SMTP.cpp"));
+		if(mail.Send())
+			Cout() << "OK\n";
+		else
+			Cout() << "Failed: " << mail.GetError() << '\n';
+	}
+}
