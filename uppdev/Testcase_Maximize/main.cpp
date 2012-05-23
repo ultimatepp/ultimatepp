efb51c61740d32b1fa041c156f683162325139f7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-05-23 13:26:14 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5000 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/Testcase_Maximize/main.cpp
+++ uppdev/Testcase_Maximize/main.cpp
@@ -2,42 +2,18 @@
 
 using namespace Upp;
 
-class MainWindow : public TopWindow
-{
-	public:
-		MainWindow();
-		void LeftUp(Point p, dword keyflags);
-		void Paint(Draw &w);
-		
-	protected:
-		bool	m_clicked;
-		Point	m_point;
-};
-
-MainWindow::MainWindow()
-: m_clicked(false), m_point(0, 0)
-{}
-
-void MainWindow::LeftUp(Point p, dword keyflags)
-{
-	m_clicked = true;
-	m_point = p;
-	Refresh();
-	PromptOK("!!!");
-}
-
-void MainWindow::Paint(Draw &w)
-{
-	w.DrawRect(GetWorkArea(), Color(255, 255, 255));
+struct MyApp : public TopWindow {
+	void Serialize(Stream& s) {
+		SerializePlacement(s);
+	}
 	
-	if(m_clicked)
-		w.DrawImage(m_point.x, m_point.y, CtrlImg::arrow);
-}
+	MyApp() { Sizeable().Zoomable(); }
+};
 
 GUI_APP_MAIN
 {
-	MainWindow mw;
-	mw.Title("Doubleclick Titlebar").Sizeable().MaximizeBox().MinimizeBox()
-		.SetRect(0, 0, 800, 600);
-	mw.Run();
+	MyApp app;
+	LoadFromFile(app);
+	app.Execute();
+	StoreToFile(app);
 }
