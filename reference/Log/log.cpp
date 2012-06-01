252dca7c2f3c68f07941a665c675279a94673ad0
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-06-01 11:59:44 +0000

    .reference: Log (LOGHEX)
    git-svn-id: svn://ultimatepp.org/upp/trunk@5028 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/Log/log.cpp
+++ reference/Log/log.cpp
@@ -20,12 +20,27 @@ CONSOLE_APP_MAIN
 		DUMP(i);
 	LOGEND();
 	DUMP(Foo(1));
+
 	Vector<String> x;
 	x.Add("One");
 	x.Add("Two");
 	x.Add("Three");
 	DUMPC(x);
+	
+	VectorMap<String, String> y;
+	y.Add("Key1", "Value1");
+	y.Add("Key2", "Value2");
+	DUMPC(y);
+	
 	static char h[200] = "alsjkdfhlaksjdhfklajshdfklj";
 	LOGHEXDUMP(h, 200);
-	RLOG("This will appear even in the release mode");
+	
+	String s = "Some text for hex dumps";
+	LOGHEX(s);
+	DUMPHEX(s);
+
+	RLOG("Prepending 'R' forces log even in the release mode");
+	
+	// You need to comment out following line in order to compile in release mode
+	DLOG("Prepending 'D' issues error when compiled in release mode. This is to avoid forgotten debug logs");
 }
