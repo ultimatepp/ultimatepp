d9e51975b00d3673e540ca7acd02e210f56518ab
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-06-19 20:43:47 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5060 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/Lang/main.cpp
+++ uppdev/Lang/main.cpp
@@ -2,11 +2,39 @@
 
 using namespace Upp;
 
-#define   TFILE <Lang/test.t>
-#include  <Core/t.h>
+//#define   TFILE <Lang/test.t>
+//#include  <Core/t.h>
+
+void ThreadTest(int lang)
+{
+	RLOG("In thread0: " << LNGAsText(lang) << ' ' << t_("Invalid date/time:"));
+	RLOG("In thread0: " << LNGAsText(lang) << ' ' << t_("Cancel"));
+	RLOG("In thread0 S: " << LNGAsText(lang) << ' ' << GetLngString("Cancel"));		
+	RLOG("In thread0: " << LNGAsText(lang) << ' ' << AsString(GetSysDate()));
+	SetLanguage(lang);
+	for(int i = 0; i < 10; i++) {
+		Sleep(100);
+		RLOG("In thread: " << LNGAsText(lang) << ' ' << t_("Invalid date/time:"));
+		RLOG("In thread: " << LNGAsText(lang) << ' ' << t_("Cancel"));
+		RLOG("In thread S: " << LNGAsText(lang) << ' ' << GetLngString("Cancel"));		
+		RLOG("In thread: " << LNGAsText(lang) << ' ' << AsString(GetSysDate()));
+	}
+	
+}
 
 GUI_APP_MAIN
 {
-	DUMP(LNGFromText(String()));
-	DUMP(LNGAsText(GetSystemLNG()));
+	RDUMP(LNGAsText(GetSystemLNG()));
+	RDUMP(t_("Sunday"));
+	Thread::Start(callback1(ThreadTest, LNG_('P','L','P','L')));
+	Thread::Start(callback1(ThreadTest, LNG_('F','R','F','R')));
+	Thread::Start(callback1(ThreadTest, LNG_ENGLISH));
+	SetLanguage(LNG_('C','S','C','Z'));
+	for(int i = 0; i < 10; i++) {
+		Sleep(100);
+		RLOG("Main: " << t_("Sunday"));
+		RLOG("Main: " << t_("Cancel"));
+		RLOG("Main S: " << GetLngString("Cancel"));		
+		RLOG("Main: " << AsString(GetSysDate()));
+	}
 }
