ad7f1c8c6918a9b6868396857d5cbb2f10d71d7e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-06-28 22:35:37 +0000

    reference: Demonstration of .ini helpers INI_BOOL, INI_STRING, INI_INT
    git-svn-id: svn://ultimatepp.org/upp/trunk@5085 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/INI/INI.cpp
@@ -0,0 +1,35 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+namespace Config {
+
+INI_BOOL(flag1, false, "This is bool parameter 1")
+INI_BOOL(flag2, true, "This is bool parameter 2")
+
+INI_STRING(text, "default text", "Text parameter");
+
+INI_INT(number, 123456, "Number parameter");
+
+};
+
+extern void ExternalFn();
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	
+	SetIniFile(GetDataFile("test.ini"));
+	
+	DDUMP(Config::flag1);
+	DDUMP(Config::flag2);
+	DDUMP(Config::text);
+	DDUMP(Config::number);
+	
+	LOG(GetIniInfoFormatted());
+
+	Config::number = 321;
+	DDUMP(Config::number);
+	
+	LOG(GetIniInfoFormatted());
+}
