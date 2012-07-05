55ac69869123326d277dc506a7dc676fef0253b8
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-07-05 19:15:36 +0000

    .tutorial: Skylark01
    git-svn-id: svn://ultimatepp.org/upp/trunk@5129 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- tutorial/Skylark01/Skylark01.cpp
+++ tutorial/Skylark01/Skylark01.cpp
@@ -2,29 +2,44 @@
 
 using namespace Upp;
 
-SKYLARK(HomePage, "**")
+SKYLARK(HomePage, "")
 {
 	http << "<html><body>Hello world!</body></html>";
 }
 
 SKYLARK(Param, "*/param")
 {
-	http << "<html><body>Param entered: " << http[0] << "</html></body>";
+	http << "<html><body>Parameter: " << http[0] << "</html></body>";
+}
+
+SKYLARK(Params, "params/**")
+{
+	http << "<html><body>Parameters: ";
+	for(int i = 0; i < http.GetParamCount(); i++)
+		http << http[i] << " ";
+	http << "</html></body>";
+}
+
+SKYLARK(CatchAll, "**")
+{
+	http.Redirect(HomePage);
 }
 
 struct MyApp : SkylarkApp {
 	MyApp() {
 		root = "myapp";
+	#ifdef _DEBUG
 		prefork = 0;
 		use_caching = false;
+	#endif
 	}
 };
 
 CONSOLE_APP_MAIN
 {
-#ifdef DEBUG
+#ifdef _DEBUG
 	StdLogSetup(LOG_FILE|LOG_COUT);
-	skylark_log = true;
+	Ini::skylark_log = true;
 #endif
 
 	MyApp().Run();	
