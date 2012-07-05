41a4c9686f285d278956b222a73d85c5db4f1354
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-07-05 18:38:44 +0000

    tutorial: Skylark01
    git-svn-id: svn://ultimatepp.org/upp/trunk@5127 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- tutorial/Skylark01/Skylark01.cpp
+++ tutorial/Skylark01/Skylark01.cpp
@@ -1,8 +1,31 @@
-#include <Core/Core.h>
+#include <Skylark/Skylark.h>
 
 using namespace Upp;
 
+SKYLARK(HomePage, "**")
+{
+	http << "<html><body>Hello world!</body></html>";
+}
+
+SKYLARK(Param, "*/param")
+{
+	http << "<html><body>Param entered: " << http[0] << "</html></body>";
+}
+
+struct MyApp : SkylarkApp {
+	MyApp() {
+		root = "myapp";
+		prefork = 0;
+		use_caching = false;
+	}
+};
+
 CONSOLE_APP_MAIN
 {
-	
+#ifdef DEBUG
+	StdLogSetup(LOG_FILE|LOG_COUT);
+	skylark_log = true;
+#endif
+
+	MyApp().Run();	
 }
