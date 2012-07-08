2f886953f658363919502a2da1b801d6e325b450
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-07-08 09:56:09 +0000

    examples: AddressBookWeb
    git-svn-id: svn://ultimatepp.org/upp/trunk@5147 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ examples/AddressBookWeb/AdrBook.h
@@ -0,0 +1,15 @@
+#ifndef _AdrBook_AdrBook_h_
+#define _AdrBook_AdrBook_h_
+
+#include <Skylark/Skylark.h>
+#include <plugin/sqlite3/Sqlite3.h>
+
+using namespace Upp;
+
+#define  MODEL <AddressBookWeb/Model.sch>
+
+#define  SCHEMADIALECT <plugin/sqlite3/Sqlite3Schema.h>
+#include <Sql/sch_header.h>
+
+
+#endif
