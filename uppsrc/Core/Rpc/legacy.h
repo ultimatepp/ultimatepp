a175d10e243209bfc838015b72d24cc218bc6978
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-07-11 06:25:44 +0000

    *Core/Rpc: Improved legacy
    git-svn-id: svn://ultimatepp.org/upp/trunk@5175 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Rpc/legacy.h
+++ uppsrc/Core/Rpc/legacy.h
@@ -1,8 +1,25 @@
+enum {
+	XMLRPC_SERVER_JSON_ERROR = RPC_SERVER_JSON_ERROR,
+	XMLRPC_SERVER_JSON_REQUEST_ERROR = RPC_SERVER_JSON_REQUEST_ERROR,
+	XMLRPC_UNKNOWN_METHOD_ERROR = RPC_UNKNOWN_METHOD_ERROR,
+	XMLRPC_SERVER_PARAM_ERROR = RPC_SERVER_PARAM_ERROR,
+	XMLRPC_SERVER_PROCESSING_ERROR = RPC_SERVER_PROCESSING_ERROR,
+
+	XMLRPC_SERVER_XML_ERROR = RPC_SERVER_XML_ERROR,
+
+	XMLRPC_CLIENT_HTTP_ERROR = RPC_CLIENT_HTTP_ERROR,
+	XMLRPC_CLIENT_XML_ERROR = RPC_CLIENT_XML_ERROR,
+	XMLRPC_CLIENT_JSON_ERROR = RPC_CLIENT_JSON_ERROR,
+	XMLRPC_CLIENT_RESPONSE_ERROR = RPC_CLIENT_RESPONSE_ERROR,
+};
+
 #define XMLRPC_METHOD(x)  RPC_METHOD(x)
 
 #define XMLRPC_GMETHOD(x, group) RPC_GMETHOD(x, group)
 
 typedef RpcError XmlRpcError;
+
+typedef RpcData XmlRpcData;
 
 inline
 void   SetXmlRpcMethodFilter(String (*filter)(const String& methodname))
@@ -31,13 +48,13 @@ bool   XmlRpcServerLoop(int port, const char *group = NULL)
 inline
 void   ThrowXmlRpcError(int code, const char *s)
 {
-	ThrowXmlRpcError(code, s);
+	ThrowRpcError(code, s);
 }
 
 inline
 void   ThrowXmlRpcError(const char *s)
 {
-	ThrowXmlRpcError(s);
+	ThrowRpcError(s);
 }
 
 inline
