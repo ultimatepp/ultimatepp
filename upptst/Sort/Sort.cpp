bcafaea7b311ed2d39fa834211b886ad156a8010
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-08-03 16:13:52 +0000

    upptst: Sort test
    git-svn-id: svn://ultimatepp.org/upp/trunk@5245 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/Sort/Sort.cpp
@@ -0,0 +1,48 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+Vector<String> GetStringData()
+{
+	Vector<String> data;
+	int n = Random(100) * Random(100) * Random(10);
+	for(int i = 0; i < n; i++)
+		data.Add(AsString(Random(n)));
+	return data;
+}
+
+Vector<int> GetIntData()
+{
+	Vector<int> data;
+	int n = Random(100) * Random(100) * Random(10);
+	for(int i = 0; i < n; i++)
+		data.Add(Random(n));
+	return data;
+}
+
+template <class C>
+void CheckSorted(C& data)
+{
+	for(int i = 0; i < data.GetCount() - 1; i++) {
+		if(!(data[i] <= data[i + 1])) {
+			RDUMPC(data);
+			RLOG("ERROR! " << i);				
+			Exit(1);
+		}
+	}
+}
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_FILE|LOG_COUT);
+	for(;;) {
+		Vector<int> data = GetIntData();
+		LOG("int " << data.GetCount());
+		Sort(data);
+		CheckSorted(data);
+		Vector<String> sdata = GetStringData();
+		LOG("String " << sdata.GetCount());
+		Sort(sdata);
+		CheckSorted(sdata);
+	}
+}
