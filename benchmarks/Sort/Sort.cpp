412bd4301a4e879f2aa10d535c9614f878e6a571
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-08-29 17:15:41 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@5299 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- benchmarks/Sort/Sort.cpp
+++ benchmarks/Sort/Sort.cpp
@@ -5,58 +5,86 @@
 
 using namespace Upp;
 
-#define BM_(k, x1, x, sort) { d1 <<= d; RTIMING(k " " #sort); sort(d1); }
+template <class I, class T>
+void Sort__(I begin, I end, const T&)
+{
+	Sort(begin, end, StdLess<T>());
+}
 
-#define BV(k, sort) BM_(k, d1, d, sort)
-#define BA(k, sort) BM_(k " Array", a1, a, sort)
+template <class I>
+void Sort(I begin, I end)
+{
+	Sort__(begin, end, *begin);
+}
+
+#define BM_(k, x1, x, sort) { d1 <<= d; RTIMING(k); sort(d1); }
+
+#define BV(k, sort) BM_(k " "  #sort " Vector<String>", d1, d, sort)
+#define BA(k, sort) BM_(k " "  #sort " Array<String>", a1, a, sort)
 #define Bv(k, sort) { s1 = s; RTIMING(k " " #sort " vector<string>"); sort(s1.begin(), s1.end()); }
-#define BS(k, sort) { d1 <<= d; RTIMING(k " " #sort " String"); sort(d1.Begin(), d1.End()); }
+#define BS(k, sort) { d1 <<= d; RTIMING(k " " #sort " Vector<String>"); sort(d1.Begin(), d1.End()); }
+
+#define NN 10
 
 #define Bm(k)\
+for(int i = 0; i < NN; i++) { \
+	Bv(k, std::sort)\
+	Bv(k, Sort)\
+	BS(k, std::sort)\
+	BV(k, Sort)\
+}
+
+/*
+BV(k, GetStableSortOrderCmp)\
+BV(k, StableSortCmp)\
 BV(k, Sort)\
 BV(k, GetSortOrder)\
 BV(k, StableSort)\
 BV(k, GetStableSortOrder)\
-BV(k, StableSortCmp)\
-BV(k, GetStableSortOrderCmp)\
-BS(k, std::sort)\
-BS(k, std::stable_sort)\
-Bv(k, std::sort)\
-Bv(k, std::stable_sort)\
 BA(k, Sort)\
 BA(k, StableSort)\
 BA(k, StableSortCmp)\
+Bv(k, std::sort)\
+Bv(k, std::stable_sort)\
+Bv(k, Sort)\
+BS(k, std::stable_sort)\
+*/
+
+#define N 20
 
 CONSOLE_APP_MAIN
 {
-	for(int q = 0; q < 100; q++) {
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	for(int q = 1; q < N; q++) {
 		Vector<String> d, d1;
 		Array<String> a, a1;
 		std::vector<std::string> s, s1;
-		for(int i = 0; i < 10000; i++) {
+		for(int i = 0; i < 2000 * q; i++) {
 			String x = AsString(rand());
 			d.Add(x);
 			a.Add(x);
 			s.push_back(to_string(x));
 		}
+		RLOG("S" << q);
 		Bm("S");
 	}
 
-	for(int q = 0; q < 100; q++) {
+	for(int q = 1; q < N; q++) {
 		Vector<String> d, d1;
 		std::vector<std::string> s, s1;
-		for(int i = 0; i < 10000; i++) {
+		for(int i = 0; i < 2000 * q; i++) {
 			String x = AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand());
 			d.Add(x);
 			s.push_back(to_string(x));
 		}
+		RLOG("M" << q);
 		Bm("M");
 	}
 	
-	for(int q = 0; q < 100; q++) {
+	for(int q = 1; q < N; q++) {
 		Vector<String> d, d1;
 		std::vector<std::string> s, s1;
-		for(int i = 0; i < 10000; i++) {
+		for(int i = 0; i < 2000 * q; i++) {
 			String x = AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand())
 			         + AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand())
 			         + AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand()) + AsString(rand())
@@ -64,6 +92,7 @@ CONSOLE_APP_MAIN
 			d.Add(x);
 			s.push_back(to_string(x));
 		}
+		RLOG("L" << q);
 		Bm("L");
 	}
 }
