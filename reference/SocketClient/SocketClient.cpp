01a29612408fdcd6ff94c298cac00c8df7bf2b9b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-09-22 08:12:58 +0000

    reference: SocketClient and SocketServer changed to use Core:TcpSocket
    git-svn-id: svn://ultimatepp.org/upp/trunk@5382 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/SocketClient/SocketClient.cpp
+++ reference/SocketClient/SocketClient.cpp
@@ -1,18 +1,20 @@
-#include <Web/Web.h>
+#include <Core/Core.h>
 
 using namespace Upp;
 
 String Request(const String& r)
 {
-	Socket s;
-	if(!ClientSocket(s, CommandLine().GetCount() ? CommandLine()[0] : "127.0.0.1", 3214)) {
+	TcpSocket s;
+	if(!s.Connect(CommandLine().GetCount() ? CommandLine()[0] : "127.0.0.1", 3214)) {
 		Cout() << "Unable to connect to server!\n";
 		SetExitCode(1);
 		return Null;
 	}
-	s.Write(r + '\n');
-	return s.ReadUntil('\n');
+	s.Put(r + '\n');
+	return s.GetLine();
 }
+
+// Start reference/SocketServer before starting this program
 
 CONSOLE_APP_MAIN
 {
