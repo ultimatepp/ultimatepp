cc0cd38f12f4fe310636347c4ffe5c435f9f449f
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-10-31 19:04:23 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5502 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/PDF/main.cpp
+++ uppdev/PDF/main.cpp
@@ -10,10 +10,12 @@ CONSOLE_APP_MAIN
 {
 	PdfDraw pdf;
 	pdf.DrawRect(400, 400, 500, 500, Blue);
-	pdf.DrawText(100, 100, "Ahoj", Roman(100), Black);
-	pdf.DrawText(100, 200, "Ahoj", Arial(100), Black);
 	pdf.DrawImage(100, 500, 200, 200, AppImg::test());
 	pdf.DrawImage(300, 500, 200, 200, AppImg::testbw());
 	pdf.DrawImage(500, 500, 200, 200, AppImg::testg());
+	pdf.DrawText(100, 100, "Ahoj", Roman(100), Black);
+	pdf.DrawText(100, 200, "Ahoj", Arial(100), Black);
+	pdf.DrawText(300, 100, "Ahoj", Roman(150).Italic(), Black);
+	pdf.DrawText(300, 200, "Ahoj", Arial(150).Italic(), Black);
 	SaveFile(GetHomeDirFile("pdf.pdf"), pdf.Finish());
 }
