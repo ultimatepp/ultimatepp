80aa1c9d8e9ee1d0c1100653450bba0aa8c1c51a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-11-04 08:05:30 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5511 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/TreeRefresh/main.cpp
+++ uppdev/TreeRefresh/main.cpp
@@ -2,11 +2,22 @@
 
 using namespace Upp;
 
+struct MyDisplay : public Display {
+  virtual void MyDisplay::Paint(Draw& w,const Rect& r,
+                                const Value& q, Color ink,
+                                Color paper,dword style) const
+  {
+    w.DrawRect(r, paper);
+    w.DrawText(r.left, r.top, (String)q, StdFont(), Blue()); // <- the color I want goes in here
+  }
+ };
+
 GUI_APP_MAIN
 {
 //	Ctrl::ShowRepaint(100);
 	TopWindow win;
 	TreeCtrl tree;
+	tree.SetDisplay(Single<MyDisplay>());
 	tree.SetRoot(Null, "Tree");
 	Array<EditString> s;
 	tree.BackPaint();
