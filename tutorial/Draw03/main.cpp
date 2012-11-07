ca62524c26050032b529080616083c8811319103
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-11-07 19:25:04 +0000

    .tutorial
    git-svn-id: svn://ultimatepp.org/upp/trunk@5529 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- tutorial/Draw03/main.cpp
+++ tutorial/Draw03/main.cpp
@@ -10,14 +10,13 @@ struct MyApp : TopWindow {
 		w.Offset(50, 50);
 		const char *text = "Programming is fun";
 		Font fnt(~fontlist, 60);
-		FontInfo fi = fnt.Info();
 		int x = 0;
 		Vector<int> dx;
 		for(const char *s = text; *s; s++) {
-			int width = fi[*s];
-			w.DrawRect(x, 0, width - 1, fi.GetAscent(), Color(255, 255, 200));
-			w.DrawRect(x, fi.GetAscent(), width - 1, fi.GetDescent(), Color(255, 200, 255));
-			w.DrawRect(x + width - 1, 0, 1, fi.GetHeight(), Black());
+			int width = fnt[*s];
+			w.DrawRect(x, 0, width - 1, fnt.GetAscent(), Color(255, 255, 200));
+			w.DrawRect(x, fnt.GetAscent(), width - 1, fnt.GetDescent(), Color(255, 200, 255));
+			w.DrawRect(x + width - 1, 0, 1, fnt.GetHeight(), Black());
 			dx.Add(width + 4);
 			x += width;
 		}
