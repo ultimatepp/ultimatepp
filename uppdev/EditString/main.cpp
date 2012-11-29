5a7f8e5e8020bd7831f6eecc7506f04d752b4c87
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-11-29 19:47:00 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5595 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/EditString/main.cpp
+++ uppdev/EditString/main.cpp
@@ -6,7 +6,16 @@ GUI_APP_MAIN
 {
 	EditString es;
 	TopWindow win;
+<<<<<<< .mine
+	String h;
+	h << "CCCCCCCC";
+	es <<= h;
+=======
 	es <<= "CCCCCCCCCCCCCCC";
+>>>>>>> .r5593
 	win.Add(es.HSizePos().TopPos(0, Ctrl::STDSIZE));
 	win.Run();
+	
+	DDUMP(StoreAsXML(es));
+	DDUMP(StoreAsJson(es, true));
 }
--- uppdev/PDF/main.cpp
+++ uppdev/PDF/main.cpp
@@ -9,23 +9,28 @@ using namespace Upp;
 CONSOLE_APP_MAIN
 {
 	PdfDraw pdf;
-	pdf.DrawRect(400, 400, 500, 500, Blue);
-	pdf.DrawImage(100, 500, 200, 200, AppImg::test());
-	pdf.DrawImage(300, 500, 200, 200, AppImg::testbw());
-	pdf.DrawImage(500, 500, 200, 200, AppImg::testg());
-	pdf.DrawText(100, 100, "Ahoj", Roman(100), Black);
-	pdf.DrawText(100, 200, "Ahoj", Arial(100), Black);
-	pdf.DrawText(300, 100, "Ahoj", Roman(150).Italic(), Black);
-	pdf.DrawText(300, 200, "Ahoj", Arial(150).Italic(), Black);
-	
-	int cy = 1000;
-	for(int bold = 0; bold < 2; bold++)
-		for(int italic = 0; italic < 2; italic++) {
-			pdf.DrawText(500, 1000, "Ahoj", Roman(100).Bold(), Black);
-			pdf.DrawText(500, 1200, "Ahoj", Arial(100).Bold(), Black);
-			pdf.DrawText(500, 1300, "Ahoj", Courier(100).Bold(), Black);
-		}
-
-
+#if 0
+	pdf.DrawText(100, 100, "Ahoj", Courier(100).Bold(), Black);
+#else
+	int cy = 0;
+	int height = 50;
+	for(int face = Font::SERIF; face <= Font::MONOSPACE; face++)
+		for(int underline = 0; underline < 2; underline++)
+			for(int italic = 0; italic < 2; italic++)
+				for(int bold = 0; bold < 2; bold++) {
+					String h;
+					if(bold)
+						h << " bold";
+					if(italic)
+						h << " italic";
+					if(underline)
+						h << " underline";
+					Font fnt(face, height);
+					fnt.Bold(bold).Italic(italic).Underline(underline);
+					pdf.DrawText(100, cy, fnt.GetFaceName() + h, fnt, Black);
+					pdf.DrawText(2000, cy, 200, fnt.GetFaceName() + h, fnt, Black);
+					cy += height;
+				}
+#endif
 	SaveFile(GetHomeDirFile("pdf.pdf"), pdf.Finish());
 }
