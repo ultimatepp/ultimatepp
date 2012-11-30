eb07a09155d08e5f7701441242a0f15bcdd844cb
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-11-30 17:37:08 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5603 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/PDF/main.cpp
+++ uppdev/PDF/main.cpp
@@ -14,7 +14,7 @@ CONSOLE_APP_MAIN
 #else
 	int cy = 0;
 	int height = 50;
-	for(int face = Font::SERIF; face <= Font::MONOSPACE; face++)
+	for(int face = Font::SERIF; face < min(10, Font::GetFaceCount()); face++)
 		for(int underline = 0; underline < 2; underline++)
 			for(int italic = 0; italic < 2; italic++)
 				for(int bold = 0; bold < 2; bold++) {
@@ -27,9 +27,15 @@ CONSOLE_APP_MAIN
 						h << " underline";
 					Font fnt(face, height);
 					fnt.Bold(bold).Italic(italic).Underline(underline);
+					Cout() << fnt << "\n";
 					pdf.DrawText(100, cy, fnt.GetFaceName() + h, fnt, Black);
-					pdf.DrawText(2000, cy, 200, fnt.GetFaceName() + h, fnt, Black);
+				//	pdf.DrawText(2000, cy, 200, fnt.GetFaceName() + h, fnt, Black);
 					cy += height;
+					if(cy > 6000) {
+						pdf.EndPage();
+						pdf.StartPage();
+						cy = 0;
+					}
 				}
 #endif
 	SaveFile(GetHomeDirFile("pdf.pdf"), pdf.Finish());
