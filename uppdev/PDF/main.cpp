4d2c1b3bd708ba8f4d1f267c01d84561c2fa2452
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-12-01 10:24:52 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5606 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/PDF/main.cpp
+++ uppdev/PDF/main.cpp
@@ -13,8 +13,8 @@ CONSOLE_APP_MAIN
 	pdf.DrawText(100, 100, "Ahoj", Courier(100).Bold(), Black);
 #else
 	int cy = 0;
-	int height = 50;
-	for(int face = Font::SERIF; face < min(10, Font::GetFaceCount()); face++)
+//	for(int face = Font::SERIF; face < min(99999, Font::GetFaceCount()); face++)
+	int face = 6;
 		for(int underline = 0; underline < 2; underline++)
 			for(int italic = 0; italic < 2; italic++)
 				for(int bold = 0; bold < 2; bold++) {
@@ -25,16 +25,19 @@ CONSOLE_APP_MAIN
 						h << " italic";
 					if(underline)
 						h << " underline";
-					Font fnt(face, height);
+					Font fnt(face, 50);
 					fnt.Bold(bold).Italic(italic).Underline(underline);
 					Cout() << fnt << "\n";
-					pdf.DrawText(100, cy, fnt.GetFaceName() + h, fnt, Black);
-				//	pdf.DrawText(2000, cy, 200, fnt.GetFaceName() + h, fnt, Black);
-					cy += height;
-					if(cy > 6000) {
-						pdf.EndPage();
-						pdf.StartPage();
-						cy = 0;
+					LOG(face << ' ' << fnt << ", TTF: " << fnt.IsTrueType());
+					if(1 || fnt.IsTrueType()) {
+						pdf.DrawText(100, cy, fnt.GetFaceName() + h, fnt, Black);
+					//	pdf.DrawText(2000, cy, 200, fnt.GetFaceName() + h, fnt, Black);
+						cy += fnt.GetLineHeight();
+						if(cy > 6000) {
+							pdf.EndPage();
+							pdf.StartPage();
+							cy = 0;
+						}
 					}
 				}
 #endif
