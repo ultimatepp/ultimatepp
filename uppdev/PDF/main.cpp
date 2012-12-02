9fb98817a0e08f885fd5a35ad35ebee2ce2b53a5
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-12-02 10:39:27 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5610 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/PDF/main.cpp
+++ uppdev/PDF/main.cpp
@@ -13,33 +13,27 @@ CONSOLE_APP_MAIN
 	pdf.DrawText(100, 100, "Ahoj", Courier(100).Bold(), Black);
 #else
 	int cy = 0;
-//	for(int face = Font::SERIF; face < min(99999, Font::GetFaceCount()); face++)
-	int face = 6;
-		for(int underline = 0; underline < 2; underline++)
-			for(int italic = 0; italic < 2; italic++)
-				for(int bold = 0; bold < 2; bold++) {
-					String h;
-					if(bold)
-						h << " bold";
-					if(italic)
-						h << " italic";
-					if(underline)
-						h << " underline";
-					Font fnt(face, 50);
-					fnt.Bold(bold).Italic(italic).Underline(underline);
-					Cout() << fnt << "\n";
-					LOG(face << ' ' << fnt << ", TTF: " << fnt.IsTrueType());
-					if(1 || fnt.IsTrueType()) {
-						pdf.DrawText(100, cy, fnt.GetFaceName() + h, fnt, Black);
-					//	pdf.DrawText(2000, cy, 200, fnt.GetFaceName() + h, fnt, Black);
-						cy += fnt.GetLineHeight();
-						if(cy > 6000) {
-							pdf.EndPage();
-							pdf.StartPage();
-							cy = 0;
+	for(int face = Font::SERIF; face < min(9, Font::GetFaceCount()); face++)
+//	int face = 6;
+		for(int strikeout = 0; strikeout < 2; strikeout++)
+			for(int underline = 0; underline < 2; underline++)
+				for(int italic = 0; italic < 2; italic++)
+					for(int bold = 0; bold < 2; bold++) {
+						Font fnt(face, 100);
+						fnt.Bold(bold).Italic(italic).Underline(underline).Strikeout(strikeout);
+						Cout() << fnt << "\n";
+						LOG(face << ' ' << fnt << ", TTF: " << fnt.IsTrueType());
+						if(1 || fnt.IsTrueType()) {
+							pdf.DrawText(100, cy, AsString(fnt), fnt, Black);
+							pdf.DrawText(2000, cy, 200, AsString(fnt), fnt, Black);
+							cy += fnt.GetLineHeight();
+							if(cy > 6000) {
+								pdf.EndPage();
+								pdf.StartPage();
+								cy = 0;
+							}
 						}
 					}
-				}
 #endif
 	SaveFile(GetHomeDirFile("pdf.pdf"), pdf.Finish());
 }
