96bd6ba9c419814ad82ef36e62947b5d5fb15cb9
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-12-06 20:06:23 +0000

    .developing gtk backend
    git-svn-id: svn://ultimatepp.org/upp/trunk@5629 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- rainbow/Gtk/Ctrl.h
+++ rainbow/Gtk/Ctrl.h
@@ -2,6 +2,7 @@
 	void Create(bool popup);
 	static gboolean GtkProc(GtkWidget *widget, GdkEvent *event, gpointer user_data);
 	bool Proc(GdkEvent *event);
+	void GtkMouseEvent(int action, GdkEvent *event);
 	
 	GdkWindow *win() { return top->window->window; }
 	
