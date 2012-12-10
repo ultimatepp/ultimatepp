fd9fba267567a2f1cfad37977393c276f32f330a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2012-12-10 19:14:19 +0000

    .removed DDUMP
    git-svn-id: svn://ultimatepp.org/upp/trunk@5639 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlLib/RichTextView.cpp
+++ uppsrc/CtrlLib/RichTextView.cpp
@@ -12,7 +12,6 @@ Rect  RichTextView::GetPage() const
 void  RichTextView::Paint(Draw& w)
 {
 	Size sz = GetSize();
-	DDUMP(background);
 	w.DrawRect(sz, background);
 	sz.cx -= margin.left + margin.right;
 	sz.cy -= margin.top + margin.bottom;
