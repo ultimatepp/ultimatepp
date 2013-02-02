b70af542d0f94c9847fa7156c7bc3ec20630f451
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-02-02 16:05:13 +0000

    *SDL: Fixed .h
    git-svn-id: svn://ultimatepp.org/upp/trunk@5755 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SDL/SDLWrapper.h
+++ bazaar/SDL/SDLWrapper.h
@@ -1,15 +1,17 @@
 #ifndef _SDL_SDLWrapper_h_
 #define _SDL_SDLWrapper_h_
 
-#if defined(PLATFORM_WIN32)
+#if defined(_WIN32)
 	#include <SDL.h> 
 	#include <SDL_thread.h> 
 	#include <SDL_syswm.h>
 #else
-	#include <SDL/SDL.h> 
-	#include <SDL/SDL_thread.h> 
+	#include <SDL/SDL.h>
+	#include <SDL/SDL_thread.h>
 	#include <SDL/SDL_syswm.h>
 #endif
+
+#undef main
 
 #define INT64_C(val) val##LL
 #define UINT64_C(val) val##ULL 
