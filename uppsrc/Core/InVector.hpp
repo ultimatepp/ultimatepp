0fde48a364aaea656d5ee6bc52baf5dc523848cf
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-02-11 10:49:47 +0000

    Core: SortedIndex
    git-svn-id: svn://ultimatepp.org/upp/trunk@5791 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/InVector.hpp
+++ uppsrc/Core/InVector.hpp
@@ -720,3 +720,40 @@ InArray<T>::InArray(const InArray& v, int)
 	while(n--)
 		*it++ = new T(*s++);
 }
+
+template <class T, class Less>
+int SortedIndex<T, Less>::FindAdd(const T& key)
+{
+	int i = FindLowerBound(key);
+	if(!less(key, iv[i]))
+		return i;
+	return Add(key);
+}
+
+template <class T, class Less>
+int SortedIndex<T, Less>::FindNext(int i) const
+{
+	return i + 1 < iv.GetCount() && !less(iv[i], iv[i + 1]) ? i + 1 : -1;
+}
+
+template <class T, class Less>
+int SortedIndex<T, Less>::FindLast(const T& x) const
+{
+	int i = iv.FindUpperBound(x, less);
+	return i > 0 && !less(iv[i - 1], x) ? i - 1 : -1;
+}
+
+template <class T, class Less>
+int SortedIndex<T, Less>::FindPrev(int i) const
+{
+	return i > 0 && !less(iv[i - 1], iv[i]) ? i - 1 : -1;
+}
+
+template <class T, class Less>
+int SortedIndex<T, Less>::RemoveKey(const T& x)
+{
+	int l = FindLowerBound(x);
+	int count = FindUpperBound(x) - l;
+	Remove(l, count);
+	return count;
+}
