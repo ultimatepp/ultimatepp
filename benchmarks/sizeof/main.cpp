0b470667d805ad5d6fa2fa10e7c269e74294cbcb
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-02-16 10:16:12 +0000

    .benchmarks: sizeof
    git-svn-id: svn://ultimatepp.org/upp/trunk@5809 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- benchmarks/sizeof/main.cpp
+++ benchmarks/sizeof/main.cpp
@@ -20,16 +20,6 @@ GUI_APP_MAIN
 	RDUMP(sizeof(float));
 	RDUMP(sizeof(double));
 	RDUMP(sizeof(long double));
-	RDUMP(sizeof(Vector<int>));
-	RDUMP(sizeof(Array<int>));
-	RDUMP(sizeof(Index<int>));
-	RDUMP(sizeof(ArrayIndex<int>));
-	RDUMP(sizeof(ArrayMap<int, int>));
-	RDUMP(sizeof(VectorMap<int, int>));
-	RDUMP(sizeof(BiArray<int>));
-	RDUMP(sizeof(BiVector<int>));
-	RDUMP(sizeof(String));
-	RDUMP(sizeof(WString));
 	RDUMP(sizeof(Date));
 	RDUMP(sizeof(Time));
 	RDUMP(sizeof(Size));
@@ -47,4 +37,17 @@ GUI_APP_MAIN
 	RDUMP(sizeof(AttrText));
 	RDUMP(sizeof(Painting));
 	RDUMP(sizeof(Drawing));
+	RDUMP(sizeof(Vector<int>));
+	RDUMP(sizeof(Array<int>));
+	RDUMP(sizeof(Index<int>));
+	RDUMP(sizeof(ArrayIndex<int>));
+	RDUMP(sizeof(ArrayMap<int, int>));
+	RDUMP(sizeof(VectorMap<int, int>));
+	RDUMP(sizeof(BiArray<int>));
+	RDUMP(sizeof(BiVector<int>));
+	RDUMP(sizeof(InVector<int>));
+	RDUMP(sizeof(InArray<int>));
+	RDUMP(sizeof(SortedIndex<int>));
+	RDUMP(sizeof(String));
+	RDUMP(sizeof(WString));
 }
