8610a0edb0a2fb16a999193127d0929d9103739b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-02-19 16:04:09 +0000

    plugin\RegExp: Replace methods (thanks Navi)
    git-svn-id: svn://ultimatepp.org/upp/trunk@5836 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/plugin/pcre/RegExp.cpp
+++ uppsrc/plugin/pcre/RegExp.cpp
@@ -162,4 +162,219 @@ void RegExp::GetMatchPos(int i, int& iPosStart, int& iPosAfterEnd)
 	iPosAfterEnd = pos[i + 1];
 }
 
+bool RegExp::ReplacePos(String& t, int p, int q, const String& r)
+{
+	if(p>=0 && q<=t.GetLength() && q>p){		
+		
+		t.Remove(p, q-p);
+		if(!r.IsEmpty()) t.Insert(p, r);	
+		
+		return true;
+	}
+	
+	return false;					
+}
+
+Vector<String> RegExp::Make_rv(const String& r)
+{	
+	RegExp reg("\\((.*?)\\)");
+	
+	Vector<String> rv;
+	
+	while(reg.GlobalMatch(r)){
+		rv.Add(reg.GetString(0));	
+	}
+	
+	return rv;
+}
+
+Vector<String> RegExp::ResolveBackRef(const Vector<String>& rv)
+{	
+	Vector<String> new_rv;
+	
+	int rv_count = rv.GetCount();
+	int match_count = GetCount();
+	
+	RegExp reg("\\\\(\\d+)");
+	
+	String log;
+		
+	if(match_count>0 && rv_count>0) {
+		
+		// deep copy rv vector.
+		new_rv<<=rv;
+		
+		// loop throug number of replace string
+		// run reg regexp on each to find if they
+		// have any "match referance" ie /1 /2 /3 etc
+		for(int i=0; i<rv_count; i++){
+						
+			while(reg.GlobalMatch(new_rv[i])){				
+				
+				// for each matched "match ref"
+				for(int j=0, jMx=reg.GetCount(); j< jMx; j++){
+					
+					String p;
+					
+					// convert the ref to number
+					int m=StrIntValue(reg.GetString(j));
+					
+					// make "find" string litaral					
+					p<<"\\"<<m;	
+
+					// reduce 1 from back ref to be used as index.
+					m--;		
+					
+					// doing actual replacement.
+					if(m<match_count) new_rv[i].Replace(p, GetString(m));																
+				}									
+			}						
+		}
+	}
+	
+	return new_rv;		
+}
+
+
+
+int RegExp::Replace0(String& t, const Vector<String>& rv, const int& rv_count, int& offset)
+{
+	
+	int x=0, y=0;
+	int count=GetCount();
+
+		
+	for(int i=0; i<count; i++){
+				
+		GetMatchPos(i, x, y);
+		
+		if(i<rv_count) {
+			
+			ReplacePos(t, x+offset, y+offset, rv[i]);
+							
+			offset = offset + rv[i].GetLength() - (y-x);
+			
+		}
+		else{
+	
+			ReplacePos(t, x+offset, y+offset, "");	
+			
+			offset = offset - (y-x);				 
+		}										
+	}
+		
+	return count;		
+}
+
+int RegExp::Replace(String& t, const Vector<String>& rv, bool backref)
+{
+	String t_copy(t);
+	int count=0;
+	int pos_offset=0;
+	int rv_count=rv.GetCount();	
+	Vector<String> new_rv; 
+	
+	new_rv.Shrink();
+	first=false;
+	
+	Match(t_copy);
+	
+	if(backref){
+		
+		 new_rv.Clear();
+		 new_rv = ResolveBackRef(rv);	
+		 count  = Replace0(t, new_rv, rv_count, pos_offset);				
+		 
+	}
+	else count = Replace0(t, rv, rv_count, pos_offset);	
+
+	return count;
+}
+
+int RegExp::Replace(String& t, const String& r, bool backref)
+{
+	Vector<String> rv(Make_rv(r)); 
+	return Replace(t, rv, backref);
+}
+
+
+int RegExp::Replace(String& t, Callback1<Vector<String>&> cbr)
+{
+	String t_copy(t);
+	int count=0;
+	int pos_offset=0;
+	int rv_count=0;
+	Vector<String> rv; 
+
+	first=false;
+	
+	Match(t_copy);
+	
+	rv=GetStrings();	
+	
+	cbr(rv);	
+	
+	rv_count=rv.GetCount();
+	
+	Replace0(t, rv, rv_count, pos_offset);	
+
+	return count;	
+	
+}
+
+int RegExp::ReplaceGlobal(String& t, const Vector<String>& rv, bool backref)
+{
+	String t_copy(t);
+	int count=0;
+	int pos_offset=0;
+	int rv_count=rv.GetCount();	
+	Vector<String> new_rv; 
+	
+	new_rv.Shrink();
+	first=false;
+	
+	while(GlobalMatch(t_copy)){
+		
+			if(backref){
+				
+				 new_rv.Clear();
+				 new_rv = ResolveBackRef(rv);	
+				 count += Replace0(t, new_rv, rv_count, pos_offset);				
+				 
+			}
+			else count += Replace0(t, rv, rv_count, pos_offset);	
+			
+	}
+	
+	return count;
+	
+}
+
+int RegExp::ReplaceGlobal(String& t, const String& r, bool backref)
+{
+	Vector<String> rv(Make_rv(r)); 
+	return ReplaceGlobal(t, rv, backref);
+}
+
+int RegExp::ReplaceGlobal(String& t, Callback1<Vector<String>&> cbr)
+{
+	String t_copy(t);
+	int count=0;
+	int pos_offset=0;
+	int rv_count=0;
+	Vector<String> rv; 
+	
+	first = false;
+	while(GlobalMatch(t_copy)) {
+		rv.Clear();
+		rv=GetStrings();
+		cbr(rv);
+		rv_count=rv.GetCount();
+		count += Replace0(t, rv, rv_count, pos_offset);	
+	}
+	
+	return count;
+	
+}
+
 END_UPP_NAMESPACE
