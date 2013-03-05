4950bb6115edf1127f162c7105f68310ecd3dcf7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-03-05 19:19:18 +0000

    .upptst: FileSel
    git-svn-id: svn://ultimatepp.org/upp/trunk@5884 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/FileSel/main.cpp
@@ -0,0 +1,35 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+GUI_APP_MAIN
+{
+	FileSelector sel;
+	sel.AllFilesType();
+	sel.Type("Test2", "*.tst");
+	sel.ActiveType(1);
+	sel.Multi();
+	if(!sel.ExecuteSaveAs("Test!"))
+		return;
+	for(int i = 0; i < sel.GetCount(); i++)
+		DDUMP(sel[i]);
+	sel = "/home/cxl/Temp/Gui2/main.cpp";
+	if(!sel.ExecuteSaveAs("as"))
+		return;
+	DDUMP(~sel);
+	sel.NoAsking();
+	if(!sel.ExecuteSaveAs("as"))
+		return;
+	return;
+/*
+	DocEdit a;
+	TopWindow w1, w2;
+	w1.Add(a.SizePos());
+	w1.OpenMain();
+	Button b;
+	b <<= callback(Sel);
+	w2.Add(b.SizePos());
+	w2.Open(&w1);
+	w2.Run();
+*/
+}
