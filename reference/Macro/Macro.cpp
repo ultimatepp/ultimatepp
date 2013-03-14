a69dc9f6f4a242a6a7899d3e4dbe85989d5988b7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-03-14 19:47:46 +0000

    reference: Macro #455
    git-svn-id: svn://ultimatepp.org/upp/trunk@5912 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/Macro/Macro.cpp
@@ -0,0 +1,11 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	Cout() <<
+	"This application does nothing. It's only purpose is to demonstrate\n"
+	"the capabilities of the macro language embeded in TheIDE.\n"
+	"Try pressing Ctrl+Alt+S in TheIDE...\n";
+}
