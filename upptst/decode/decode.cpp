14ce94c8636b75bb79b85ae6b6b98f2e4f818b26
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-03-23 10:07:01 +0000

    .upptst: decode
    git-svn-id: svn://ultimatepp.org/upp/trunk@5919 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/decode/decode.cpp
@@ -0,0 +1,19 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	ASSERT(decode(0, 1, "one", 2, "two", 3, "three", "unknown") == String("unknown"));
+	ASSERT(decode(1, 1, "one", 2, "two", 3, "three", "unknown") == String("one"));
+	ASSERT(decode(2, 1, "one", 2, "two", 3, "three", "unknown") == String("two"));
+	ASSERT(decode(3, 1, "one", 2, "two", 3, "three", "unknown") == String("three"));
+	ASSERT(decode(3, 1, "one", 2, "two", 3.0, "three", "unknown") == String("three"));
+	ASSERT(decode(4, 1, "one", 2, "two", 3, "three", "unknown") == String("unknown"));
+
+	String s = "3";
+	ASSERT(findarg(s, "0", "1", "2", "3") == 3);
+	
+	ASSERT(decode(s, "3", 3, -1) == 3);
+	ASSERT(decode(s, "4", 3, -1) == -1);
+}
