a9353d8b6942297918bf43e755f2a17328c4208c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-04-07 15:33:22 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@5967 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/TestDroplist/main.cpp
+++ uppdev/TestDroplist/main.cpp
@@ -8,10 +8,14 @@ GUI_APP_MAIN
 	for(int i = 0; i < 100; i++)
 		dl.Add(i);
 	dl <<= 20;
-	dl.SetReadOnly();
+//	dl.SetReadOnly();
+//	dl.DropWidth(120);
 	TopWindow w;
+	StaticRect sr;
+	sr.Color(LtCyan);
+	w.Add(sr.SizePos());
 	w.Add(dl.LeftPos(10, 150).TopPos(0, 20));
-	dl.DropWidth(120);
 	dl.Tip("Tip!");
+//	dl.NoBackground();
 	w.Run();
 }
