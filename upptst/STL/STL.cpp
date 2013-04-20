0b74164b14b7659f35aff852ddcbbb84d76ba7c5
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-04-20 16:58:35 +0000

    .upptst: STL compatiblity check
    git-svn-id: svn://ultimatepp.org/upp/trunk@5993 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/STL/STL.cpp
@@ -0,0 +1,34 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+template <class T>
+void Check()
+{
+	T a;
+	std::includes(a.begin(), a.end(), a.begin(), a.end());
+	
+	const T& b = a;
+	std::includes(b.begin(), b.end(), b.begin(), b.end());
+}
+
+CONSOLE_APP_MAIN
+{
+	Check< Vector<int> > ();
+	Check< Array<int> > ();
+
+	Check< BiVector<int> > ();
+	Check< BiArray<int> > ();
+
+	Check< Index<int> > ();
+	Check< ArrayIndex<int> > ();
+	Check< VectorMap<int, int> > ();
+	Check< ArrayMap<int, int> > ();
+
+	Check< InVector<int> > ();
+	Check< InArray<int> > ();
+
+	Check< SortedIndex<int> > ();
+	Check< SortedVectorMap<int, int> > ();
+	Check< SortedArrayMap<int, int> > ();
+}
