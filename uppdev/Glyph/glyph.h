5c9ae2ae1932178488cc0716acb78fd1a3749edd
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-05-13 07:11:53 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@6060 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/Glyph/glyph.h
+++ uppdev/Glyph/glyph.h
@@ -6,54 +6,6 @@
 
 using namespace Upp;
 
-class MiniRenderer {
-	struct Segment : Moveable<Segment> {
-		int  l;
-		int  h;
-		bool flag;
-		
-		bool operator<(const Segment& b) const { return l < b.l; }
-	};
-	
-	struct Segments;
-	
-	int           cy;
-	int           diry;
-	int           dirx;
-	One<Segments> pseg;
-	Point         p0, p1;
-	int           width;
-
-	void AHorz(int x, int y, int cx);
-	void AVert(int x, int y, int cy);
-	void DoLine(Point p1, Point p2, bool last);
-
-public:
-	void FatLine(Point p1, Point p2, int n);
-
-	virtual void PutHorz(int x, int y, int cx) = 0;
-	virtual void PutVert(int x, int y, int cy) = 0;
-	
-	int  GetDirx() const                      { return dirx; }
-	int  GetDiry() const                      { return diry; }
-	
-	MiniRenderer& Move(Point p);
-	MiniRenderer& Line(Point p);
-	MiniRenderer& Close();
-	
-	MiniRenderer& Polygon();
-	MiniRenderer& Fill();
-	
-	MiniRenderer& Ellipse(const Rect& rect);
-	
-	MiniRenderer& Width(int width_)           { width = width_; return *this; }
-
-	void Cy(int cy_)                          { cy = cy_; }
-	
-	MiniRenderer();
-	~MiniRenderer();
-};
-
 
 Image  AutoCrop(const Image& m, RGBA c);
 Image  RenderGlyph(Font fnt, int chr);
