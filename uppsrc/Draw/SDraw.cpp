944802d105f4c20c3d9296c6b3ae79b9972fb423
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-05-27 11:52:04 +0000

    Draw: SDraw polygon/polyline support
    git-svn-id: svn://ultimatepp.org/upp/trunk@6116 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Draw/SDraw.cpp
+++ uppsrc/Draw/SDraw.cpp
@@ -25,14 +25,59 @@ void SDraw::DrawLineOp(int x1, int y1, int x2, int y2, int width, Color color)
 	Line(Point(x2, y2));
 }
 
-void SDraw::DrawPolyPolyPolygonOp(const Point *vertices, int vertex_count, const int *subpolygon_counts, int scc, const int *disjunct_polygon_counts, int dpcc, Color color, int width, Color outline, uint64 pattern, Color doxor)
+void SDraw::DrawPolyPolyPolygonOp(const Point *vertices0, int /*vertex_count*/,
+                                  const int *subpolygon_counts0,
+                                  int /*scc*/, const int *disjunct_polygon_counts0, int dpcc0,
+                                  Color color, int width, Color outline,
+                                  uint64 /*pattern*/, Color /*doxor*/) // Last two parameters ignored
 {
-	// TODO
+	for(int pass = 0; pass < 1 + (width > 0); pass++) {
+		const Point *vertices = vertices0;
+		const int *subpolygon_counts = subpolygon_counts0;
+		const int *disjunct_polygon_counts = disjunct_polygon_counts0;
+		int dpcc = dpcc0;
+		while(--dpcc >= 0) {
+			const Point *sp = vertices;
+			vertices += *disjunct_polygon_counts++;
+			if(pass == 0)
+				Polygon();
+			else {
+				docolor = outline;
+				Width(width);
+			}
+			while(sp < vertices) {
+				const Point *pp = sp;
+				sp += *subpolygon_counts++;
+				Move(*pp);
+				while(++pp < sp)
+					Line(*pp);
+				Close();
+			}
+			if(pass == 0 && !IsNull(color)) {
+				docolor = color;
+				Fill();
+			}
+		}
+	}
 }
 
-void SDraw::DrawPolyPolylineOp(const Point *vertices, int vertex_count, const int *counts, int count_count, int width, Color color, Color doxor)
+void SDraw::DrawPolyPolylineOp(const Point *vertices, int vertex_count,
+                               const int *counts, int count_count, int width, Color color,
+                               Color /*doxor*/) // Last parameter ignored
 {
-	// TODO
+	if(width == 0)
+		width = 1;
+	if(width < 0 || IsNull(color))
+		return;
+	Width(width);
+	docolor = color;
+	while(--count_count >= 0) {
+		const Point *lp = vertices;
+		vertices += *counts++;
+		Move(*lp);
+		while(++lp < vertices)
+			Line(*lp);
+	}
 }
 
 END_UPP_NAMESPACE
