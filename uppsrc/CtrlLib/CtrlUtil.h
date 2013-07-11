c83c4ded6345679fa4aee44884fadfd958d072d2
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-07-11 11:46:54 +0000

    CtrlLib: EditNumber
    git-svn-id: svn://ultimatepp.org/upp/trunk@6183 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlLib/CtrlUtil.h
+++ uppsrc/CtrlLib/CtrlUtil.h
@@ -9,6 +9,9 @@ bool EditText(String& s, const char *title, const char *label, int maxlen = 0);
 bool EditText(WString& s, const char *title, const char *label, int (*filter)(int), int maxlen = 0);
 bool EditText(WString& s, const char *title, const char *label, int maxlen = 0);
 
+bool EditNumber(int& n, const char *title, const char *label, int min = INT_MIN, int max = INT_MAX, bool notnull = false);
+bool EditNumber(double& n, const char *title, const char *label, double min = DBL_MIN, double max = DBL_MAX, bool notnull = false);
+
 void Show2(Ctrl& ctrl1, Ctrl& ctrl, bool show = true);
 void Hide2(Ctrl& ctrl1, Ctrl& ctrl);
 
