040797fd5d87cd6bac2ac476d2e4748664ef6185
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-09-16 08:27:59 +0000

    uppsrc: GLDraw
    git-svn-id: svn://ultimatepp.org/upp/trunk@6352 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/GLDraw/GLDraw.h
@@ -0,0 +1,23 @@
+#ifndef _GlDraw_GlDraw_h_
+#define _GlDraw_GlDraw_h_
+
+#include <Draw/Draw.h>
+#include <GL/gl.h>
+
+namespace Upp {
+
+struct GLDraw : SDraw {
+	void SetColor(Color c);
+
+	virtual void  PutImage(Point p, const Image& m, const Rect& src);
+	virtual void  PutRect(const Rect& r, Color color);
+	
+	void InitGL(Size sz);
+	
+	static void ClearCache();
+	static void ResetCache();
+};
+
+};
+
+#endif
