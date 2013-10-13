a919366d95f31fdba9db6cd23f217704f407e06c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-10-13 16:52:20 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@6428 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/BasicAlgo/BasicAlgo.cpp
+++ reference/BasicAlgo/BasicAlgo.cpp
@@ -5,10 +5,21 @@ using namespace Upp;
 CONSOLE_APP_MAIN{
 	StdLogSetup(LOG_FILE|LOG_COUT);
 	
+	DUMP(sgn(10));
+	DUMP(sgn(0));
+	DUMP(sgn(-7));
+	
+	DDUMP(min(6, 2, 7, 4));
+	DDUMP(max(6, 2, 7, 4));
+	
+	DUMP(findarg(3, 1, 7, 6, 3, 2));
+	DUMP(findarg(5, 1, 7, 6));
+	
+	DUMP(decode(2, 1, "one", 2, "two", 3, "three", "?"));
+	DUMP(decode(5, 1, "one", 2, "two", 3, "three", "?"));
+
 	Vector<int> data, empty;
 	data << 10 << 5 << 11 << 9 << 2;
-
-	DUMP(Sum(data));
 
 	DUMP(FindMin(data, 0, data.GetCount() - 1));
 	DUMP(FindMin(data));
@@ -25,14 +36,7 @@ CONSOLE_APP_MAIN{
 	
 	Vector<String> s = Split("one;two;three", ';');
 	DUMP(Sum(s, ""));
+
+	DUMP(Sum(data));
 	
-	DUMP(sgn(10));
-	DUMP(sgn(0));
-	DUMP(sgn(-7));
-	
-	DUMP(findarg(3, 1, 7, 6, 3, 2));
-	DUMP(findarg(5, 1, 7, 6));
-	
-	DUMP(decode(2, 1, "one", 2, "two", 3, "three", "?"));
-	DUMP(decode(5, 1, "one", 2, "two", 3, "three", "?"));
 }
