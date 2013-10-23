a8f8bb55f6890acafb909a4579e66c56710f3e01
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-10-23 06:50:21 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@6470 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ benchmarks/sizeof_gui/main.cpp
@@ -0,0 +1,26 @@
+#include <CtrlLib/CtrlLib.h>
+#include <RichEdit/RichEdit.h>
+
+using namespace Upp;
+
+GUI_APP_MAIN
+{
+	StdLogSetup(LOG_FILE|LOG_COUT);
+
+	RDUMP(sizeof(Image));
+	RDUMP(sizeof(RichText));
+	RDUMP(sizeof(Ctrl));
+	RDUMP(sizeof(Button));
+	RDUMP(sizeof(Switch));
+	RDUMP(sizeof(Label));
+	RDUMP(sizeof(EditField));
+	RDUMP(sizeof(EditString));
+	RDUMP(sizeof(EditIntSpin));
+	RDUMP(sizeof(ArrayCtrl));
+	RDUMP(sizeof(TreeCtrl));
+	RDUMP(sizeof(TreeCtrl::Node));
+	RDUMP(sizeof(FileSel));
+	RDUMP(sizeof(RichTextView));
+	RDUMP(sizeof(ColumnList));
+	RDUMP(sizeof(RichEdit));
+}
