87e51111c0179c965acd8bc0182e10d583d7f8eb
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-10-26 07:38:32 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@6490 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/AccessKey/main.cpp
+++ uppdev/AccessKey/main.cpp
@@ -15,6 +15,21 @@ AccessKey::AccessKey()
 	CtrlLayoutOKCancel(*this, "Window title");
 }
 
+struct Test {
+	Test();
+};
+
+
+void Test::Ahoj()
+{
+}
+
+12234523.234
+1234
+123456789000000000.124124
+
+1000000000
+10000000000000000000000000000000000000000000.234234234
 GUI_APP_MAIN
 {
 	Ctrl::AddFlags(Ctrl::AKD_CONSERVATIVE);
