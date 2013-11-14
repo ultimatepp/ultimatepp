fa8c42418614b88c192007d57b97e09e431f437a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-11-14 19:42:02 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@6553 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/XmlTag/XmlTag.cpp
+++ upptst/XmlTag/XmlTag.cpp
@@ -2,7 +2,23 @@
 
 using namespace Upp;
 
-#define XML(x) LOG(#x << " ------------\n" << x << "-------\n\n")
+#define XML(x) LOG(#x << " ------------\n" << x << "\n-------\n\n")
+
+void Check(const String& s)
+{
+	String xml = XmlTag("outer")(XmlTag("inner").Text(s));
+	LOG("==================== " << AsCString(s));
+	LOG(xml);
+	XmlParser p(xml);
+	p.Tag("outer");
+	p.Tag("inner");
+	String h = p.ReadText();
+	LOG("-----");
+	LOG(AsCString(h));
+	ASSERT(h == s);
+
+//	String h = ParseXML(xml)["outer"]["inner"].GatherText();
+}
 
 CONSOLE_APP_MAIN
 {
@@ -10,14 +26,28 @@ CONSOLE_APP_MAIN
 //	LOG(h);
 //	LOG("=============");
 //	LOG(XmlTag("hhh")(h));
+
+	Check("text");
+	Check("text\n");
+	Check("\ntext");
+	Check("\ntext\n");
+	Check("\nte\nxt\n");
+	Check("\n");
+	Check("\n\n");
+	Check("\n\n\n");
 	
+	LOG("----------------------------------------------------");
 	XML(XmlTag("a")(XmlTag("b")()));
-	XML(XmlTag("a")(XmlTag("b").Text("text")));
+	XML(XmlTag("a")(XmlTag("b").Text("text/")));
 	XML(XmlTag("simple")("text"));
 	XML(XmlTag("tag2").Text("text"));
 	XML(XmlTag("tag2").Text("text\n"));
+	XML(XmlTag("tag2").Text("\n"));
+	XML(XmlTag("tag2").Text("\n\n"));
 	XML(XmlTag("tag")(XmlTag("tag2").Text("text")));
 	XML(XmlTag("tag")(XmlTag("tag2").Text("text\n")));
+	XML(XmlTag("tag")(XmlTag("tag2").Text("\n")));
+	XML(XmlTag("tag")(XmlTag("tag2").Text("\n\n")));
 	XML(XmlTag("tag3")(XmlTag("tag")(XmlTag("tag2").Text("text"))));
 	XML(XmlTag("tag3")(XmlTag("tag")(XmlTag("tag2").Text("text\n"))));
 	XML(XmlTag("tag3")(XmlTag("tag")(XmlTag("tag2").Text("text\n"))));
@@ -27,9 +57,13 @@ CONSOLE_APP_MAIN
 	                         XmlTag("params")(
 	                         	XmlTag("param")(
 	                         		XmlTag("int")("123")))));
+	XML(XmlTag("methodCall")(XmlTag("methodName")("METHOD") +
+	                         XmlTag("params")(
+	                         	XmlTag("param")(
+	                         		XmlTag("string")("\n")))));
 	XML(XmlTag("param")(XmlTag("int")("123")));
 	XML(XmlTag("params")(XmlTag("param")(XmlTag("int")("123"))));
-//	XML(XmlTag("tag2")(XmlTag("tag")("attrib","bad\n<value").Text("text")));4
+	XML(XmlTag("tag2")(XmlTag("tag")("attrib","bad\n<value").Text("text")));
 	
 	return;
 	String h = XmlTag("int")("123");
