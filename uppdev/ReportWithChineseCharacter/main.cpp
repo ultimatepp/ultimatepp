69f7c9f016ede02c67c9a55f0eec1ef5b39181aa
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-11-19 15:14:03 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@6579 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/ReportWithChineseCharacter/main.cpp
+++ uppdev/ReportWithChineseCharacter/main.cpp
@@ -5,14 +5,15 @@ using namespace Upp;
 GUI_APP_MAIN
 {
 	Report r;
-	r.Header("[A2> Page $$P");
+	r.Header("[A2< Header Page $$P");
+	r.Footer("[A2> Footer Page $$P");
 	r << "This is some [* QTF text";
 	r << t_("顯示[* QTF 文字");
 	r << "&&Let's report some table:";
 	r << t_("輸出表格式報表：");
 	String tab;
 	tab << "{{1:1:1:1:1:1:2 A:: B:: C:: D:: E:: F:: G (wider)";
-	for(int row = 0; row < 20; row++)
+	for(int row = 0; row < 200; row++)
 		for(int column = 0; column < 7; column++)
 			tab << ":: " << row << ":" << column << t_("測試");
 	r << tab;
