db81120405d5398fa9f9c2a869fa591e8f52e867
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-11-20 19:46:16 +0000

    ide: Assist++ support for .iml files #580
    git-svn-id: svn://ultimatepp.org/upp/trunk@6590 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/AccessKey/main.cpp
+++ uppdev/AccessKey/main.cpp
@@ -1,35 +1,5 @@
 ﻿#include "AccessKey.h"
 
-#define KEYGROUPNAME "Ide"
-#define KEYNAMESPACE IdeKeys
-#define KEYFILE      <AccessKey/test.key>
-#include             <CtrlLib/key_header.h> 
-
-#define KEYGROUPNAME "Ide"
-#define KEYNAMESPACE IdeKeys
-#define KEYFILE      <AccessKey/test.key>
-#include             <CtrlLib/key_source.h>
-
-AccessKey::AccessKey()
-{
-	CtrlLayoutOKCancel(*this, "Window title");
-}
-
-struct Test {
-	Test();
-};
-
-
-void Test::Ahoj()
-{
-}
-
-12234523.234
-1234
-123456789000000000.124124
-
-1000000000
-10000000000000000000000000000000000000000000.234234234
 GUI_APP_MAIN
 {
 	Ctrl::AddFlags(Ctrl::AKD_CONSERVATIVE);
