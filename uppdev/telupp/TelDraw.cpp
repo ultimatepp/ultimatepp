bece7e8d612428a92ba800eb93a95a3dd40fff41
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-11-30 16:59:19 +0000

    .telupp
    git-svn-id: svn://ultimatepp.org/upp/trunk@6622 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/telupp/TelDraw.cpp
+++ uppdev/telupp/TelDraw.cpp
@@ -23,26 +23,34 @@ void TelDraw::Put(const Rect& r)
 	Put(r.GetSize());
 }
 
-void TelDraw::PutImage(Point p, const Image& img, const Rect& src)
+Index<int64> TelDraw::img_index;
+
+int TelDraw::GetImageI(TelDraw& w, const Image& img)
 {
 	int id = img.GetSerialId();
 	int q = img_index.Find(id);
 	if(q < 0) { // TODO: Implement some sort of victim elimination
 		q = img_index.GetCount();
 		img_index.Add(id);
-		Put8(SETIMAGE);
-		Put16(q);
-		Put(img.GetSize());
+		w.Put8(SETIMAGE);
+		w.Put16(q);
+		w.Put(img.GetSize());
 		const RGBA *end = ~img + img.GetLength();
 		for(const RGBA *s = ~img; s < end; s++) {
-			Put8(s->r);
-			Put8(s->g);
-			Put8(s->b);
-			Put8(s->a);
+			w.Put8(s->r);
+			w.Put8(s->g);
+			w.Put8(s->b);
+			w.Put8(s->a);
 		}
 	}
+	return q;
+}
+
+void TelDraw::PutImage(Point p, const Image& img, const Rect& src)
+{
+	int i = GetImageI(*this, img);
 	Put8(IMAGE);
-	Put16(q);
+	Put16(i);
 	Put(p);
 	Put(src);
 }
