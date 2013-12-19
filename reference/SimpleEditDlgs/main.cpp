cd56f1e4a5c9bf4380e5296e5f1b700840bbeac8
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-12-19 13:59:31 +0000

    .reference: SimpleEditDlgs
    git-svn-id: svn://ultimatepp.org/upp/trunk@6680 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/SimpleEditDlgs/main.cpp
+++ reference/SimpleEditDlgs/main.cpp
@@ -6,13 +6,14 @@ GUI_APP_MAIN
 {
 	String text = "Original text";
 	if(EditText(text, "Please edit a text", "Text"))
-		PromptOK("New value:&\1" + text);
+		PromptOK(text);
 
 	int number = 10;
 	if(EditNumber(number, "Please enter a number between 1 and 100", "Number", 1, 100, true))
-		PromptOK("New value:&\1" + AsString(number));
+		PromptOK(AsString(number));
 	
 	Date dt = GetSysDate();
 	if(EditDateDlg(dt, "Please enter a date", "Date"))
-		PromptOK("New value:&\1" + AsString(dt));
+		PromptOK(AsString(dt));
 }
+
