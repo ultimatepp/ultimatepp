a7a8aaafccb67add70b6f29f2fa4be74c98196d8
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-12-28 13:45:12 +0000

    rainbow: Telpp, WebWord
    git-svn-id: svn://ultimatepp.org/upp/trunk@6699 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- rainbow/Telpp/TelDraw.cpp
+++ rainbow/Telpp/TelDraw.cpp
@@ -37,17 +37,23 @@ void SystemDraw::Put(const String& s)
 	result.Cat(s);
 }
 
-Index<int64> SystemDraw::img_index;
+Index<int64> SystemDraw::img_index[3];
 
-int SystemDraw::GetImageI(SystemDraw& w, const Image& img)
+int SystemDraw::GetImageI(int from, Index<int64>& img_index, int maxcount, SystemDraw& w, const Image& img)
 {
 	int64 id = img.GetSerialId();
 	int q = img_index.Find(id);
-	if(q < 0) { // TODO: Implement some sort of victim elimination
-		q = img_index.GetCount();
-		img_index.Add(id);
+	if(q < 0) {
+		if(img_index.GetCount() < maxcount) {
+			q = img_index.GetCount();
+			img_index.Add(id);
+		}
+		else {
+			q = Random(maxcount);
+			img_index.Set(q, id);
+		}
 		w.Put8(SETIMAGE);
-		w.Put16(q);
+		w.Put16(q + from);
 		w.Put(img.GetSize());
 		const RGBA *end = ~img + img.GetLength();
 		for(const RGBA *s = ~img; s < end; s++) {
@@ -57,7 +63,15 @@ int SystemDraw::GetImageI(SystemDraw& w, const Image& img)
 			w.Put8(s->a);
 		}
 	}
-	return q;
+	return q + from;
+}
+
+int SystemDraw::GetImageI(SystemDraw& w, const Image& img)
+{
+	int area = img.GetWidth() * img.GetHeight();
+	return area <= 64*64     ? GetImageI(0, img_index[0], 2048, w, img) :
+	       area <= 512 * 512 ? GetImageI(2048, img_index[1], 64, w, img) :
+	                           GetImageI(2048 + 64, img_index[2], 8, w, img);
 }
 
 void SystemDraw::PutImage(Point p, const Image& img, const Rect& src)
