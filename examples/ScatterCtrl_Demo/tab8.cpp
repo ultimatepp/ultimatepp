15dd22a6e2cd3b0fd7d4d9ce605d404e24dc1c1f
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2013-12-31 13:43:50 +0000

    ScatterCtrl_Demo: Included units
    git-svn-id: svn://ultimatepp.org/upp/trunk@6705 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/ScatterCtrl_Demo/tab8.cpp
+++ examples/ScatterCtrl_Demo/tab8.cpp
@@ -13,17 +13,17 @@ void Tab8::Init()
 	scatter.AddSeries(s3).Legend("Device 1").PlotStyle<BarSeriesPlot>(barWidth).NoMark().Stroke(2, Gray()).Dash(LINE_SOLID).Fill(LtGray());
 	
 	s1<<Pointf(1,-6)<<Pointf(2,-4)<<Pointf(3,2)<<Pointf(4,8)<<Pointf(5,8)<<Pointf(6,15)<<Pointf(7,20)<<Pointf(8,30)<<Pointf(9,14)<<Pointf(10,10)<<Pointf(11,3)<<Pointf(12,-11);
-	scatter.AddSeries(s1).Legend("Temperature").Stroke(3, Null).Dash(LINE_DASHED).MarkStyle<SquareMarkPlot>();
+	scatter.AddSeries(s1).Legend("Temperature").Units("ºC").Stroke(3, Null).Dash(LINE_DASHED).MarkStyle<SquareMarkPlot>();
 	
 	s2 <<Pointf(1,1008)<<Pointf(2,1012)<<Pointf(3,1016)<<Pointf(4,1012)<<Pointf(5,1008)<<Pointf(6,1016)<<Pointf(7,1012)<<Pointf(8,1004)<<Pointf(9,1000)<<Pointf(10,1001)<<Pointf(11,1000)<<Pointf(12,1002);
-	scatter.AddSeries(s2).Legend("Pressure").Stroke(3, Null).Dash("15 6 6 6 3 6 6 6").SetDataPrimaryY(false);
+	scatter.AddSeries(s2).Legend("Pressure").Units("mBar").Stroke(3, Null).Dash("15 6 6 6 3 6 6 6").SetDataPrimaryY(false);
 	scatter.SetDrawY2Reticle();
 	
 	scatter.cbModifFormatX = THISBACK(MyFormat);
 	scatter.cbModifFormatDeltaX = THISBACK(MyFormatDelta);
 	scatter.SetMaxRange(40).SetMinRange(2, 20)
-		   .SetLabelY("Temperature [C]")
-	       .SetLabelY2("Pressure [mBar]")
+		   .SetLabelY("Temperature")
+	       .SetLabelY2("Pressure")
 		   .SetMarkWidth(1, 4);
 	scatter.ShowInfo()
 		   .ShowContextMenu();	
