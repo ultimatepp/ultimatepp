8d0af6cdc43542495e70f228ade4fb65f5a9a88d
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-01-13 20:26:53 +0000

    ScatterCtrl_Demo: Added units to some demos
    git-svn-id: svn://ultimatepp.org/upp/trunk@6773 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/ScatterCtrl_Demo/tab8.cpp
+++ examples/ScatterCtrl_Demo/tab8.cpp
@@ -10,7 +10,7 @@ void Tab8::Init()
 
 	s3 <<Pointf(1,-12)<<Pointf(2,-17)<<Pointf(3,-13)<<Pointf(4,15)<<Pointf(5,18)<<Pointf(6,25)<<Pointf(7,27)<<Pointf(8,20)<<Pointf(9,5)<<Pointf(10,-10)<<Pointf(11,24)<<Pointf(12,17);
 	double barWidth = 0.4;
-	scatter.AddSeries(s3).Legend("Device 1").PlotStyle<BarSeriesPlot>(barWidth).NoMark().Stroke(2, Gray()).Dash(LINE_SOLID).Fill(LtGray());
+	scatter.AddSeries(s3).Legend("Speed").Units("km/h").PlotStyle<BarSeriesPlot>(barWidth).NoMark().Stroke(2, Gray()).Dash(LINE_SOLID).Fill(LtGray());
 	
 	s1<<Pointf(1,-6)<<Pointf(2,-4)<<Pointf(3,2)<<Pointf(4,8)<<Pointf(5,8)<<Pointf(6,15)<<Pointf(7,20)<<Pointf(8,30)<<Pointf(9,14)<<Pointf(10,10)<<Pointf(11,3)<<Pointf(12,-11);
 	scatter.AddSeries(s1).Legend("Temperature").Units("ºC").Stroke(3, Null).Dash(LINE_DASHED).MarkStyle<SquareMarkPlot>();
