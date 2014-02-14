7ecb536444133ce557219ad0b15a4bfbf0058282
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-02-14 10:42:38 +0000

    CtrlCore: lay.h new variant SetLayout_xx(ctrl, parent,...)
    git-svn-id: svn://ultimatepp.org/upp/trunk@6917 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/lay.h
+++ uppsrc/CtrlCore/lay.h
@@ -27,7 +27,19 @@
 #undef  END_LAYOUT
 
 #define LAYOUT(nm, x, y)       template<class T> inline void SetLayout_##nm(T& parent, bool add = false, bool show = false) {
-#define UNTYPED(var, param)       parent.var.param; if(add) parent.Add(parent.var); if(show) parent.var.param.Show();
+#define UNTYPED(var, param)       parent.var.param; if(add) parent.Add(parent.var); if(show) parent.var.Show();
+#define ITEM(clss, var, param)    UNTYPED(var, param);
+#define END_LAYOUT             };
+
+#include LAYOUTFILE
+
+#undef  LAYOUT
+#undef  UNTYPED
+#undef  ITEM
+#undef  END_LAYOUT
+
+#define LAYOUT(nm, x, y)       template<class T, class D> inline void SetLayout_##nm(T& ctrl, D& parent, bool add = false, bool show = false) {
+#define UNTYPED(var, param)       parent.var.param; if(add) ctrl.Add(parent.var); if(show) parent.var.Show();
 #define ITEM(clss, var, param)    UNTYPED(var, param);
 #define END_LAYOUT             };
 
