4bf302bf2d35ac5ee34ebfc5bd637dd51f9421d5
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-10 09:57:48 +0000

    examples: CodeMetric fixed for C++11
    git-svn-id: svn://ultimatepp.org/upp/trunk@7213 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/CodeMetric/Analyse.cpp
+++ examples/CodeMetric/Analyse.cpp
@@ -81,22 +81,22 @@ void CodeMetric::StoreError(int line, const String &msg)
 
 int CodeMetric::LogicalLinesOfCode(const LexSymbolStat &symbolStat)
 {
-	static Vector<int> oneLiners(
+	static Vector<int> oneLiners(pick(
 	  Vector<int>() << tk_if << tk_else << tk_switch << tk_case
 	                << tk_for << tk_do << tk_while << tk_try << tk_catch
 	                << tk_struct << tk_class << tk_namespace
 	                << tk_public << tk_private << tk_protected
-	                << ';');
+	                << ';'));
 	return symbolStat.SumStat( oneLiners );
 }
 
 void CodeMetric::StoreMetric(const Parser::FunctionStat & functionStat)
 {
-	static Vector<int> cc1_symbols(
-	  Vector<int>() << tk_if << tk_case << tk_for << tk_while << tk_catch);
+	static Vector<int> cc1_symbols(pick(
+	  Vector<int>() << tk_if << tk_case << tk_for << tk_while << tk_catch));
 
-	static Vector<int> cc2_symbols(
-	  Vector<int>() << t_and << t_or << '?');
+	static Vector<int> cc2_symbols(pick(
+	  Vector<int>() << t_and << t_or << '?'));
 
 	if(!functionStat.cppItem.impl)
 		return;
