3359abf8618a67cc11ca8ee423dfe678bb1421b3
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-16 06:55:58 +0000

    .autotest: Qtf now testing PdfDraw too
    git-svn-id: svn://ultimatepp.org/upp/trunk@7262 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/Qtf/Qtf.cpp
+++ autotest/Qtf/Qtf.cpp
@@ -8,13 +8,30 @@ CONSOLE_APP_MAIN
 
 	StdLogSetup(LOG_COUT|LOG_FILE);
 
+	String outdir = GetHomeDirFile("autotest.qtf");
+	DeleteFolderDeep(outdir);
+	RealizeDirectory(outdir);
+
 	for(FindFile ff(GetDataFile("*.qtf")); ff; ff.Next()) {
 		String qtf = LoadFile(ff.GetPath());
 		LOG(ff.GetName() << ' ' << qtf.GetCount());
 		ASSERT(qtf.GetLength() > 10000);
-		String qtf2 = AsQTF(ParseQTF(qtf));
+		RichText txt = ParseQTF(qtf);
+		String qtf2 = AsQTF(txt);
 		ASSERT(qtf2 == qtf);
+		SaveFile(AppendFileName(outdir, ff.GetName()), qtf2);
+		
+		String pdfname = ForceExt(ff.GetName(), ".pdf");
+		String pdf = Pdf(txt);
+		SaveFile(AppendFileName(outdir, pdfname), pdf);
+		
+		String h = LoadDataFile(pdfname);
+		ASSERT(h.GetCount() == pdf.GetCount());
+		int q = h.ReverseFind("trailer");
+		ASSERT(pdf.Mid(0, q) == h.Mid(0, q));
 	}
+
+	DeleteFolderDeep(outdir);
 	
 	LOG("========== OK in " << tm << " s");
 }
