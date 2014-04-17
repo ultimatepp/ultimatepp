d78330129fc35a1fcc3612060f09119b7440372c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-17 12:07:36 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7270 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/Convert/Convert.cpp
+++ autotest/Convert/Convert.cpp
@@ -2,6 +2,16 @@
 
 using namespace Upp;
 
+void Test(Convert& cv, const char *s, Value res)
+{
+	DDUMP(cv.Scan(s));
+	Value v = cv.Scan(s);
+	if(IsError(res))
+		ASSERT(IsError(v));
+	else
+		ASSERT(v == res);
+}
+
 CONSOLE_APP_MAIN
 {
 	StdLogSetup(LOG_FILE|LOG_COUT);
@@ -34,4 +44,44 @@ CONSOLE_APP_MAIN
 
 	DUMP(cv.Scan("15.6.2010"));
 	ASSERT(cv.Scan("15.6.2010") == Time(2010, 6, 15, 23, 59, 59));
+	
+	ConvertDouble d;
+
+	DUMP(d.Scan("12-12"));
+	ASSERT(IsError(d.Scan("12-12")));
+
+	Test(d, "12-12", ErrorValue());
+	Test(d, "12+12", ErrorValue());
+	Test(d, "12.-12", ErrorValue());
+	Test(d, "12.+12", ErrorValue());
+	Test(d, "a", ErrorValue());
+	Test(d, " ", ErrorValue());
+
+	Test(d, "123 ", 123);
+
+	Test(d, "0", 0);
+	Test(d, "123", 123);
+	Test(d, "+123", 123);
+	Test(d, "-123", -123);
+	Test(d, "1.1", 1.1);
+	Test(d, "+1.1", 1.1);
+	Test(d, "-1.1", -1.1);
+	Test(d, "1.1e10", 1.1e10);
+	Test(d, "+1.1e10", 1.1e10);
+	Test(d, "-1.1e10", -1.1e10);
+	Test(d, "1.1e+10", 1.1e10);
+	Test(d, "+1.1e+10", 1.1e10);
+	Test(d, "-1.1e+10", -1.1e10);
+	Test(d, "1.1e-10", 1.1e-10);
+	Test(d, "+1.1e-10", 1.1e-10);
+	Test(d, "-1.1e-10", -1.1e-10);
+	Test(d, "1.1E10", 1.1E10);
+	Test(d, "+1.1E10", 1.1E10);
+	Test(d, "-1.1E10", -1.1E10);
+	Test(d, "1.1E+10", 1.1E10);
+	Test(d, "+1.1E+10", 1.1E10);
+	Test(d, "-1.1E+10", -1.1E10);
+	Test(d, "1.1E-10", 1.1E-10);
+	Test(d, "+1.1E-10", 1.1E-10);
+	Test(d, "-1.1E-10", -1.1E-10);
 }
