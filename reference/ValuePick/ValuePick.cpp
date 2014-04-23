0028f31751d328b0c84b016a425049e46afd912a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-23 07:14:50 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@7303 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/ValuePick/ValuePick.cpp
@@ -0,0 +1,22 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	ValueArray va;
+	va << 12 << 3 << 5 << 1 << 10;
+	DUMP(va);
+	Vector<Value> v = va.Pick();
+	Sort(v);
+	va = pick(v);
+	DUMP(va);
+	
+	ValueMap vm;
+	vm(1, "world")(2, "hello");
+	DUMP(vm);
+	VectorMap<Value, Value> m = vm.Pick();
+	SortByValue(m);
+	vm = pick(m);
+	DUMP(vm);
+}
