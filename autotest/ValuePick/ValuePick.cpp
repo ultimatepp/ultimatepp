8c1ff0cef8f549afea2e763cdca3e394c28d0df9
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-23 07:14:39 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7301 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/ValuePick/ValuePick.cpp
@@ -0,0 +1,51 @@
+#include <Core/Core.h>
+
+#define N 500000
+
+using namespace Upp;
+
+#define TEST(fn, s) { \
+	String r = AsString(fn); \
+	LOG("TEST(" << #fn << ", " << AsCString(r) << ");"); \
+	if(s) ASSERT((char *)s == r); \
+}
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	{
+		Vector<Value> v;
+		v << 5 << 1 << 3;
+		ValueArray va(v);
+		ASSERT(v.IsPicked());
+		TEST(va, "[5, 1, 3]");
+		Vector<Value> v2 = va.Pick();
+		TEST(v2, "[5, 1, 3]");
+		Sort(v2);
+		va = pick(v2);
+		ASSERT(v2.IsPicked());
+		TEST(va, "[1, 3, 5]");
+	}
+	{
+		VectorMap<Value, Value> x;
+		x(1, "kamarade");
+		x(2, "ahoj");
+		
+		ValueMap m = pick(x);
+		
+		TEST(m, "{ 1: kamarade, 2: ahoj }");
+		
+		x = m.Pick();
+	
+		TEST(x, "{1: kamarade, 2: ahoj}");
+		
+		SortByValue(x);
+	
+		TEST(x, "{2: ahoj, 1: kamarade}");
+		
+		m = pick(x);
+	
+		TEST(m, "{ 2: ahoj, 1: kamarade }");
+	}
+}
