d333a5b749df5c71518c90b389fa0e1e426dfcab
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-26 18:04:56 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7319 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/CParserWspc/CParserWspc.cpp
+++ autotest/CParserWspc/CParserWspc.cpp
@@ -6,17 +6,28 @@ CONSOLE_APP_MAIN
 {
 	StdLogSetup(LOG_COUT|LOG_FILE);
 
-	CParser p("  someid     $$      and");
-	LOG("GetPtr:      " << AsCString(p.GetPtr()));
-	LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
-	p.Id("someid");
-	LOG("GetPtr:      " << AsCString(p.GetPtr()));
-	LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
-	p.Char('$');
-	LOG("GetPtr:      " << AsCString(p.GetPtr()));
-	LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
-	p.Char('$');
-	LOG("GetPtr:      " << AsCString(p.GetPtr()));
-	LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
+	{
+		CParser p("  someid     $$      and");
+		LOG("GetPtr:      " << AsCString(p.GetPtr()));
+		LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
+		ASSERT(p.Id("someid"));
+		LOG("GetPtr:      " << AsCString(p.GetPtr()));
+		LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
+		ASSERT(p.Char('$'));
+		LOG("GetPtr:      " << AsCString(p.GetPtr()));
+		LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
+		ASSERT(p.Char('$'));
+		LOG("GetPtr:      " << AsCString(p.GetPtr()));
+		LOG("GetSpacePtr: " << AsCString(p.GetSpacePtr()));
+	}
+	
+	{
+		CParser p("/* TEST */");
+		p.NoSkipComments();
+		ASSERT(p.Char2('/','*'));
+		ASSERT(p.Id("TEST"));
+		ASSERT(p.Char2('*','/'));
+	}
+
 	LOG("========== OK");
 }
