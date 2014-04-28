4c1f98ca3cdc0608a8b94627893a40486c8fca11
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-04-28 13:17:21 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7327 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/LoadIniStream/LoadIniStream.cpp
+++ autotest/LoadIniStream/LoadIniStream.cpp
@@ -4,19 +4,30 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	DUMP(Environment());
+
 	VectorMap<String, String> map = LoadIniFile(GetDataFile("test.ini"));
 	DDUMPM(map);
 
-	const Tuple2<const char *, const char *> et[] = {
+	const Tuple2<String, String> et[] = {
 		{ "alfa", "alfa_value" },
 		{ "_beta", "beta_value" },
 		{ "gamma", "gamma_value" },
+		{ "test", "//TEST" },
 		{ "included", "included_value" },
 		{ "delta", "delta_value" },
+		{ "etest", GetEnv("UPP_MAIN__") },
+		{ "var", "file://" + GetEnv("UPP_MAIN__") },
 	};
 	ASSERT(map.GetCount() == __countof(et));
 	for(int i = 0; i < map.GetCount(); i++) {
+		LOG(map.GetKey(i) << " :: " << et[i].a);
 		ASSERT(map.GetKey(i) == et[i].a);
+		LOG(map[i] << " :: " << et[i].b);
 		ASSERT(map[i] == et[i].b);
 	}
+	
+	LOG("=========== OK");
 }
