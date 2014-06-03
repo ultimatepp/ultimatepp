c9a1c25a016fed11027469e5fc61343f3d8d2d2a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-06-03 07:19:12 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@7412 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/Watermark/main.cpp
+++ reference/Watermark/main.cpp
@@ -7,12 +7,13 @@ void DrawWatermark(Report *report)
 	report->DrawText(1000, 1000, 3000, "Watermark", Serif(1000), WhiteGray());
 }
 
-
 GUI_APP_MAIN
 {
 	Report report;
 	report.OnPage(callback1(DrawWatermark, &report)); // called after new page is created
-	for(int i = 0; i < 100; i++)
-		report << "Report with watermark";
+	for(int i = 0; i < 100; i++) {
+		report << "{{1:1:1@n Report:: with:: Watermark}}"; // Need to use @n to make table background transparent
+		report << "Report with Watermark";
+	}
 	Perform(report);
 }
