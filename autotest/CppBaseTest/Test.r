0788c0ee199d760d16ef6838d04679802931bde9
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-07-24 06:59:17 +0000

    .autotest: CppBaseTest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7537 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/CppBaseTest/Test.r
@@ -0,0 +1,88 @@
+ERROR at 63: Name expected
+======== GLOBALS
+? VARIABLE PUBLIC FILE_CPP lvalue 3
+      natural: NAMESPACE_UPP
+      type: NAMESPACE_UPP
+      qtype: NAMESPACE_UPP
+? VARIABLE PUBLIC FILE_CPP lvalue 63
+      natural: void
+? VARIABLE PUBLIC FILE_CPP lvalue 201
+      natural: END_UPP_NAMESPACE
+      type: END_UPP_NAMESPACE
+      qtype: END_UPP_NAMESPACE
+Foo(int,const String&) FUNCTION PUBLIC FILE_CPP decla lvalue 65
+      item: Foo(int,const String&)
+      name: Foo
+      uname: FOO
+      natural: void Foo(int x, const String& y)
+      param: int x;const String& y
+      pname: x;y
+      ptype: ;String
+      qptype: ;String
+Foo(int,const String&) FUNCTION PUBLIC FILE_CPP decla lvalue impl 67
+      item: Foo(int,const String&)
+      name: Foo
+      uname: FOO
+      natural: void Foo(int x, const String& y)
+      param: int x;const String& y
+      pname: x;y
+      ptype: ;String
+      qptype: ;String
+GetCppFile(int) FUNCTION PUBLIC FILE_CPP decla lvalue impl 21
+      item: GetCppFile(int)
+      name: GetCppFile
+      uname: GETCPPFILE
+      natural: const String& GetCppFile(int i)
+      type: String
+      qtype: String
+      param: int i
+      pname: i
+GetCppFileIndex(const String&) FUNCTION PUBLIC FILE_CPP decla lvalue impl 13
+      item: GetCppFileIndex(const String&)
+      name: GetCppFileIndex
+      uname: GETCPPFILEINDEX
+      natural: int GetCppFileIndex(const String& path)
+      param: const String& path
+      pname: path
+      ptype: String
+      qptype: String
+PreProcess(Stream&) FUNCTION PUBLIC FILE_CPP decla lvalue impl 69
+      item: PreProcess(Stream&)
+      name: PreProcess
+      uname: PREPROCESS
+      natural: SrcFile PreProcess(Stream& in)
+      type: SrcFile
+      qtype: SrcFile
+      param: Stream& in
+      pname: in
+      ptype: Stream
+      qptype: Stream
+SLPos(SrcFile&) FUNCTION PUBLIC FILE_CPP decla lvalue impl 51
+      item: SLPos(SrcFile&)
+      name: SLPos
+      uname: SLPOS
+      natural: void SLPos(SrcFile& res)
+      param: SrcFile& res
+      pname: res
+      ptype: SrcFile
+      qptype: SrcFile
+cpp_file VARIABLE PUBLIC FILE_CPP lvalue 11
+      item: cpp_file
+      name: cpp_file
+      uname: CPP_FILE
+      natural: static Index<String> cpp_file
+      type: Index<String> 
+      qtype: Index<String>
+cpp_file_mutex VARIABLE PUBLIC FILE_CPP lvalue 10
+      item: cpp_file_mutex
+      name: cpp_file_mutex
+      uname: CPP_FILE_MUTEX
+      natural: static StaticMutex cpp_file_mutex
+      type: StaticMutex
+      qtype: StaticMutex
+======== SrcFile
+SrcFile() CONSTRUCTOR PUBLIC FILE_CPP decla lvalue impl 56
+      item: SrcFile()
+      name: SrcFile
+      uname: SRCFILE
+      natural: SrcFile()
