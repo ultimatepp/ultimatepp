55bde24c5d6dab1b98da64f8d87d0a98148ee39b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-08-05 17:02:16 +0000

    Core: missing typename in Algo.h
    git-svn-id: svn://ultimatepp.org/upp/trunk@7562 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Algo.h
+++ uppsrc/Core/Algo.h
@@ -89,13 +89,13 @@ int FindMin(const C& c)
 }
 
 template <class C>
-const C::ValueType& Min(const C& c)
+const typename C::ValueType& Min(const C& c)
 {
 	return c[FindMin(c)];
 }
 
 template <class C>
-const C::ValueType& Min(const C& c, const typename C::ValueType& def)
+const typename C::ValueType& Min(const C& c, const typename C::ValueType& def)
 {
 	int q = FindMin(c);
 	return q < 0 ? def : c[q];
@@ -114,13 +114,13 @@ int FindMax(const C& c)
 }
 
 template <class C>
-const C::ValueType& Max(const C& c)
+const typename C::ValueType& Max(const C& c)
 {
 	return c[FindMax(c)];
 }
 
 template <class C>
-const C::ValueType& Max(const C& c, const typename C::ValueType& def)
+const typename C::ValueType& Max(const C& c, const typename C::ValueType& def)
 {
 	int q = FindMax(c);
 	return q < 0 ? def : c[q];
