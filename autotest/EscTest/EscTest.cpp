204ec7ea85aabdf6b4819806493006d6ea489b4b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-09-22 09:37:04 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7715 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/EscTest/EscTest.cpp
+++ autotest/EscTest/EscTest.cpp
@@ -67,8 +67,22 @@ CHK(a, |, b); \
 LOG(#val << " = " << v << ", ref: " << ref); \
 ASSERT(v == ref); }
 
+void SIC_Print(EscEscape& e)
+{
+	if(e[0].IsArray())
+		LOG((String) e[0]);
+	else
+	if(e[0].IsNumber())
+		LOG(e[0].GetNumber());
+	else
+	if(!e[0].IsVoid())
+		e.ThrowError("invalid argument to 'Print'");
+}
+
 CONSOLE_APP_MAIN
 {
+	StdLogSetup(LOG_FILE|LOG_COUT);
+
 	CHKDBL(3.1, 2.1);
 	CHKDBL(3.0, 2.0);
 	CHKDBL(3.0, (int64)2);
@@ -96,4 +110,22 @@ CONSOLE_APP_MAIN
 	CHKV(global.Get("a").GetNumber(), 3);
 	CHKV(Evaluate("--a", global).GetNumber(), 2);
 	CHKV(global.Get("a").GetNumber(), 2);
+	
+	LOG("----");
+	global.GetAdd("s", "Test");
+	CHKV((WString)Evaluate("s", global), "Test");
+	
+	LOG("----");
+	Escape(global, "Print(x)", SIC_Print);
+	try {
+		Scan(global, LoadFile(GetDataFile("script.esc")));
+		Execute(global, "main", INT_MAX);
+	}
+	catch(CParser::Error e) {
+		Cout() << "ERROR: " << e << "\n";
+	}
+	
+	DDUMP(global.Get("out"));
+	DDUMP(global.Get("out2"));
+	DDUMP(global.Get("out3"));
 }
