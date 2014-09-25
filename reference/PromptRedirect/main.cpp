dbcb75c8e7dc5ae9befcb9dea3b195eab761df5e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-09-25 10:19:04 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@7726 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/PromptRedirect/main.cpp
@@ -0,0 +1,21 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+int MyPrompt(Callback1<const String&> WhenLink,
+             const char *title, const Image& iconbmp, const char *qtf, bool okcancel,
+             const char *button1, const char *button2, const char *button3,
+             int cx, Image im1, Image im2, Image im3)
+{
+	LOG(title << ": " << qtf << "\n");
+	return IDCANCEL;
+}
+
+INITBLOCK {
+	RedirectPrompts(MyPrompt);
+}
+
+GUI_APP_MAIN
+{
+	PromptYesNo("Test");
+}
