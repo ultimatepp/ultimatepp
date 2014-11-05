fdfb2ff312018155aa5ff9803090db5dabf8fc77
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-11-05 19:27:13 +0000

    .dev
    git-svn-id: svn://ultimatepp.org/upp/trunk@7865 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/LZF/LZF4.cpp
+++ uppdev/LZF/LZF4.cpp
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 
 #include <Core/Core.h>
 
@@ -11,6 +11,10 @@ using namespace Upp;
 #define LLOG(x)     // LOG(x)
 #define LTIMING(x)  // RTIMING(x)
 
+//#define HASH4
+//#define HASH8
+
+const int HASH_MASK = 128 * 1024 - 1;
 
 #if 0
 force_inline int Hash(const char *s)
@@ -31,31 +35,39 @@ force_inline int Hash16(const char *s)
 	return CombineHash(Hash8(s), Hash8(s + 8));
 }
 #else
+
+force_inline int FoldHash(int h)
+{
+//	return 2654435761U * h;
+	return 31 * HIWORD(h) - LOWORD(h);
+//	return (((h >> 23) - (h >> 15) - (h >> 7) - h));
+}
+
 force_inline int Hash(const char *s)
 {
 //	return Peek32le(s) % 8191;
 //	return memhash(s, 4) & 8191;
-	dword h = Peek32le(s); return (((h >> 23) - (h >> 15) - (h >> 7) - h));
+//	return (Peek16le(s + 2) * 17) ^ Peek16le(s);
+	return FoldHash(Peek32le(s));
 //	return (2654435761U * Peek32le(s)) & 8191;
 }
 
 force_inline int Hash8(const char *s)
 {
-	dword h = Peek32le(s) - Peek32le(s + 4);
-	return (((h >> 23) - (h >> 15) - (h >> 7) - h));
+	return FoldHash(CombineHash(Peek32le(s), Peek32le(s + 4)));
 }
 
 force_inline int Hash16(const char *s)
 {
 	dword h = Peek32le(s) - Peek32le(s + 4) - Peek32le(s + 8) - Peek32le(s + 12);
-	return (((h >> 23) - (h >> 15) - (h >> 7) - h));
+	return FoldHash(h);
 }
 
 force_inline int Hash32(const char *s)
 {
 	dword h = Peek32le(s) - Peek32le(s + 4) - Peek32le(s + 8) - Peek32le(s + 12) -
 	          Peek32le(s + 16) - Peek32le(s + 20) - Peek32le(s + 24) - Peek32le(s + 28);
-	return (((h >> 23) - (h >> 15) - (h >> 7) - h));
+	return FoldHash(h);
 }
 #endif
 
@@ -72,6 +84,7 @@ int FarOffsets;
 int HugeOffsets;
 int Extended;
 int LargeMatch;
+int Simple;
 
 force_inline char *OutLen(char *out, int len, int maxn, int& stat)
 {
@@ -90,7 +103,7 @@ force_inline char *OutLen(char *out, int len, int maxn, int& stat)
 
 int LZFCompress(char *out, const char *s, int length)
 {
-	RTIMING("LZFCompress");
+	RTIMESTOP("LZFCompress");
 	Literals.Clear();
 	LiteralsCount = 0;
 	Matches = 0;
@@ -103,21 +116,22 @@ int LZFCompress(char *out, const char *s, int length)
 	HugeOffsets = 0;
 	Extended = 0;
 	LargeMatch = 0;
+	Simple = 0;
 
 	const char *out0 = out;
 	const char *end = s + length;
 	
 //	const int HASH_MASK = 2048 * 1024 - 1;
-	const int HASH_MASK = 32768 * 1024 - 1;
 	
 	static const char *hash_table[HASH_MASK + 1];
 	static const char *hash_table8[HASH_MASK + 1];
 	static const char *hash_table16[HASH_MASK + 1];
 	static const char *hash_table32[HASH_MASK + 1];
-	
+
 	for(int i = 0; i < HASH_MASK + 1; i++)
 		hash_table32[i] = hash_table16[i] = hash_table8[i] = hash_table[i] = s;
-	
+
+	RTIMING("Part2");
 	const char *literal_ptr = s;
 	int literal_len = min(length, 32);
 	s += literal_len;
@@ -133,8 +147,9 @@ int LZFCompress(char *out, const char *s, int length)
 
 //			LLOG(AsCString(hptr, hptr + 4) << ' ' << AsCString(s, s + 4) << ' ' << hash);
 
+#if 0
 			hptr = hash_table32[Hash32(s) & HASH_MASK];
-			if(s - hptr < 0xfffff && Peek64le(hptr) == Peek64le(s) &&
+			if(0 && s - hptr < 0xfffff && Peek64le(hptr) == Peek64le(s) &&
 			   Peek64le(hptr + 8) == Peek64le(s + 8) &&
 			   Peek64le(hptr + 16) == Peek64le(s + 16) &&
 			   Peek64le(hptr + 24) == Peek64le(s + 24)) {
@@ -144,36 +159,41 @@ int LZFCompress(char *out, const char *s, int length)
 			}
 	
 			hptr = hash_table16[Hash16(s) & HASH_MASK];
-			if(s - hptr < 0xfffff && Peek64le(hptr) == Peek64le(s) && Peek64le(hptr + 8) == Peek64le(s + 8)) {
+			if(0 && s - hptr < 0xfffff && Peek64le(hptr) == Peek64le(s) && Peek64le(hptr + 8) == Peek64le(s + 8)) {
 				Match16++;
 				match_len = 16;
 				break;
 			}
-			
+#endif
+
+#ifdef HASH8			
 			hptr = hash_table8[Hash8(s) & HASH_MASK];
 			if(s - hptr < 0xfffff && Peek64le(hptr) == Peek64le(s)) {
 				Match8++;
 				match_len = 8;
 				break;
 			}
-			
+#endif			
 //			hptr = hash_table[hash4 & 8181];
 //			LLOG(AsCString(hptr, hptr + 4) << ' ' << AsCString(s, s + 4) << ' ' << hash);
-	
+#ifdef HASH4	
 			hptr = hash_table[Hash(s) & HASH_MASK];
 			if(s - hptr < 0xfffff && Peek32le(hptr) == Peek32le(s)) {
 				Match4++;
 				match_len = 4;
 				break;
 			}
+#endif
 			}
 			
 			LTIMING("Hashing1");
 			literal_len++;
-			hash_table32[Hash32(s - 31) & HASH_MASK] = s - 31;
-			hash_table16[Hash16(s - 15) & HASH_MASK] = s - 15;
-			hash_table8[Hash8(s - 7) & HASH_MASK] = s - 7;
+		//	hash_table32[Hash32(s - 31) & HASH_MASK] = s - 31;
+		//	hash_table16[Hash16(s - 15) & HASH_MASK] = s - 15;
 			hash_table[Hash(s - 3) & HASH_MASK] = s - 3;
+		#ifdef HASH8
+			hash_table8[Hash8(s - 7) & HASH_MASK] = s - 7;
+		#endif
 		
 //			hash_table[hash4 & 8191] = s - 3;
 //			word hash4 = Hash(s - 3);
@@ -183,7 +203,7 @@ int LZFCompress(char *out, const char *s, int length)
 				*out++ = 0xf0 | max(literal_len, 15);
 				out = OutLen(out, literal_len, 15, BigLiterals);
 				SVO_MEMCPY(out, literal_ptr, literal_len);
-			//	Literals.Cat(literal_ptr, literal_len);
+				Literals.Cat(literal_ptr, literal_len);
 				return out + literal_len - out0;
 			}
 		}
@@ -205,10 +225,13 @@ int LZFCompress(char *out, const char *s, int length)
 		LiteralsCount += literal_len > 0;
 
 		if(literal_len == 0 && offset <= 0xfff && ml < 8) {
+			LTIMING("Simple match");
 			*out++ = (ml << 4) & HIBYTE(offset);
 			*out++ = LOBYTE(offset);
+			Simple++;
 		}
 		else {
+			LTIMING("Extended match");
 			*out++ = 0x80 | (max(literal_len, 7) << 4) | max(ml, 15);
 			offset -= 0xfff;
 			if(offset < 0xfff0) {
@@ -231,24 +254,24 @@ int LZFCompress(char *out, const char *s, int length)
 			out = OutLen(out, ml, 15, BigMatches);
 			SVO_MEMCPY(out, literal_ptr, literal_len);
 			out += literal_len;
-//			Literals.Cat(literal_ptr, literal_len);
+			Literals.Cat(literal_ptr, literal_len);
 		}
 
 //		literals.Cat(literal_ptr, literal_len);
-		
+
 		LTIMING("Hashing 2");
 		const char *e = s + match_len;
 		while(s < e) {
+			dword h1 = Peek32le(s - 3);
+			dword h2 = Peek32le(s - 7);
 			hash_table[Hash(s - 3) & HASH_MASK] = s - 3;
+#ifdef HASH8
 			hash_table8[Hash8(s - 7) & HASH_MASK] = s - 7;
-			hash_table16[Hash16(s - 15) & HASH_MASK] = s - 15;
-			hash_table32[Hash32(s - 31) & HASH_MASK] = s - 31;
-//			word hash4 = Hash(s - 3);
-//			hash_table[hash4 & 8191] = s - 3;
-//			hash_table8[CombineHash(Hash(s - 7), hash4) & 8191] = s - 7;
+#endif
+//			hash_table16[Hash16(s - 15) & HASH_MASK] = s - 15;
+//			hash_table32[Hash32(s - 31) & HASH_MASK] = s - 31;
 			s++;
 		}
-		
 		literal_ptr = s;
 		literal_len = 0;
 	}
--- uppdev/LZF/main.cpp
+++ uppdev/LZF/main.cpp
@@ -4,12 +4,20 @@
 
 using namespace Upp;
 
-extern int Literals;
-extern int LiteralsLen;
+extern String Literals;
+extern int LiteralsCount;
 extern int Matches;
 extern int BigMatches;
 extern int BigLiterals;
 extern int FarOffsets;
+extern int HugeOffsets;
+extern int Simple;
+extern int Extended;
+extern int LargeMatch;
+extern int Match32;
+extern int Match16;
+extern int Match8;
+extern int Match4;
 
 int    LZFCompress(char *out, const char *s, int length);
 
@@ -19,28 +27,54 @@ String DEECompress(const char *data, const char *end);
 CONSOLE_APP_MAIN
 {
 //	String data = LoadFile("C:/u/aws.data/Viking_Chanarambie_Fenton_With_SurroundingFarms_Backcast_EPEs_14Sep11_v1004_allTI_PCs.blb");
-//	String data = LoadFile("C:/u/aws.data/Viking_Chanarambie_Fenton_With_SurroundingFarms_Backcast_EPEs_14Sep11_v1004_allTI_PCs.blb");
-	String data = LoadFile("D:/enwik8");
-//	String data = LoadFile("C:/u/upp.src/uppsrc/CtrlLib/ArrayCtrl.cpp");
+//	String data = LoadFile("C:/u/aws.data/c12_OCOE_env.blb");
+//	String data = LoadFile("D:/enwik8");
+//	String data = LoadFile("D:/canterbury_last.tar");
+	String data = LoadFile("C:/u/upp.src/uppsrc/CtrlLib/ArrayCtrl.cpp");
 //	String data = LoadFile(GetDataFile("LZF.cpp"));
+//	String data = LoadFile("C:/u/client/nos74.exe");
+//	String data = LoadFile("C:/upp/theide.exe");
+	
 	String l;
-	Buffer<char> out(11 * data.GetLength() / 10);
-	RDUMP(LZFCompress(out, ~data, data.GetLength()));
+	Buffer<char> out(2 * data.GetLength());
+	
+	{
+		RTIMESTOP("LZFCompress");
+		int n = LZFCompress(out, ~data, data.GetLength());
+		RDUMP(n);
+		RDUMP(data.GetLength());
+	}
 //	RDUMP(cdata.GetCount());
 
-	RDUMP(Literals);
-	RDUMP(LiteralsLen);
+	RDUMP(Match32);
+	RDUMP(Match16);
+	RDUMP(Match8);
+	RDUMP(Match4);
 	RDUMP(Matches);
+	RDUMP(Extended);
+	RDUMP(FarOffsets);
+	RDUMP(HugeOffsets);
+	RDUMP(LiteralsCount);
+	RDUMP(LargeMatch);
 	RDUMP(BigMatches);
 	RDUMP(BigLiterals);
-	RDUMP(FarOffsets);
+	RDUMP(Literals.GetCount());
 	
-	RDUMP(/*Literals + */BigLiterals + LiteralsLen + 2 * Matches + BigMatches + FarOffsets);
+//	RDUMP(Literals + BigLiterals + LiteralsLen + 2 * Matches + Extended);
 
 
 //	RDUMP(AsCString(l, 100));
 //	RDUMP(l.GetCount());
-//	RDUMP(DEECompress(l, l.End()).GetLength());
+	int dee = DEECompress(Literals, Literals.End()).GetLength();
+	
+	SaveFile("d:/literals", Literals);
+	
+	if(Literals.GetCount() < 10000000)
+		RDUMP(ZCompress(Literals).GetCount());
+
+	RDUMP(dee);
+	RDUMP(Literals.GetCount() - dee);
+	RDUMP(Literals.GetCount());
 	RDUMP(data.GetLength());
 
 	RLOG("==========");	
@@ -49,6 +83,7 @@ CONSOLE_APP_MAIN
 		RDUMP(LZ4Compress(data).GetLength());
 	#ifdef flagLZ4STATS
 		RDUMP(lz4stat_LiteralLen);
+		RDUMP(lz4stat_Literals);
 		RDUMP(lz4stat_Matches);
 		RDUMP(lz4stat_BigMatch);
 		RDUMP(lz4stat_BigLiteral);
@@ -61,4 +96,11 @@ CONSOLE_APP_MAIN
 	}
 	if(data.GetLength() < 10000000)
 		RDUMP(ZCompress(data).GetCount());
+
+	{
+		RTIMESTOP("LZ4 direct");
+		Buffer<char> out(2 * data.GetLength());
+		RDUMP(LZ4_compress(data, out, data.GetLength()));
+	}
+
 }
