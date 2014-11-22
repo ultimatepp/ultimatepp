7da4bd9e5d45b7a820241fe3f90d3ca795916945
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-11-22 10:03:31 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@7914 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/CheckBak/CheckBak.cpp
+++ autotest/CheckBak/CheckBak.cpp
@@ -5,16 +5,18 @@ using namespace Upp;
 CONSOLE_APP_MAIN
 {
 	String s = Sys("svn info svn://www.ultimatepp.org/bak");
-	DDUMP(s);
+	DUMP(s);
 	int q = s.Find("Last Changed Date: ");
 	ASSERT(q >= 0);
-	ScanWwwTime()
-	Last Changed Date: 2014-10-30 01:01:56 +0100 (Ŕt, 30 10 2014)
+	s = s.Mid(q);
+	DUMP(s);
+	ASSERT(s.GetCount() > 18);
+	Date d;
+	// 2014-10-30 01:01:56
+	// 0123456789012345678
+	d.year = atoi(s);
+	d.month = atoi(~s + 5);
+	d.day = atoi(~s + 8);
+	DUMP(d);
+	ASSERT(d >= GetSysDate() - 1);
 }
-
-		SQL * Update(SETTINGS)
-				(VALUE,
-				 Format("%04d-%02d-%02d %02d:%02d:%02d",
-				        last.year, last.month, last.day, last.hour, last.minute, last.second))
-		      .Where(ID == "LastKeboola");
-update settings set value = '2014-11-19 00:00:00' where ID = 'LastKeboola'
