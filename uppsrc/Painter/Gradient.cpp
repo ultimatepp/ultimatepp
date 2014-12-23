df9deb3e2fcc9c303ad80e56e85026d18fe9fe09
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2014-12-23 11:44:38 +0000

    Painter: cosmetics
    git-svn-id: svn://ultimatepp.org/upp/trunk@7993 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Painter/Gradient.cpp
+++ uppsrc/Painter/Gradient.cpp
@@ -44,40 +44,35 @@ void BufferPainter::Gradient(const RGBA& color1, const RGBA& color2, const Point
 	MakeGradient(color1, color2, minmax(int(Distance(p1, p2) * pathattr.mtx.GetScale()), 2, 4096));
 }
 
+static dword sLinearStyle(int style)
+{
+	dword s = style & ~FILL_FAST;
+	return FILL_VPAD | (style & FILL_FAST) |
+	       decode(s, GRADIENT_PAD, FILL_HPAD, GRADIENT_REPEAT, FILL_HREPEAT, FILL_HREFLECT);
+}
+
 void BufferPainter::FillOp(const Pointf& p1, const RGBA& color1, const Pointf& p2, const RGBA& color2, int style)
 {
 	Gradient(color1, color2, p1, p2);
-	Fill(gradient, p1, p2,
-	     FILL_VPAD | (style & FILL_FAST) |
-	     (style == GRADIENT_PAD ? FILL_HPAD : style == GRADIENT_REPEAT
-	                            ? FILL_HREPEAT : FILL_HREFLECT));
+	Fill(gradient, p1, p2, sLinearStyle(style));
 }
 
 void BufferPainter::FillOp(const RGBA& color1, const RGBA& color2, const Xform2D& transsrc, int style)
 {
 	MakeGradient(color1, color2, 500);
-	Fill(gradient, Xform2D::Scale(1.0 / 500) * transsrc,
-	     FILL_VPAD | (style & FILL_FAST) |
-	     (style == GRADIENT_PAD ? FILL_HPAD : style == GRADIENT_REPEAT
-	                            ? FILL_HREPEAT : FILL_HREFLECT));
+	Fill(gradient, Xform2D::Scale(1.0 / 500) * transsrc, sLinearStyle(style));
 }
 
 void BufferPainter::StrokeOp(double width, const Pointf& p1, const RGBA& color1, const Pointf& p2, const RGBA& color2, int style)
 {
 	Gradient(color1, color2, p1, p2);
-	Stroke(width, gradient, p1, p2,
-	       FILL_VPAD | (style & FILL_FAST) |
-	       (style == GRADIENT_PAD ? FILL_HPAD : style == GRADIENT_REPEAT
-	                              ? FILL_HREPEAT : FILL_HREFLECT));
+	Stroke(width, gradient, p1, p2, sLinearStyle(style));
 }
 
 void BufferPainter::StrokeOp(double width, const RGBA& color1, const RGBA& color2, const Xform2D& transsrc, int style)
 {
 	MakeGradient(color1, color2, 500);
-	Stroke(width, gradient, Xform2D::Scale(1.0 / 500) * transsrc,
-	       FILL_VPAD | (style & FILL_FAST) |
-	       (style == GRADIENT_PAD ? FILL_HPAD : style == GRADIENT_REPEAT
-	                              ? FILL_HREPEAT : FILL_HREFLECT));
+	Stroke(width, gradient, Xform2D::Scale(1.0 / 500) * transsrc, sLinearStyle(style));
 }
 
 END_UPP_NAMESPACE
