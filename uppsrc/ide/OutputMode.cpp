95d6452bbac145975b7d1416d0d805fe5e1a12cf
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-01-02 16:17:23 +0000

    ide: Output mode: Commandline options generator #935
    git-svn-id: svn://ultimatepp.org/upp/trunk@8021 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/OutputMode.cpp
+++ uppsrc/ide/OutputMode.cpp
@@ -130,8 +130,11 @@ struct OutMode : WithOutputModeLayout<TopWindow> {
 	void Save();
 	void Preset();
 	void SyncLock();
-	
+
 	void Export(int kind);
+
+	void ConfigChange();
+	void CmdOptions();
 
 	typedef OutMode CLASSNAME;
 
@@ -140,6 +143,11 @@ struct OutMode : WithOutputModeLayout<TopWindow> {
 
 void OutMode::Load()
 {
+	config.Clear();
+	const DropList& list = ide.mainconfiglist;
+	for (int i = 0; i < list.GetCount(); ++i)
+		config.Add(list.GetKey(i), list[i]);
+	config <<= ide.mainconfigparam;
 	method.Clear();
 	FindFile ff(ConfigFile("*.bm"));
 	while(ff) {
@@ -182,6 +190,146 @@ void OutMode::Export(int kind)
 	else
 		ide.ExportProject(ep, kind, true);
 	Break(IDOK);
+}
+
+void OutMode::ConfigChange()
+{
+	if (config.GetIndex() < 0)
+		return;
+	ide.mainconfigparam = ~config;
+}
+
+class CmdBuildOptionsWindow : public WithCmdBuildOptionsLayout<TopWindow> {
+private:
+	String cmdAssembly, cmdBuildMode, cmdPackage, cmdMethod, cmdMainConfig;
+
+	struct PathConvert : ConvertString {
+		virtual int Filter(int chr) const
+		{
+			if (chr == '<' || chr == '>' || chr == '|' || chr == '*' || chr == '?' || chr == '\"') return Null;
+			return ConvertString::Filter(chr);
+		}
+	};
+
+public:
+	typedef CmdBuildOptionsWindow CLASSNAME;
+
+	CmdBuildOptionsWindow(const String& package, const String& method, const String& mainconfigparam, const String& output,
+		int targetmode, int hydra1_threads, int linkmode, bool blitzbuild, bool createmap, bool verbosebuild) :
+		cmdPackage(package), cmdMethod(method)
+	{
+		CtrlLayout(*this, t_("Command line options for building"));
+		MinimizeBox().CloseBoxRejects();
+		threads <<= hydra1_threads;
+		threads.NotNull(false);
+		cmdAssembly = GetVarsName();
+		cmdBuildMode = (targetmode == 1 ? "r" : targetmode == 2 ? "1" : targetmode == 3 ? "2" : "d");
+		if (mainconfigparam.GetCount()) {
+			cmdMainConfig = mainconfigparam;
+			cmdMainConfig.Replace(" ", ",");
+			cmdMainConfig.Insert(0, " +");
+		}
+		if (blitzbuild) blitz <<= true;
+		switch(linkmode) {
+			case 1: shared <<= true;  break;
+			case 2: sharedbuild <<= true; break;
+		}
+		exportproject <<= 0;
+		map <<= createmap;
+		verbose <<= verbosebuild;
+		out <<= output;
+		out.SetConvert(Single<PathConvert>());
+		outoption <<= true;
+		umk <<= true;
+
+		rebuild.WhenAction = blitz.WhenAction = msgonfail.WhenAction = silent.WhenAction =
+			map.WhenAction = verbose.WhenAction = makefile.WhenAction =
+			savetargetdir.WhenAction = exportproject.WhenAction = umk.WhenAction =
+			threads.WhenAction = out.WhenAction = outoption.WhenAction = THISBACK(GenerateCmd);
+		shared.WhenAction = THISBACK(OnShared);
+		sharedbuild.WhenAction << THISBACK(OnSharedBuild);
+		btnCopy.WhenPush = callback(&cmd, &TextCtrl::Copy);
+	}
+
+	void OnShared()      { if (shared && sharedbuild) sharedbuild <<= false; GenerateCmd(); }
+	void OnSharedBuild() { if (sharedbuild && shared) shared <<= false; GenerateCmd(); }
+
+	void GenerateCmd()
+	{
+		String cmdBuild("-");
+		if (rebuild) cmdBuild << 'a';
+		if (blitz) cmdBuild << 'b';
+		if (!umk && msgonfail) cmdBuild << 'e';
+		if (silent) cmdBuild << 'l';
+		if (map) cmdBuild << 'm';
+		cmdBuild << cmdBuildMode;
+		if (shared) cmdBuild << 's';
+		else if (sharedbuild) cmdBuild << 'S';
+		if (verbose) cmdBuild << 'v';
+		if (savetargetdir) cmdBuild << 'k';
+		switch (exportproject) {
+			case 1: cmdBuild << 'x'; break;
+			case 2: cmdBuild << 'X'; break;
+		}
+		if (makefile) cmdBuild << 'M';
+		int numThreads = ~threads;
+		if (umk && !IsNull(numThreads) && numThreads > 0)
+			cmdBuild << 'H' << numThreads;
+		String output;
+		if (outoption) {
+			output = TrimBoth(~out);
+			if (output.Find(" ") >= 0)
+				output = String().Cat() << "\"" << output << "\"";
+		}
+		cmd <<= TrimRight(Format("%s %s %s %s%s %s",
+			cmdAssembly, cmdPackage, cmdMethod, cmdBuild, cmdMainConfig, output));
+	}
+
+	virtual void Serialize(Stream& s)
+	{
+		s % rebuild % msgonfail % silent % verbose % makefile % savetargetdir % exportproject
+			% threads % outoption % umk ;
+	}
+
+	virtual bool Key(dword key, int count)
+	{
+		if (key == K_ESCAPE) {
+			Close();
+			return true;
+		}
+		return false;
+	}
+};
+
+void OutMode::CmdOptions()
+{
+	const Workspace& wspc = ide.IdeWorkspace();
+	int pi = wspc.GetCount() > 0 ? 0 : -1;
+	if (pi < 0) {
+		PromptOK("No main package");
+		return;
+	}
+	VectorMap<String, String> bm = ide.GetMethodVars(~method);
+	if (bm.GetCount() == 0) {
+		PromptOK("Invalid build method");
+		return;
+	}
+	One<Host> host = ide.CreateHost(false);
+	One<Builder> b = ide.CreateBuilder(~host);
+	const String& p = wspc[pi];
+	String output = NativePath(ide.OutDir(ide.PackageConfig(wspc, pi, bm, ~config, *host, *b), p, bm, true));
+	if (output.Right(1) == ".")
+		output = output.Left(output.GetCount() - 1);
+	const ModePane& pane = ~mode == 0 ? debug : release;
+	int blitzpackage = pane.package.Get(0, 2);
+	bool blitzbuild = !wspc.package[pi].noblitz && pane.blitz
+		&& (IsNull(blitzpackage) ? true : blitzpackage);
+	CmdBuildOptionsWindow window(p, ~method, ~config, output,
+		~mode, ide.hydra1_threads, pane.linkmode, blitzbuild, pane.map, ide.console.verbosebuild);
+	LoadFromGlobal(window, "CmdBuildOptionsWindow");
+	window.GenerateCmd();
+	window.Run();
+	StoreToGlobal(window, "CmdBuildOptionsWindow");
 }
 
 bool MapFlag(const VectorMap<String, String>& map, const char *key)
@@ -242,7 +390,9 @@ OutMode::OutMode(Ide& ide)
 :	ide(ide)
 {
 	CtrlLayoutOKCancel(*this, "Output mode");
+	config <<= THISBACK(ConfigChange);
 	method <<= THISBACK(Preset);
+	cmd_options <<= THISBACK(CmdOptions);
 	SyncLock();
 	DirSel(export_dir, dsb);
 	export_all <<= THISBACK1(Export, 1);
@@ -253,12 +403,17 @@ OutMode::OutMode(Ide& ide)
 
 void Ide::SetupOutputMode()
 {
+	String prevmainconfig = mainconfigparam;
 	SetupDefaultMethod();
 	OutMode m(*this);
 	m.Load();
-	if(m.Execute() != IDOK)
+	if(m.Execute() != IDOK) {
+		mainconfigparam = prevmainconfig;
 		return;
+	}
 	m.Save();
+	if(prevmainconfig != mainconfigparam)
+		SetMainConfigList();
 	SyncBuildMode();
 	SetBar();
 }
