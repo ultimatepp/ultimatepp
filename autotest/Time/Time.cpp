71562048e7ab31bdf223a20600023c7b76d7e92a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-01-03 15:32:39 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@8022 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/Time/Time.cpp
+++ autotest/Time/Time.cpp
@@ -2,6 +2,12 @@
 
 using namespace Upp;
 
+#define TEST(a, b) \
+DUMP(a); \
+LOG(b); \
+ASSERT(a == b); \
+LOG("===================");
+
 CONSOLE_APP_MAIN
 {
 	StdLogSetup(LOG_COUT|LOG_FILE);
@@ -10,27 +16,27 @@ CONSOLE_APP_MAIN
 
 	DUMP(GetTimeZone());	
 	Date d = GetSysDate();
-	if(d < Date(d.year, 3, 20) && d > Date(d.year - 1, 10, 30)) {
-		LOG("Normalni CZ time");
+	if(d < Date(d.year, 3, 20) && d > Date(d.year, 10, 30)) {
+		LOG("Normal CZ time");
 		ASSERT(GetTimeZone() == 60);
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1200") == Time(2014, 3, 25, 23, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1000") == Time(2014, 3, 25, 21, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0100") == Time(2014, 3, 25, 12, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0000") == Time(2014, 3, 25, 11, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -0100") == Time(2014, 3, 25, 10, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1000") == Time(2014, 3, 25,  1, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1200") == Time(2014, 3, 24, 23, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1200"), Time(2014, 3, 25, 23, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1000"), Time(2014, 3, 25, 21, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0100"), Time(2014, 3, 25, 12, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0000"), Time(2014, 3, 25, 11, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -0100"), Time(2014, 3, 25, 10, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1000"), Time(2014, 3, 25,  1, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1200"), Time(2014, 3, 24, 23, 1, 15));
 	}
 	if(d > Date(d.year, 3, 30) && d < Date(d.year, 10, 20)) {
 		LOG("Daylight saving CZ time");
 		ASSERT(GetTimeZone() == 120);
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1200") == Time(2014, 3, 25, 22, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1000") == Time(2014, 3, 25, 20, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0100") == Time(2014, 3, 25, 11, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0000") == Time(2014, 3, 25, 10, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -0100") == Time(2014, 3, 25,  9, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1000") == Time(2014, 3, 25,  0, 1, 15));
-		ASSERT(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1200") == Time(2014, 3, 24, 22, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1200"), Time(2014, 3, 25, 22, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +1000"), Time(2014, 3, 25, 20, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0100"), Time(2014, 3, 25, 11, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 +0000"), Time(2014, 3, 25, 10, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -0100"), Time(2014, 3, 25,  9, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1000"), Time(2014, 3, 25,  0, 1, 15));
+		TEST(ScanWwwTime("Tue, 25 Mar 2014 12:01:15 -1200"), Time(2014, 3, 24, 22, 1, 15));
 	}
 	ASSERT(ScanTimeZone("-1000") == -600);
 	DUMP(ScanTimeZone(GetTimeZoneText()));
