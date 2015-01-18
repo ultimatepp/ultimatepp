88b9c32739d8a43771115fc304a7a47c2c322892
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-01-18 17:56:24 +0000

    autotest: PeekPokeSwap
    git-svn-id: svn://ultimatepp.org/upp/trunk@8059 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/PeekPokeSwap/PeekPokeSwap.cpp
@@ -0,0 +1,30 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_FILE|LOG_COUT);
+
+	char h[] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99 };
+	word v16 = Peek16le(h + 1);
+	RDUMP(Format64Hex(v16));
+
+	ASSERT(v16 == 0x3322);
+	v16 = SwapEndian16(v16);
+	RDUMP(Format64Hex(v16));
+	ASSERT(v16 == 0x2233);
+	dword v32 = Peek32le(h + 1);
+	RDUMP(Format64Hex(v32));
+	ASSERT(v32 == 0x55443322);
+	v32 = SwapEndian32(v32);
+	RDUMP(Format64Hex(v32));
+	ASSERT(v32 == 0x22334455);
+	uint64 v64 = Peek64le(h + 1);
+	RDUMP(Format64Hex(v64));
+	ASSERT(v64 == 0x9988776655443322);
+	v64 = SwapEndian64(v64);
+	RDUMP(Format64Hex(v64));
+	ASSERT(v64 == 0x2233445566778899);
+}
+
