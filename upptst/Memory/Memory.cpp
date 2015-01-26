11b1328ba73f39513d4d9e894603245b818b3709
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-01-26 19:04:47 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@8096 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/Memory/Memory.cpp
@@ -0,0 +1,23 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+void Test(int ini, int re)
+{
+	RLOG("----------");
+	RLOG("Test trying realloc from " << ini << " to " << re);
+	byte *h = new byte[ini];
+
+	RDUMP(GetMemoryBlockSize(h));
+	RDUMP(TryRealloc(h, re));
+	RDUMP(GetMemoryBlockSize(h));
+}
+
+CONSOLE_APP_MAIN
+{
+	Test(200, 220);
+	Test(194, 204);
+	Test(2000, 2273);
+	Test(500000, 600000);
+	Test(500000, 500002);
+}
