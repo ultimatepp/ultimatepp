a2d5b15f58c4d2fce8705e3d92d23c61e69123e9
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-02-14 18:13:37 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@8175 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/CppParser/CppParser.cpp
@@ -0,0 +1,67 @@
+#include <CppBase/CppBase.h>
+
+using namespace Upp;
+
+Vector<String> errs;
+
+void AddError(int ln, const String& s)
+{
+	errs.Add(AsString(ln) + ": " + s);
+}
+
+void Test(const char *path)
+{
+	CppBase base;
+	
+	LOG("**** " << GetFileName(path));
+
+	FileIn in(path);
+	errs.Clear();
+
+	Parser p;
+	p.Do(in, Vector<String>(), base, path, callback(AddError));
+
+	if(errs.GetCount())
+		DUMPC(errs);
+	Qualify(base);
+	String out;
+	for(int i = 0; i < base.GetCount(); i++) {
+		out << Nvl(base.GetKey(i), "<globals>") << " {\n";
+		const Array<CppItem>& ma = base[i];
+		for(int j = 0; j < ma.GetCount(); j++) {
+			const CppItem& m = ma[j];
+			out << '\t' << CppItemKindAsString(m.kind) << ' ' << m.qitem << ", line " << m.line << "\n";
+		}
+		out << "}\n";
+	}
+	
+	p.dobody = true;
+	in.Seek(0);
+	p.Do(in, Vector<String>(), base, path, callback(AddError));
+	
+	out << "<locals> {\n";
+	for(int i = 0; i < p.local.GetCount(); i++) {
+		out << p.local.GetKey(i) << " " << p.local[i].type;
+		if(p.local[i].isptr)
+			out << " pointer";
+		out << ", line: " << p.local[i].line << "\n";
+	}
+	out << "}";
+	LOG("====");
+	LOG(out);
+	LOG("-------------------------------------------------------------------------------");
+	String h = LoadFile(ForceExt(path, ".out"));
+	h.Replace("\r", "");
+	ASSERT(out == h);
+}
+
+CONSOLE_APP_MAIN {
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	
+	FindFile ff(GetDataFile("*.in"));
+	while(ff) {
+		Test(ff.GetPath());
+		ff.Next();
+	}
+	LOG("=========== OK");
+}
