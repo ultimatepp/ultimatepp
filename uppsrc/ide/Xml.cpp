dc82ffa6af027e73eee5002ddcbe44e92952c367
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-03-01 07:36:55 +0000

    ide: Json/Xml features #605
    git-svn-id: svn://ultimatepp.org/upp/trunk@8225 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Xml.cpp
+++ uppsrc/ide/Xml.cpp
@@ -1,24 +1,30 @@
 #include "ide.h"
 
-struct XmlView : public TopWindow {
+class XmlView : public TopWindow {
+	typedef XmlView CLASSNAME;
+
+public:
 	TreeCtrl              xml;
 	LineEdit              view;
 	FrameTop<StaticRect>  errorbg;
 	Label                 error;
 	ParentCtrl            data;
-
+	
+	XmlView();
+	
 	virtual bool Key(dword key, int count);
 	virtual void Close();
 
-	void Load(int parent, XmlParser& p);
 	void Load(const String& txt);
 	void CopyPath();
 
-	typedef XmlView CLASSNAME;
-
 	void Serialize(Stream& s);
 
-	XmlView();
+private:
+	String Load0(XmlParser& p);
+	void   Load0(int parent, XmlParser& p);
+	
+	void   Reset();
 };
 
 bool XmlView::Key(dword key, int count)
@@ -30,56 +36,44 @@ bool XmlView::Key(dword key, int count)
 	return false;
 }
 
-void XmlView::Load(int parent, XmlParser& p)
+XmlView::XmlView()
 {
-	if(p.IsTag()) {
-		String tag = p.ReadTag();
-		String txt = tag;
-		for(int i = 0; i < p.GetAttrCount(); i++)
-			txt << ' ' << p.GetAttr(i) << "=\"" << p[i] << "\"";
-		parent = xml.Add(parent, IdeImg::XmlTag(), tag, txt);
-		while(!p.End()) {
-			if(p.IsEof())
-				throw XmlError("Unexpected end of text.");
-			Load(parent, p);
-		}
-	}
-	else
-	if(p.IsText())
-		xml.Add(parent, IdeImg::XmlText(), Null, NormalizeSpaces(p.ReadText()));
-	else
-	if(p.IsPI())
-		xml.Add(parent, IdeImg::XmlPI(), Null, NormalizeSpaces(p.ReadPI()));
-	else
-	if(p.IsDecl())
-		xml.Add(parent, IdeImg::XmlDecl(), Null, NormalizeSpaces(p.ReadDecl()));
-	else
-	if(p.IsComment())
-		xml.Add(parent, IdeImg::XmlComment(), Null, NormalizeSpaces(p.ReadComment()));
-	else
-		throw XmlError("Unexpected input.");
+	Title("XML view");
+	Sizeable().Zoomable();
+	Icon(IdeCommonImg::xml());
+
+	xml.NoRoot();
+
+	error.SetFont(Arial(20)).SetInk(Red);
+	errorbg.Height(25).Add(error.SizePos());
+	view.SetReadOnly();
+	view.SetColor(LineEdit::PAPER_READONLY, SColorPaper());
+
+	Add(xml.SizePos());
+	Add(view.SizePos());
+
+	xml.WhenLeftDouble = THISBACK(CopyPath);
 }
 
 void XmlView::Load(const String& txt)
 {
-	xml.Clear();
+	Reset();
+	
 	XmlParser p(txt);
-	p.Relaxed();
-	try {
-		while(!p.IsEof())
-			Load(0, p);
-	}
-	catch(XmlError e) {
-		error = "XML parsing error: " + e;
+	String parsingError = Load0(p);
+	if(parsingError.GetCount() > 0) {
+		parsingError.Set(0, ToLower(parsingError[0]));
+		
+		error = "XML parsing error: " + parsingError + ".";
+		AddFrame(errorbg);
 		view.Show();
-		xml.Hide();
 		view <<= txt;
 		view.SetCursor(view.GetPos(p.GetLine() - 1, p.GetColumn() - 1));
 		view.SetFocus();
 		return;
 	}
+	
 	xml.Show();
-	view.Hide();
 	xml.SetFocus();
 }
 
@@ -109,29 +103,61 @@ void XmlView::Close()
 	TopWindow::Close();
 }
 
-XmlView::XmlView()
-{
-	Title("XML view");
-
-	xml.NoRoot();
-
-	error.SetFont(Arial(20)).SetInk(Red);
-	errorbg.Height(25).Add(error.SizePos());
-	view.AddFrame(errorbg);
-	view.SetReadOnly();
-	view.SetColor(LineEdit::PAPER_READONLY, SColorPaper());
-
-	xml.Hide();
-	view.Hide();
-
-	Add(xml.SizePos());
-	Add(view.SizePos());
-
-	Sizeable().Zoomable();
-
-	Icon(IdeCommonImg::xml());
+String XmlView::Load0(XmlParser& p) {
+	String parsingError;
 	
-	xml.WhenLeftDouble = THISBACK(CopyPath);
+	try {
+		while(!p.IsEof())
+			Load0(0, p);
+	}
+	catch(const XmlError& e) {
+		parsingError = e;
+	}
+	
+	if(!parsingError.GetCount() && !xml.GetChildCount(0))
+	 	parsingError = "Not found any XML tags";
+	
+	return parsingError;
+}
+
+void XmlView::Load0(int parent, XmlParser& p)
+{
+	if(p.IsTag()) {
+		String tag = p.ReadTag();
+		String txt = tag;
+		for(int i = 0; i < p.GetAttrCount(); i++)
+			txt << ' ' << p.GetAttr(i) << "=\"" << p[i] << "\"";
+		parent = xml.Add(parent, IdeImg::XmlTag(), tag, txt);
+		while(!p.End()) {
+			if(p.IsEof())
+				throw XmlError("Unexpected end of text");
+			Load0(parent, p);
+		}
+	}
+	else
+	if(p.IsText())
+		xml.Add(parent, IdeImg::XmlText(), Null, NormalizeSpaces(p.ReadText()));
+	else
+	if(p.IsPI())
+		xml.Add(parent, IdeImg::XmlPI(), Null, NormalizeSpaces(p.ReadPI()));
+	else
+	if(p.IsDecl())
+		xml.Add(parent, IdeImg::XmlDecl(), Null, NormalizeSpaces(p.ReadDecl()));
+	else
+	if(p.IsComment())
+		xml.Add(parent, IdeImg::XmlComment(), Null, NormalizeSpaces(p.ReadComment()));
+	else
+		throw XmlError("Unexpected input");
+}
+
+void XmlView::Reset()
+{
+	RemoveFrame(errorbg);
+	
+	xml.Clear();
+	xml.Hide();
+	view.Clear();
+	view.Hide();
 }
 
 void Ide::Xml()
