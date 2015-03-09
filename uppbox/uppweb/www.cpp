eafa7252c640842939ca88d2d252bcb9de018568
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-03-09 07:44:11 +0000

    .uppweb
    git-svn-id: svn://ultimatepp.org/upp/trunk@8247 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/uppweb/www.cpp
+++ uppbox/uppweb/www.cpp
@@ -815,7 +815,7 @@ CONSOLE_APP_MAIN
 	if (outHtml)
 		SaveFile(AppendFileName(targetdir, "sdj.gif"), LoadFile(GetRcFile("sdj.gif")));
 	
-	String release = "5485"; 
+	String release = "8227"; 
 	escape.Add("RELEASE", release);
 	escape.Add("RELEASET", release);
 	escape.Add("UPDATETIME", Format("%`", GetUtcTime()));
