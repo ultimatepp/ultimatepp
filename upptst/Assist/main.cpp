a5045552bad4538d0190c61957eb7b09c5a95b61
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-06-18 07:57:37 +0000

    .upptst: Assist
    git-svn-id: svn://ultimatepp.org/upp/trunk@8557 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/Assist/main.cpp
@@ -0,0 +1,54 @@
+#include <CtrlLib/CtrlLib.h>
+
+#if 0 // Do not compile these tests
+
+// Each test in separate function to activate grounding
+
+void Test()
+{
+	String x;
+	// Should not show anything here - qualification missing
+	x.
+}
+
+void Test()
+{
+	Upp::String x;
+	x.
+}
+
+using namespace Upp;
+
+void Test()
+{
+	String h;
+	h.
+}
+
+void Test()
+{
+	One<String>()->
+}
+
+void Test()
+{
+	Single<String>()->
+}
+
+void Test()
+{
+	EditDouble x;
+	x.HSizePos().
+}
+
+void Test()
+{
+	EditDouble x;
+	x.SetFilter(Char
+}
+
+#endif
+
+GUI_APP_MAIN
+{
+}
