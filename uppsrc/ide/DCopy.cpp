300d087651b9775f09500e0665f9a99907f42ea5
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-07-13 10:12:43 +0000

    ide: Copy definition/declaration now able to extract functions from THISBACKs #1180
    git-svn-id: svn://ultimatepp.org/upp/trunk@8677 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/DCopy.cpp
+++ uppsrc/ide/DCopy.cpp
@@ -46,7 +46,6 @@ void AssistEditor::DCopy()
 	Parser ctx;
 	Context(ctx, l);
 	String txt = Get(l, h - l);
-	StringStream ss(txt);
 	String cls = ctx.current_scope;
 	int best = 0;
 	const Index<String>& ns = CodeBase().namespaces;
@@ -59,77 +58,96 @@ void AssistEditor::DCopy()
 		}
 	}
 	cls.Remove(0, best);
+
 	CppBase cpp;
 	Parser parser; // we do not need/want preprocessing here
+	StringStream ss(txt);
 	parser.Do(ss, cpp, Null, Null, Null, CNULL, Split(cls, ':'),
 	          Vector<String>(), Index<String>());
 
-	for(int i = 0; i < cpp.GetCount(); i++) {
-		const Array<CppItem>& n = cpp[i];
-		bool decl = decla;
-		for(int j = 0; j < n.GetCount(); j++)
-			if(n[j].impl)
-				decl = false;
-		for(int j = 0; j < n.GetCount(); j++) {
-			const CppItem& m = n[j];
-			if(m.IsCode()) {
-				if(decl)
-					r << MakeDefinition(cls, m.natural) << "\n{\n}\n\n";
-				else {
-					if(cpp.IsType(i))
-					   r << String('\t', Split(cpp.GetKey(i), ':').GetCount());
-					r << m.natural << ";\n";
+	if(cpp.GetCount() == 0) { // scan for THISBACKs
+		Index<String> id;
+		CParser p(txt);
+		while(!p.IsEof()) {
+			try {
+				if(p.Id("THISBACK") && p.Char('(')) {
+					id.FindAdd(p.ReadId());
 				}
 			}
-			if(m.IsData()) {
-				String nat = m.natural;
-				if(cls.GetCount()) {
-					nat.Replace("static", "");
-					nat = TrimLeft(nat);
-					const char *s = nat;
-					while(*s) {
-						if(iscib(*s)) {
-							const char *b = s;
-							while(iscid(*s)) s++;
-							String id(b, s);
-							if(m.name == id) {
-								if(cls.GetCount())
-									r << cls << "::" << m.name << s;
-								else
-									r << m.name << s;
-								break;
+			catch(CParser::Error) {
+			}
+			p.SkipTerm();
+		}
+		for(int i = 0; i < id.GetCount(); i++)
+			r << "\tvoid " << id[i] << "();\n";
+	}
+	else 
+		for(int i = 0; i < cpp.GetCount(); i++) {
+			const Array<CppItem>& n = cpp[i];
+			bool decl = decla;
+			for(int j = 0; j < n.GetCount(); j++)
+				if(n[j].impl)
+					decl = false;
+			for(int j = 0; j < n.GetCount(); j++) {
+				const CppItem& m = n[j];
+				if(m.IsCode()) {
+					if(decl)
+						r << MakeDefinition(cls, m.natural) << "\n{\n}\n\n";
+					else {
+						if(cpp.IsType(i))
+						   r << String('\t', Split(cpp.GetKey(i), ':').GetCount());
+						r << m.natural << ";\n";
+					}
+				}
+				if(m.IsData()) {
+					String nat = m.natural;
+					if(cls.GetCount()) {
+						nat.Replace("static", "");
+						nat = TrimLeft(nat);
+						const char *s = nat;
+						while(*s) {
+							if(iscib(*s)) {
+								const char *b = s;
+								while(iscid(*s)) s++;
+								String id(b, s);
+								if(m.name == id) {
+									if(cls.GetCount())
+										r << cls << "::" << m.name << s;
+									else
+										r << m.name << s;
+									break;
+								}
+								r << id;
 							}
-							r << id;
+							else
+								r << *s++;
+						}
+					}
+					else {
+						int q = nat.ReverseFind("::");
+						if(q >= 0) { // Foo Class2 :: Class::variable; -> static Foo variable;
+							int e = q + 2;
+							for(;;) {
+								while(q >= 0 && nat[q - 1] == ' ')
+									q--;
+								if(q == 0 || !iscid(nat[q - 1]))
+									break;
+								while(q >= 0 && iscid(nat[q - 1]))
+									q--;
+								int w = nat.ReverseFind("::", q);
+								if(w < 0)
+									break;
+								q = w;
+							}
+							nat.Remove(q, e - q);
+							r << "static " << nat;
 						}
 						else
-							r << *s++;
+							r << "extern " << nat;
 					}
+					r << ";\n";
 				}
-				else {
-					int q = nat.ReverseFind("::");
-					if(q >= 0) { // Foo Class2 :: Class::variable; -> static Foo variable;
-						int e = q + 2;
-						for(;;) {
-							while(q >= 0 && nat[q - 1] == ' ')
-								q--;
-							if(q == 0 || !iscid(nat[q - 1]))
-								break;
-							while(q >= 0 && iscid(nat[q - 1]))
-								q--;
-							int w = nat.ReverseFind("::", q);
-							if(w < 0)
-								break;
-							q = w;
-						}
-						nat.Remove(q, e - q);
-						r << "static " << nat;
-					}
-					else
-						r << "extern " << nat;
-				}
-				r << ";\n";
 			}
 		}
-	}
 	WriteClipboardText(r);
 }
