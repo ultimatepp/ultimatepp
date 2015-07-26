a9e4035fd7f64a425b892159d80f753f7a2564f1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-07-26 19:17:31 +0000

    .removed DDUMPs
    git-svn-id: svn://ultimatepp.org/upp/trunk@8755 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Browser/ItemDisplay.cpp
+++ uppsrc/ide/Browser/ItemDisplay.cpp
@@ -144,7 +144,7 @@ int CppItemInfoDisplay::DoPaint(Draw& w, const Rect& r, const Value& q,
 		for(int i = 1; i < min(m.inherited, 5); i++)
 			w.DrawRect(x + 10, r.top + 7 + 2 * i, 7, 1, SColorText);
 	}
-	x += 15;
+	x += 16;
 	int y = ry - Draw::GetStdFontCy() / 2;
 	int x0 = x;
 	Vector<ItemTextPart> n = ParseItemNatural(m);
