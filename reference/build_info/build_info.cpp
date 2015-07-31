8793e8acd5aca1480d1edeb60c2276ad9e48f1ef
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-07-31 11:47:59 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@8776 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/build_info/build_info.cpp
+++ reference/build_info/build_info.cpp
@@ -8,9 +8,18 @@ CONSOLE_APP_MAIN
 	StdLogSetup(LOG_COUT|LOG_FILE);
 
 #ifdef bmTIME
-	LOG("Compiled " << bmTIME);
+	LOG("Compiled " << bmTIME << ", " << (GetSysDate() - (Date)bmTIME) << " day(s) ago");
+#endif
+#ifdef bmUSER
+	LOG("Compiled by user " << bmUSER);
+#endif
+#ifdef bmMACHINE
+	LOG("Compiled on machine " << bmMACHINE);
 #endif
 #ifdef bmSVN_REVISION
 	LOG("Svn revision " << bmSVN_REVISION);
+#endif
+#ifdef bmSVN_REVISION
+	LOG("Svn url " << bmSVN_URL);
 #endif
 }
