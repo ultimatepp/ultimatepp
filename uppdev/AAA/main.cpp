dc6f0ebc4e63decffc84d79e3140a6d0804a00b9
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-08-07 13:48:09 +0000

    .uppdev
    git-svn-id: svn://ultimatepp.org/upp/trunk@8798 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppdev/AAA/main.cpp
+++ uppdev/AAA/main.cpp
@@ -1,7 +1,5 @@
 #include <CtrlLib/CtrlLib.h>
 
-#include <vector> 
-
 using namespace Upp;
 
 #define IMAGECLASS TestImg
@@ -11,6 +9,10 @@ using namespace Upp;
 #define IMAGECLASS TestImg
 #define IMAGEFILE <AAA/test.iml>
 #include <Draw/iml_source.h>
+Image Upscale(const Image& simg);
+Image Upscale2(const Image& simg);
+Image UpscaleA(const Image& simg);
+Image UpscaleA2(const Image& simg);
 
 struct Test : TopWindow {
 	virtual void Paint(Draw& w);
@@ -30,122 +32,10 @@ struct Test : TopWindow {
 	Test();
 };
 
-int sqr(int x)
-{
-	return x * x;
-}
-
-RGBA Blend(RGBA a, RGBA b)
-{
-	RGBA r;
-	r.a = ((int)a.a + b.a) >> 1;
-	r.r = ((int)a.r + b.r) >> 1;
-	r.g = ((int)a.g + b.g) >> 1;
-	r.b = ((int)a.b + b.b) >> 1;
-	return r;
-}
-
-RGBA Get(const Image& img, int x, int y)
-{
-	return Rect(img.GetSize()).Contains(x, y) ? img[y][x] : RGBAZero();
-}
-
-bool IsSimilar(RGBA a, RGBA b, int max)
-{
-	return sqr(77 * ((int)a.r - b.r)) + sqr(151 * ((int)a.g - b.g)) + sqr(28 * ((int)a.b - b.b))
-	       <= sqr(max);
-}
-
-int GetColorIndex(Vector<RGBA>& colors, Color c, int max_distance)
-{
-	for(int i = 0; i < colors.GetCount(); i++)
-		if(IsSimilar(colors[i], c, max_distance))
-			return i;
-	colors.Add(c);
-	return colors.GetCount() - 1;
-}
-
-struct Colors : Vector<RGBA> {
-	const Image& m;
-	int    x, y;
-	
-	int  DX(int r) {
-		r &= 7;
-		return findarg(r, 0, 7, 6) >= 0 ? -1 : findarg(r, 2, 3, 4) >= 0 ? 1 : 0;
-	}
-	
-	int DY(int r) {
-		r &= 7;
-		return findarg(r, 0, 1, 2) >= 0 ? -1 : findarg(r, 4, 5, 6) >= 0 ? 1 : 0;
-	}
-	
-	RGBA Get(int r) {
-		return ::Get(m, x + DX(r), y + DY(r));
-	}
-
-	int GetCI(Color c)
-	{
-		return GetColorIndex(*this, c, 500);
-	}
-
-	int  GetCI(int r) {
-		return GetCI(Get(r));
-	}
-	
-	Colors(const Image& m, int x, int y) : m(m), x(x), y(y) {}
-};
-
-Image Upscale(const Image& simg)
-{
-	Size isz = simg.GetSize();
-
-	ImageBuffer ib(2 * isz);
-	
-	Fill(~ib, RGBAZero(), ib.GetLength());
-
-	for(int x = 0; x < isz.cx; x++)
-		for(int y = 0; y < isz.cy; y++) {
-			DLOG("---------- " << x << ", " << y);
-			RGBA c = simg[y][x];
-			int p[16];
-			// 0 1 2
-			// 7 c 3
-			// 6 5 4
-			Colors colors(simg, x, y);
-			for(int i = 0; i < 8; i++) {
-				p[i] = p[i + 8] = colors.GetCI(i);
-				DLOG(i << ": " << p[i]);
-			}
-			int ci = colors.GetCI(c);
-			DDUMP(colors);
-			// 0 1
-			// 3 2
-			int *pp = p;
-			for(int r = 0; r < 4; r++) {
-				RGBA& t = ib[2 * y + (r >= 2)][2 * x + (r == 1 || r == 2)];
-				t = c;
-				if(pp[1] == pp[7]
-				   && ci != pp[0]
-				   && (pp[0] != pp[1] || pp[0] != pp[6] || pp[0] != pp[2])
-//				   && (pp[7] != pp[3] && pp[7] != pp[4])
-//				   && (pp[1] != pp[5] && pp[1] != pp[4])
-				) {
-				    DLOG(r);
-					DLOG("x: " << (r == 1 || r == 2));
-					DLOG("y: " << (r >= 2));
-					t = Blend(colors.Get(2 * r + 1), colors.Get(2 * r + 7));
-				}
-				pp += 2;
-			}
-		}
-	
-	return ib;
-}
-
 void Test::Paint(Draw& w)
 {
 	w.DrawRect(GetSize(), SColorFace());
-	Image simg = TestImg::test1();
+	Image simg = TestImg::test2();
 	int y = 10;
 	PutImage(w, y, simg);
 
@@ -155,7 +45,7 @@ void Test::Paint(Draw& w)
 	simg = Unmultiply(simg);
 	
 
-	Image r = Upscale(simg);
+	Image r = UpscaleA2(simg);
 	w.DrawImage(10, 100, r);
 
 	w.DrawImage(10, 150 + 2 * isz.cy, Magnify(r, 4, 4));
