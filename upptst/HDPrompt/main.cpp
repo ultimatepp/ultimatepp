4de4f82200b519bf8dc2bbdf630cbf2fe52b5ec7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-10-05 07:31:32 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@8984 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/HDPrompt/main.cpp
+++ upptst/HDPrompt/main.cpp
@@ -4,7 +4,7 @@ using namespace Upp;
 
 GUI_APP_MAIN
 {
-	Ctrl::SetHiDPIEnabled();
+	Ctrl::SetUHDEnabled();
 	PromptOK("This is a very long line in PromptOK, line1 line2 line3 line4 line5 line6 line7 "
 	         "line8 line9 line10 eol11 line12 line13");
 }
