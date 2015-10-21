a4f3d829108a6015ee7cc3325a93e31af23b55d1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-10-21 13:56:48 +0000

    ide: Changed default font size
    git-svn-id: svn://ultimatepp.org/upp/trunk@9063 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/idewin.cpp
+++ uppsrc/ide/idewin.cpp
@@ -505,8 +505,8 @@ Ide::Ide()
 
 	Icon(IdeImg::Package(), IdeImg::Package());
 
-	tfont = editorfont = font2 = CourierZ(13);
-	veditorfont = consolefont = font1 = CourierZ(11);
+	tfont = editorfont = font2 = CourierZ(12);
+	veditorfont = consolefont = font1 = CourierZ(10);
 	editortabsize = 4;
 	indent_amount = 4;
 	indent_spaces = false;
