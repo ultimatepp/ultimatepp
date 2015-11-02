3e3660144a6deecec50cc927417f047ef3b1c554
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-11-02 16:32:58 +0000

    bazaar: Protect doc fixes
    git-svn-id: svn://ultimatepp.org/upp/trunk@9139 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/ProtectTest/main.cpp
+++ bazaar/ProtectTest/main.cpp
@@ -21,7 +21,6 @@ double CryptedTest(double d, double e)
 	double f;
 	f = d * e;
 
-	PromptOK("CryptedTest DECRYPTED SUCCESFULLY!!!");
 	return 2 * f + e;
 
 	PROTECT_END_FUNC;
@@ -29,15 +28,12 @@ double CryptedTest(double d, double e)
 
 double ObfuscatedTest(double d, double e)
 {
-
 	// WARNING -- DON'T PUT ANY return STATEMENT BETWEEN
 	// OBFUSCATE_START and OBFUSCATE_END
 	OBFUSCATE_START_FUNC;
 
 	double f;
 	f = d * e;
-
-	PromptOK("ObfuscatedTest DEOBFUSCATED SUCCESFULLY!!!");
 
 	OBFUSCATE_END_FUNC;
 
@@ -46,11 +42,14 @@ double ObfuscatedTest(double d, double e)
 
 GUI_APP_MAIN
 {
+	bool bad_key = false;
 	ON_PROTECT_BAD_KEY(Decrypt)
 	{
-		bool res = PromptYesNo("Bad key !!&Do you want to continue anyways ?");
-		if(!res)
-			exit(0);
+		bad_key = true;
+	}
+
+	if(bad_key && PromptYesNo("Bad key !!&Do you want to continue anyways ?")) {
+		Exit(0);
 	}
 		
 	double d = CryptedTest(3, 4);
@@ -59,6 +58,5 @@ GUI_APP_MAIN
 	d = ObfuscatedTest(3, 4);
 	
 	PromptOK("FINISHED OK !!");
-	
 }
 
