c3b11321d5f4f564dc4a7be52643982eb5f84e99
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-11-12 17:14:59 +0000

    Added menu separator to Select Package dialog
    git-svn-id: svn://ultimatepp.org/upp/trunk@9182 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/SelectPkg.cpp
+++ uppsrc/ide/SelectPkg.cpp
@@ -10,6 +10,7 @@ void SelectPackageDlg::PackageMenu(Bar& menu)
 {
 	bool b = GetCurrentName().GetCount();
 	menu.Add("New package..", THISBACK(OnNew));
+	menu.Separator();
 	menu.Add(b, "Rename package..", THISBACK(RenamePackage));
 	menu.Add(b, "Delete package", THISBACK(DeletePackage));
 }
