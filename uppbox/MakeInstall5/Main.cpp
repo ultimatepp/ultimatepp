efbd53075f2b728aafb622a6a492a13fe59f18b7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-11-20 09:33:32 +0000

    .uppbox
    git-svn-id: svn://ultimatepp.org/upp/trunk@9195 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/MakeInstall5/Main.cpp
+++ uppbox/MakeInstall5/Main.cpp
@@ -75,7 +75,7 @@ int FilterVersion(int c) { return c == ':' ? '_' : c; }
 
 void Make(String pkg, String exe, String method = "MSC9")
 {
-	Syx("c:/upp/umk " + ass + " " + pkg + " c:/upp/" + method + ".bm -arv " + upptmp + "/" + exe);
+	Syx("u:/upp/umk " + ass + " " + pkg + " u:/upp/" + method + ".bm -arv " + upptmp + "/" + exe);
 	FileDelete(upptmp + "/" + ForceExt(exe, ".map"));
 }
 
