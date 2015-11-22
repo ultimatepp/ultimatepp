895536b72a39e2e24400f562d19eb01758e17a15
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-11-22 12:56:43 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@9210 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/CheckBak/CheckBak.cpp
+++ autotest/CheckBak/CheckBak.cpp
@@ -23,17 +23,32 @@ CONSOLE_APP_MAIN
 	LOG("Bak repository last date: " << d);
 	ASSERT(d >= GetSysDate() - 1);
 	
-	FindFile ff("/mnt/nas/bak/*.*");
-	d = Date::Low();
+	for(int pass = 0; pass < 3; pass++) {
+		String dir = decode(pass, 0, "/mnt/nas1/bak", 1, "/mnt/nas/bak", "/bak");
+		FindFile ff(dir + "/*");
+		d = Date::Low();
+		int64 len = 0;
+		String fn;
+		
+		while(ff) {
+			if(ff.IsFile()) {
+				Date d1 = (Date)Time(ff.GetLastWriteTime());
+				if(d1 > d) {
+					d = d1;
+					len = ff.GetLength();
+					fn = ff.GetName();
+				}
+			}
+			ff.Next();
+		}
+		
+		len = len >> 20;
+		LOG(dir << ": " << fn << ", last date: " << d << ", size " << len << " MB");
 	
-	while(ff) {
-		d = max(d, (Date)Time(ff.GetLastWriteTime()));
-		ff.Next();
+		ASSERT(d >= GetSysDate() - 1);
+		ASSERT(len > 500 && len < 1500);
 	}
-	
-	LOG("mnt/nas/bak last date: " << d);
 
-	ASSERT(d >= GetSysDate() - 1);
 	
 	s = HttpRequest("http://www.ultimatepp.org/df.info").Execute();
 	LOG(s);
