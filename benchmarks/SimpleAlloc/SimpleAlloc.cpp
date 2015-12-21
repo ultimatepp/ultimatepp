faab6a79915b37213c3d44e7843adbc93e0fb764
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-12-21 19:49:46 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@9315 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- benchmarks/SimpleAlloc/SimpleAlloc.cpp
+++ benchmarks/SimpleAlloc/SimpleAlloc.cpp
@@ -2,9 +2,12 @@
 
 using namespace Upp;
 
-#define N 20000000
+#define N 50000000
+#define MIX 30
 
 void *ptr[N];
+void *mix[MIX];
+int   len[N];
 
 CONSOLE_APP_MAIN
 {
@@ -19,5 +22,17 @@ CONSOLE_APP_MAIN
 		for(int i = 0; i < N; i++)
 			delete[] ptr[i];
 		RLOG("Free " << tm);
+	}
+	{
+		for(int i = 0; i < N; i++)
+			len[i] = Random(1000 + 500);
+		TimeStop tm;
+		for(int i = 0; i < N / MIX; i++) {
+			int ii = i % MIX;
+			if(mix[ii])
+				delete[] mix[ii];
+			mix[ii] = new byte[len[i]];
+		}
+		RLOG("Mix " << tm);
 	}
 }
