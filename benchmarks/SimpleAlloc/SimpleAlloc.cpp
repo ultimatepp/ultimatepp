d949ec4c3fde60231869740318c9580a4a1c4714
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2015-12-21 20:30:42 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@9316 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- benchmarks/SimpleAlloc/SimpleAlloc.cpp
+++ benchmarks/SimpleAlloc/SimpleAlloc.cpp
@@ -3,14 +3,30 @@
 using namespace Upp;
 
 #define N 50000000
+#define NMIX 100000000
 #define MIX 30
 
 void *ptr[N];
-void *mix[MIX];
-int   len[N];
+int   len[NMIX];
+
+void MixTest()
+{
+	void *mix[MIX];
+	for(int i = 0; i < MIX; i++)
+		mix[i] = NULL;
+	for(int i = 0; i < NMIX; i++) {
+		int ii = i % MIX;
+		if(mix[ii])
+			delete[] mix[ii];
+		mix[ii] = new byte[len[i]];
+	}
+	for(int i = 0; i < MIX; i++)
+		delete[] mix[i];
+}
 
 CONSOLE_APP_MAIN
 {
+	StdLogSetup(LOG_COUT|LOG_FILE);
 	{
 		TimeStop tm;
 		for(int i = 0; i < N; i++)
@@ -24,15 +40,20 @@ CONSOLE_APP_MAIN
 		RLOG("Free " << tm);
 	}
 	{
-		for(int i = 0; i < N; i++)
-			len[i] = Random(1000 + 500);
+		for(int i = 0; i < NMIX; i++)
+			len[i] = Random(500);
 		TimeStop tm;
-		for(int i = 0; i < N / MIX; i++) {
-			int ii = i % MIX;
-			if(mix[ii])
-				delete[] mix[ii];
-			mix[ii] = new byte[len[i]];
-		}
+		MixTest();
 		RLOG("Mix " << tm);
+	}
+	Vector<int> h = { 1, 2, 3, 4, 5, 6, 7, 8, 12, 16 };
+	for(auto n : h) {
+		TimeStop tm;
+		Buffer<Thread> t(n);
+		for(int i = 0; i < n; i++)
+			t[i].Run(callback(MixTest));
+		for(int i = 0; i < n; i++)
+			t[i].Wait();
+		RLOG(n << " threads Mix " << tm);
 	}
 }
