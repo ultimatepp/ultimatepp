fbf1657a7fc9e5166362841e168c5894db170008
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-01-16 14:27:11 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@9404 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- benchmarks/LongSort/LongSort.cpp
+++ benchmarks/LongSort/LongSort.cpp
@@ -7,23 +7,31 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
+	SeedRandom(0);
+
 	Vector<String> a;
 	std::vector<std::string> c;
 	for(int i = 0; i < N; i++) {
 		String s = AsString(Random()) + AsString(Random()) +
-		           AsString(Random()) + AsString(Random());
+		           AsString(Random()) + AsString(Random()) +
+		           AsString(Random()) + AsString(Random())
+		;
 		a.Add(s);
 		c.push_back(s.ToStd());
 	}
 
 	{
 		Vector<String> b = clone(a);
-		RTIMING("Sort");
-		Sort(b);
+		{
+			RTIMING("Sort");
+			Sort(b);
+		}
 	}
 	{
 		std::vector<std::string> d = c;
-		RTIMING("std::sort");
-		std::sort(d.begin(), d.end());
+		{
+			RTIMING("std::sort");
+			std::sort(d.begin(), d.end());
+		}
 	}
 }
