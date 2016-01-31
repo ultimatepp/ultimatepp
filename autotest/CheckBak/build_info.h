272bcffc0c0a26f09c5553e8c4330e44d0d818e3
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-01-31 10:33:11 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@9441 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/CheckBak/build_info.h
@@ -0,0 +1,9 @@
+#define bmYEAR   2016
+#define bmMONTH  1
+#define bmDAY    31
+#define bmHOUR   11
+#define bmMINUTE 9
+#define bmSECOND 45
+#define bmTIME   Time(2016, 1, 31, 11, 9, 45)
+#define bmMACHINE "cxl-H61M-D2-B3"
+#define bmUSER    "cxl"
