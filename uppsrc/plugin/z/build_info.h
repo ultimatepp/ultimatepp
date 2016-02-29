e953bcbd18ee467f4f3696fc739d5319a81ea667
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-02-29 17:29:34 +0000

    plugin/Zip now supports storing uncompressed files
    git-svn-id: svn://ultimatepp.org/upp/trunk@9548 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/plugin/z/build_info.h
@@ -0,0 +1,9 @@
+#define bmYEAR   2016
+#define bmMONTH  2
+#define bmDAY    6
+#define bmHOUR   23
+#define bmMINUTE 2
+#define bmSECOND 31
+#define bmTIME   Time(2016, 2, 6, 23, 2, 31)
+#define bmMACHINE "MAIN"
+#define bmUSER    "cxl"
