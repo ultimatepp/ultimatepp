ca839d7efd606869005243b675b2a26c7ce46537
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-03-06 12:48:54 +0000

    .examples documented loading library code path in AndroidMath
    git-svn-id: svn://ultimatepp.org/upp/trunk@9575 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/AndroidMath/AndroidMathActivity.java
+++ examples/AndroidMath/AndroidMathActivity.java
@@ -62,8 +62,12 @@ public class AndroidMathActivity extends Activity
 	
 	static {
 		// For Android version less than 5.0 we need to specific standard library
+		// If you have a crash uncomment library that you specific in your Android Builder
+		// specification.
+		
+		// System.loadLibrary("stlport_shared");
 		// System.loadLibrary("gnustl_shared");
-		System.loadLibrary("c++_shared");
+		// System.loadLibrary("c++_shared");
 		
 		// In this place we are loading native libraries (In revers dependency order).
 		// IMPORTANT: Native library always has upp package name!!!
