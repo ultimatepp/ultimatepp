b471d0dd6f8d9409dd6890feb57bf1b43367662c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-03-07 10:54:58 +0000

    ide: fixed to compile
    git-svn-id: svn://ultimatepp.org/upp/trunk@9581 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/plugin/ndisasm/build_info.h
@@ -0,0 +1,9 @@
+#define bmYEAR   2016
+#define bmMONTH  3
+#define bmDAY    6
+#define bmHOUR   14
+#define bmMINUTE 3
+#define bmSECOND 5
+#define bmTIME   Time(2016, 3, 6, 14, 3, 5)
+#define bmMACHINE "MAIN"
+#define bmUSER    "cxl"
