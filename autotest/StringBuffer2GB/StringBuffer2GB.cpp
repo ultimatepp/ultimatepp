facc786a668fbe827bc5f9df9315c3ac2999cd99
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-05-03 08:47:13 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@9777 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/StringBuffer2GB/StringBuffer2GB.cpp
+++ autotest/StringBuffer2GB/StringBuffer2GB.cpp
@@ -5,29 +5,63 @@ using namespace Upp;
 CONSOLE_APP_MAIN
 {
 	StdLogSetup(LOG_COUT|LOG_FILE);
-	
-	StringBuffer sb, sb2;
-	WStringBuffer wb, wb2;
-	
-	for(int i = 0; i < INT_MAX; i++) {
-		sb.Cat(i % 37);
-		char h[2] = { i % 43, 0 };
-		sb2.Cat(h, 1);
-		wb.Cat(i % 37777);
-		wchar wh[2] = { wchar(i % 37771), 0 };
-		wb2.Cat(wh, 1);
-	}
-	
-	ASSERT(sb.GetCount() == INT_MAX);
-	ASSERT(sb2.GetCount() == INT_MAX);
-	ASSERT(wb.GetCount() == INT_MAX);
-	ASSERT(wb2.GetCount() == INT_MAX);
 
-	for(int i = 0; i < INT_MAX; i++) {
-		ASSERT((byte)sb[i] == i % 37);
-		ASSERT((byte)sb2[i] == i % 43);
-		ASSERT(wb[i] == i % 37777);
-		ASSERT(wb2[i] == i % 37771);
+	{
+		StringBuffer sb;
+		
+		for(int i = 0; i < INT_MAX; i++) {
+			sb.Cat(i % 37);
+		}
+		
+		ASSERT(sb.GetCount() == INT_MAX);
+	
+		for(int i = 0; i < INT_MAX; i++) {
+			ASSERT((byte)sb[i] == i % 37);
+		}
+	}
+
+	{
+		StringBuffer sb2;
+		
+		for(int i = 0; i < INT_MAX; i++) {
+			char h[2] = { char(i % 43), 0 };
+			sb2.Cat(h, 1);
+		}
+		
+		ASSERT(sb2.GetCount() == INT_MAX);
+	
+		for(int i = 0; i < INT_MAX; i++) {
+			ASSERT((byte)sb2[i] == i % 43);
+		}
+	}
+
+	{
+		WStringBuffer wb;
+		
+		for(int i = 0; i < INT_MAX; i++) {
+			wb.Cat(i % 37777);
+		}
+		
+		ASSERT(wb.GetCount() == INT_MAX);
+	
+		for(int i = 0; i < INT_MAX; i++) {
+			ASSERT(wb[i] == i % 37777);
+		}
+	}
+
+	{
+		WStringBuffer wb2;
+		
+		for(int i = 0; i < INT_MAX; i++) {
+			wchar wh[2] = { wchar(i % 37771), 0 };
+			wb2.Cat(wh, 1);
+		}
+
+		ASSERT(wb2.GetCount() == INT_MAX);
+	
+		for(int i = 0; i < INT_MAX; i++) {
+			ASSERT(wb2[i] == i % 37771);
+		}
 	}
 
 	LOG("=========== Everything OK!");
