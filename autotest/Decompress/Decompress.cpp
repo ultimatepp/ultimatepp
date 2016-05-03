a1d0d63e524e2b2045a0e87a712cc7e96543e7f5
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-05-03 06:49:41 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@9770 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/Decompress/Decompress.cpp
+++ autotest/Decompress/Decompress.cpp
@@ -6,6 +6,8 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
 	String orig = LoadDataFile("X11App.cpp");
 	ASSERT(orig.GetCount());
 	{
@@ -24,4 +26,6 @@ CONSOLE_APP_MAIN
 	{
 		ASSERT(LZMADecompress(LoadDataFile("X11App.cpp.lzma")) == orig);
 	}
+	
+	LOG("================== OK");
 }
