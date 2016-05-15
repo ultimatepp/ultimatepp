8628a185853ceb63d03eaf212ae9720c61a3b9c0
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-05-15 18:36:48 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@9837 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/UrlInfo/UrlInfo.cpp
@@ -0,0 +1,22 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	UrlInfo f("http://username:password@hostname:9090/path?arg=value&aarg[]=item1&aarg[]=item2#anchor");
+
+	DUMP(f.url);
+	DUMP(f.scheme);
+	DUMP(f.host);
+	DUMP(f.port);
+	DUMP(f.user);
+	DUMP(f.password);
+	DUMP(f.path);
+	DUMP(f.query);
+	DUMP(f.fragment);
+	DUMP(f.parameters);
+	DUMP(f.array_parameters);
+}
