17d6ec8c4aaf2552037cc97e0c66cbe37f9a302d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-05-27 13:39:54 +0000

    Core: Win32 DHCtrl accelerator support
    git-svn-id: svn://ultimatepp.org/upp/trunk@9887 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/DHCtrl.cpp
+++ uppsrc/CtrlCore/DHCtrl.cpp
@@ -6,14 +6,41 @@ NAMESPACE_UPP
 
 #ifndef PLATFORM_WINCE
 
+Vector<DHCtrl *> DHCtrl::all_active;
+
+void DHCtrl::RemoveActive()
+{
+	for(;;) {
+		int q = FindIndex(all_active, this);
+		if(q < 0)
+			return;
+		all_active.Remove(q);
+	}
+}
+
+bool DHCtrl::PreprocessMessageAll(MSG& msg)
+{
+	for(auto q : all_active)
+		if(q->PreprocessMessage(msg))
+			return true;
+	return false;
+}
+
+bool DHCtrl::PreprocessMessage(MSG& msg)
+{
+	return false;
+}
+
 void DHCtrl::NcCreate(HWND _hwnd)
 {
 	hwnd = _hwnd;
+	all_active.Add(this);
 }
 
 void DHCtrl::NcDestroy()
 {
 	hwnd = NULL;
+	RemoveActive();
 }
 
 LRESULT DHCtrl::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
@@ -28,6 +55,7 @@ void DHCtrl::CloseHWND()
 	if(hwnd) {
 		DestroyWindow(hwnd);
 		hwnd = NULL;
+		RemoveActive();
 	}
 }
 
@@ -80,6 +108,7 @@ DHCtrl::~DHCtrl()
 {
 	CloseHWND();
 	BackPaint(EXCLUDEPAINT);
+	RemoveActive();
 }
 
 #endif
