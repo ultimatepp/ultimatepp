4d1f27cc9b3a713f9fee163821c198f353a578eb
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-07-19 09:39:12 +0000

    .devloping LZ4 stream
    git-svn-id: svn://ultimatepp.org/upp/trunk@10075 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/plugin/lz4/lz4.h
+++ uppsrc/plugin/lz4/lz4.h
@@ -7,6 +7,23 @@
 
 namespace Upp {
 
+enum {
+	LZ4F_MAGIC       = 0x184D2204,
+
+	LZ4F_VERSIONMASK = 0b11000000,
+	LZ4F_VERSION     = 0b01000000,
+	LZ4F_BLOCKINDEPENDENCE = (1 << 5),
+	LZ4F_BLOCKCHECKSUM     = (1 << 4),
+	LZ4F_CONTENTSIZE       = (1 << 3),
+	LZ4F_CONTENTCHECKSUM   = (1 << 2),
+	
+	LZ4F_MAXSIZEMASK      = 0x70,
+    LZ4F_MAXSIZE_64KB     = 0x40,
+    LZ4F_MAXSIZE_256KB    = 0x50,
+    LZ4F_MAXSIZE_1024KB   = 0x60,
+    LZ4F_MAXSIZE_4096KB   = 0x70,
+};
+
 class Lz4 {
 	StringBuffer buffer; // to be able to pass as String
 	int8         compress;
@@ -61,6 +78,61 @@ public:
 
 	Lz4();
 	~Lz4();
+};
+
+class LZ4CompressStream : public Stream  {
+public:
+	virtual   void  Close();
+	virtual   bool  IsOpen() const;
+
+protected:
+	virtual   void  _Put(int w);
+	virtual   void  _Put(const void *data, dword size);
+	
+	Stream      *out;
+
+	StringBuffer buffer; // to be able to pass it as String
+	
+	enum { BLOCK_BYTES = 1024 * 1024 };
+	
+	xxHashStream xxh;
+	int          maxblock;
+	int          blockchksumsz;
+	bool         header;
+	byte         lz4hdr;
+	String       header_data;
+
+    String       sout;
+	
+	bool              parallel;
+	CoWork            co;
+	Mutex             lock;
+	ConditionVariable cond;
+	int               outblock;
+	int               inblock;
+    
+	void          Init();
+	void          SetupBuffer();
+	void          FinishBlock(char *outbuf, int clen, const char *origdata, int origsize);
+	void          FlushOut();
+
+	void          PutOut(const void *ptr, int size);
+
+public:
+	Event<int64>                 WhenPos;
+
+	void Clear();
+	
+	const String& Get() const              { return sout; }
+	operator const String&() const         { return sout; }
+	const String& operator~() const        { return sout; }
+	void   ClearOut()                      { sout.Clear(); }
+
+	void Parallel(bool b = true)           { parallel = b; }
+	void Out(Stream& o)                    { out = &o; }
+
+	LZ4CompressStream();
+	~LZ4CompressStream();
 };
 
 int64  LZ4Compress(Stream& out, Stream& in, Gate2<int64, int64> progress = false);
