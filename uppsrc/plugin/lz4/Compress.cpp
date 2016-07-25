093f973e53b6cae816c0e93ea5bdefd5e7bea245
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-07-25 11:20:00 +0000

    .developing LZ4
    git-svn-id: svn://ultimatepp.org/upp/trunk@10097 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/plugin/lz4/Compress.cpp
+++ uppsrc/plugin/lz4/Compress.cpp
@@ -1,119 +1,112 @@
 #include "lz4.h"
 
-#ifdef flagCOMPLEXLZ4
+#ifndef flagCOMPLEXLZ4
 
 #define LLOG(x) // LOG(x)
 
 namespace Upp {
 
-void LZ4CompressStream::Init()
+void LZ4CompressStream::Open(Stream& out_)
 {
+	out = &out_;
 	ClearError();
 	pos = 0;
-	header = false;
 	xxh.Reset();
-#ifdef _MULTITHREADED
-	outblock = inblock = 0;
-#endif
-	SetupBuffer();
+	Alloc();
+	pos = 0;
+	byte h[7];
+	Poke32le(h, LZ4F_MAGIC);
+	h[4] = LZ4F_VERSION | LZ4F_BLOCKINDEPENDENCE | LZ4F_CONTENTCHECKSUM;
+	h[5] = LZ4F_MAXSIZE_1024KB;
+	h[6] = xxHash(h + 4, 2) >> 8;
+	out->Put(h, 7);
 }
 
-void LZ4CompressStream::FinishBlock(char *outbuf, int clen, const char *origdata, int origsize)
+void LZ4CompressStream::Alloc()
 {
-	if(IsError() || out && out->IsError())
-		return;
-	if(clen < 0) {
-		SetError();
-		return;
-	}
-	if(clen >= origsize) {
-		Poke32le(outbuf, 0x80000000 | origsize);
-		memcpy(outbuf + 4, origdata, origsize);
-		out->Put(outbuf, origsize + 4);
-	}
-	else {
-		Poke32le(outbuf, clen);
-		out->Put(outbuf, clen + 4);
-	}
+	int N = 16;
+	int sz = concurrent ? N * BLOCK_BYTES : BLOCK_BYTES;
+	buffer.Alloc(sz);
+	outbuf.Alloc(N * LZ4_compressBound(BLOCK_BYTES));
+	outsz.Alloc(N);
+	wrlim = ~buffer + sz;
+	ptr = ~buffer;
 }
 
-void LZ4CompressStream::SetupBuffer()
+void LZ4CompressStream::Concurrent(bool b)
 {
-	buffer.SetCount(BLOCK_BYTES);
-	wrlim = (byte *)~buffer + BLOCK_BYTES;
-	ptr =  (byte *)~buffer;
+	FlushOut();
+	concurrent = b;
+	Alloc();
 }
 
 void LZ4CompressStream::FlushOut()
 {
-	if(!header) {
-		byte h[7];
-		Poke32le(h, LZ4F_MAGIC);
-		h[4] = LZ4F_VERSION | LZ4F_BLOCKINDEPENDENCE | LZ4F_CONTENTCHECKSUM;
-		h[5] = LZ4F_MAXSIZE_1024KB;
-		h[6] = xxHash(h + 4, 2) >> 8;
-		out->Put(h, 7);
-		header = true;
-	}
-	
 	if(ptr == (byte *)~buffer)
 		return;
-
-	int origsize = int((char *)ptr - ~buffer);
 	
+	CoWork co;
+	
+	int osz = LZ4_compressBound(BLOCK_BYTES);
+	byte *t = ~outbuf;
+	int   ii = 0;
+	for(byte *s = ~buffer; s < ptr; s += BLOCK_BYTES) {
+		int origsize = min((int)BLOCK_BYTES, int(ptr - s));
+		if(concurrent)
+			co & [=] {
+				outsz[ii] = LZ4_compress_default((char *)s, (char *)t, origsize, osz);
+			};
+		else
+			outsz[ii] = LZ4_compress_default((char *)s, (char *)t, origsize, osz);
+		ii++;
+		t += osz;
+	}
+	
+	if(concurrent)
+		co.Finish();
+	
+	byte *s = ~buffer;
+	t = ~outbuf;
+	for(int i = 0; i < ii; i++) {
+		int origsize = min((int)BLOCK_BYTES, int(ptr - s));
+		int clen = outsz[i];
+		if(clen < 0) {
+			SetError();
+			return;
+		}
+		if(clen >= origsize || clen == 0) {
+			out->Put32le(0x80000000 | origsize);
+			out->Put(s, origsize);
+		}
+		else {
+			out->Put32le(clen);
+			out->Put(t, clen);
+		}
+		s += BLOCK_BYTES;
+		t += osz;
+	}
+	
+	int origsize = int(ptr - ~buffer);
+	xxh.Put(~buffer, origsize);
 	pos += origsize;
-	
 	WhenPos(pos);
-
-#ifdef _MULTITHREADED
-	if(co) {
-		String bs = buffer;
-		int    inblk = inblock++;
-		CoWork::Start([=] {
-			Buffer<char> outbuf(4 + LZ4_compressBound(BLOCK_BYTES));
-			int clen = LZ4_compress(~bs, ~outbuf + 4, origsize);
-			Mutex::Lock __(lock);
-			while(outblock != inblk)
-				cond.Wait(lock);
-			FinishBlock(outbuf, clen, ~bs, origsize);
-			outblock++;
-			cond.Broadcast();
-		});
-		xxh.Put(~bs, origsize);
-		SetupBuffer();
-	}
-	else
-#endif
-	{
-		Buffer<char> outbuf(4 + LZ4_compressBound(BLOCK_BYTES));
-		xxh.Put(~buffer, origsize);
-		int clen = LZ4_compress(~buffer, ~outbuf + 4, origsize);
-		FinishBlock(outbuf, clen, buffer, origsize);
-		SetupBuffer();
-	}
+	ptr = ~buffer;
 }
 
 void LZ4CompressStream::Close()
 {
 	ASSERT(compress >= 0);
-	FlushOut();
-#ifdef _MULTITHREADED
-	if(co) {
-		Mutex::Lock __(lock);
-		while(outblock != inblock)
-			cond.Wait(lock);
+	if(out) {
+		FlushOut();
+		out->Put32le(0);
+		out->Put32le(xxh.Finish());
+		out = NULL;
 	}
-#endif
-	byte h[8];
-	Poke32le(h, 0);
-	Poke32le(h + 4, xxh.Finish());
-	out->Put(h, 8);
-	buffer.Clear();
 }
 
 bool LZ4CompressStream::IsOpen() const
 {
-	return (!out || out->IsOpen()) && buffer.GetCount();
+	return out && out->IsOpen();
 }
 
 void LZ4CompressStream::_Put(int w)
@@ -133,7 +126,7 @@ void LZ4CompressStream::_Put(const void *data, dword size)
 	while(size > 0) {
 		if(IsError() || out && out->IsError())
 			return;
-		dword n = BLOCK_BYTES - int((char *)ptr - ~buffer);
+		dword n = dword(wrlim - ptr);
 		if(size >= n) {
 			memcpy(ptr, s, n);
 			ptr = wrlim;
@@ -152,14 +145,14 @@ void LZ4CompressStream::_Put(const void *data, dword size)
 LZ4CompressStream::LZ4CompressStream()
 {
 #ifdef _MULTITHREADED
-	co = false;
+	concurrent = false;
 #endif
 	out = NULL;
-	Init();
 }
 
 LZ4CompressStream::~LZ4CompressStream()
 {
+	Close();
 }
 
 };
