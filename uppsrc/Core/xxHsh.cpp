d19dd269018c0e47e2ec6b9d894bbed7933c49ab
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-07-30 12:38:24 +0000

    Core: xxHash64Stream
    git-svn-id: svn://ultimatepp.org/upp/trunk@10114 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/xxHsh.cpp
+++ uppsrc/Core/xxHsh.cpp
@@ -38,4 +38,38 @@ int xxHash(const String& s)
 	return xxHash(~s, s.GetCount());
 }
 
+xxHash64Stream::xxHash64Stream(dword seed)
+{
+	STATIC_ASSERT(sizeof(context) >= sizeof(XXH64_state_t));
+	Reset(seed);
+}
+
+void xxHash64Stream::Reset(dword seed)
+{
+	XXH64_reset((XXH64_state_t *)context, seed);
+}
+
+void xxHash64Stream::Out(const void *data, dword size)
+{
+	XXH64_update((XXH64_state_t *)context, data, size);
+}
+
+int64 xxHash64Stream::Finish()
+{
+	Flush();
+	return XXH64_digest((XXH64_state_t *)context);
+}
+
+int64 xxHash64(const void *data, size_t len)
+{
+	xxHash64Stream h;
+	h.Put64(data, len);
+	return h.Finish();
+}
+
+int64 xxHash64(const String& s)
+{
+	return xxHash64(~s, s.GetCount());
+}
+
 };
