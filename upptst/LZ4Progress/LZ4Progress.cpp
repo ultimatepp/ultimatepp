b54282bdb667598a9423d3c8900764c03fe39c6b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-07-30 06:30:01 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@10112 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/LZ4Progress/LZ4Progress.cpp
@@ -0,0 +1,42 @@
+#include <Core/Core.h>
+#include <plugin/lz4/lz4.h>
+#include <plugin/zstd/zstd.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	String data = LoadFile("C:/xxx/xxx/upp-x11-src-9041.tar");
+	
+	auto cp = [](int64 pos, int64 size) -> bool { DLOG("Compress: " << pos << " " << size); return false; };
+	auto dp = [](int64 pos, int64 size) -> bool { DLOG("DeCompress: " << pos << " " << size); return false; };
+
+	{
+		String c = ZstdCompress(data, cp);
+		DDUMP(c.GetCount());
+		String d = ZstdDecompress(c, dp);
+		ASSERT(data == d);
+	}
+
+	{
+		String c = CoZstdCompress(data, cp);
+		DDUMP(c.GetCount());
+		String d = CoZstdDecompress(c, dp);
+		ASSERT(data == d);
+	}
+	
+	{
+		String c = LZ4Compress(data, cp);
+		DDUMP(c.GetCount());
+		String d = LZ4Decompress(c, dp);
+		ASSERT(data == d);
+	}
+
+	{
+		String c = CoLZ4Compress(data, cp);
+		DDUMP(c.GetCount());
+		String d = CoLZ4Decompress(c, dp);
+		ASSERT(data == d);
+	}
+}
