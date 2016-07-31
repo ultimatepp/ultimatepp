19571f34b18f3f055d343c5b33921246cda81075
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-07-31 06:51:05 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@10118 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/Compression/Compression.cpp
@@ -0,0 +1,50 @@
+#include <Core/Core.h>
+#include <plugin/lz4/lz4.h>
+#include <plugin/zstd/zstd.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_FILE|LOG_COUT);
+
+	String cpath = GetHomeDirFile("test");
+	
+	{
+		FileOut out(cpath);
+		ZCompressStream z(out);
+		z.Put("Compressed by zlib");
+	}
+	
+	{
+		FileIn in(cpath);
+		ZDecompressStream z(in);
+		DLOG(z.GetLine());
+	}
+
+	{
+		FileOut out(cpath);
+		LZ4CompressStream z(out);
+		z.Put("Compressed by LZ4");
+	}
+	
+	{
+		FileIn in(cpath);
+		LZ4DecompressStream z(in);
+		DLOG(z.GetLine());
+	}
+
+	{
+		FileOut out(cpath);
+		ZstdCompressStream z(out);
+		z.Put("Compressed by LZ4");
+	}
+	
+	{
+		FileIn in(cpath);
+		ZstdDecompressStream z(in);
+		DLOG(z.GetLine());
+	}
+	
+	FileDelete(cpath);
+}
