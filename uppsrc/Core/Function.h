4740fbafb456f63e47a560c9933eb0a7d3ab83ab
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-10-04 08:30:32 +0000

    Merge continued
    git-svn-id: svn://ultimatepp.org/upp/trunk@10261 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/Core/Function.h
@@ -0,0 +1,91 @@
+enum CNULLer { CNULL };
+
+template<typename Res, typename... ArgTypes>
+class Function<Res(ArgTypes...)> : Moveable<Function<Res(ArgTypes...)>> {
+	struct WrapperBase {
+		Atomic  refcount;
+
+		virtual Res Execute(ArgTypes... args) = 0;
+		
+		WrapperBase() { refcount = 1; }
+		virtual ~WrapperBase() {}
+	};
+
+	template <class F>
+	struct Wrapper : WrapperBase {
+		F fn;
+		virtual Res Execute(ArgTypes... args) { return fn(args...); }
+
+		Wrapper(F&& fn) : fn(pick(fn)) {}
+	};
+
+	template <class F>
+	struct Wrapper2 : WrapperBase {
+		Function l;
+		F        fn;
+
+		virtual Res Execute(ArgTypes... args) { l(args...); return fn(args...); }
+
+		Wrapper2(const Function& l, F&& fn) : l(l), fn(pick(fn)) {}
+		Wrapper2(const Function& l, const F& fn) : l(l), fn(fn) {}
+	};
+
+	WrapperBase *ptr;
+	
+	static void Free(WrapperBase *ptr) {
+		if(ptr && AtomicDec(ptr->refcount) == 0)
+			delete ptr;
+	}
+	
+	void Copy(const Function& a) {
+		ptr = a.ptr;
+		if(ptr)
+			AtomicInc(ptr->refcount);
+	}
+	
+	void Pick(Function&& src) {
+		ptr = src.ptr;
+		src.ptr = NULL;
+	}
+
+public:
+	Function()                                 { ptr = NULL; }
+	Function(CNULLer)                          { ptr = NULL; }
+	Function(const Nuller&)                    { ptr = NULL; }
+	
+	template <class F> Function(F fn)          { ptr = new Wrapper<F>(pick(fn)); }
+	
+	Function(const Function& src)              { Copy(src); }
+	Function& operator=(const Function& src)   { auto b = ptr; Copy(src); Free(b); return *this; }
+
+	Function(Function&& src)                   { Pick(pick(src)); }
+	Function& operator=(Function&& src)        { if(&src != this) { Free(ptr); ptr = src.ptr; src.ptr = NULL; } return *this; }
+	
+	Function Proxy() const                     { return [=] (ArgTypes... args) { return (*this)(args...); }; }
+
+	template <class F>
+	Function& operator<<(F fn)                 { if(!ptr) { Copy(fn); return *this; }
+	                                             WrapperBase *b = ptr; ptr = new Wrapper2<F>(*this, fn); Free(b); return *this; }
+
+	Function& operator<<(const Function& fn)   { if(!ptr) { Copy(fn); return *this; }
+	                                             WrapperBase *b = ptr; ptr = new Wrapper2<Function>(*this, fn); Free(b); return *this; }
+
+	Function& operator<<(Function&& fn)        { if(!ptr) { Pick(pick(fn)); return *this; }
+	                                             WrapperBase *b = ptr; ptr = new Wrapper2<Function>(*this, pick(fn)); Free(b); return *this; }
+
+	Res operator()(ArgTypes... args) const     { return ptr ? ptr->Execute(args...) : Res(); }
+	
+	operator bool() const                      { return ptr; }
+	void Clear()                               { Free(ptr); ptr = NULL; }
+
+	~Function()                                { Free(ptr); }
+
+	friend Function Proxy(const Function& a)   { return a.Proxy(); }
+	friend void Swap(Function& a, Function& b) { UPP::Swap(a.ptr, b.ptr); }
+};
+
+template <typename... ArgTypes>
+using Event = Function<void (ArgTypes...)>;
+
+template <typename... ArgTypes>
+using Gate = Function<bool (ArgTypes...)>;
