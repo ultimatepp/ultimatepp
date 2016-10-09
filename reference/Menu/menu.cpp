665f76015b5938cb2cc0f44b3df44f02d3923d9c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-10-09 17:43:40 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@10283 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/Menu/menu.cpp
+++ reference/Menu/menu.cpp
@@ -5,29 +5,21 @@ using namespace Upp;
 struct App : public TopWindow {
 	bool numbers_enabled;
 
-	void Exit()
-	{
-		Close();
-	}
-
-	void SubMenu(Bar& bar)
-	{
-		for(int i = 0; i < 10; i++)
-			bar.Add(~AsString(i), [=] { PromptOK(AsString(i)); });
-	}
-
 	void Menu(Bar& bar)
 	{
 		bar.Add("Enable numbers", [=] { numbers_enabled = !numbers_enabled; })
 		   .Check(numbers_enabled);
-		bar.Add(numbers_enabled, "Numbers", THISBACK(SubMenu));
-		bar.Add("Exit", [=] { Exit(); })
+		bar.Sub(numbers_enabled, "Numbers", [=](Bar& bar) {
+			for(int i = 0; i < 10; i++)
+				bar.Add(~AsString(i), [=] { PromptOK(AsString(i)); });
+		});
+		bar.Add("Exit", [=] { Close(); })
 		   .Key(K_CTRL_E);
 	}
 
 	void MainBar(Bar& bar)
 	{
-		bar.Add("Numbers", THISBACK(Menu));
+		bar.Sub("Numbers", THISFN(Menu));
 		bar.Sub("Items", [=](Bar& bar) {
 			bar.Add("Item 1", [&] { Exclamation("Item 1 invoked"); });
 			bar.Add("Item 2", [&] { Exclamation("Item 2 invoked"); });
@@ -42,7 +34,7 @@ struct App : public TopWindow {
 	{
 		numbers_enabled = false;
 		AddFrame(menu);
-		menu.Set(THISBACK(MainBar));
+		menu.Set(THISFN(MainBar));
 	}
 };
 
