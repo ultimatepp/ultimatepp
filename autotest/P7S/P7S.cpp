03b647b007826c61e3a2db489a40decda4915413
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-10-21 12:23:04 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@10342 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/P7S/P7S.cpp
@@ -0,0 +1,45 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	String dir = GetHomeDirFile("autotest.p7s");
+
+	DeleteFolderDeep(dir);
+	DirectoryCreate(dir);
+	
+	String content = AppendFileName(dir, "content.txt");
+	SaveFile(content, "Hello world!");
+	
+	DUMP(SHA1String(content));
+	
+	String signature = AppendFileName(dir, "signature.der");
+	
+	String cert = LoadDataFile("public.pem");
+	String pkey = LoadDataFile("private.pem");
+	
+	SaveFile(AppendFileName(dir, "public.pem"), cert);
+	SaveFile(AppendFileName(dir, "private.pem"), pkey);
+
+	String sgn = GetP7Signature(LoadFile(content), cert, pkey);
+	ASSERT(sgn.GetCount());
+	SaveFile(signature, sgn);
+	
+	const char *cmd =
+	"openssl smime -verify -CAfile ~/autotest.p7s/public.pem "
+	"-content ~/autotest.p7s/content.txt "
+	"-inform der -in ~/autotest.p7s/signature.der";
+	
+	LOG(cmd);
+	
+	int code = system(cmd);
+	
+	DUMP(code);
+	
+	ASSERT(code == 0);
+	
+	DeleteFolderDeep(dir);
+	
+	LOG("===================== OK");
+}
