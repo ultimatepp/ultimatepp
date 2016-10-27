bb29ee70e3291557ae69d14fd970b1804184b4b1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-10-27 05:20:31 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@10361 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/SignPDF/SignPDF.cpp
@@ -0,0 +1,27 @@
+#include <Core/SSL/SSL.h>
+#include <PdfDraw/PdfDraw.h>
+#include <RichText/RichText.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	PdfDraw pdf;
+	
+	PdfSignatureInfo f;
+
+	f.cert = LoadDataFile("certificate.pem");
+	f.pkey = LoadDataFile("privatekey.pem");
+
+	f.name = "Mahrai Ziller";
+	f.location = "Masaq";
+	f.reason = "I'd like to sign this pdf";
+	f.contact_info = "Email me!";
+
+	pdf.DrawText(200, 200, "Hello world!", Serif(200), Magenta());
+	SaveFile(GetHomeDirFile("invislble_signature.pdf"), pdf.Finish(&f));
+	
+	SaveFile(GetHomeDirFile("signature_field.pdf"),
+	         Pdf(ParseQTF("[9* Hello world!&&&&&&&&[^<<signature>>^ Mahrai Ziller]"),
+	         Size(3968, 6074), 200, false, &f));
+}
