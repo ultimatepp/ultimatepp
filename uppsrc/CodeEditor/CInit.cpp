7bcb50670e49e744ace3e8c993b7a7457ebbaa19
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-11-14 08:38:36 +0000

    ide: more file types in create file, INITIALIZER/INITIALIZE highlighting
    git-svn-id: svn://ultimatepp.org/upp/trunk@10432 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CodeEditor/CInit.cpp
+++ uppsrc/CodeEditor/CInit.cpp
@@ -78,7 +78,7 @@ void CSyntax::InitKeywords()
 		"byte", "word", "dword", "__countof", "pick_", "wchar", "NULL", "Null",
 		"int8", "uint8", "int16", "uint16", "int32", "uint32", "int64", "uint64", "qword",
 		"INTERLOCKED_", "INTERLOCKED", "ONCELOCK", "ONCELOCK_", "INITBLOCK", "EXITBLOCK",
-		"rval_", "rval_default", "size_t", "intptr_t", "uintptr_t",
+		"rval_", "rval_default", "size_t", "intptr_t", "uintptr_t", "INITIALIZER", "INITIALIZE",
 		NULL
 	};
 	static const char *usc[] = {
