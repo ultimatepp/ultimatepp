8d5b3e9f95ee8e20cdf2fc5cc09346d352a801f1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-11-15 10:21:40 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@10441 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/CoFindAll/CoFindAll.cpp
@@ -0,0 +1,32 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	SeedRandom();
+	
+	for(int ii = 0; ii < 200; ii++) {
+		DUMP(ii);
+		int N = Random(20000000);
+		int mod = 6 * Random(50000) + 1;
+		int from = Random(N);
+		DUMP(N);
+		DUMP(mod);
+		DUMP(from);
+		Vector<int> data;
+		for(int i = 0; i < N; i++)
+			data.Add(Random());
+		
+		auto f = FindAll(data, [=](int c) { return c % mod == 0; }, from);
+		auto cf = CoFindAll(data, [=](int c) { return c % mod == 0; }, from);
+		DUMP(f.GetCount());
+		DUMP(cf.GetCount());
+		ASSERT(f == cf);
+		LOG("================");
+	}
+	
+	LOG("===================== OK");
+}
