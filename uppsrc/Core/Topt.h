4e9a7b5256f64b87ea2921982e9a395edeed3f4d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-11-19 22:45:54 +0000

    Core: .docs
    git-svn-id: svn://ultimatepp.org/upp/trunk@10466 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Topt.h
+++ uppsrc/Core/Topt.h
@@ -348,16 +348,7 @@ public:
 
 
 template <class Range>
-using ValueTypeOf = typename std::remove_reference<decltype(*((Range *)0)->begin())>::type;
-
-template <class Range>
 using ValueTypeOfArray = typename std::remove_reference<decltype((*((Range *)0))[0])>::type;
-
-template <class Range>
-using IteratorOf = decltype(((Range *)0)->begin());
-
-template <class Range>
-using ConstIteratorOf = decltype(((const Range *)0)->begin());
 
 template <class V>
 class ConstIIterator {
