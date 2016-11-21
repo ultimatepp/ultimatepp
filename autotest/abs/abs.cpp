c4d5d0b86e867a0a4406e3ef6aaf29335709ff89
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-11-21 19:23:28 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@10475 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/abs/abs.cpp
+++ autotest/abs/abs.cpp
@@ -2,13 +2,28 @@
 
 using namespace Upp;
 
+bool FltMatch(double x, double y)
+{
+	return abs(x - y) < min(abs(x), abs(y)) / 1e5;
+}
+
+bool DblMatch(double x, double y)
+{
+	return abs(x - y) < min(abs(x), abs(y)) / 1e10;
+}
+
 CONSOLE_APP_MAIN
 {
-	ASSERT(abs(-1) == 1);
-	ASSERT(abs((int16)-1) == 1);
-	ASSERT(abs((int64)-1) == 1);
-	ASSERT(abs((int8)-1) == 1);
-	ASSERT(abs((float)-1) == 1);
-	ASSERT(abs((double)-1) == 1);
-	LOG("OK");
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	ASSERT(abs(-0x40000000) == 0x40000000);
+	ASSERT(abs((int16)-0x4000) == 0x4000);
+	ASSERT(abs((int64)-0x400000000000) == 0x400000000000);
+	ASSERT(abs((int8)-0x40) == 0x40);
+	ASSERT(FltMatch(abs((float)-1.1), 1.1));
+	ASSERT(DblMatch(abs((double)-1.1), 1.1));
+	
+	ASSERT(abs(Complex(3, -4)) == 5);
+
+	LOG("=============== OK");
 }
