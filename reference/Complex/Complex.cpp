50dcf06e7c0617ace1901368466e5ddd605d6752
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2016-11-22 09:10:55 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@10478 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/Complex/Complex.cpp
+++ reference/Complex/Complex.cpp
@@ -2,15 +2,20 @@
 
 using namespace Upp;
 
-CONSOLE_APP_MAIN{
-	Complex a(1,2);
-	Cout() << "a = " << a << "\n";
-	Cout() << "Im(a) = " << a.imag() << "\n";
-	Cout() << "Re(a) = " << a.real() << "\n";
-	Cout() << "|a| = " << abs(a) << "\n";
-	Cout() << "a+a = " << a+a << "\n";
-	Cout() << "2a = " << 2.0*a << "\n";
-	Cout() << "a^2 = " << a*a << "\n";
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	Complex a(3, 4);
+	
+	DUMP(a);
+	DUMP(a.imag());
+	DUMP(a.real());
+	DUMP(abs(a));
+	DUMP(a + a);
+	DUMP(2.0 + a);
+	DUMP(2.0 * a);
+	DUMP(a * a);
 }
 
 
