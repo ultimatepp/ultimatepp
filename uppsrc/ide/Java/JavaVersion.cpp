953865418ed161659a40de0c705230b74fcdf244
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-01-01 19:00:32 +0000

    Commit missing JavaVersion.cpp file.
    git-svn-id: svn://ultimatepp.org/upp/trunk@10618 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/ide/Java/JavaVersion.cpp
@@ -0,0 +1,32 @@
+#include "Java.h"
+
+namespace Upp {
+
+JavaVersion::JavaVersion()
+	: mMajor(0)
+	, mMinor(0)
+{
+}
+
+JavaVersion::JavaVersion(int major, int minor)
+	: mMajor(major)
+	, mMinor(minor)
+{
+}
+
+bool JavaVersion::IsGreate(int major) const
+{
+	return major > mMajor;
+}
+
+bool JavaVersion::IsGreaterOrEqual(int major) const
+{
+	return major >= mMajor;
+}
+
+bool JavaVersion::IsGreaterOrEqual(int major, int minor) const
+{
+	return IsGreate(major) || (IsGreaterOrEqual(major) && minor >= mMinor);
+}
+
+}
