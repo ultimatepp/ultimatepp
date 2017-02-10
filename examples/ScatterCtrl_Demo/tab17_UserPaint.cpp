e6eda5bc7217e0f0801074105300011ece787500
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-02-10 22:02:26 +0000

    ScatterCtrl: Added OnPaint
    git-svn-id: svn://ultimatepp.org/upp/trunk@10837 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ examples/ScatterCtrl_Demo/tab17_UserPaint.cpp
@@ -0,0 +1,30 @@
+#include "ScatterCtrl_Demo.h"
+
+void Tab_UserPaint::Init()
+{
+	CtrlLayout(*this);	
+	SizePos();
+	
+	double a = 1, b = 0, c = 1;
+	for (double x = -6; x < 6; x += 0.1)
+		s1 << Pointf(x, a*exp(-sqr(x - b)/(2*c*c))); 
+	scatter.AddSeries(s1).Legend("Gauss").Opacity(0.3).Fill().NoMark();
+	
+	scatter.ShowInfo().ShowContextMenu().ShowPropertiesDlg().ShowProcessDlg().
+			SetMouseHandling(true, true);
+	scatter.SetLegendPos(Point(20, 20)).SetLegendAnchor(ScatterDraw::LEGEND_ANCHOR_RIGHT_BOTTOM);
+	scatter.ZoomToFit(true, true);
+	scatter.WhenPainter = THISBACK(OnPainter);
+	scatter.WhenDraw = THISBACK(OnDraw);
+}
+	
+
+ScatterDemo *Construct17()
+{
+	static Tab_UserPaint tab;
+	return &tab;
+}
+
+INITBLOCK {
+	RegisterExample("User paint", Construct17, __FILE__);
+}
