ef59417bca1e49e98946d3403cfd49abcf9781d4
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-02-18 23:26:57 +0000

    .bazaar Added Google Mock simply example
    git-svn-id: svn://ultimatepp.org/upp/trunk@10859 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ bazaar/GoogleMockExample/GoogleMockExample.cpp
@@ -0,0 +1,39 @@
+#include <Core/Core.h>
+#include <plugin/gmock/gmock.h>
+
+using namespace Upp;
+
+class Car {
+public:
+	virtual ~Car() {}
+	
+	virtual void OpenHood() = 0;
+};
+
+class MockCar : public Car {
+public:
+	MOCK_METHOD0(OpenHood, void());
+};
+
+class CarRepairShop final {
+public:
+	bool Service(Car& car) {
+		car.OpenHood();
+	}
+};
+
+TEST(CarRepairShopTest, ServiceChecksAllMainCarElements) {
+	MockCar car;
+	
+	EXPECT_CALL(car, OpenHood())
+		.Times(::testing::AtLeast(1));
+	
+	CarRepairShop().Service(car);
+}
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	
+	return RUN_ALL_TESTS();
+}
