48f63bce648714ea8340ab3081379112c4828231
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-02-21 10:32:51 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@10865 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ reference/One/One.cpp
@@ -0,0 +1,30 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+void DumpFile(One<Stream>& s)
+{
+	LOG("=========");
+	int ii = 0;
+	while(!s->IsEof())
+		LOG(++ii << ": " << s->GetLine());
+}
+
+CONSOLE_APP_MAIN
+{
+	String fn = GetDataFile("One.cpp");
+	{
+		One<Stream> s;
+		s.Create<FileIn>(fn);
+		DumpFile(s);
+	}
+	{
+		MakeOne<FileIn> in(fn);
+		One<Stream> s = pick(in);
+		DumpFile(s);
+	}
+	{
+		One<Stream> s = MakeOne<FileIn>(fn);
+		DumpFile(s);
+	}
+}
