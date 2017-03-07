7b7a8e40d31c1524262ce23cd40c7a8a63cbcd4b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-03-07 10:35:30 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@10910 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/RichRawSvoValue/RichRawSvoValue.cpp
@@ -0,0 +1,51 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+template <class T>
+void Test(T c)
+{
+	T h = c;
+
+	{
+		Value v = h;
+		ASSERT(~v == AsString(c));
+		ASSERT(v.Is<T>());
+		ASSERT(v.To<T>() == h);
+		T h1 = v;
+		ASSERT(h == h1);
+	}
+	{
+		Value v = RawToValue(h);
+		ASSERT(v.Is<T>());
+		ASSERT(v.To<T>() == h);
+		T h1 = v.To<T>();
+		ASSERT(h == h1);
+	}
+	{ // this is not supported by Value definition (RichToValue should only be used in client type)
+		Value v = RichToValue(h);
+		ASSERT(~v == AsString(c));
+		ASSERT(v.Is<T>());
+		T h1 = v.To<T>();
+		ASSERT(h == h1);
+	}
+}
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	{
+		struct Foo { int x; } x;
+		x.x = 123;
+		Value v = RawToValue(x);
+		ASSERT(v.Is<Foo>());
+		ASSERT(v.To<Foo>().x == 123);
+	}
+
+	Test<int>(1234);
+	Test<String>("1234");
+	Test<WString>("1234");
+
+	LOG("=== Everything OK");
+}
