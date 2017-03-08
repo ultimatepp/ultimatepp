93714d9605f8dd9ff227de49006cfdffe7d59c0b
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-03-08 22:07:28 +0000

    .bazaar .google_test added google mock example,
    git-svn-id: svn://ultimatepp.org/upp/trunk@10926 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/GoogleMockExample/GoogleMockExample.cpp
+++ bazaar/GoogleMockExample/GoogleMockExample.cpp
@@ -1,39 +1,54 @@
-#include <Core/Core.h>
 #include <plugin/gmock/gmock.h>
+#include <plugin/gtest/gtest.h>
+
+#include <Core/Core.h>
 
 using namespace Upp;
+
+const String CAR_REGISTRATION_NUMBER = "UPP IS THE BEST";
 
 class Car {
 public:
 	virtual ~Car() {}
 	
 	virtual void OpenHood() = 0;
+	virtual String ReadRegistrationNumbers() = 0;
 };
 
 class MockCar : public Car {
 public:
 	MOCK_METHOD0(OpenHood, void());
+	MOCK_METHOD0(ReadRegistrationNumbers, String());
 };
 
 class CarRepairShop final {
 public:
-	void Service(Car& car) {
+	CarRepairShop()
+		: carsWaitingForService({ CAR_REGISTRATION_NUMBER })
+	{}
+
+	bool Service(Car& car) {
+		if (carsWaitingForService.Find(car.ReadRegistrationNumbers()) < 0) {
+			return false;
+		}
+		
 		car.OpenHood();
+		
+		return true;
 	}
+	
+private:
+	Index<String> carsWaitingForService;
 };
 
 TEST(CarRepairShopTest, ServiceChecksAllMainCarElements) {
 	MockCar car;
+	CarRepairShop repairShop;
 	
-	EXPECT_CALL(car, OpenHood())
-		.Times(::testing::AtLeast(1));
+	EXPECT_CALL(car, ReadRegistrationNumbers()).WillOnce(::testing::Return(String(CAR_REGISTRATION_NUMBER)));
+	EXPECT_CALL(car, OpenHood()).Times(::testing::AtLeast(1));
 	
-	CarRepairShop().Service(car);
+	EXPECT_TRUE(repairShop.Service(car));
 }
 
-int main(int argc, char *argv[])
-{
-	testing::InitGoogleTest(&argc, argv);
-	
-	return RUN_ALL_TESTS();
-}
+TEST_APP_MAIN {}
