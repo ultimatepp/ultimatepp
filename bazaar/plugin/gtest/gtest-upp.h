15bc24f4578f878c7999e0bc9c1655288d893b02
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-03-08 21:51:04 +0000

    .bazaar .google_test memory now is now checked on operating systems diffrent than Windows. Improve template for google test (google mock option).
    git-svn-id: svn://ultimatepp.org/upp/trunk@10925 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/plugin/gtest/gtest-upp.h
+++ bazaar/plugin/gtest/gtest-upp.h
@@ -1,20 +1,38 @@
 #ifndef GTEST_INCLUDE_GTEST_UPP_H
 #define GTEST_INCLUDE_GTEST_UPP_H
 
-#define TEST_APP_MAIN \
-namespace Upp { extern bool PanicMode; } \
-\
-void TestMainFn(int argc, char** argv); \
-\
-int main(int argc, char** argv) \
-{ \
-	Upp::PanicMode = true; \
-\
-	testing::InitGoogleTest(&argc, argv); \
-	TestMainFn(argc, argv); \
-	return RUN_ALL_TESTS(); \
-} \
-\
-void TestMainFn(int argc, char** argv)
+#if defined(PLATFORM_WIN32)
+
+	#define TEST_APP_MAIN \
+	namespace Upp { extern bool PanicMode; } \
+	\
+	void TestMainFn(int argc, char** argv); \
+	\
+	int main(int argc, char** argv) \
+	{ \
+		Upp::PanicMode = true; \
+	\
+		testing::InitGoogleTest(&argc, argv); \
+		TestMainFn(argc, argv); \
+		return RUN_ALL_TESTS(); \
+	} \
+	\
+	void TestMainFn(int argc, char** argv)
+	
+#else
+
+	#define TEST_APP_MAIN \
+	void TestMainFn(int argc, char** argv); \
+	\
+	int main(int argc, char** argv) \
+	{ \
+		testing::InitGoogleTest(&argc, argv); \
+		TestMainFn(argc, argv); \
+		return RUN_ALL_TESTS(); \
+	} \
+	\
+	void TestMainFn(int argc, char** argv)
+	
+#endif
 
 #endif
