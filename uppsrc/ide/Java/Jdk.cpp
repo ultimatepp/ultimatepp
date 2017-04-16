76197d7a0d52f0f9c9d3bc6d69798b619c484da1
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-04-16 16:28:58 +0000

    .ide Channge logger subclasses to more simple Logd, Logi, Logw, Loge.
    git-svn-id: svn://ultimatepp.org/upp/trunk@11009 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Java/Jdk.cpp
+++ uppsrc/ide/Java/Jdk.cpp
@@ -27,13 +27,13 @@ bool Jdk::Validate() const
 void Jdk::FindVersion(Host* host)
 {
 	if (!Validate()) {
-		LoggerDebug() << METHOD_NAME << "Path to JDK is wrong or files are corrupted.";
+		Logw() << METHOD_NAME << "Path to JDK is wrong or files are corrupted.";
 		return;
 	}
 	
 	StringStream ss;
 	if (host->Execute(GetJavacPath() + " -version", ss) != 0) {
-		LoggerDebug() << METHOD_NAME << "Cannot obtain version due to command execution failure.";
+		Logw() << METHOD_NAME << "Cannot obtain version due to command execution failure.";
 		return;
 	}
 	
@@ -41,25 +41,25 @@ void Jdk::FindVersion(Host* host)
 	output.Replace("\n", "");
 	Vector<String> splitedOutput = Split(output, " ");
 	if (splitedOutput.GetCount() != 2) {
-		LoggerDebug() << METHOD_NAME << "Splited output is too short (" + output + ").";
+		Logw() << METHOD_NAME << "Splited output is too short (" + output + ").";
 		return;
 	}
 	
 	Vector<String> splitedVersion = Split(splitedOutput[1], ".");
 	if (splitedVersion.GetCount() != 3) {
-		LoggerDebug() << METHOD_NAME << "Splited version is too short (" + output + ").";
+		Logw() << METHOD_NAME << "Splited version is too short (" + output + ").";
 		return;
 	}
 	
 	int major = StrInt(splitedVersion[0]);
 	if (major == INT_NULL) {
-		LoggerDebug() << METHOD_NAME << "Major version conversion to int failed (" + splitedVersion[0] + ").";
+		Logw() << METHOD_NAME << "Major version conversion to int failed (" + splitedVersion[0] + ").";
 		return;
 	}
 	
 	int minor = StrInt(splitedVersion[1]);
 	if (minor == INT_NULL) {
-		LoggerDebug() << METHOD_NAME << "Minor version conversion to int failed (" + splitedVersion[1] + ").";
+		Logw() << METHOD_NAME << "Minor version conversion to int failed (" + splitedVersion[1] + ").";
 		return;
 	}
 	
