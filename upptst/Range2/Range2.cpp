93dcca46afd7b024aea34bf350c31628045a19f4
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-06-04 18:22:08 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@11134 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/Range2/Range2.cpp
@@ -0,0 +1,27 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	DUMP(MakeIota(10));
+	DUMP(MakeIota(10, 1, 3));
+
+	// Testing r-value argument
+	Sort(SubRange(MakeIota(10), 1, 4));
+	Sort(ReverseRange(MakeIota(10)));
+	Sort(ViewRange(MakeIota(10), MakeIota(3)));
+	Sort(FilterRange(MakeIota(10), [=](int x) { return x > 4; }));
+	Sort(SortedRange(MakeIota(10)));
+
+	// Testing const argument
+	auto h = MakeIota(10);american go
+	const auto& x = h;
+
+	DUMP(SubRange(x, 1, 4));
+	DUMP(SortedRange(x));
+	DUMP(ReverseRange(x));
+	DUMP(ViewRange(x, MakeIota(3)));
+	DUMP(FilterRange(x, [=](int x) { return x > 4; }));
+	DUMP(SortedRange(x));
+}
