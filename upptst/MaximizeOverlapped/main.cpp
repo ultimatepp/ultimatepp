d5e0a964eee9655d73a44853ae55a6214391512a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-07-12 07:44:54 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@11224 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ upptst/MaximizeOverlapped/main.cpp
@@ -0,0 +1,20 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+GUI_APP_MAIN
+{
+	TopWindow w;
+	w.Sizeable().Zoomable();
+	w.Title("Maximize me and close me - it should be possible to 'unmaximize' this window after it reopens");
+
+	TopWindow main;
+	main.Open();
+	for(;;) {
+		if(!w.IsOpen())
+			w.OpenMain();
+		if(!main.IsOpen())
+			break;
+		Ctrl::ProcessEvents();
+	}
+}
