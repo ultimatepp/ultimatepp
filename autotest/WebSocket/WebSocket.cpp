abab2490a32d00bf519ccebda69761bfd4dcda26
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-09-17 20:21:17 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@11326 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/WebSocket/WebSocket.cpp
@@ -0,0 +1,40 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	for(int ssl = 0; ssl < 2; ssl++)
+		for(int blocking = 0; blocking < 2; blocking++) {
+			DDUMP(ssl);
+			DDUMP(blocking);
+			
+			WebSocket ws;
+
+			ws.Connect((ssl ? "wss" : "ws") + String("://echo.websocket.org"));
+		
+			ws.NonBlocking(!blocking);
+			
+			ws.SendText("Test");
+			
+			while(ws.IsOpen()) {
+				if(ws.IsError()) {
+					DDUMP(ws.GetError());
+					ASSERT(0);
+					break;
+				}
+				String s = ws.Receive();
+				if(s.GetCount()) {
+					DLOG(s);
+					ASSERT(s == "Test");
+					ws.Close();
+				}
+			}
+			
+			DLOG("... ok");
+		}
+	
+	DLOG("============= OK");
+}
