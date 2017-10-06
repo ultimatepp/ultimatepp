05987128b16a24757584b8280deb8db3fd82356d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-10-06 17:09:42 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@11348 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/MaximizeOverlapped/main.cpp
+++ upptst/MaximizeOverlapped/main.cpp
@@ -6,6 +6,7 @@ GUI_APP_MAIN
 {
 	TopWindow w;
 	w.Sizeable().Zoomable();
+	w.SetMinSize(Size(600, 600));
 	w.Title("Maximize me and close me - it should be possible to 'unmaximize' this window after it reopens");
 
 	TopWindow main;
