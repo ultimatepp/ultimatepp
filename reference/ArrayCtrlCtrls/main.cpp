96bb619d9832e4847154215d133f090cd5992c0d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-10-07 09:49:43 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@11351 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/ArrayCtrlCtrls/main.cpp
+++ reference/ArrayCtrlCtrls/main.cpp
@@ -4,14 +4,32 @@ using namespace Upp;
 
 GUI_APP_MAIN
 {
-	ArrayCtrl a;
-	a.AddColumn("Text").Ctrls<EditString>();
+	ArrayCtrl list;
+	list.AddColumn("Trivial").Ctrls<EditString>(); // specify type of widget as template argument
+	list.AddColumn("Factory").With( // class factory creates widget - allows customization of widget
+		[](One<Ctrl>& x) {
+			x.Create<Option>().NoWantFocus();
+		}
+	);
+	list.AddColumn("Lined").WithLined( // passes the row index as the first argument
+		[](int line, One<Ctrl>& x)
+		{
+			if(line & 1)
+				x.Create<EditString>();
+			else
+				x.Create<Option>().SetLabel("Line " + AsString(line));
+		}
+	);
+
 	for(int i = 0; i < 300; i++)
-		a.Add(AsString(i));
-	a.SetLineCy(Draw::GetStdFontCy() + 8);
+		list.Add(AsString(i), i & 1, AsString(i));
+	for(int i = 1; i < 300; i += 3)
+		list.GetCtrl(i, 0)->Disable();
+
+	list.SetLineCy(Draw::GetStdFontCy() + DPI(8));
 
 	TopWindow app;
-	app.Add(a.SizePos());
+	app.Add(list.SizePos());
 	app.Sizeable();
 	app.Run();
 }
