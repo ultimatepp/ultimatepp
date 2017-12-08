6d2d1e073955a20ba8330b39503195b24dc35546
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-12-08 18:28:36 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@11538 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/XmlCharset/XmlCharset.cpp
+++ autotest/XmlCharset/XmlCharset.cpp
@@ -4,7 +4,16 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
-	XmlNode xml = ParseXML(LoadFile(GetDataFile("1.xml")));
-	DDUMP(xml["test"]["tag"].GatherText());
-	DDUMP(xml["test"]["tag"].Attr(0));
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+	for(int i = 0; i < 2; i++) {
+		XmlNode xml = ParseXML(LoadFile(GetDataFile(AsString(i) + ".xml")));
+		DDUMP(TrimBoth(xml["test"]["tag"].GatherText()));
+		DDUMP(xml["test"]["tag"].Attr(0));
+
+		ASSERT(TrimBoth(xml["test"]["tag"].GatherText()) == "Úplně nevinný tag");
+		ASSERT(xml["test"]["tag"].Attr(0) == "Nějaký šikovný attribut");
+	}
+	
+	LOG("============== OK");
 }
