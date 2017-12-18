38dd3daafbc378d01ee180d3dc9c44f988f338f1
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-12-18 12:54:26 +0000

    ScatterCtrl_Demo: Some fixes and improvements
    git-svn-id: svn://ultimatepp.org/upp/trunk@11578 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/ScatterCtrl_Demo/tab1_Basic.cpp
+++ examples/ScatterCtrl_Demo/tab1_Basic.cpp
@@ -9,7 +9,7 @@ void Tab1_Basic::Init()
 	CtrlLayout(*this);	
 	SizePos();
 	
-	s1 << Pointf(10, 26) << Pointf(20, 37) << Pointf(30, 31) << Pointf(40, 33) << Pointf(50, 28);
+	s1 << Pointf(12, 26) << Pointf(20, 37) << Pointf(30, 31) << Pointf(40, 33) << Pointf(50, 28);
 	scatter.AddSeries(s1).Legend("Series 1").Opacity(0.3).Fill().MarkBorderColor();
 	
 	s2y[0] = 22; s2y[1] = 33; s2y[2] = 27; s2y[3] = 29; s2y[4] = 24;
@@ -17,7 +17,7 @@ void Tab1_Basic::Init()
 					.Dash("").MarkColor(Red()).Fill().Opacity(0.5).MarkBorderColor();
 	
 	s3y[0] = 18; s3y[1] = 29; s3y[2] = 23; s3y[3] = 25; s3y[4] = 20;
-	s3x[0] = 10; s3x[1] = 20; s3x[2] = 30; s3x[3] = 40; s3x[4] = 50;
+	s3x[0] = 8; s3x[1] = 20; s3x[2] = 30; s3x[3] = 40; s3x[4] = 50;
 	scatter.AddSeries(s3x, s3y, 5).Legend("Series 3").MarkBorderColor();
 	
 	s4y << 14 << 25 << 19 << 21 << 16;
@@ -30,7 +30,7 @@ void Tab1_Basic::Init()
 	s5.Add(40, 16);
 	s5.Add(50, 12);
 	int barWidth = 4;
-	scatter.AddSeries(s5).Legend("Series 5").PlotStyle<BarSeriesPlot>(barWidth).Dash("").NoMark().Fill();
+	scatter.AddSeries(s5).Legend("Series 5").PlotStyle<BarSeriesPlot>().BarWidth(barWidth).Dash("").NoMark().Fill();
 	
 	scatter.SetRange(60, 50).SetMajorUnits(10, 10);
 	scatter.ShowInfo().ShowContextMenu().ShowPropertiesDlg().ShowProcessDlg().SetPopText("h", "v", "v2").SetMouseHandling(true, true);
