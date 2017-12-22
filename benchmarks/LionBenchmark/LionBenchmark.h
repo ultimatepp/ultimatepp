ce74d1fc8ff18f83f6bd64e76fa0e52118f6056d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-12-22 08:17:36 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@11603 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ benchmarks/LionBenchmark/LionBenchmark.h
@@ -0,0 +1,11 @@
+#ifndef _LionBenchmark_LionBenchmark_h
+#define _LionBenchmark_LionBenchmark_h
+
+#include <CtrlLib/CtrlLib.h>
+#include <Painter/Painter.h>
+
+using namespace Upp;
+
+void PaintLion(Painter& sw);
+
+#endif
