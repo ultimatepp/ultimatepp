8fc6e52ba75dfd94ec47c9a06b9bf48b614da38a
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-12-23 00:22:21 +0000

    ScatterCtrl_Demo: Improvement in linked ScatterCtrls
    git-svn-id: svn://ultimatepp.org/upp/trunk@11605 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/ScatterCtrl_Demo/tab12_Linked.cpp
+++ examples/ScatterCtrl_Demo/tab12_Linked.cpp
@@ -14,7 +14,7 @@ void Tab12_Linked::Init()
 	scatter3.SetMouseHandling(true).SetMaxRange(5000).SetMinRange(2);
 	for (int t = 0; t < 3000; ++t) {
 		s1 << Pointf(t, 20*sin(6*M_PI*t/500) + 5*sin(3*M_PI*t/500 + M_PI/3) + 2*sin(14*M_PI*t/500 + M_PI/5));
-		s2 << Pointf(t, 20*sin(6*1.1*M_PI*t/500));
+		s2 << Pointf(t, 50*sin(6*1.1*M_PI*t/500));
 		s3 << Pointf(t, 20*sin(6*1.2*M_PI*t/500));
 	}
 	scatter1.AddSeries(s1).Legend("Series 1").NoMark().Stroke(2, LtRed());
@@ -23,7 +23,7 @@ void Tab12_Linked::Init()
 	scatter1.ShowInfo().ShowContextMenu().ShowPropertiesDlg().ShowProcessDlg();	
 	scatter2.ShowInfo().ShowContextMenu().ShowPropertiesDlg().ShowProcessDlg();	
 	scatter3.ShowInfo().ShowContextMenu().ShowPropertiesDlg().ShowProcessDlg();	
-	link = true;
+	link <<= true;
 	OnLink();
 	link.WhenAction = THISBACK(OnLink);
 }
