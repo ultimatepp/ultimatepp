41ec7d1ad9376e1798747ea08c1c7deadcba5511
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2017-12-31 17:02:35 +0000

    .ScatterCtrl_Demo: Included etacked plots
    git-svn-id: svn://ultimatepp.org/upp/trunk@11645 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/ScatterCtrl_Demo/tab20_Pie.cpp
+++ examples/ScatterCtrl_Demo/tab20_Pie.cpp
@@ -9,6 +9,4 @@ void TabPie::Init()
 	chart.AddCategory("Sunny",  8, ::Color(90, 150, 255));
 	chart.AddCategory("Cloudy", 4, ::Color(90, 255, 150));	
 	chart.AddCategory("Rainy",  2, ::Color(250, 150, 90));
-	
-	chart.SetColor(Null);
 }
