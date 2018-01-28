4e945630e0166df0b99fae2088a18010ed70978e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-01-28 09:44:32 +0000

    plugin/bz2: Streaming support
    git-svn-id: svn://ultimatepp.org/upp/trunk@11739 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/plugin/bz2/bz2upp.cpp
+++ uppsrc/plugin/bz2/bz2upp.cpp
@@ -1,22 +1,192 @@
 #include <Core/Core.h>
 #include <plugin/bz2/bz2.h>
 
-#ifdef flagWIN32
-	#include "lib/bzlib.h"
-#else
-	#include <bzlib.h>
-#endif
+#define LDUMP(x)  // DDUMP(x)
+#define LLOG(x)   // DLOG(x)
 
 namespace Upp {
-
-static void* bzalloc_new(void *opaque, int items, int size)
-{
-	return new byte[items * size];
-}
-
-static void bzfree_new(void *opaque, void *addr)
-{
-	delete[] (byte *)addr;
+namespace bz2 {
+	static void* bzalloc_new(void *opaque, int items, int size)
+	{
+		return new byte[items * size];
+	}
+	
+	static void bzfree_new(void *opaque, void *addr)
+	{
+		delete[] (byte *)addr;
+	}
+	
+	
+	void Lib::Begin()
+	{
+		Free();
+		SetError(false);
+		out.Clear();
+	}
+	
+	void Lib::Compress()
+	{
+		Begin();
+		if(BZ2_bzCompressInit(&z, compression_level, 0, 30) != BZ_OK)
+			Panic("BZ2_bzCompressInit failed");
+		mode = DEFLATE;
+	}
+	
+	void Lib::Decompress(bool all)
+	{
+		Begin();
+		rdall = all;
+		eos = false;
+		if(BZ2_bzDecompressInit(&z, 0, 0) != BZ_OK)
+			Panic("BZ2_bzDecompressInit failed");
+		mode = INFLATE;
+	}
+	
+	void Lib::Pump(bool finish)
+	{
+		if(error)
+			return;
+		ASSERT(mode);
+		if(!output)
+			output.Alloc(chunk);
+		if (mode == INFLATE) {
+			z.avail_out = chunk;
+			z.next_out = output;
+			while (z.avail_in) {
+				const int code = BZ2_bzDecompress(&z);
+				const int count = chunk - z.avail_out;
+				if(count) {
+					WhenOut((const char *)~output, count);
+					if (error) {
+						Free();
+						break;
+					}
+					total += count;
+					z.avail_out = chunk;
+					z.next_out = output;
+				}
+				if(code == BZ_STREAM_END) {
+					if (rdall) {
+						BZ2_bzDecompressEnd(&z);
+						if(BZ2_bzDecompressInit(&z, 0, 0) != BZ_OK)
+							Panic("BZ2_bzDecompressInit failed");
+					}
+					else
+						eos = true;
+				}
+				if(code != BZ_OK && code != BZ_STREAM_END) {
+					LLOG("BZLIB ERROR " << code);
+					Free();
+					SetError(true);
+					break;
+				}
+			}
+		} else {
+			const int flush = finish ? BZ_FINISH : BZ_RUN;
+			z.avail_out = chunk;
+			z.next_out = output;
+			while (z.avail_in) {
+				const int code = BZ2_bzCompress(&z, flush);
+				const int count = chunk - z.avail_out;
+				if(count) {
+					WhenOut((const char *)~output, count);
+					if (error) {
+						Free();
+						break;
+					}
+					z.avail_out = chunk;
+					z.next_out = output;
+				}
+				if(code != BZ_RUN_OK && code != BZ_FINISH_OK) {
+					LLOG("BZLIB ERROR " << code);
+					Free();
+					SetError(true);
+					break;
+				}
+			}
+		}
+	}
+	
+	void Lib::Put(const void *ptr, int size)
+	{
+		if(error)
+			return;
+		LLOG("BZLIB Put " << size);
+		const char *p = reinterpret_cast<const char *>(ptr);
+		while(size) {
+			int psz = (int) min(size, INT_MAX / 4);
+			if(mode == DEFLATE)
+				total += size;
+			z.next_in = const_cast<char *>(p);
+			z.avail_in = size;
+			Pump(false);
+			size -= psz;
+			p += psz;
+		}
+	}
+	
+	void Lib::PutOut(const void *ptr, int size)
+	{
+		LLOG("BZLIB PutOut " << out.GetCount());
+		out.Cat((const char *)ptr, (int)size);
+	}
+	
+	void Lib::End()
+	{
+		LLOG("BZLIB End");
+		if(mode != INFLATE)
+			Pump(true);
+		Free();
+	}
+	
+	void Lib::Free()
+	{
+		if(mode == INFLATE)
+			BZ2_bzDecompressEnd(&z);
+		else if(mode == DEFLATE)
+			BZ2_bzCompressEnd(&z);
+		mode = NONE;
+		total = 0;
+	}
+	
+	void Lib::Init()
+	{
+		mode = NONE;
+		SetError(false);
+		rdall = true;
+		eos = false;
+		Zero(z);
+	}
+	
+	void Lib::Clear()
+	{
+		Free();
+		Init();
+	}
+	
+	Lib& Lib::ChunkSize(int n)
+	{
+		ASSERT(n < INT_MAX / 4);
+		output.Clear();
+		chunk = n;
+		return *this;
+	}
+	
+	Lib::Lib()
+	{
+		Init();
+		z.bzalloc = bzalloc_new;
+		z.bzfree = bzfree_new;
+		z.opaque = 0;
+		chunk = 4096;
+		WhenOut = callback(this, &Lib::PutOut);
+		compression_level = 9;
+	}
+	
+	Lib::~Lib()
+	{
+		Free();
+	}
 }
 
 void BZ2Decompress(Stream& out, Stream& in, Gate<int, int> progress)
@@ -28,8 +198,8 @@ void BZ2Decompress(Stream& out, Stream& in, Gate<int, int> progress)
 		return;
 	bz_stream z;
 	Zero(z);
-	z.bzalloc = bzalloc_new;
-	z.bzfree = bzfree_new;
+	z.bzalloc = bz2::bzalloc_new;
+	z.bzfree = bz2::bzfree_new;
 	z.opaque = 0;
 	if(BZ2_bzDecompressInit(&z, 0, 0) != BZ_OK)
 	{
@@ -80,8 +250,8 @@ void BZ2Compress(Stream& out, Stream& in, Gate<int, int> progress)
 	enum { BUF_SIZE = 65536 };
 	Buffer<char> input(BUF_SIZE), output(BUF_SIZE);
 	bz_stream z;
-	z.bzalloc = bzalloc_new;
-	z.bzfree = bzfree_new;
+	z.bzalloc = bz2::bzalloc_new;
+	z.bzfree = bz2::bzfree_new;
 	z.opaque = 0;
 	if(BZ2_bzCompressInit(&z, 9, 0, 30) != BZ_OK)
 	{
