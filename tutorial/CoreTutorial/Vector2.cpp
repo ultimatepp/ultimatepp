ce084538a0cf7db16ae0076d125b9d3e06cf1a30
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-02-09 11:57:41 +0000

    .tutorial
    git-svn-id: svn://ultimatepp.org/upp/trunk@11769 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- tutorial/CoreTutorial/Vector2.cpp
+++ tutorial/CoreTutorial/Vector2.cpp
@@ -26,7 +26,7 @@ void Vector2()
 	
 	DUMP(v);
 	
-	/// At method returns element at specified position ensuring that such a position exists.
+	/// `At` method returns element at specified position ensuring that such a position exists.
 	/// If there is not enough elements in `Vector`, required number of elements is added. If
 	/// second parameter of `At` is present, newly added elements are initialized to this value.
 
@@ -35,6 +35,14 @@ void Vector2()
 		v.At(Random(10), 0)++;
 	
 	DUMP(v);
+	
+	/// Referencing invalid index is undefined operation. Sometimes however it is useful to
+	/// return the element value if index is valid and some default value if it is not. This
+	/// can be achieved with two parameter Get method:
+		
+	DUMP(v.Get(4, 0));
+	DUMP(v.Get(-10, 0));
+	DUMP(v.Get(13, -1));
 
 	/// You can apply algorithms on containers, e.g. Sort
 
