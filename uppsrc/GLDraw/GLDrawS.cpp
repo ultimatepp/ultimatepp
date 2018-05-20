15a774ccfcf54cd3abf20bf6ddbbdaa1e83fcb63
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-05-20 14:09:34 +0000

    GLDraw: cosmetics
    git-svn-id: svn://ultimatepp.org/upp/trunk@11958 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/GLDraw/GLDrawS.cpp
+++ uppsrc/GLDraw/GLDrawS.cpp
@@ -74,7 +74,7 @@ void initializeGL()
 		uniform sampler2D s_texture;
 		void main()
 		{
-		   gl_FragColor = texture2D(s_texture, v_texCoord);
+//		   gl_FragColor = texture2D(s_texture, v_texCoord);
 		   gl_FragColor = v_color;
 		   gl_FragColor[3] = texture2D(s_texture, v_texCoord)[3];
 		}
