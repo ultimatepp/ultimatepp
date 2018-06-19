b247b26da956d96401550d8412f3108455e25699
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-06-19 10:32:40 +0000

    ide: Nests setup new GUI dialog
    git-svn-id: svn://ultimatepp.org/upp/trunk@12004 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/BaseDlg.cpp
+++ uppsrc/ide/BaseDlg.cpp
@@ -33,25 +33,166 @@ PackageInfo GetPackageInfo(const String& name)
 	return pi;
 }
 
+void AddAssemblyPaths(FileSel& dir)
+{
+	dir.ClearPlaces();
+	dir.AddStandardPlaces();
+	VectorMap<String, int> paths;
+	for(FindFile ff(ConfigFile("*.var")); ff; ff.Next()) {
+		if(ff.IsFile()) {
+			Nest n;
+			if(n.Load(ff.GetPath())) {
+				for(String p : Split(n.Get("UPP"), ';')) {
+					FindFile ff(p);
+					if(ff) {
+						String h = NormalizePath(GetFileFolder(p));
+					#ifdef PLATFORM_WIN32
+						h = ToLower(h);
+					#endif
+						paths.GetAdd(AppendFileName(h, ff.GetName()), 0)++;
+					}
+				}
+			}
+		}
+	}
+	SortByKey(paths);
+	StableSortByValue(paths, std::greater<int>());
+	Vector<String> p = paths.PickKeys();
+	Index<String> was;
+	if(p.GetCount()) {
+		dir.AddPlaceSeparator();
+		for(String h : p) {
+			String name = GetFileName(h);
+			if(was.Find(name) >= 0)
+				name << " (" << GetFileName(GetFileFolder(h)) << ")";
+			else
+				was.Add(name);
+			dir.AddPlace(h, IdeImg::Icon(), name);
+		}
+	}
+}
+
+void NestEditorDlg::Set(const String& s)
+{
+	Vector<String> l = Split(s, ';');
+	nests.Clear();
+	for(int i = 0; i < l.GetCount(); i++)
+		nests.Add(l[i]);
+}
+
+String NestEditorDlg::Get() const
+{
+	String s;
+	for(int i = 0; i < nests.GetCount(); i++)
+		MergeWith(s, ";", ~nests.Get(i, 0));
+	return s;
+}
+
+NestEditorDlg::NestEditorDlg()
+{
+	static FileSel dir;
+
+	CtrlLayoutOKCancel(*this, "Nests");
+	Sizeable().Zoomable();
+
+	add.SetImage(IdeImg::add()) << [=] {
+		if(dir.ExecuteSelectDir("Insert nest directory")) {
+			int q = max(nests.GetCursor(), 0);
+			nests.Insert(q);
+			nests.Set(q, 0, ~dir);
+			nests.SetCursor(q);
+			Sync();
+		}
+	};
+
+	nests.WhenLeftDouble = edit.SetImage(IdeImg::pencil()) ^= [=] {
+		if(nests.IsCursor()) {
+			String h = nests.Get(0);
+			if(EditText(h, "Nest directory", "Directory"))
+				nests.Set(0, h);
+		}
+	};
+
+	remove.SetImage(IdeImg::remove()) << [=] {
+		nests.DoRemove();
+		Sync();
+	};
+	
+	up.SetImage(IdeImg::arrow_up()) << [=] {
+		nests.SwapUp();
+	};
+
+	down.SetImage(IdeImg::arrow_down()) << [=] {
+		nests.SwapDown();
+	};
+
+	nests.AutoHideSb().Moving()
+		 .NoGrid().EvenRowColor().SetLineCy(nests.GetLineCy() + DPI(2))
+		 .NoHeader().AddColumn();
+	nests.WhenSel = nests.WhenStartEdit = [=] { Sync(); };
+
+	nests.WhenDrag = [=] {
+		nests.DoDragAndDrop(InternalClip(nests, "nest-item"), nests.GetDragSample(), DND_MOVE);
+	};
+	nests.WhenDropInsert = [=](int line, PasteClip& d) {
+		if(GetInternalPtr<ArrayCtrl>(d, "nest-item") == &nests && nests.IsCursor() && d.Accept()) {
+			int q = nests.GetCursor();
+			if(q == line)
+				return;
+			String h = nests.Get(0);
+			nests.Remove(q);
+			if(q < line)
+				line--;
+			if(line >= 0 && line <= nests.GetCount()) {
+				nests.Insert(line);
+				nests.Set(line, 0, h);
+				nests.SetCursor(line);
+			}
+		}
+	};
+
+	AddAssemblyPaths(dir);
+
+	Sync();
+}
+
+void NestEditorDlg::Sync()
+{
+	edit.Enable(nests.GetCount());
+	remove.Enable(nests.GetCount());
+}
+
 bool BaseSetup(String& vars) { return BaseSetupDlg().Run(vars); }
 
 BaseSetupDlg::BaseSetupDlg()
 : browse_out("Output & intermediate files")
 {
 	CtrlLayoutOKCancel(*this, "Assembly setup");
-	browse_upp.SetImage(CtrlImg::right_arrow());
+	browse_upp.SetImage(CtrlImg::right_arrow()).Tip("Select path");
 	browse_upp <<= THISBACK(OnBrowseUpp);
 	upp.AddFrame(browse_upp);
+
+	setup_nest.Tip("Open nest editor");
+	setup_nest << [=] {
+		NestEditorDlg ndlg;
+		ndlg.Set(~upp);
+		if(!ndlg.ExecuteOK())
+			return;
+		upp <<= ndlg.Get();
+		OnUpp();
+	};
+
 	upp <<= THISBACK(OnUpp);
 	browse_out.Attach(output);
 }
 
 bool BaseSetupDlg::Run(String& vars)
 {
-	upp <<= GetVar("UPP");
-	output <<= GetVar("OUTPUT");
-	base <<= vars;
+	upp     <<= GetVar("UPP");
+	output  <<= GetVar("OUTPUT");
+	base    <<= vars;
 	new_base = IsNull(vars);
+	
 	while(TopWindow::Run() == IDOK)
 	{
 		String varname = ~base;
@@ -91,11 +232,14 @@ void BaseSetupDlg::OnBrowseUpp()
 		b--;
 	while(e < s.GetLength() && s[e] != ';')
 		e++;
-	FileSel fsel;
+	SelectDirButton dirsel;
 	String pre = s.Left(b), post = s.Mid(e);
-	fsel.ActiveDir(s.Mid(b, e - b));
-	if(fsel.ExecuteSelectDir()) {
-		String newdir = ~fsel;
+	dirsel.ActiveDir(s.Mid(b, e - b));
+	
+	AddAssemblyPaths((FileSel&) dirsel);
+
+	if(dirsel.ExecuteSelectDir()) {
+		String newdir = ~dirsel;
 		upp <<= pre + newdir + Nvl(post, ";");
 		upp.SetWantFocus();
 		OnUpp();
