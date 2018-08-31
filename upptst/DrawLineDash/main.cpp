435a8ef500f09f7eb914ffa345046725890ebac2
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-08-31 16:53:48 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@12219 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/DrawLineDash/main.cpp
+++ upptst/DrawLineDash/main.cpp
@@ -5,7 +5,7 @@ using namespace Upp;
 struct MyApp : TopWindow {
 	void Paint(Draw& w) {
 		w.DrawRect(GetSize(), White());
-		static const int d[] = { PEN_NULL, PEN_SOLID, PEN_DASH, PEN_DOT, PEN_DASHDOT, PEN_DASHDOTDOT, 1, 2, 3, 10 };
+		static const int d[] = { PEN_NULL, PEN_SOLID, PEN_DASH, PEN_DOT, PEN_DASHDOT, PEN_DASHDOTDOT, PEN_SOLID, 1, 2, 3, 10 };
 		for(int i = 0; i < __countof(d); i++) {
 			w.DrawLine(10, i * 10, 200, i * 10, d[i], Red());
 			w.DrawLine(300, i * 10, 400, i * 10 + 15, d[i], Red());
