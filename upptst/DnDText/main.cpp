334ada90ad9b56012f1940189307f7a8ddb6d40c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-09-27 13:58:13 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@12314 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/DnDText/main.cpp
+++ upptst/DnDText/main.cpp
@@ -4,8 +4,11 @@ using namespace Upp;
 
 struct MyApp : TopWindow {
 	String text;
+	
+	Point  drop;
+	String type;
 
-	void LeftDrag(Point, dword) {
+	void LeftDrag(Point, dword) override {
 		VectorMap<String, ClipData> data;
 		Append(data, String("Hello world!"));
 		text = decode(DoDragAndDrop(data, CtrlImg::exclamation()), DND_MOVE, "MOVE",
@@ -13,10 +16,32 @@ struct MyApp : TopWindow {
 		                                                           "NONE");
 		Refresh();
 	}
+	
+	void DragAndDrop(Point p, PasteClip& d) override
+	{
+		drop = p;
+		type = "?";
+		if(d.IsAvailable("text"))
+			type = "text";
+		if(AcceptText(d))
+			text = ~d;
+		Refresh();
+	}
 
-	void Paint(Draw& w) {
+	void DragLeave() override
+	{
+		drop = Null;
+		Refresh();
+	}
+
+	void Paint(Draw& w) override {
 		w.DrawRect(GetSize(), White());
 		w.DrawText(10, 10, text, Arial(50));
+		if(!IsNull(drop)) {
+			Size sz = GetTextSize(type, StdFont());
+			w.DrawRect(Rect(drop, sz), Blue());
+			w.DrawText(drop.x, drop.y, type, StdFont(), Yellow());
+		}
 	}
 };
 
