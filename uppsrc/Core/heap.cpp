489c3ed1aedb92388bd030fcd5d0e42d66c99c41
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-11-12 10:14:19 +0000

    .cosmetics
    git-svn-id: svn://ultimatepp.org/upp/trunk@12522 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/heap.cpp
+++ uppsrc/Core/heap.cpp
@@ -246,20 +246,6 @@ void MemoryShrink()
 #pragma GCC diagnostic ignored "-Wdeprecated" // silence modern GCC warning about throw(std::bad_alloc)
 #endif
 
-/*
-void *operator new(size_t size) throw(std::bad_alloc)             { void *ptr = UPP::MemoryAlloc(size); return ptr; }
-void operator  delete(void *ptr) throw()                          { UPP::MemoryFree(ptr); }
-
-void *operator new[](size_t size) throw(std::bad_alloc)           { void *ptr = UPP::MemoryAlloc(size); return ptr; }
-void operator  delete[](void *ptr) throw()                        { UPP::MemoryFree(ptr); }
-
-void *operator new(size_t size, const std::nothrow_t&) throw()    { void *ptr = UPP::MemoryAlloc(size); return ptr; }
-void operator  delete(void *ptr, const std::nothrow_t&) throw()   { UPP::MemoryFree(ptr); }
-
-void *operator new[](size_t size, const std::nothrow_t&) throw()  { void *ptr = UPP::MemoryAlloc(size); return ptr; }
-void operator  delete[](void *ptr, const std::nothrow_t&) throw() { UPP::MemoryFree(ptr); }
-*/
-
 void *operator new(size_t size)                                    { void *ptr = UPP::MemoryAlloc(size); return ptr; }
 void operator  delete(void *ptr) noexcept(true)                    { UPP::MemoryFree(ptr); }
 
