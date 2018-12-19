65d94921f6a3714900f0640a3e905facc2ba1712
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2018-12-19 13:38:06 +0000

    Core/Rpc: ProtocolVersion for XMLRPC setting
    git-svn-id: svn://ultimatepp.org/upp/trunk@12624 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Rpc/imp/Client.cpp
+++ uppsrc/Core/Rpc/imp/Client.cpp
@@ -120,6 +120,8 @@ RpcGet RpcRequest::Execute()
 	else {
 		ContentType("text/xml");
 		request = XmlHeader();
+		if(protocol_version.GetCount())
+			request << "<!--" << "protocolVersion=\\\"" << protocol_version << "\\\"" << "-->\r\n";
 		request << XmlTag("methodCall")(XmlTag("methodName")(method) + FormatXmlRpcParams(data.out));
 	}
 	if(sLogRpcCalls) {
