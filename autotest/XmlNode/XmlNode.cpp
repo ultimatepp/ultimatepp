50922288b3d0589642c83b84450f5d2cd0bd100f
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-01-06 10:00:27 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@12645 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/XmlNode/XmlNode.cpp
+++ autotest/XmlNode/XmlNode.cpp
@@ -18,7 +18,7 @@ CONSOLE_APP_MAIN
 	DDUMP(AsXML(a, XML_HEADER|XML_DOCTYPE));
 	DDUMP(AsXML(b, XML_HEADER|XML_DOCTYPE));
 
-	ASSERT(AsXML(a, XML_HEADER|XML_DOCTYPE) == "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?><!DOCTYPE Configuration><Configuration><Language id=\"CS-CZ\"/><Dimension x=\"10\" y=\"20\"/><text>Some text</text><Number value=\"1.2345e100\"/><TEST test_attr=\"23\"/><cdata>&lt;cdata&amp;test&gt;</cdata><empty1/><empty2/><empty_cdata/></Configuration>");
+	ASSERT(AsXML(a, XML_HEADER|XML_DOCTYPE) == "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><!DOCTYPE Configuration><Configuration><Language id=\"CS-CZ\"/><Dimension x=\"10\" y=\"20\"/><text>Some text</text><Number value=\"1.2345e100\"/><TEST test_attr=\"23\"/><cdata>&lt;cdata&amp;test&gt;</cdata><empty1/><empty2/><empty_cdata/></Configuration>");
 	
 	DLOG("================ OK");
 }
