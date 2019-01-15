296aa5fc0d415638b5b9d9b723509ae6184ce474
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-01-15 15:24:16 +0000

    Draw: RenderCharSys in Win32 now returns unhinted fonts
    git-svn-id: svn://ultimatepp.org/upp/trunk@12670 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Draw/FontWin32.cpp
+++ uppsrc/Draw/FontWin32.cpp
@@ -407,11 +407,11 @@ void RenderCharacterSys(FontGlyphConsumer& sw, double x, double y, int ch, Font 
 		memset(&m_matrix, 0, sizeof(m_matrix));
 		m_matrix.eM11.value = 1;
 		m_matrix.eM22.value = 1;
-		int gsz = GetGlyphOutlineW(hdc, ch, GGO_NATIVE, &gm, 0, NULL, &m_matrix);
+		int gsz = GetGlyphOutlineW(hdc, ch, GGO_NATIVE|GGO_UNHINTED, &gm, 0, NULL, &m_matrix);
 		if(gsz < 0)
 			return;
 		StringBuffer gb(gsz);
-		gsz = GetGlyphOutlineW(hdc, ch, GGO_NATIVE, &gm, gsz, ~gb, &m_matrix);
+		gsz = GetGlyphOutlineW(hdc, ch, GGO_NATIVE|GGO_UNHINTED, &gm, gsz, ~gb, &m_matrix);
 		if(gsz < 0)
 			return;
 		RenderCharPath(~gb, gsz, sw, x, y + fnt.GetAscent());
