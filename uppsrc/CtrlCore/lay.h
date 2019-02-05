d19c2ef02180d4949d1247007fe05ba16005dbcc
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-02-05 09:03:19 +0000

    CtrlCore: Fixed issues with Layout
    git-svn-id: svn://ultimatepp.org/upp/trunk@12744 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/lay.h
+++ uppsrc/CtrlCore/lay.h
@@ -40,7 +40,7 @@
 #undef  END_LAYOUT
 
 #define LAYOUT(nm, x, y)       template<class T, class D> inline void SetLayout_##nm(T& ctrl, D& parent, bool add = false, bool show = false) { \
-                                  SetLayout_Size(parent, Zx(x), Zy(y));
+                                  SetLayout_Size(ctrl, Zx(x), Zy(y));
 #define UNTYPED(var, param)       parent.var.param; if(add) ctrl.Add(parent.var); if(show) parent.var.Show();
 #define ITEM(clss, var, param)    UNTYPED(var, param);
 #define END_LAYOUT             }
