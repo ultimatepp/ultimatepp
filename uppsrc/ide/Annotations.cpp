d781f7076cca92953487fabb20bdfbcc485468c0
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-02-10 15:20:02 +0000

    .DLOG
    git-svn-id: svn://ultimatepp.org/upp/trunk@12758 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Annotations.cpp
+++ uppsrc/ide/Annotations.cpp
@@ -133,7 +133,6 @@ void AssistEditor::SyncAnnotationPopup()
 	if(y < GetWorkArea().top)
 		y = r.bottom;
 	annotation_popup.SetRect(r.left, y, Zx(600), h + DPI(16));
-	DLOG("POPUP");
 	annotation_popup.Ctrl::PopUp(this, false, false, true);
 }
 
