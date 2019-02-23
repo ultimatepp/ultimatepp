af23b5900996ae5b5953df54c6ce4ac3aef6c466
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-02-23 17:43:54 +0000

    CtrlCore: Image cache optimized in Linux too
    git-svn-id: svn://ultimatepp.org/upp/trunk@12790 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlCore/ImageX11.cpp
+++ uppsrc/CtrlCore/ImageX11.cpp
@@ -251,6 +251,12 @@ void SystemDraw::SysDrawImageOp(int x, int y, const Image& img, const Rect& src,
 	LLOG("SysDrawImageOp " << img.GetSerialId() << ' ' << img.GetSize());
 	ImageSysDataMaker m;
 	static LRUCache<ImageSysData, int64> cache;
+	static int Rsz;
+	Rsz += img.GetLength();
+	if(Rsz > 200 * 200) { // we do not want to do this for each small image painted...
+		Rsz = 0;
+		cache.Remove([](const ImageSysData& object) { return object.img.GetRefCount() == 1; });
+	}
 	LLOG("SysImage cache pixels " << cache.GetSize() << ", count " << cache.GetCount());
 	m.img = img;
 	cache.Get(m).Paint(*this, x, y, src, color);
