590089f54233508f0b9b879ee85aaa39a1079b0b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-03-09 18:50:29 +0000

    ide: SlideShow improvement
    git-svn-id: svn://ultimatepp.org/upp/trunk@12823 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/SlideShow.cpp
+++ uppsrc/ide/SlideShow.cpp
@@ -19,20 +19,26 @@ struct SlideShowSettingsDlg : WithSlideShowSettings<TopWindow> {
 
 struct SlideShow : TopWindow {
 	virtual bool Key(dword key, int count);
+	virtual void Layout();
 
 	SlideShowSettingsDlg settings;
 	SlideShowView   text;
-	Vector<String> path;
-	int            page;
-	int            rp;
+	RichTextView    title;
+	Vector<String>  path;
+	int             page;
+	int             rp;
 
-	int            zoom = 4;
-	int            margins = 1;
+	int             zoom = 16;
+	int             margins = 1;
+	int             titleh = DPI(200);
 
 	void SetPage();
 	void Settings();
+	
+	void Serialize(Stream& s);
 
 	SlideShow();
+	~SlideShow();
 };
 
 SlideShowSettingsDlg::SlideShowSettingsDlg()
@@ -49,7 +55,26 @@ SlideShowSettingsDlg::SlideShowSettingsDlg()
 	zoom_minus.WhenRepeat = zoom_minus ^= [=] { slideshow->Key(K_CTRL|K_SUBTRACT, 1); };
 	margin_plus.WhenRepeat = margin_plus ^= [=] { slideshow->Key(K_ALT|K_ADD, 1); };
 	margin_minus.WhenRepeat = margin_minus ^= [=] { slideshow->Key(K_ALT|K_SUBTRACT, 1); };
+	title_plus.WhenRepeat = title_plus ^= [=] { slideshow->Key(K_SHIFT|K_ALT|K_ADD, 1); };
+	title_minus.WhenRepeat = title_minus ^= [=] { slideshow->Key(K_SHIFT|K_ALT|K_SUBTRACT, 1); };
 }
+
+void SlideShow::Serialize(Stream& s)
+{
+	int version = 1;
+	s / version;
+	s % zoom % margins % titleh;
+	bool h = ~settings.title;
+	s % h;
+	settings.title <<= h;
+	h = ~settings.vcenter;
+	s % h;
+	settings.vcenter <<= h;
+}
+
+INITBLOCK {
+	RegisterGlobalConfig("SlideShow");
+};
 
 SlideShow::SlideShow()
 {
@@ -58,7 +83,17 @@ SlideShow::SlideShow()
 	Add(text.SizePos());
 	text.NoHyperlinkDecoration();
 	text.NoSb();
+	text.SetFrame(NullFrame());
 	rp = -1;
+	title.NoSb();
+	Add(title);
+	title.SetFrame(NullFrame());
+	LoadFromGlobal(*this, "SlideShow");
+}
+
+SlideShow::~SlideShow()
+{
+	StoreToGlobal(*this, "SlideShow");
 }
 
 void SlideShow::Settings()
@@ -105,6 +140,12 @@ bool SlideShow::Key(dword key, int count)
 	case K_PAGEDOWN:
 		page++;
 		break;
+	case K_CTRL|K_ALT|K_ADD:
+		titleh = min(titleh + 5, DPI(500));
+		break;
+	case K_CTRL|K_ALT|K_SUBTRACT:
+		titleh = max(titleh - 5, 10);
+		break;
 	case K_ALT|K_ADD:
 		margins = min(margins + 1, 100);
 		break;
@@ -112,7 +153,7 @@ bool SlideShow::Key(dword key, int count)
 		margins = max(margins - 1, 1);
 		break;
 	case K_CTRL|K_ADD:
-		zoom = min(zoom + 1, 20);
+		zoom = min(zoom + 1, 100);
 		break;
 	case K_CTRL|K_SUBTRACT:
 		zoom = max(zoom - 1, 1);
@@ -127,15 +168,37 @@ bool SlideShow::Key(dword key, int count)
 	return true;
 }
 
+void SlideShow::Layout()
+{
+	SetPage();
+}
+
 void SlideShow::SetPage()
 {
-	text.SetZoom(Zoom(DPI(zoom), 20));
-	text.VCenter(settings.vcenter);
-	text.HMargins(8 * margins);
 	page = minmax(page, 0, path.GetCount() - 1);
+	Topic t;
+	if(page >= 0)
+		t = ReadTopic(LoadFile(path[page]));
+	if(settings.title) {
+		title <<= "{{1@C~ [*R+" + AsString(titleh) + " " + t.title + "}}";
+		title.HMargins(DPI(8) * margins);
+		title.VMargins(0);
+		int cy = min(title.GetHeight() + DPI(20), GetSize().cx / 2);
+		title.HSizePos().TopPos(0, cy);
+		text.VSizePos(cy, 0);
+		title.Show();
+	}
+	else {
+		text.VSizePos(0, 0);
+		title.Hide();
+	}
+
+	text.SetZoom(Zoom(DPI(zoom), 100));
+	text.VCenter(settings.vcenter);
+	text.HMargins(DPI(8) * margins);
 	if(page != rp) {
 		rp = page;
-		text <<= ReadTopic(LoadFile(path[page])).text;
+		text <<= t.text;
 	}
 }
 
@@ -145,6 +208,7 @@ void TopicCtrl::SShow()
 	TopicLink tl = ParseTopicLink(GetCurrent());
 	if(IsNull(tl.package))
 		return;
+	
 	String folder = AppendFileName(PackageDirectory(tl.package), tl.group + ".tpp");
 	FindFile ff(AppendFileName(folder, "*.tpp"));
 	Array<String> l;
@@ -156,6 +220,7 @@ void TopicCtrl::SShow()
 	if(l.GetCount() == 0)
 		return;
 	Sort(l);
+	ss.page = 0;
 	for(int i = 0; i < l.GetCount(); i++) {
 		if(tl.topic == l[i])
 			ss.page = i;
