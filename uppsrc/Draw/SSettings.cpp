46ad436a25c72ecd833712b8dab5ef215c232d1d
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-03-25 11:26:15 +0000

    CtrlLib: Improved ColumnList documentation (thanks Oblivion), Draw: SWhite etc.. now reflect dark theme
    git-svn-id: svn://ultimatepp.org/upp/trunk@12893 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Draw/SSettings.cpp
+++ uppsrc/Draw/SSettings.cpp
@@ -2,24 +2,24 @@
 
 namespace Upp {
 
-CH_COLOR(SBlack, Black());
-CH_COLOR(SGray, Gray());
-CH_COLOR(SLtGray, LtGray());
-CH_COLOR(SWhiteGray, WhiteGray());
-CH_COLOR(SWhite, White());
-CH_COLOR(SRed, Red());
-CH_COLOR(SGreen, Green());
-CH_COLOR(SBrown, Brown());
-CH_COLOR(SBlue, Blue());
-CH_COLOR(SMagenta, Magenta());
-CH_COLOR(SCyan, Cyan());
-CH_COLOR(SYellow, Yellow());
-CH_COLOR(SLtRed, LtRed());
-CH_COLOR(SLtGreen, LtGreen());
-CH_COLOR(SLtYellow, LtYellow());
-CH_COLOR(SLtBlue, LtBlue());
-CH_COLOR(SLtMagenta, LtMagenta());
-CH_COLOR(SLtCyan, LtCyan());
+CH_COLOR(SBlack, AdjustIfDark(Black()));
+CH_COLOR(SGray, AdjustIfDark(Gray()));
+CH_COLOR(SLtGray, AdjustIfDark(LtGray()));
+CH_COLOR(SWhiteGray, AdjustIfDark(WhiteGray()));
+CH_COLOR(SWhite, AdjustIfDark(White()));
+CH_COLOR(SRed, AdjustIfDark(Red()));
+CH_COLOR(SGreen, AdjustIfDark(Green()));
+CH_COLOR(SBrown, AdjustIfDark(Brown()));
+CH_COLOR(SBlue, AdjustIfDark(Blue()));
+CH_COLOR(SMagenta, AdjustIfDark(Magenta()));
+CH_COLOR(SCyan, AdjustIfDark(Cyan()));
+CH_COLOR(SYellow, AdjustIfDark(Yellow()));
+CH_COLOR(SLtRed, AdjustIfDark(LtRed()));
+CH_COLOR(SLtGreen, AdjustIfDark(LtGreen()));
+CH_COLOR(SLtYellow, AdjustIfDark(LtYellow()));
+CH_COLOR(SLtBlue, AdjustIfDark(LtBlue()));
+CH_COLOR(SLtMagenta, AdjustIfDark(LtMagenta()));
+CH_COLOR(SLtCyan, AdjustIfDark(LtCyan()));
 
 CH_COLOR(SColorPaper, White());
 CH_COLOR(SColorFace, LtGray());
