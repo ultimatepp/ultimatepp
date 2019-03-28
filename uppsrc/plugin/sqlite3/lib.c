368d754a48808843a22936ad424968eef2b80966
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-03-28 16:25:08 +0000

    plugin/Sqlite3: changed to sqleet to support compression
    git-svn-id: svn://ultimatepp.org/upp/trunk@12910 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ uppsrc/plugin/sqlite3/lib.c
@@ -0,0 +1,8 @@
+#define SQLITE_ENABLE_FTS3 1
+#define SQLITE_ENABLE_FTS5 1
+#define SQLITE_ENABLE_RTREE 1
+#define SQLITE_ENABLE_DBSTAT_VTAB 1
+#define SQLITE_ENABLE_JSON1 1
+#define SQLITE_ENABLE_RBU 1
+
+#include "lib/sqleet.c"
--- uppsrc/plugin/sqlite3/sqlite3.c
+++
@@ -1,8 +0,0 @@
-#define SQLITE_ENABLE_FTS3 1
-#define SQLITE_ENABLE_FTS5 1
-#define SQLITE_ENABLE_RTREE 1
-#define SQLITE_ENABLE_DBSTAT_VTAB 1
-#define SQLITE_ENABLE_JSON1 1
-#define SQLITE_ENABLE_RBU 1
-
-#include "lib/sqlite3.c"
