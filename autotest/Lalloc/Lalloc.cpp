673625705eb3c637770f61959efe5f10eddacdd1
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-06-05 22:55:07 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@13354 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/Lalloc/Lalloc.cpp
@@ -0,0 +1,26 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+void *ptr[1000];
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	SeedRandom(0);
+	PeakMemoryProfile();
+	for(int i = 0; i < 100000000; i++) {
+		if(i % 10000 == 0)
+			DLOG(i);
+		int ii = Random(1000);
+		if(ptr[ii])
+			MemoryFree(ptr[ii]);
+		ptr[ii] = MemoryAlloc(Random(400000));
+	}
+	for(void *p : ptr)
+		MemoryFree(p);
+	
+	DLOG(*PeakMemoryProfile());
+	DLOG(MemoryProfile());
+	DLOG("================ OK");
+}
