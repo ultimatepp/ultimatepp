7202a788c4f9e9cd18847da9e9d5a1069c7e8984
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-06-05 17:17:02 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@13352 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ benchmarks/IndexMem/IndexMem.cpp
@@ -0,0 +1,46 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
+#ifdef _DEBUG
+	const int v_num = 10000;
+#else
+	const int v_num = 1000000;
+#endif
+
+	RDUMP(sizeof(Index<int>));
+
+	Vector<Index<int> > v;
+	v.SetCount(v_num);
+	RDUMP(MemoryUsedKb());
+	const int isize = 100;
+	TimeStop ts;
+	for (int i = 0; i < isize; ++i) {
+		const int jsize = v_num;
+		for (int j = 0; j < jsize; ++j)
+			v[j].FindAdd(i);
+	}
+	RLOG("Index<int> FindAdd: " << ts);
+	RDUMP(MemoryUsedKb());
+	RLOG(MemoryProfile());
+	ts.Reset();
+	for (int i = 0; i < isize; ++i) {
+		const int jsize = v_num;
+		for (int j = 0; j < jsize; ++j)
+			v[j].UnlinkKey(i);
+	}
+	RLOG("Index<int> UnlinkKey: " << ts);
+	ts.Reset();
+	const int jsize = v_num;
+	for (int j = 0; j < jsize; ++j) {
+		v[j].Sweep();
+		v[j].Shrink();
+	}
+	RLOG("Index<int> Sweep: " << ts);
+	RDUMP(MemoryUsedKb());
+	RLOG(MemoryProfile());
+}
