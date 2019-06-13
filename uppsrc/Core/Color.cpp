c2f85b968ca5c617ef00df4040e425943fc42c30
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-06-13 09:00:26 +0000

    uppsrc: thread__ -> thread_local
    git-svn-id: svn://ultimatepp.org/upp/trunk@13388 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Color.cpp
+++ uppsrc/Core/Color.cpp
@@ -304,7 +304,14 @@ Color DarkThemeCached(Color c)
 				ocolor[i] = Color(255, 255, 255);
 			}
 		}
-	} cache;
+	}
+#ifdef COMPILER_MINGW
+	tcache;
+	static FastMingwTls<Cache *> tls_cache = &tcache;
+	Cache& cache = *tls_cache;
+#else
+	cache;
+#endif
 	#define DO(i) if(cache.icolor[i] == c) return cache.ocolor[i];
 	DO(0); DO(1); DO(2); DO(3); DO(4); DO(5); DO(6); DO(7);
 	cache.ii = (cache.ii + 1) & (N - 1);
