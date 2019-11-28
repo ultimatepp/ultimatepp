9cd8196aca32168275c73b2110e193769cd1b7aa
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-11-28 09:09:39 +0000

    ide: PDB pretty std::string support
    git-svn-id: svn://ultimatepp.org/upp/trunk@13720 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Debuggers/Pretty.cpp
+++ uppsrc/ide/Debuggers/Pretty.cpp
@@ -39,7 +39,7 @@ void Pdb::PrettyVector(Pdb::Val val, const Vector<String>& tparam, int64 from, i
 void Pdb::PrettyBiVector(Pdb::Val val, const Vector<String>& tparam, int64 from, int count, Pdb::Pretty& p)
 {
 	p.data_count = GetInt64(GetAttr(val, "items"));
-	int start = GetInt(GetAttr(val, "items"));
+	int start = GetInt(GetAttr(val, "start"));
 	int alloc = GetInt(GetAttr(val, "alloc"));
 	Val item = DeRef(GetAttr(val, "vector"));
 	int sz = SizeOfType(tparam[0]);
@@ -190,6 +190,34 @@ void Pdb::PrettyValue(Pdb::Val val, const Vector<String>&, int64 from, int count
 	}
 }
 
+void Pdb::PrettyStdVector(Pdb::Val val, const Vector<String>& tparam, int64 from, int count, Pdb::Pretty& p)
+{
+	Val q = GetAttr(GetAttr(val, "_Mypair"), "_Myval2");
+	int sz = SizeOfType(tparam[0]);
+	Val begin = DeRef(GetAttr(q, "_Myfirst"));
+	Val end = DeRef(GetAttr(q, "_Mylast"));
+	p.data_count = (end.address - begin.address) / sz;
+	for(int i = 0; i < count; i++)
+		p.data_ptr.Add(begin.address + (i + from) * sz);
+}
+
+void Pdb::PrettyStdString(Pdb::Val val, const Vector<String>& tparam, int64 from, int count, Pdb::Pretty& p)
+{
+	Val q = GetAttr(GetAttr(val, "_Mypair"), "_Myval2");
+	bool w = tparam[0] == "wchar_t";
+	int size = GetIntAttr(q, "_Mysize");
+	int res = GetIntAttr(q, "_Myres");
+	p.data_count = size;
+	adr_t a = GetAttr(GetAttr(q, "_Bx"), "_Buf").address;
+	if(res >= (w ? 8 : 16))
+		a = PeekPtr(a);
+	p.data_type << (w ? "short int" : "char");
+	int sz = w + 1;
+	for(int i = 0; i < count; i++)
+		p.data_ptr.Add(a + sz * (from + i));
+	p.kind = TEXT;
+}
+
 Pdb::Val Pdb::MakeVal(const String& type, adr_t address)
 {
 	Val item;
@@ -198,22 +226,12 @@ Pdb::Val Pdb::MakeVal(const String& type, adr_t address)
 	return item;
 }
 
-bool Pdb::PrettyData(Visual& result, Pdb::Val val, dword flags)
+bool Pdb::PrettyVal(Pdb::Val val, int64 from, int count, Pretty& p)
 {
 	const Type& t = GetType(val.type);
 	
 	current_modbase = t.modbase; // so that we do not need to pass it as parameter in Pretty routines
 
-	int count = 0;
-	
-	auto ResultCount = [&](int64 count) {
-		if(!(flags & MEMBER)) {
-			result.Cat("[", SLtBlue);
-			result.Cat(AsString(count), SRed);
-			result.Cat("] ", SLtBlue);
-		}
-	};
-	
 	String type = t.name;
 	Vector<String> type_param;
 	int q = type.Find('<');
@@ -244,7 +262,6 @@ bool Pdb::PrettyData(Visual& result, Pdb::Val val, dword flags)
 		}
 	}
 
-
 	if(pretty.GetCount() == 0) {
 		pretty.Add("Upp::Date", { 0, THISFN(PrettyDate) });
 		pretty.Add("Upp::Time", { 0, THISFN(PrettyTime) });
@@ -260,80 +277,98 @@ bool Pdb::PrettyData(Visual& result, Pdb::Val val, dword flags)
 		pretty.Add("Upp::Index", { 1, THISFN(PrettyIndex) });
 		pretty.Add("Upp::VectorMap", { 2, THISFN(PrettyVectorMap) });
 		pretty.Add("Upp::ArrayMap", { 2, THISFN(PrettyArrayMap) });
+
+		pretty.Add("std::vector", { 1, THISFN(PrettyStdVector) });
+		pretty.Add("std::basic_string", { 1, THISFN(PrettyStdString) });
 	}
 
 	int ii = pretty.Find(type);
-	while(ii >= 0) {
+	if(ii >= 0) {
 		const auto& pr = pretty[ii];
 		if(type_param.GetCount() >= pr.a) {
-			Pretty p;
 			p.kind = CONTAINER;
 			p.data_type.Append(type_param, 0, pr.a);
-			pr.b(val, type_param, 0, 0, p);
-			p.data_type.Trim(pr.a);
-			p.data_ptr.Clear();
-			
-			if(p.kind == TEXT) {
-				int count = (int)min(p.data_count, (int64)200);
-				pr.b(val, type_param, 0, count, p);
-				if(p.data_type.GetCount()) {
-					String s;
-					WString ws;
-					int sz = SizeOfType(p.data_type[0]);
-					for(adr_t a : p.data_ptr) {
-						if(sz == 1)
-							s.Cat(PeekByte(a));
-						else
-							ws.Cat(PeekWord(a));
-					}
-					ResultCount(p.data_count);
-					result.Cat(AsCString(sz == 1 ? s : ws.ToString()), SRed);
-					if(p.data_count > p.data_ptr.GetCount())
-						result.Cat("..", SGray);
-				}
-			}
-			else
-			if(p.kind == SINGLE_VALUE) {
-				pr.b(val, type_param, 0, 1, p);
-				if(p.text.GetCount())
-					result.Cat(p.text, SRed);
-				else
-				if(p.data_type.GetCount() && p.data_ptr.GetCount())
-					Visualise(result, MakeVal(p.data_type[0], p.data_ptr[0]), flags);
-				else
-					Visualise(result, val, flags | RAW);
-			}
-			else { // CONTAINER
-				int count = (int)min(p.data_count, (int64)40);
-				pr.b(val, type_param, 0, count, p);
-	
-				ResultCount(p.data_count);
-				if(p.data_type.GetCount()) {
-					Buffer<Val> item(p.data_type.GetCount());
-					for(int i = 0; i < p.data_type.GetCount(); i++) {
-						(TypeInfo &)item[i] = GetTypeInfo(p.data_type[i]);
-						item[i].context = val.context;
-					}
-					int ii = 0;
-					int n = p.data_ptr.GetCount() / p.data_type.GetCount();
-					for(int i = 0; i < n; i++) {
-						if(i)
-							result.Cat(", ", SGray);
-						for(int j = 0; j < p.data_type.GetCount(); j++) {
-							if(j)
-								result.Cat(": ", SBlue);
-							item[j].address = p.data_ptr[ii++];
-							Visualise(result, item[j], flags | MEMBER);
-						}
-					}
-				}
-		
-				if(p.data_count > count)
-					result.Cat("..", SGray);
-			}
+			pr.b(val, type_param, from, count, p);
 			return true;
 		}
-		ii = pretty.FindNext(ii);
+	}
+	return false;
+}
+
+bool Pdb::VisualisePretty(Visual& result, Pdb::Val val, dword flags)
+{
+	auto ResultCount = [&](int64 count) {
+		if(!(flags & MEMBER)) {
+			result.Cat("[", SLtBlue);
+			result.Cat(AsString(count), SRed);
+			result.Cat("] ", SLtBlue);
+		}
+	};
+	
+	Pretty p;
+	if(PrettyVal(val, 0, 0, p)) {
+		if(p.kind == TEXT) {
+			int count = (int)min(p.data_count, (int64)200);
+			Pretty p;
+			PrettyVal(val, 0, count, p);
+			if(p.data_type.GetCount()) {
+				String s;
+				WString ws;
+				int sz = SizeOfType(p.data_type[0]);
+				for(adr_t a : p.data_ptr) {
+					if(sz == 1)
+						s.Cat(PeekByte(a));
+					else
+						ws.Cat(PeekWord(a));
+				}
+				ResultCount(p.data_count);
+				result.Cat(AsCString(sz == 1 ? s : ws.ToString()), SRed);
+				if(p.data_count > p.data_ptr.GetCount())
+					result.Cat("..", SGray);
+			}
+		}
+		else
+		if(p.kind == SINGLE_VALUE) {
+			Pretty p;
+			PrettyVal(val, 0, 1, p);
+			if(p.text.GetCount())
+				result.Cat(p.text, SRed);
+			else
+			if(p.data_type.GetCount() && p.data_ptr.GetCount())
+				Visualise(result, MakeVal(p.data_type[0], p.data_ptr[0]), flags);
+			else
+				Visualise(result, val, flags | RAW);
+		}
+		else { // CONTAINER
+			int count = (int)min(p.data_count, (int64)40);
+			Pretty p;
+			PrettyVal(val, 0, count, p);
+
+			ResultCount(p.data_count);
+			if(p.data_type.GetCount()) {
+				Buffer<Val> item(p.data_type.GetCount());
+				for(int i = 0; i < p.data_type.GetCount(); i++) {
+					(TypeInfo &)item[i] = GetTypeInfo(p.data_type[i]);
+					item[i].context = val.context;
+				}
+				int ii = 0;
+				int n = p.data_ptr.GetCount() / p.data_type.GetCount();
+				for(int i = 0; i < n; i++) {
+					if(i)
+						result.Cat(", ", SGray);
+					for(int j = 0; j < p.data_type.GetCount(); j++) {
+						if(j)
+							result.Cat(": ", SBlue);
+						item[j].address = p.data_ptr[ii++];
+						Visualise(result, item[j], flags | MEMBER);
+					}
+				}
+			}
+	
+			if(p.data_count > count)
+				result.Cat("..", SGray);
+		}
+		return true;
 	}
 	return false;
 }
