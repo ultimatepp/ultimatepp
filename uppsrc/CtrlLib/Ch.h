78224198e83b087cb011ddab8d01905f7639c3d7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-12-27 09:12:13 +0000

    CtrlLib: Common chameleon support routines
    git-svn-id: svn://ultimatepp.org/upp/trunk@13783 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CtrlLib/Ch.h
+++ uppsrc/CtrlLib/Ch.h
@@ -1,3 +1,14 @@
 void ChStdSkin();
 void ChClassicSkin();
 void ChHostSkin();
+
+enum {
+	CORNER_TOP_LEFT = 0x01,
+	CORNER_TOP_RIGHT = 0x02,
+	CORNER_BOTTOM_LEFT = 0x04,
+	CORNER_BOTTOM_RIGHT = 0x08,
+};
+
+void  RoundedRect(Painter& w, double x, double y, double cx, double cy, double rx, double ry, dword corners);
+void  RoundedRect(Painter& w, Rectf r, double rx, double ry, dword corner);
+Image MakeButton(int radius, const Image& face, int border_width, Color border_color, dword corner);
