b64efacba54107139708a90d9c689f6df8462154
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2019-12-31 16:45:04 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@13794 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/TestChStyle/main.cpp
+++ upptst/TestChStyle/main.cpp
@@ -19,16 +19,21 @@ TestChStyle::TestChStyle()
 {
 	AddFrame(menu);
 	menu.Sub("Something", [=](Bar& bar) {
-		bar.Add("Foo", [] {});
+		bar.Add("Open..", [] { SelectFileOpen("*"); });
+		bar.Add("Information..", [] { PromptOK("information."); });
+		bar.Add("Exclamation..", [] { Exclamation("exclamation!"); });
+		bar.Add("Question..", [] { PromptYesNo("question?"); });
 		bar.Add(false, "Disabled", [] {});
 		static bool check;
 		bar.Add("Check", [] { check = !check; }).Check(check);
+		bar.Separator();
+		bar.Sub("Submenu", [](Bar& bar) { bar.Add("Something", []{}); });
 	});
 
 	AddFrame(bar);
 	bar.Set([](Bar& bar) { bar.Add(CtrlImg::Diskette(), [] {}).Tip("This is test"); });
 	
-	CtrlLayout(*this, "Window title");
+	CtrlLayoutOKCancel(*this, "Window title");
 	
 	normal.NullText("Normal");
 	readonly <<= "Read only";
