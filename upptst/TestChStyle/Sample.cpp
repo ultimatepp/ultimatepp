4254bc8d299fbb9e8657a60f8057f7d945a37a0c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-01-02 08:21:00 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@13798 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/TestChStyle/Sample.cpp
+++ upptst/TestChStyle/Sample.cpp
@@ -4,8 +4,10 @@ void Sample::Paint(Draw& w)
 {
 	w.DrawRect(GetSize(), White());
 	
-	ChPaint(w, 10, 10, 100, 20, CtrlsImg::EFE());
-	ChPaint(w, 10, 30, 100, 20, GetChameleonSample());
-	
-	EditFieldFrame().FramePaint(w, RectC(10, 50, 100, 20));
+	w.DrawImage(10, 10, CtrlImg::spinup());
+//	ChPaint(w, 10, 10, 100, 20, CtrlsImg::EFE());
+//	ChPaint(w, 101, 0, 17, 23, DropList::StyleDefault().rmiddle[0]);
+	ChPaint(w, 101, 0, 17, 23, DropList::StyleDefault().rmiddle[0]);
+
+//	EditFieldFrame().FramePaint(w, RectC(10, 50, 100, 20));
 }
