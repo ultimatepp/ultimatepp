f71afbf0a0583e26c2ebdb14274edaaf381114d2
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-02-10 14:11:57 +0000

    .upptst
    git-svn-id: svn://ultimatepp.org/upp/trunk@13980 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- upptst/TestChStyle/Sample.cpp
+++ upptst/TestChStyle/Sample.cpp
@@ -8,9 +8,9 @@ void Sample::Paint(Draw& w)
 //	ChPaint(w, 10, 10, 100, 20, CtrlsImg::EFE());
 //	ChPaint(w, 101, 0, 17, 23, DropList::StyleDefault().rmiddle[0]);
 #ifdef _DEBUG
-	w.DrawImage(10, 10, (Image)GetChameleonSample());
-	w.DrawImage(150, 10, Magnify((Image)GetChameleonSample(), 4, 4));
-	w.DrawImage(10, 50, CtrlsImg::O1d());
+//	w.DrawImage(10, 10, (Image)GetChameleonSample());
+//	w.DrawImage(200, 10, Magnify((Image)GetChameleonSample(), 4, 4));
+	w.DrawImage(200, 10, CtrlsImg::HorzPos());
 #endif
 //	ChPaint(w, 10, 10, 32, 32, GetChameleonSample());
 
