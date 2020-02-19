0b91e2b10b77199575b8b969812230aee865d9f7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-02-19 14:55:49 +0000

    .examples
    git-svn-id: svn://ultimatepp.org/upp/trunk@14044 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ examples/TextToSvgPath/TextToSvgPath.h
@@ -0,0 +1,28 @@
+#ifndef _TextToSvgPath_TextToSvgPath_h
+#define _TextToSvgPath_TextToSvgPath_h
+
+#include <CtrlLib/CtrlLib.h>
+#include <RichEdit/RichEdit.h>
+
+using namespace Upp;
+
+#define LAYOUTFILE <TextToSvgPath/TextToSvgPath.lay>
+#include <CtrlCore/lay.h>
+
+String TextToSvgPath(double x, double y, const char *text, Font fnt, bool singleline);
+
+struct Preview : Ctrl {
+	String svgpath;
+	
+	virtual void Paint(Draw& w);
+};
+
+struct TextToSvgPathDlg : public WithTextToSvgPathLayout<TopWindow> {
+	Preview preview;
+
+	void Render();
+	
+	TextToSvgPathDlg();
+};
+
+#endif
