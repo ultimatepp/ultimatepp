1452209c9b74250c84a60ad1f388fd817242f28e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-03-12 22:27:08 +0000

    .cosmetics
    git-svn-id: svn://ultimatepp.org/upp/trunk@14162 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Setup.cpp
+++ uppsrc/ide/Setup.cpp
@@ -77,7 +77,19 @@ void FontSelectManager::Set(DropList& _face, DropList& _height,
 		for(int i = Font::COURIER + 1; i < Font::GetFaceCount(); i++)
 			if(Font::GetFaceInfo(i) & Font::SCALEABLE)
 				face->Add(i);
-		SetupFaceList(*face);
+
+		struct DisplayFace : public Display {
+			void Paint(Draw& w, const Rect& r, const Value& q, Color ink, Color paper, dword style) const {
+				int ii = q;
+				Font fnt = StdFont();
+				if(!(Font::GetFaceInfo(ii) & Font::SPECIAL))
+					fnt.Face(ii);
+				w.DrawRect(r, paper);
+				w.DrawText(r.left, r.top + (r.Height() - fnt.Info().GetHeight()) / 2,
+				           Font::GetFaceName((int)q), fnt, ink);
+			}
+		};
+		face->SetDisplay(Single<DisplayFace>());
 	}
 	else {
 		Index<String> fni;
