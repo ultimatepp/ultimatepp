7abb1e4bbbba8e804885333f620dcfbb066a59ad
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-04-04 08:25:24 +0000

    Core: t_ macro replaced with t_ inline function
    git-svn-id: svn://ultimatepp.org/upp/trunk@14212 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/t_.h
+++ uppsrc/Core/t_.h
@@ -1,3 +1,6 @@
+#ifndef t_h
+#define t_h
+
 #ifdef t_
 #undef t_
 #endif
@@ -6,5 +9,9 @@
 #undef tt_
 #endif
 
-#define t_(x)          t_GetLngString(x)
-#define tt_(x)         x
+inline const char *t_(const char *s)  { return t_GetLngString(s); }
+inline const char *tt_(const char *s) { return s; }
+
+// #define t_(x)          t_GetLngString(x)
+// #define tt_(x)         x
+#endif
