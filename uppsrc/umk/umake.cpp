cbb168bfcf5bbc652e1067648831d8299b0cb95b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-04-13 16:16:55 +0000

    umk: Can now name makefile, can now execute the binary
    git-svn-id: svn://ultimatepp.org/upp/trunk@14303 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/umk/umake.cpp
+++ uppsrc/umk/umake.cpp
@@ -4,6 +4,8 @@ bool SilentMode;
 
 String GetUmkFile(const char *fn)
 {
+	if(DirectoryExists(fn) || *fn == '.')
+		return Null;
 	String h = ConfigFile(fn);
 	if(FileExists(h))
 		return h;
@@ -48,21 +50,6 @@ void Puts(const char *s)
 		Cout() << s;
 }
 
-int CommaSpace(int c)
-{
-	return c == ',' ? ' ' : c;
-}
-
-int IsCommaOrColon(int c)
-{
-	return c == ':' || c == ',' ? c : 0;
-}
-
-int GetHydraThreads()
-{
-	return CPU_Cores();
-}
-
 String GetAndroidSDKPath()
 {
 	return String();
@@ -82,24 +69,100 @@ CONSOLE_APP_MAIN
 	TheIde(&ide);
 	ide.console.SetSlots(CPU_Cores());
 	ide.console.console = true;
-	const Vector<String>& arg = CommandLine();
-	if(arg.GetCount() >= 3) {
-		for(int i = 3; i < arg.GetCount(); i++)
-			if(arg[i][0] == '-') {
-				String x = arg[i];
-				for(int i = 1; i < x.GetCount(); i++) {
-					if(x[i] == 'l')
-						SilentMode = true;
-					if(x[i] == 'v')
-						ide.console.verbosebuild = true;
+
+	ide.debug.def.blitz = ide.release.def.blitz = 0;
+	ide.debug.def.debug = 2;
+	ide.release.def.debug = 0;
+	ide.debug.package.Clear();
+	ide.release.package.Clear();
+	ide.debug.linkmode = ide.release.linkmode = 0;
+	ide.release.createmap = ide.debug.createmap = false;
+	ide.targetmode = 0;
+	ide.use_target = false;
+	ide.makefile_svn_revision = false;
+	bool clean = false;
+	bool makefile = false;
+	bool deletedir = true;
+	int  exporting = 0;
+	bool run = false;
+	String mkf;
+
+	Vector<String> param, runargs;
+	
+	const Vector<String>& args = CommandLine();
+	for(int i = 0; i < args.GetCount(); i++) {
+		String a = args[i];
+		if(*a == '-') {
+			for(const char *s = ~a + 1; *s; s++)
+				switch(*s) {
+				case 'a': clean = true; break;
+				case 'r': ide.targetmode = 1; break;
+				case 'm': ide.release.createmap = ide.debug.createmap = true; break;
+				case 'b': ide.release.def.blitz = ide.debug.def.blitz = 1; break;
+				case 's': ide.debug.linkmode = ide.release.linkmode = 1; break;
+				case 'd': ide.debug.def.debug = 0; break;
+				case 'S': ide.debug.linkmode = ide.release.linkmode = 2; break;
+				case 'v': ide.console.verbosebuild = true; break;
+				case 'l': SilentMode = true; break;
+				case 'x': exporting = 1; break;
+				case 'X': exporting = 2; break;
+				case 'k': deletedir = false; break;
+				case 'u': ide.use_target = true; break;
+				case 'M': {
+					makefile = true;
+					if(s[1] == '=') {
+						mkf = NormalizePath(s + 2);
+						PutVerbose("Generating Makefile: " + mkf);
+						goto endopt;
+					}
+					else
+						PutVerbose("Generating Makefile");
+					break;
 				}
-			}
-		String v = GetUmkFile(arg[0] + ".var");
-		if(*arg[0] == '.' || IsNull(v) || !FileExists(v)) {
+				case 'H': {
+					int n = 0;
+					while(IsDigit(s[1])) {
+						n = 10 * n + s[1] - '0';
+						s++;
+					}
+					n = minmax(n, 1, 32);
+					PutVerbose("Hydra threads: " + AsString(n));
+					ide.console.SetSlots(n);
+					break;
+				}
+				default:
+					SilentMode = false;
+					Puts("Invalid build option(s)");
+					SetExitCode(3);
+					return;
+				}
+		endopt:;
+		}
+		else
+		if(*a == '+')
+			ide.mainconfigparam = Filter(~a + 1, [](int c) { return c == ',' ? ' ' : c; });
+		else
+		if(*a == '!') {
+			run = true;
+			for(int j = i + 1; j < args.GetCount(); j++)
+				runargs.Add(args[j]);
+			if(runargs)
+				PutVerbose("Set to execute the result with args: " << Join(runargs, " "));
+			else
+				PutVerbose("Set to execute the result");
+			break;
+		}
+		else
+			param.Add(a);
+	}
+	
+	if(param.GetCount() >= 2) {
+		String v = GetUmkFile(param[0] + ".var");
+		if(IsNull(v)) {
 		#ifdef PLATFORM_POSIX
-			Vector<String> h = Split(arg[0], IsCommaOrColon);
+			Vector<String> h = Split(param[0], [](int c) { return c == ':' || c == ',' ? c : 0; });
 		#else
-			Vector<String> h = Split(arg[0], ',');
+			Vector<String> h = Split(param[0], ',');
 		#endif
 			for(int i = 0; i < h.GetCount(); i++)
 				h[i] = GetFullPath(TrimBoth(h[i]));
@@ -120,14 +183,14 @@ CONSOLE_APP_MAIN
 			PutVerbose("Assembly: " + GetVar("UPP"));
 		}
 		PutVerbose("Output directory: " + GetVar("OUTPUT"));
-		v = SourcePath(arg[1], GetFileTitle(arg[1]) + ".upp");
+		v = SourcePath(param[1], GetFileTitle(param[1]) + ".upp");
 		PutVerbose("Main package: " + v);
 		if(!FileExists(v)) {
 			Puts("Package does not exist\n");
 			SetExitCode(2);
 			return;
 		}
-		ide.main = arg[1];
+		ide.main = param[1];
 		ide.wspc.Scan(ide.main);
 		const Workspace& wspc = ide.IdeWorkspace();
 		if(!wspc.GetCount()) {
@@ -138,7 +201,7 @@ CONSOLE_APP_MAIN
 		const Array<Package::Config>& f = wspc.GetPackage(0).config;
 		if(f.GetCount())
 			ide.mainconfigparam = f[0].param;
-		String m = arg[2];
+		String m = 2 < param.GetCount() ? param[2] : "CLANG";
 		String bp = GetBuildMethodPath(m);
 		PutVerbose("Build method: " + bp);
 		if(bp.GetCount() == 0) {
@@ -147,90 +210,14 @@ CONSOLE_APP_MAIN
 			SetExitCode(3);
 			return;
 		}
-		ide.method = m;
-		ide.debug.def.blitz = ide.release.def.blitz = 0;
-		ide.debug.def.debug = 2;
-		ide.release.def.debug = 0;
-		ide.debug.package.Clear();
-		ide.release.package.Clear();
-		ide.debug.linkmode = ide.release.linkmode = 0;
-		ide.release.createmap = ide.debug.createmap = false;
-		ide.targetmode = 0;
-		ide.use_target = false;
-		ide.makefile_svn_revision = false;
-		bool clean = false;
-		bool makefile = false;
-		bool deletedir = true;
-		int  exporting = 0;
-		String mkf;
 
-		for(int i = 3; i < arg.GetCount(); i++)
-			if(arg[i][0] == '+' || arg[i][0] == '>')
-				ide.mainconfigparam = Filter(~arg[i] + 1, CommaSpace);
-			else
-			if(arg[i][0] == '-') {
-				String x = arg[i];
-				for(int i = 1; i < x.GetCount(); i++)
-					switch(x[i]) {
-					case 'a':
-						clean = true;
-						break;
-					case 'r':
-						ide.targetmode = 1;
-						break;
-					case 'm':
-						ide.release.createmap = ide.debug.createmap = true;
-						break;
-					case 'b':
-						ide.release.def.blitz = ide.debug.def.blitz = 1;
-						break;
-					case 's':
-						ide.debug.linkmode = ide.release.linkmode = 1;
-						break;
-					case 'd':
-						ide.debug.def.debug = 0;
-						break;
-					case 'S':
-						ide.debug.linkmode = ide.release.linkmode = 2;
-						break;
-					case 'M':
-						makefile = true;
-						break;
-					case 'v':
-						ide.console.verbosebuild = true;
-						break;
-					case 'l':
-						break;
-					case 'x':
-						exporting = 1;
-						break;
-					case 'X':
-						exporting = 2;
-						break;
-					case 'k':
-						deletedir = false;
-						break;
-					case 'H':
-						if(i + 1 < x.GetCount() && x[i + 1] >= '1' && x[i + 1] <= '9')
-							ide.console.SetSlots(x[++i] - '0');
-						else
-							ide.console.SetSlots(1);
-						break;
-					case 'u':
-						ide.use_target = true;
-						break;
-					default:
-						SilentMode = false;
-						Puts("Invalid build option(s)");
-						SetExitCode(3);
-						return;
-					}
-			}
-			else {
-				ide.debug.target_override = ide.release.target_override = true;
-				ide.debug.target = ide.release.target = NormalizePath(arg[i]);
-				PutVerbose("Target override: " << ide.debug.target);
-			}
+		if(3 < param.GetCount()) {
+			ide.debug.target_override = ide.release.target_override = true;
+			ide.debug.target = ide.release.target = NormalizePath(param[3]);
+			PutVerbose("Target override: " << ide.debug.target);
+		}
+
+		ide.method = m;
 
 		if(clean)
 			ide.Clean();
@@ -249,13 +236,29 @@ CONSOLE_APP_MAIN
 			SetExitCode(0);
 		}
 		else
-		if(ide.Build())
+		if(ide.Build()) {
 			SetExitCode(0);
+			if(run) {
+				Vector<char *> args;
+				Vector<Buffer<char>> buffer;
+				auto Add = [&](const String& s) {
+					auto& b = buffer.Add();
+					b.Alloc(s.GetCount() + 1);
+					memcpy(b, s, s.GetCount() + 1);
+					args.Add(b);
+				};
+				Add(ide.target);
+				for(const String& s : runargs)
+					Add(s);
+				args.Add(NULL);
+				SetExitCode(execv(ide.target, args.begin()));
+			}
+		}
 		else
 			SetExitCode(1);
 	}
 	else
-		Puts("Usage: umk assembly main_package build_method -options [+flags] [output]\n\n"
+		Puts("Usage: [-options] umk assembly main_package [build_method] [+flags] [output]\n\n"
 		     "Examples: umk examples Bombs GCC -ab +GUI,SHARED ~/bombs\n"
 		     "          umk examples,uppsrc Bombs ~/GCC.bm -rv +GUI,SHARED ~/bin\n\n"
 		     "See http://www.ultimatepp.org/app$ide$umk$en-us.html for details\n");
