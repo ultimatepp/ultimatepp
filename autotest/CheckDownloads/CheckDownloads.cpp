ad43a6b2a2be1d7f9a55be583854fd33de97fd49
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-04-28 15:15:40 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@14397 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/CheckDownloads/CheckDownloads.cpp
+++ autotest/CheckDownloads/CheckDownloads.cpp
@@ -7,26 +7,28 @@ CONSOLE_APP_MAIN
 	StdLogSetup(LOG_COUT|LOG_FILE);
 
 	Date d;
-	String downloads = HttpRequest("http://www.ultimatepp.org/www$uppweb$nightly$en-us.html").Execute();
-	int q = downloads.Find("<a href=\"downloads/upp-posix");
-	ASSERT(q > 0);
-	q = downloads.ReverseFind(">", q - 1);
-	ASSERT(q > 0);
+	String downloads = HttpRequest("https://www.ultimatepp.org/www$uppweb$download$en-us.html").Execute();
+	for(String s : { "posix", "macos", "win" }) {
+		int q = downloads.Find("<a href=\"downloads/upp-" + s);
+		ASSERT(q > 0);
+		q = downloads.ReverseFind(">", q - 1);
+		ASSERT(q > 0);
+		
+		CParser p(~downloads + q + 1);
 	
-	CParser p(~downloads + q + 1);
-
-	try {
-		d.year = p.ReadInt();
-		p.PassChar('-');
-		d.month = p.ReadInt();
-		p.PassChar('-');
-		d.day = p.ReadInt();
+		try {
+			d.year = p.ReadInt();
+			p.PassChar('-');
+			d.month = p.ReadInt();
+			p.PassChar('-');
+			d.day = p.ReadInt();
+		}
+		catch(...) {
+			NEVER();
+		}
+	
+		LOG(s << " downloads last date: " << d);
+		ASSERT(d >= GetSysDate() - 1);
 	}
-	catch(...) {
-		NEVER();
-	}
-
-	LOG("downloads last date: " << d);
-	ASSERT(d >= GetSysDate() - 1);
 	LOG("============== OK");
 }
