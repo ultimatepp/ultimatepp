0af49dcb5c762762559a62600f3ac64b9397ed03
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-05-03 07:46:09 +0000

    .uppbox
    git-svn-id: svn://ultimatepp.org/upp/trunk@14423 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/MakeInstall6/Main.cpp
+++ uppbox/MakeInstall6/Main.cpp
@@ -96,11 +96,19 @@ CONSOLE_APP_MAIN
 	CopyFolder(bin + "/win32", upptmp + "/bin");
 	SaveFile(upptmp + "/dbghelp.dll", LoadFile(bin + "/win32_dlls/dbghelp.dll"));
 	SaveFile(upptmp + "/README",
-R"--(Ultimate++ Windows releases
+///////////////////////////////////////////////////////////////////////////////
+R"--(U++ Windows Installation
+Windows release is a simple archive .7z archive. Unpack to directory of your
+preference, then just run theide.exe.
 
-Windows release a simple .7z archive. Unpack to directory of your
-preference, then just run theide.exe. U++ does not write anything to registry
-or outside its directory.)--");
+U++ does not write anything to registry or outside its directory.
+
+The archive contains CLANG based Win32 toolchain and also SDL 2, OpenSSL, MySql
+and PostgreSQL client libraries. TheIDE will also setup Microsoft C++ compiler
+if it finds one.
+	
+U++ minimum requirement is Windows 7 64 with latest patches for theide to run
+and Windows 7 64 or 32 with latest patches for compiled applications.)--");
 	
 	CopyFolders(uppsrc, ass, uppsrc + "/packages");
 	CopyFolders(uppsrc, ass, uppsrc + "/packages1", false);
