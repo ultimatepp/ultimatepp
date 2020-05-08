ad1cd09d10a9709bc772f12c95d9849dfe140838
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-05-08 08:09:34 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@14435 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ benchmarks/StreamGet32/StreamGet32.cpp
@@ -0,0 +1,21 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+dword qqq;
+
+CONSOLE_APP_MAIN
+{
+	String s;
+	for(int i = 0; i < 1000000; i++)
+		s.Cat(i);
+
+	for(int i = 0; i < 100; i++) {
+		StringStream ss(s);
+		RTIMING("Time");
+		int q = 0;
+		while(!ss.IsEof())
+			q += ss.Get32();
+		qqq += q;
+	}
+}
