40833781c77477caf8b3a0c26d9133466e4987ca
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-05-13 12:10:14 +0000

    Eigen: Added simpler nonlinear functions
    git-svn-id: svn://ultimatepp.org/upp/trunk@14465 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/plugin/Eigen/Eigen.cpp
+++ uppsrc/plugin/Eigen/Eigen.cpp
@@ -1,1 +1,34 @@
-// Dummy file
+#include <Core/Core.h>
+#include <Eigen.h>
+
+
+namespace Upp {
+
+using namespace Eigen;
+
+bool NonLinearOptimization(VectorXd &y, int numData, Function <int(const VectorXd &b, VectorXd &residual)> Residual) {
+	Basic_functor functor(Residual);
+	functor.unknowns = y.size();
+	functor.datasetLen = numData;
+	Eigen::NumericalDiff<Basic_functor> numDiff(functor);
+	Eigen::LevenbergMarquardt<Eigen::NumericalDiff<Basic_functor> > lm(numDiff);
+	int ret = lm.minimize(y);
+	if (ret == Eigen::LevenbergMarquardtSpace::ImproperInputParameters || 
+		ret == Eigen::LevenbergMarquardtSpace::TooManyFunctionEvaluation)
+		return false;
+	return true;
+}
+
+bool NonLinearSolver(VectorXd &y, Function <int(const VectorXd &b, VectorXd &residual)> Residual) {
+	Basic_functor functor(Residual);
+	HybridNonLinearSolver<Basic_functor> solver(functor);
+	int ret = solver.solveNumericalDiff(y);
+	if (ret == HybridNonLinearSolverSpace::ImproperInputParameters ||
+	    ret == HybridNonLinearSolverSpace::TooManyFunctionEvaluation ||
+	    ret == HybridNonLinearSolverSpace::NotMakingProgressJacobian ||
+	    ret == HybridNonLinearSolverSpace::NotMakingProgressIterations)
+		return false;
+	return true;
+}
+
+}
---
+++ uppsrc/plugin/Eigen/src.tpp/all.i
@@ -0,0 +1,4 @@
+TOPIC("NonLinear_en-us")
+#include "NonLinear_en-us.tppi"
+END_TOPIC
+
