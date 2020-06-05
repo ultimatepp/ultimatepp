8b495413cf3da82d8e7ea7d89e267cdd811aad0a
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-06-05 11:47:35 +0000

    ide: Python syntax highlighting refactored
    git-svn-id: svn://ultimatepp.org/upp/trunk@14555 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/CodeEditor/PythonSyntax.h
+++ uppsrc/CodeEditor/PythonSyntax.h
@@ -1,4 +1,9 @@
 class PythonSyntax : public EditorSyntax {
+public:
+	virtual void Highlight(const wchar *start, const wchar *end, HighlightOutput& hls,
+	                       CodeEditor *editor, int line, int64 pos);
+	virtual void IndentInsert(CodeEditor& e, int chr, int count);
+
 private:
 	struct Identation {
 		enum Type {
@@ -8,30 +13,9 @@ private:
 		};
 	};
 
-public:
-	PythonSyntax();
-
-	virtual void Highlight(const wchar *start, const wchar *end, HighlightOutput& hls,
-	                       CodeEditor *editor, int line, int64 pos);
-	virtual void IndentInsert(CodeEditor& e, int chr, int count);
-	
-private:
-	bool IsSeparator(const wchar* c);
-
-	bool IsKeyword(const WString& w);
-	bool IsSpecialVar(const WString& w);
-	bool IsNumber(const WString& w);
-	
-	void InitKeywords();
-	void LoadSyntax(const char* keywordsArray[], const char* specialVarsArray[]);
-	
 	bool             LineHasColon(const WString& line);
 	int              CalculateLineIndetations(const WString& line, Identation::Type type);
 	int              CalculateSpaceIndetationSize(CodeEditor& editor);
 	Identation::Type FindIdentationType(CodeEditor& editor, const WString& line);
 	char             GetIdentationByType(Identation::Type type);
-	
-private:
-	Index<String> keywords;
-	Index<String> specialVars;
 };
