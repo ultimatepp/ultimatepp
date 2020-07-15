bda2c600afab4d63a59edf7a79705b2f36a3ce64
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-07-15 07:55:37 +0000

    Eigen: Improvements in NonLinearOptimization()
    git-svn-id: svn://ultimatepp.org/upp/trunk@14682 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/plugin/Eigen/Eigen.cpp
+++ uppsrc/plugin/Eigen/Eigen.cpp
@@ -5,15 +5,23 @@ namespace Upp {
 
 using namespace Eigen;
 	
-bool NonLinearOptimization(VectorXd &y, Eigen::Index numData, Function <int(const VectorXd &b, VectorXd &residual)> Residual) {
+bool NonLinearOptimization(VectorXd &y, Eigen::Index numData, 
+			Function <int(const VectorXd &b, VectorXd &residual)> Residual,
+			double xtol, double ftol, int maxfev) {
 	Basic_functor functor(Residual);
 	functor.unknowns = y.size();
 	functor.datasetLen = numData;
 	Eigen::NumericalDiff<Basic_functor> numDiff(functor);
 	Eigen::LevenbergMarquardt<Eigen::NumericalDiff<Basic_functor> > lm(numDiff);
+	if (!IsNull(xtol))
+		lm.parameters.xtol *= xtol;
+	if (!IsNull(ftol))
+		lm.parameters.ftol *= ftol;
+	if (!IsNull(maxfev))
+		lm.parameters.maxfev = maxfev;
 	int ret = lm.minimize(y);
 	if (ret == Eigen::LevenbergMarquardtSpace::ImproperInputParameters || 
-		ret == Eigen::LevenbergMarquardtSpace::TooManyFunctionEvaluation)
+		ret == Eigen::LevenbergMarquardtSpace::TooManyFunctionEvaluation) 
 		return false;
 	return true;
 }
