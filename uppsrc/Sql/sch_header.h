5ff7677ca8999a4225708d31a860f2ff3d35d347
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-07-16 09:13:21 +0000

    Sql: Added Upp:: qualifications to .sch includes
    git-svn-id: svn://ultimatepp.org/upp/trunk@14686 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Sql/sch_header.h
+++ uppsrc/Sql/sch_header.h
@@ -8,40 +8,40 @@ private: \
 	static const S_info        *info; \
 \
 public: \
-	static String               TableName; \
-	static const SqlSet&        ColumnSet()                       { return GetInfo().set; } \
-	static SqlSet               ColumnSet(const String& prefix)   { return GetInfo().GetSet(prefix); } \
-	static SqlSet               Of(SqlId table)                   { return GetInfo().GetOf(table); } \
-	static const Vector<SqlId>& GetColumnIds()                    { return GetInfo().ids; } \
+	static Upp::String          TableName; \
+	static const Upp::SqlSet&   ColumnSet()                           { return GetInfo().set; } \
+	static Upp::SqlSet          ColumnSet(const Upp::String& prefix)  { return GetInfo().GetSet(prefix); } \
+	static Upp::SqlSet          Of(Upp::SqlId table)                  { return GetInfo().GetOf(table); } \
+	static const Upp::Vector<Upp::SqlId>& GetColumnIds()              { return GetInfo().ids; } \
 	\
 	void                        Clear(); \
-	void                        FieldLayoutRaw(FieldOperator& f, const String& prefix = String()); \
-	void                        FieldLayout(FieldOperator& f); \
-	operator                    Fields()                              { return callback(this, &S_##Table::FieldLayout); } \
-	bool                        operator==(const S_##Table& x) const  { return EqualFields(const_cast<S_##Table&>(*this), const_cast<S_##Table&>(x)); } \
+	void                        FieldLayoutRaw(Upp::FieldOperator& f, const Upp::String& prefix = Upp::String()); \
+	void                        FieldLayout(Upp::FieldOperator& f); \
+	operator                    Upp::Fields()                         { return callback(this, &S_##Table::FieldLayout); } \
+	bool                        operator==(const S_##Table& x) const  { return Upp::EqualFields(const_cast<S_##Table&>(*this), const_cast<S_##Table&>(x)); } \
 	bool                        operator!=(const S_##Table& x) const  { return !operator==(x); } \
-	String                      ToString() const                      { return AsString((Fields)const_cast<S_##Table&>(*this)); } \
+	Upp::String                 ToString() const                      { return Upp::AsString((Upp::Fields)const_cast<S_##Table&>(*this)); } \
 	\
 	static int                  GetCount()                            { return GetInfo().GetCount(); } \
 	static SqlId                GetId(int i)                          { return GetInfo().GetId(i); } \
-	static int                  GetIndex(const String& id)            { return GetInfo().GetIndex(id); } \
-	static int                  GetIndex(const SqlId& id)             { return GetInfo().GetIndex(id); } \
+	static int                  GetIndex(const Upp::String& id)       { return GetInfo().GetIndex(id); } \
+	static int                  GetIndex(const Upp::SqlId& id)        { return GetInfo().GetIndex(id); } \
 	static int                  GetWidth(int i)                       { return GetInfo().GetWidth(i); } \
-	static int                  GetWidth(const SqlId& id)             { return GetInfo().GetWidth(id); } \
+	static int                  GetWidth(const Upp::SqlId& id)        { return GetInfo().GetWidth(id); } \
 	\
-	Ref                         GetRef(int i)                         { return info->GetRef(this, i); } \
-	Ref                         GetRef(const SqlId& id)               { return info->GetRef(this, id); } \
-	Value                       Get(const SqlId& id) const            { return info->Get(this, id); } \
-	Value                       Get(int i) const                      { return info->Get(this, i); } \
-	ValueMap                    Get() const                           { return info->Get(this); } \
-	void                        Set(int i, const Value& v)            { return info->Set(this, i, v); } \
-	void                        Set(const SqlId& id, const Value& v)  { return info->Set(this, id, v); } \
-	void                        Set(const ValueMap& m)                { return info->Set(this, m); } \
+	Upp::Ref                    GetRef(int i)                         { return info->GetRef(this, i); } \
+	Upp::Ref                    GetRef(const Upp::SqlId& id)          { return info->GetRef(this, id); } \
+	Upp::Value                  Get(const Upp::SqlId& id) const       { return info->Get(this, id); } \
+	Upp::Value                  Get(int i) const                      { return info->Get(this, i); } \
+	Upp::ValueMap               Get() const                           { return info->Get(this); } \
+	void                        Set(int i, const Upp::Value& v)       { return info->Set(this, i, v); } \
+	void                        Set(const Upp::SqlId& id, const Upp::Value& v)  { return info->Set(this, id, v); } \
+	void                        Set(const Upp::ValueMap& m)                     { return info->Set(this, m); } \
 	\
 	operator                    const S_info&() const                 { return *info; } \
 	\
 	S_##Table(); \
-	S_##Table(const ValueMap& m);
+	S_##Table(const Upp::ValueMap& m);
 
 #define TYPE(Table)                  CODETYPE(Table, __NIL)
 #define TYPE_I(Table, b)             CODETYPE(Table, : public S_##b)
@@ -55,12 +55,12 @@ public: \
 #define COLUMN(type, ctype, name, width, prec) \
 enum { ADD_SCHEMA_PREFIX_CPP2(name,_WIDTH) = width, ADD_SCHEMA_PREFIX_CPP2(name,_PRECISION) = prec }; \
 ctype ADD_SCHEMA_PREFIX_CPP(name); \
-static SqlId colid_##name;
+static Upp::SqlId colid_##name;
 
 #define COLUMN_ARRAY(type, ctype, name, width, prec, items) \
 enum { ADD_SCHEMA_PREFIX_CPP2(name,_WIDTH) = width, ADD_SCHEMA_PREFIX_CPP2(name,_PRECISION) = prec }; \
 ctype ADD_SCHEMA_PREFIX_CPP(name)[items]; \
-static SqlId colid_##name;
+static Upp::SqlId colid_##name;
  
 #define END_TYPE                 };
 
@@ -74,7 +74,7 @@ static SqlId colid_##name;
 
 // SqlId
 
-#define DOID(x) extern SqlId ADD_SCHEMA_PREFIX_CPP(x);
+#define DOID(x) extern Upp::SqlId ADD_SCHEMA_PREFIX_CPP(x);
 
 #define SQL_NAME(x)
 
