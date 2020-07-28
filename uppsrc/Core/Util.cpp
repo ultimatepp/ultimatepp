efeda3355d85c9a3ec93a234c3fecb67fff71679
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-07-28 07:13:00 +0000

    Core: Removed backtrace for BSDs
    git-svn-id: svn://ultimatepp.org/upp/trunk@14708 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/Core/Util.cpp
+++ uppsrc/Core/Util.cpp
@@ -38,7 +38,7 @@ void PanicMessageBox(const char *title, const char *text)
 }
 
 
-#if defined(PLATFORM_POSIX) && defined(COMPILER_GCC) && !defined(PLATFORM_ANDROID)
+#if defined(PLATFORM_LINUX) && defined(COMPILER_GCC) && !defined(PLATFORM_ANDROID)
 void AddStackTrace(char * str, int len)
 {
 	const size_t max_depth = 100;
@@ -135,7 +135,7 @@ void    AssertFailed(const char *file, int line, const char *cond)
 	PanicMode = true;
 	char s[2048];
 	sprintf(s, "Assertion failed in %s, line %d\n%s\n", file, line, cond);
-#if defined(PLATFORM_POSIX) && defined(COMPILER_GCC) && defined(flagSTACKTRACE)
+#if defined(PLATFORM_LINUX) && defined(COMPILER_GCC) && defined(flagSTACKTRACE)
 	AddStackTrace(s, sizeof(s));
 #endif
 
