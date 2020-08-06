bfd48b5ec9b759ab5bc0fdd466d33245e8046910
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-06 08:28:12 +0000

    POSIX flags fixes
    git-svn-id: svn://ultimatepp.org/upp/trunk@14754 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Builders/Install.cpp
+++ uppsrc/ide/Builders/Install.cpp
@@ -31,6 +31,62 @@ INCLUDE = "/opt/local/include;/usr/include";
 LIB = "/opt/local/lib;/usr/lib";
 LINKMODE_LOCK = "0";)";
 
+#elif PLATFORM_SOLARIS
+
+const char *gcc_bm =
+R"(BUILDER = "GCC";
+COMPILER = "";
+COMMON_OPTIONS = "";
+COMMON_CPP_OPTIONS = "-std=c++14";
+COMMON_C_OPTIONS = "";
+COMMON_LINK = "";
+COMMON_FLAGS = "";
+DEBUG_INFO = "2";
+DEBUG_BLITZ = "1";
+DEBUG_LINKMODE = "1";
+DEBUG_OPTIONS = "-O0";
+DEBUG_FLAGS = "";
+DEBUG_LINK = "";
+RELEASE_BLITZ = "0";
+RELEASE_LINKMODE = "1";
+RELEASE_OPTIONS = "-O3 -ffunction-sections -fdata-sections";
+RELEASE_FLAGS = "";
+RELEASE_LINK = "";
+DEBUGGER = "gdb";
+ALLOW_PRECOMPILED_HEADERS = "0";
+DISABLE_BLITZ = "0";
+PATH = "";
+INCLUDE = "";
+LIB = "";
+LINKMODE_LOCK = "0";)";
+
+const char *clang_bm =
+R"(BUILDER = "GCC";
+COMPILER = "clang++";
+COMMON_OPTIONS = "";
+COMMON_CPP_OPTIONS = "-std=c++14 -Wno-logical-op-parentheses";
+COMMON_C_OPTIONS = "";
+COMMON_LINK = "";
+COMMON_FLAGS = "";
+DEBUG_INFO = "2";
+DEBUG_BLITZ = "1";
+DEBUG_LINKMODE = "1";
+DEBUG_OPTIONS = "-O0";
+DEBUG_FLAGS = "";
+DEBUG_LINK = "";
+RELEASE_BLITZ = "0";
+RELEASE_LINKMODE = "1";
+RELEASE_OPTIONS = "-O3 -ffunction-sections -fdata-sections";
+RELEASE_FLAGS = "";
+RELEASE_LINK = "";
+DEBUGGER = "gdb";
+ALLOW_PRECOMPILED_HEADERS = "0";
+DISABLE_BLITZ = "0";
+PATH = "";
+INCLUDE = "";
+LIB = "";
+LINKMODE_LOCK = "0";)";
+
 #else
 
 const char *gcc_bm =
