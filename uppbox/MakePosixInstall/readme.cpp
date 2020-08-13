1d1755e4f6cf195fe4e823ac01ca98f9c4a8246b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-13 12:45:54 +0000

    .uppbox
    git-svn-id: svn://ultimatepp.org/upp/trunk@14809 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/MakePosixInstall/readme.cpp
+++ uppbox/MakePosixInstall/readme.cpp
@@ -2,8 +2,8 @@ const char *readme =
 ///////////////////////////////////////////////////////////////////////////////
 R"--(U++ POSIX Installation
 
-U++ for POSIX (OpenBSD, FreeBSD) comes as single sandboxed archive. After
-downloading, you need to unpack this archive to suitable location with
+U++ for POSIX (Linux, OpenBSD, FreeBSD, OpenSolaris) comes as single sandboxed
+archive. After downloading, you need to unpack this archive to suitable location with
 
 tar xf upp-posix-14411.tar.xz
 
@@ -17,14 +17,14 @@ cd upp
 to get into it. Now you have two options:
 
 - Use ./install script that should resolve all dependencies and build theide
-  as fast as possible (usually the process takes a couple of minutes).
+  as fast as possible (usually the process takes a couple of minutes). Note that
+  this does not work for OpenBSD (see bellow).
 
-- Resolve dependecies yourself and use make to build theide (there is no
-  ./configure or make install, it is not necessarry). Even if you do this,
+- Resolve dependecies yourself and use make to build theide. Even if you do this,
   peeking into ./install is a good idea to get a clue about dependecies
   required. You should also build umk command line too with
   make -f uMakefile, also this is not strictly necessarry for the first
-  taste of U++.
+  taste of U++. On non-Linux systems, you should run ./configure first.
 
 At the end of this process, you should have two binaries in the folder, theide
 and umk. Start theide to have some fun with U++.
@@ -41,7 +41,21 @@ The exception to the sanboxing rule is single command invoked from TheIDE menu,
 Setup / Install theide.desktop. This will write proper .desktop file to
 ~/.local/share/applications - the effect of this on most desktop environments
 is that TheIDE will appear in the desktop menu somewhere, usually in the Start
-menu under Programming category. Sometimes desktop environment needs restert
-to this to take effect.
+menu under Programming category. Sometimes desktop environment needs restart
+for this to take effect.
+
+
+OpenBSD specific notes
+======================
+
+In OpenBSD you need to resolve dependencies manually before running the ./install script by
+running
+
+pkg_add bash pkgconfig
+
+(this should work as long as you have installed desktop environment before installing U++).
+
+It is also necessary to
+
 )--";
 
