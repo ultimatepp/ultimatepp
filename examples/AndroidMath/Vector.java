a51b69549365b8cd0eccaf67e01ca816b5abe85f
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-15 20:55:11 +0000

    Examples: AndroidMath cosmetic fixes.
    git-svn-id: svn://ultimatepp.org/upp/trunk@14827 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- examples/AndroidMath/Vector.java
+++ examples/AndroidMath/Vector.java
@@ -1,13 +1,13 @@
 package org.upp.AndroidMath;
 
 /**
- * Math vector class which whole implementaiton is native (c/c++).
+ * Math vector class which whole implementation is native (c/c++).
  */
 public class Vector
 {
 	/**
-	 * Field use by MemoryManager to stroe pointer to c++ object.
-	 * If you want to use MemoryManager with your class make usre you have this field
+	 * Field use by MemoryManager to store pointer to c++ object.
+	 * If you want to use MemoryManager with your class make sure you have this field
 	 * in your class.
 	 */
 	private long nativeAdress = 0;
