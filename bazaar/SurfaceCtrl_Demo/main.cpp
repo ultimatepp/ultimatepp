82982103c4196efa36b85f2b0e2740f32652e78a
xemuth <xemuth@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-22 17:16:55 +0000

    SurfaceCtrl_Demo: working demo
    git-svn-id: svn://ultimatepp.org/upp/trunk@14880 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SurfaceCtrl_Demo/main.cpp
+++ bazaar/SurfaceCtrl_Demo/main.cpp
@@ -6,32 +6,16 @@ namespace Upp{
 	{
 		CtrlLayout(*this, "SurfaceCtrl demo. STL viewer");
 		filename.SetText( GetFileDirectory(__FILE__) + "Stanford_Bunny_sample.stl" );
-		
+				
 		butOpen.WhenAction = [&] {
 			try {
-				Surface surf;
-				{
-					Object3D& obj = canvas.CreateObject();
-					obj.LoadModel(~filename);
-					surf = obj.GetSurface();
-					canvas.DeleteObject(obj.GetID());
-				}
+
 				Object3D& obj = canvas.CreateObject();
-				obj.LoadSurface(surf, Green());
+				obj.LoadModel(~filename);
 				obj.GetTransform().Rotate(90.0f,glm::vec3(1.0f,0.0f,0.0f));
 				obj.GetTransform().SetScale(glm::vec3(0.1f,0.1f,0.1f));
 				obj.SetLineWidth(2.0f);
 				canvas.SetDefaultShader(obj);
-			
-			
-			/*
-				Object3D& obj2 = canvas.CreateObject();
-				obj2.LoadModel("C:\\Users\\hamon\\Downloads\\boat_v2_L2.123cb2c2d48f-7519-476c-8274-b5bcd578c751\\cube.obj");
-				obj2.GetTransform().Rotate(-90.0f,glm::vec3(1.0f,0.0f,0.0f));
-				//obj2.GetTransform().SetScale(glm::vec3(0.01f,0.01f,0.01f));
-				obj2.SetLineWidth(2.0f);
-				canvas.SetDefaultShader(obj2);
-				*/
 				
 				canvas.Refresh();
 			} catch (Exc e) {
