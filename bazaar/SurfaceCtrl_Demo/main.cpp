20702f1a4ade250939890f2398f6184a2fcb18d6
xemuth <xemuth@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-23 22:55:18 +0000

    SurfaceCtrl_Demo: trying to implement earth with texture
    git-svn-id: svn://ultimatepp.org/upp/trunk@14889 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SurfaceCtrl_Demo/main.cpp
+++ bazaar/SurfaceCtrl_Demo/main.cpp
@@ -11,10 +11,13 @@ namespace Upp{
 			try {
 
 				Object3D& obj = canvas.CreateObject();
-				obj.LoadModel(~filename);
+				obj.LoadModel(~filename).AttachTexture(obj.InsertTexture(TM_WATER),0,1).GenerateTextureCoordinate(0,1).Init();
+				//obj.LoadModel(~filename).Init();
+
 				obj.GetTransform().Rotate(90.0f,glm::vec3(1.0f,0.0f,0.0f));
-				obj.GetTransform().SetScale(glm::vec3(0.1f,0.1f,0.1f));
+				obj.GetTransform().SetScale(glm::vec3(1.0f,1.0f,1.0f));
 				obj.SetLineWidth(2.0f);
+				
 				canvas.SetDefaultShader(obj);
 				
 				canvas.Refresh();
@@ -22,6 +25,17 @@ namespace Upp{
 				Exclamation(DeQtf(e));
 			}
 		};
+		OpenEarth.WhenAction = [&]{
+			try {
+				Object3D& obj = canvas.CreateObject();
+				obj.LoadModel(GetFileDirectory(__FILE__) + "Earth 2K.obj").AttachTexture(obj.InsertTexture(GetFileDirectory(__FILE__) + "Diffuse_2K.png"),0,obj.GetMeshes().GetCount()).Init();
+				canvas.SetDefaultShader(obj);
+				canvas.Refresh();
+			} catch (Exc e) {
+				Exclamation(DeQtf(e));
+			}
+		};
+		
 		canvas.WhenPaint = [&] {
 			canvas.DrawAllObjects();
 		};
