622c0d014790c07aceeacb058ca720d9e28af2c4
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-25 05:58:34 +0000

    SysInfo_demo_console:  Demo is now also test
    git-svn-id: svn://ultimatepp.org/upp/trunk@14896 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SysInfo_demo_console/main.cpp
+++ bazaar/SysInfo_demo_console/main.cpp
@@ -4,194 +4,172 @@
 
 using namespace Upp;
 
-static bool end = false;
-
-void TestGetchar()
-{
-	if (end)
-		getchar();
-}
-
-void Puts(String s)
-{
-	puts(s);
-	String file = AppendFileName(GetDesktopFolder(), "log");
-	SaveFile(file, LoadFile(file) + "\n" + s);
-}
 
 void Test()
 {
-	Puts("SysInfo functions demo");
+	UppLog() << "\n" << "SysInfo functions demo";
 	
-	Puts("\nSpecial folders");
-	Puts(Format("Desktop:          %s", GetDesktopFolder()));
-	Puts(Format("Programs:         %s", GetProgramsFolder()));
-	Puts(Format("Application Data: %s", GetAppDataFolder()));
-	Puts(Format("Music:            %s", GetMusicFolder()));
-	Puts(Format("Pictures:         %s", GetPicturesFolder()));
-	Puts(Format("Video:            %s", GetVideoFolder()));
-	Puts(Format("Personal:         %s", GetPersonalFolder()));
-	Puts(Format("Templates:        %s", GetTemplatesFolder()));
-	Puts(Format("Download:         %s", GetDownloadFolder()));
-//	Puts(Format("Root:             %s", GetRootFolder()));
-	Puts(Format("Temp:             %s", GetTempFolder()));
-	Puts(Format("Os:               %s", GetOsFolder()));
-	Puts(Format("System:           %s", GetSystemFolder()));
+	UppLog() << "\n" << "\nSpecial folders";
+	UppLog() << "\n" << Format("Desktop:          %s", GetDesktopFolder());
+	UppLog() << "\n" << Format("Programs:         %s", GetProgramsFolder());
+	UppLog() << "\n" << Format("Application Data: %s", GetAppDataFolder());
+	UppLog() << "\n" << Format("Music:            %s", GetMusicFolder());
+	UppLog() << "\n" << Format("Pictures:         %s", GetPicturesFolder());
+	UppLog() << "\n" << Format("Video:            %s", GetVideoFolder());
+	UppLog() << "\n" << Format("Personal:         %s", GetPersonalFolder());
+	UppLog() << "\n" << Format("Templates:        %s", GetTemplatesFolder());
+	UppLog() << "\n" << Format("Download:         %s", GetDownloadFolder());
+//	UppLog() << "\n" << Format("Root:             %s", GetRootFolder());
+	UppLog() << "\n" << Format("Temp:             %s", GetTempFolder());
+	UppLog() << "\n" << Format("Os:               %s", GetOsFolder());
+	UppLog() << "\n" << Format("System:           %s", GetSystemFolder());
 	
-	Puts("\nNetwork info");
+	UppLog() << "\n" << "\nNetwork info";
 	String nname, domain, ip4, ip6;
-	if (GetNetworkInfo(nname, domain, ip4, ip6))
-		Puts(Format("Name '%s', Domain '%s', IP4 '%s', IP6 '%s'", nname, domain, ip4, ip6));
-	else
-		Puts("Problem reading newtwork data");
+	VERIFY(GetNetworkInfo(nname, domain, ip4, ip6));
+	UppLog() << "\n" << Format("Name '%s', Domain '%s', IP4 '%s', IP6 '%s'", nname, domain, ip4, ip6);
 	
-	Puts("\nNetwork adapter info");
+	UppLog() << "\n" << "\nNetwork adapter info";
 	Array <NetAdapter> netAdapters = GetAdapterInfo();
 	for (int i = 0; i < netAdapters.GetCount(); ++i) 
-		Puts(Format("- %s %s %s\n  %s\n  IP4: %s IP6: %s", netAdapters[i].type, 
+		UppLog() << "\n" << Format("- %s %s %s\n  %s\n  IP4: %s IP6: %s", netAdapters[i].type, 
 						netAdapters[i].mac, netAdapters[i].fullname, 
-						netAdapters[i].description, netAdapters[i].ip4, netAdapters[i].ip6));
+						netAdapters[i].description, netAdapters[i].ip4, netAdapters[i].ip6);
 	
-	Puts(Format("\nFirst free available socket port from 49152 is %d", GetAvailableSocketPort(49152)));
+	UppLog() << "\n" << Format("\nFirst free available socket port from 49152 is %d", GetAvailableSocketPort(49152));
 	
-	Puts("\nSystem info:");		
+	UppLog() << "\n" << "\nSystem info:";		
 	String manufacturer, productName, version, mbSerial;
 	Date releaseDate;
 	int numberOfProcessors;
 	GetSystemInfo(manufacturer, productName, version, numberOfProcessors, mbSerial);
-	Puts(Format("System manufacturer '%s', product name '%s',\n version '%s', number of processors: %d"
-		", motherboard serial: '%s'", manufacturer, productName, version, numberOfProcessors, mbSerial));
+	UppLog() << "\n" << Format("System manufacturer '%s', product name '%s',\n version '%s', number of processors: %d"
+		", motherboard serial: '%s'", manufacturer, productName, version, numberOfProcessors, mbSerial);
  	
- 	Puts(Format("Real CPU Speed: %.3f GHz", GetCpuSpeed()/1000.));
+ 	UppLog() << "\n" << Format("Real CPU Speed: %.3f GHz", GetCpuSpeed()/1000.);
  	
- 	Puts("\nBattery info");
+ 	UppLog() << "\n" << "\nBattery info";
  	bool present;
  	//int designCapacity,lastFullCapacity;
 	//String batVendor, type, model, serial;
 	if (!GetBatteryInfo(present/*, designCapacity, lastFullCapacity, batVendor, type, model, serial*/))
-		Puts("There is no battery");
+		UppLog() << "\n" << "There is no battery";
 	else {
 		//Puts(Format("Vendor: %s, Type: %s, Model: %s, Serial: %s", batVendor, type, model, serial));
 		//Puts(Format("Design capacity: %d mAh, Last full capacity: %d mAh", designCapacity, lastFullCapacity));
 		if (!present)
-			Puts("Battery not installed");
+			UppLog() << "\n" << "Battery not installed";
 		else {
  			bool discharging;
  			int percentage, remainingMin;
  			GetBatteryStatus(discharging, percentage, remainingMin); 
-			Puts(Format("Working with battery: %s, Percentage: %d%%, Remaining: %d min", discharging ? "yes": "no", percentage, remainingMin));
+			UppLog() << "\n" << Format("Working with battery: %s, Percentage: %d%%, Remaining: %d min", discharging ? "yes": "no", percentage, remainingMin);
 		}
 	}
  	String biosVersion, biosSerial;
  	Date biosReleaseDate;
 	GetBiosInfo(biosVersion, biosReleaseDate, biosSerial);
-	Puts(Format("\nBios version '%s',\n release date '%s', serial: '%s'", biosVersion, AsString(biosReleaseDate), biosSerial));
+	UppLog() << "\n" << Format("\nBios version '%s',\n release date '%s', serial: '%s'", biosVersion, AsString(biosReleaseDate), biosSerial);
 	String vendor, identifier, architecture; 
 	int speed;
 	for (int i = 0; i < numberOfProcessors; ++i) {
 		GetProcessorInfo(i, vendor, identifier, architecture, speed);		
-		Puts(Format("\nProcessor #%d: Vendor '%s',\n identifier '%s',\n architecture '%s', speed %d MHz", i, vendor, identifier, architecture, speed));	
+		UppLog() << "\n" << Format("\nProcessor #%d: Vendor '%s',\n identifier '%s',\n architecture '%s', speed %d MHz", i, vendor, identifier, architecture, speed);	
 	}
-	Puts("\nPress enter to continue...");	TestGetchar();
 	
-	Puts("\nMemory info:");	
+	UppLog() << "\n" << "\nMemory info:";	
 	int memoryLoad;
 	uint64 totalPhys, freePhys, totalPageFile, freePageFile, totalVirtual, freeVirtual;
-	if (!GetMemoryInfo(memoryLoad, totalPhys, freePhys, totalPageFile, freePageFile, totalVirtual, freeVirtual))
-		Puts("Impossible to get memory information");
-	else {
-		Puts(Format("Percent of memory in use: %d%c", memoryLoad, '%'));
-		Puts(Format("Total physical memory:    %s bytes (%s)", Format64(totalPhys), BytesToString(totalPhys)));
-		Puts(Format("Free physical memory:     %s bytes (%s)", Format64(freePhys), BytesToString(freePhys)));	
-		Puts(Format("Total paging file:        %s bytes (%s)", Format64(totalPageFile), BytesToString(totalPageFile)));	
-		Puts(Format("Free paging file:         %s bytes (%s)", Format64(freePageFile), BytesToString(freePageFile)));	
-		Puts(Format("Total virtual memory:     %s bytes (%s)", Format64(totalVirtual), BytesToString(totalVirtual)));	
-		Puts(Format("Free virtual memory:      %s bytes (%s)", Format64(freeVirtual), BytesToString(freeVirtual)));
-	}
-	Puts("\nOs info:");
+	VERIFY(GetMemoryInfo(memoryLoad, totalPhys, freePhys, totalPageFile, freePageFile, totalVirtual, freeVirtual));
+	UppLog() << "\n" << Format("Percent of memory in use: %d%c", memoryLoad, '%');
+	UppLog() << "\n" << Format("Total physical memory:    %s bytes (%s)", Format64(totalPhys), BytesToString(totalPhys));
+	UppLog() << "\n" << Format("Free physical memory:     %s bytes (%s)", Format64(freePhys), BytesToString(freePhys));	
+	UppLog() << "\n" << Format("Total paging file:        %s bytes (%s)", Format64(totalPageFile), BytesToString(totalPageFile));	
+	UppLog() << "\n" << Format("Free paging file:         %s bytes (%s)", Format64(freePageFile), BytesToString(freePageFile));	
+	UppLog() << "\n" << Format("Total virtual memory:     %s bytes (%s)", Format64(totalVirtual), BytesToString(totalVirtual));	
+	UppLog() << "\n" << Format("Free virtual memory:      %s bytes (%s)", Format64(freeVirtual), BytesToString(freeVirtual));
+	
+	UppLog() << "\n" << "\nOs info:";
 	String kernel, kerVersion, kerArchitecture, distro, distVersion, desktop, deskVersion;
-	if (!GetOsInfo(kernel, kerVersion, kerArchitecture, distro, distVersion, desktop, deskVersion))
-		Puts("Error getting Os info");
-	else { 	
-		Puts(Format("Kernel:  %s, version: %s,\n architecture: %s", kernel, kerVersion, kerArchitecture));
-		Puts(Format("Distro:  %s, version: %s", distro, distVersion, desktop, deskVersion));
-		Puts(Format("Desktop: %s, version: %s", desktop, deskVersion));
-	}
+	VERIFY(GetOsInfo(kernel, kerVersion, kerArchitecture, distro, distVersion, desktop, deskVersion)); 	
+	UppLog() << "\n" << Format("Kernel:  %s, version: %s,\n architecture: %s", kernel, kerVersion, kerArchitecture);
+	UppLog() << "\n" << Format("Distro:  %s, version: %s", distro, distVersion, desktop, deskVersion);
+	UppLog() << "\n" << Format("Desktop: %s, version: %s", desktop, deskVersion);
+	
 	String compiler, mode;
 	Time tim;
 	int compilerVersion, bits;
 	GetCompilerInfo(compiler, compilerVersion, tim, mode, bits);
-	Puts(Format("\nProgram compiled with %s version %d. Compilation date: %s. Mode: %s. Bits: %d", compiler, compilerVersion, Format(tim), mode, bits));
+	UppLog() << "\n" << Format("\nProgram compiled with %s version %d. Compilation date: %s. Mode: %s. Bits: %d", compiler, compilerVersion, Format(tim), mode, bits);
 	
-	Puts("\nDefault exes info:");
+	UppLog() << "\n" << "\nDefault exes info:";
 	const char *ext[] = {".html", ".doc", ".png", ".pdf", ".txt", ".xyz", ""};
 	for (int i = 0; *ext[i] != 0; ++i) 
-		Puts(Format("Default program for '%s' is '%s'", ext[i], GetExtExecutable(ext[i])));
+		UppLog() << "\n" << Format("Default program for '%s' is '%s'", ext[i], GetExtExecutable(ext[i]));
 	
-	Puts("\nPress enter to continue...");	TestGetchar();
-	
-	Puts("\nDrives list:");
+	UppLog() << "\n" << "\nDrives list:";
 	Vector<String> drives;
 	drives = GetDriveList();
 	for (int i = 0; i < drives.GetCount(); ++i) {
-		Puts(Format("Drive path:'%s'", drives[i]));
+		UppLog() << "\n" << Format("Drive path:'%s'", drives[i]);
 		String type, volume, fileSystem;
 		//uint64 serial;
 		int maxName;
 		bool mounted = GetDriveInformation(drives[i], type, volume, /*serial,*/ maxName, fileSystem);
 		if (mounted) {
-			Puts(Format(" Type: '%s', Volume: '%s', "/*Serial: %0n,*/"\n MaxName: %d, File System: %s", type, volume, /*serial, */maxName, fileSystem));
+			UppLog() << "\n" << Format(" Type: '%s', Volume: '%s', "/*Serial: %0n,*/"\n MaxName: %d, File System: %s", type, volume, /*serial, */maxName, fileSystem);
 			uint64 freeBytesUser, totalBytesUser, totalFreeBytes;
 			if(!GetDriveSpace(drives[i], freeBytesUser, totalBytesUser, totalFreeBytes))
-				Puts(" Mounted but no access to drive");
+				UppLog() << "\n" << " Mounted but no access to drive";
 			else {
-				Puts(Format(" Free Bytes User:  %s (%s)", Format64(freeBytesUser), BytesToString(freeBytesUser)));
-				Puts(Format(" Total Bytes User: %s (%s), Total Free Bytes: %s (%s)", Format64(totalBytesUser), BytesToString(totalBytesUser), Format64(totalFreeBytes), BytesToString(totalFreeBytes)));
+				UppLog() << "\n" << Format(" Free Bytes User:  %s (%s)", Format64(freeBytesUser), BytesToString(freeBytesUser));
+				UppLog() << "\n" << Format(" Total Bytes User: %s (%s), Total Free Bytes: %s (%s)", Format64(totalBytesUser), BytesToString(totalBytesUser), Format64(totalFreeBytes), BytesToString(totalFreeBytes));
 			}
 		} else
-			Puts (" Not mounted");
+			UppLog() << "\n" << " Not mounted";
 	}
-	Puts("\nOther Info:");
+	UppLog() << "\n\nOther Info:";
 	int64 id = GetProcessId();
-	Puts(Sprintf("Process Id:          %ld", id));
-	Puts(Format("Process name:        '%s'", GetProcessName(id)));
-	Puts(Format("Process file name:   '%s'", GetProcessFileName(id)));
+	UppLog() << "\n" << Sprintf("Process Id:          %ld", id);
+	UppLog() << "\n" << Format("Process name:        '%s'", GetProcessName(id));
+	UppLog() << "\n" << Format("Process file name:   '%s'", GetProcessFileName(id));
 	int priority = GetProcessPriority(id);
-	Puts(Format("Process priority is: %s", priority >= 0? AsString(priority): "Not accesible"));
-	Puts(Format("Now changed to high priority: %s", SetProcessPriority(id, 8)? "Yes": "No"));
+	UppLog() << "\n" << Format("Process priority is: %s", priority >= 0? AsString(priority): "Not accesible");
+	UppLog() << "\n" << Format("Now changed to high priority: %s", SetProcessPriority(id, 8)? "Yes": "No");
 	priority = GetProcessPriority(id);
-	Puts(Format("Process priority is: %s", priority >= 0? AsString(priority): "Not accesible"));
+	UppLog() << "\n" << Format("Process priority is: %s", priority >= 0? AsString(priority): "Not accesible");
 
-	Snap_Desktop(AppendFileName(GetDesktopFolder(), "Desktop")); 
-	Puts ("Screenshot saved in " + AppendFileName(GetDesktopFolder(), "Desktop"));
+	Snap_Desktop(GetExeDirFile("Desktop")); 
+	UppLog() << "\n" << "Screenshot saved in " + GetExeDirFile("Desktop");
 	
-	Puts("\nLaunch file 'test.txt':");
-	String fileTest = AppendFileName(GetTempFolder(), "test.txt");
+	#ifdef flagDEBUG
+	UppLog() << "\n" << "\nLaunch file 'test.txt':";
+	String fileTest = GetExeDirFile("test.txt");
 	String strTest = "\nIf modify 'test.txt' it will ask you to save or not the file" 
 					 "\nIf you answer Yes or No the program will be terminated" 
 					 "\nIf you answer Cancel or wait more than 2 seconds the program will be killed" 
 					 "\nClick enter in the command window to continue";
 	SaveFile(fileTest, "This is a test" + strTest);
-	Puts(strTest);
+	UppLog() << "\n" << strTest;
 	
 	LaunchFile(fileTest);
 	{
 		TimeStop t;
 		int64 windowId;
 		while(-1 == (windowId = GetWindowIdFromCaption("test.txt", false))) {
-			if (t.Elapsed() > 10000)
+			if (t.Seconds() > 30)
 				break;
 		}
 		if (windowId != -1) {
 			Sleep(1000);
 			int left, top, right, bottom;
 			Window_GetRect(windowId, left, top, right, bottom);	
-			Puts(Format("Editor window is located at %d, %d, %d, %d", (int)left, (int)top, (int)right, (int)bottom));
-			Puts("Editor window id is " + Format64(windowId)); 
+			UppLog() << "\n" << Format("Editor window is located at %d, %d, %d, %d", (int)left, (int)top, (int)right, (int)bottom);
+			UppLog() << "\n" << "Editor window id is " + Format64(windowId); 
 			            
 			int x, y;
 			Mouse_GetPos(x, y);
-			Puts(Format("Mouse pos is %d, %d", (int)x, (int)y));
+			UppLog() << "\n" << Format("Mouse pos is %d, %d", (int)x, (int)y);
 			Mouse_SetPos(200, 200, windowId);
 
 #if defined(PLATFORM_WIN32) || !defined(flagNO_XTEST)	
@@ -204,10 +182,9 @@ void Test()
 #if defined(PLATFORM_WIN32)			
     		Window_SetRect(windowId, 10, 10, 800, 400);
 #endif    	
-    		Window_SaveCapture(windowId, AppendFileName(GetDesktopFolder(), "Windowgrab"));
+    		Window_SaveCapture(windowId, GetExeDirFile("Windowgrab"));
 		}
 	}
-	Puts("\nPress enter to terminate 'test.txt'");	TestGetchar();
 
 	int64 processId;
 	TimeStop t;
@@ -216,61 +193,49 @@ void Test()
 			break;
 	}
 	if (processId == -1) 
-		Puts("Window was closed before!");
+		UppLog() << "\n" << "Window was closed before!";
 	else {
-		Puts("Ending process in max 2 seconds");
-		if (!ProcessTerminate(processId, 2000)) 
-			Puts("Impossible to end process");
-		else
-			Puts("Process terminated");
+		UppLog() << "\n" << "Ending process in max 2 seconds";
+		VERIFY(ProcessTerminate(processId, 2000));
 	}
-	Puts("\nPress enter to continue...");	TestGetchar();
+	#endif
 	
-	Puts("\nWindows list:");
+	UppLog() << "\n" << "\nWindows list:";
 	Array<int64> widL, pidL;
 	Array<String> name, fileName, caption;
 	GetWindowsList(widL, pidL, name, fileName, caption);
 	for (int i = 0; i < widL.GetCount(); ++i) {
-		Puts(Format("Window hwnd: %6.f, processId: %6.f, Name: %s", (double)widL[i], (double)pidL[i], name[i]));
-		Puts(Format(" File name: %s", fileName[i]));
-		Puts(Format(" Window caption: '%s'", caption[i]));
-		if ((i/20) > (i-1)/20) {
-		    Puts("\nPress enter to continue with the next 20 windows...");	TestGetchar();
-		}
+		UppLog() << "\n" << Format("Window hwnd: %6.f, processId: %6.f, Name: %s", (double)widL[i], (double)pidL[i], name[i]);
+		UppLog() << "\n" << Format(" File name: %s", fileName[i]);
+		UppLog() << "\n" << Format(" Window caption: '%s'", caption[i]);
 	}
-	Puts("\nPress enter to continue...");	TestGetchar();
 	
-	Puts("\nProcess list:");
+	UppLog() << "\n" << "\nProcess list:";
 	pidL.Clear(); 
 	Array<String> pNames;
-	if (!GetProcessList(pidL, pNames))
-		Puts("Error getting process info");
-	else {	
-		for (int i = 0; i < pidL.GetCount(); ++i) {
-			int priority = GetProcessPriority(pidL[i]);
-			Puts(Format("Id %6.f: Priority: %s, Program: %s", (double)pidL[i], priority >= 0? AsString(priority): "Not accesible", pNames[i]));
-		}
+	VERIFY(GetProcessList(pidL, pNames));	
+	for (int i = 0; i < pidL.GetCount(); ++i) {
+		int priority = GetProcessPriority(pidL[i]);
+		UppLog() << "\n" << Format("Id %6.f: Priority: %s, Program: %s", (double)pidL[i], priority >= 0? AsString(priority): "Not accesible", pNames[i]);
 	}
-	Puts("\nPress enter to end...");	TestGetchar();
 } 
 
 
 CONSOLE_APP_MAIN
 {	
-	Puts("\nBasic system identification:");
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	
+	UppLog() << "\n" << "\nSysInfo: Basic system identification:";
 	String kernel, kerVersion, kerArchitecture, distro, distVersion, desktop, deskVersion;
-	if (!GetOsInfo(kernel, kerVersion, kerArchitecture, distro, distVersion, desktop, deskVersion))
-		Puts("Error getting Os info");
-	else { 	
-		Puts(Format("Kernel:  %s, version: %s,\narchitecture: %s", kernel, kerVersion, kerArchitecture));
-		Puts(Format("Distro:  %s, version: %s", distro, distVersion, desktop, deskVersion));
-		Puts(Format("Desktop: %s, version: %s", desktop, deskVersion));
-	}
-	Puts("\nPress enter to follow with system identification:");
-	getchar();
-       
-	FileDelete(AppendFileName(GetDesktopFolder(), "log"));
-	Puts("Introduce enter or (l) to log off, (r) to reboot or (s) to shutdown");
+	VERIFY(GetOsInfo(kernel, kerVersion, kerArchitecture, distro, distVersion, desktop, deskVersion));
+	UppLog() << "\n" << Format("Kernel:  %s, version: %s,\narchitecture: %s", kernel, kerVersion, kerArchitecture);
+	UppLog() << "\n" << Format("Distro:  %s, version: %s", distro, distVersion, desktop, deskVersion);
+	UppLog() << "\n" << Format("Desktop: %s, version: %s", desktop, deskVersion);
+	
+    Test();
+    
+	#ifdef flagDEBUG
+	UppLog() << "\n" << "Introduce enter to continue, or (l) to log off, (r) to reboot or (s) to shutdown\n";
 	char str[50];
 	fgets(str, 49, stdin);
 	if (*str == 'l')
@@ -279,12 +244,10 @@ CONSOLE_APP_MAIN
 		Shutdown("reboot");
 	else if (*str == 's')
 		Shutdown("shutdown");
+	#endif
 		
-	Puts("Introduce number of test cycles or just type enter to run it once: ");
-	fgets(str, 49, stdin);
-	int numTests = atoi(str);
-	for (int i = 0; i < numTests; ++i)
-		Test();
-	end = true;
-	Test();
+	#ifdef flagDEBUG
+	Cout() << "\nPress enter key to end";
+	ReadStdIn();
+	#endif  
 }
