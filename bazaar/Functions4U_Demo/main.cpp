9a354021b03b20d8e01f5345f2a576cad550868f
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-25 05:57:02 +0000

    Functions4U_demo: Demo is now also test
    git-svn-id: svn://ultimatepp.org/upp/trunk@14893 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/Functions4U_Demo/main.cpp
+++ bazaar/Functions4U_Demo/main.cpp
@@ -5,21 +5,6 @@ using namespace Upp;
 #include "Spreadsheet.h"
 
 
-static bool end = false;
-
-void TestGetchar()
-{
-	if (end)
-		getchar();
-}
-
-void Puts(String s)
-{
-	puts(s);
-	String file = AppendFileName(GetDesktopFolder(), "log");
-	SaveFile(file, LoadFile(file) + "\n" + s);
-}
-
 void SpreadsheetDemo(Spreadsheet &spreadsheet) {
 	spreadsheet.Open("myfile.xls");
 	spreadsheet.SetData(4, 6, "Hello world");
@@ -29,12 +14,12 @@ void PluginDemo() {
 	Spreadsheet spreadsheet;
 	
 	if (!PluginInit(spreadsheet, "Open")) {
-		Puts("Impossible to init OpenOffice/LibreOffice");
+		UppLog() << "Impossible to init OpenOffice/LibreOffice";
 		return;
 	}
 	SpreadsheetDemo(spreadsheet);
 	if (!PluginInit(spreadsheet, "Excel")) {
-		Puts("Impossible to init Excel");
+		UppLog() << "Impossible to init Excel";
 		return;
 	}
 	SpreadsheetDemo(spreadsheet);
@@ -74,107 +59,100 @@ void FilesDemo() {
 		Cout() << "\nGetRelativePath(\"" << from << "\", \"" << path << "\")\n= \"" << ret << "\" (should be: Null)";
 	}
 	
-	String filename1 = AppendFileNameX(GetDesktopFolder(), "Demo", "file1.txt");
+	String filename1 = AppendFileNameX(GetExeFolder(), "Demo", "file1.txt");
 	RealizePath(filename1);
 	String str1 = "This is the First string";
 	SaveFile(filename1, str1);
-	String filename2 = AppendFileNameX(GetDesktopFolder(), "Demo", "file2.txt");
+	String filename2 = AppendFileNameX(GetExeFolder(), "Demo", "file2.txt");
 	String str2 = "This is the Second string";
 	SaveFile(filename2, str2);
 	
 	FileCat(filename1, filename2);
-	Puts(Format("\nFileCat(%s, %s)", filename1, filename2));
+	UppLog() << Format("\nFileCat(%s, %s)", filename1, filename2);
 
 //	LaunchFile(filename1);	
-//	Puts(Format("LaunchFile(%s)", filename1));		
+//	UppLog() << Format("LaunchFile(%s)", filename1);		
 
 	int intres = FileCompare(filename1, filename2);
-	Puts(Format("\nFileCompare(%s, %s) = %d (has to be -1)", filename1, filename2, intres));
+	UppLog() << Format("\nFileCompare(%s, %s) = %d (has to be -1)", filename1, filename2, intres);
 
 	int64 pos = FindStringInFile(filename1, "Second");
-	Puts(Format("\nFindStringInFile(%s, %s) = %d (has to be 35)", filename1, filename2, pos));
+	UppLog() << Format("\nFindStringInFile(%s, %s) = %d (has to be 35)", filename1, filename2, pos);
 
 	bool boolres = FileStrAppend(filename1, ". Text appended at the end.");
-	Puts(Format("\nFileStrAppend(%s, \". Text appended at the end.\") = %s (has to be true)", 
-				filename1, boolres ? "true" : "false"));
+	UppLog() << Format("\nFileStrAppend(%s, \". Text appended at the end.\") = %s (has to be true)", 
+				filename1, boolres ? "true" : "false");
 	
 	//boolres = UpperFolder(GetFileDirectory(filename1));
-	//Puts(Format("\nUpperFolder(%s) = %s (has to be true)", filename1, boolres ? "true" : "false"));
+	//UppLog() << Format("\nUpperFolder(%s) = %s (has to be true)", filename1, boolres ? "true" : "false");
 	
 	String upperFolder = GetUpperFolder(GetFileDirectory(filename1));
-	Puts(Format("\nGetUpperFolder(%s) = %s (has to be %s)", filename1, upperFolder, GetDesktopFolder()));
+	UppLog() << Format("\nGetUpperFolder(%s) = %s (has to be %s)", filename1, upperFolder, GetExeFolder());
 	
-	String stringres = GetNextFolder(GetUpperFolder(GetDesktopFolder()), GetFileDirectory(filename1));
-	Puts(Format("\nGetNextFolder(%s, %s) = %s (has to be %s)", GetUpperFolder(GetDesktopFolder()), 
-										GetFileDirectory(filename1), stringres, GetDesktopFolder()));
+	String stringres = GetNextFolder(GetUpperFolder(GetExeFolder()), GetFileDirectory(filename1));
+	UppLog() << Format("\nGetNextFolder(%s, %s) = %s (has to be %s)", GetUpperFolder(GetExeFolder()), 
+										GetFileDirectory(filename1), stringres, GetExeFolder());
 	
 	String lfilename1 = ToLower(filename1);
 	filename1 = FileRealName(lfilename1);
-	Puts(Format("\nFileRealName(%s) = %s", lfilename1, filename1));
+	UppLog() << Format("\nFileRealName(%s) = %s", lfilename1, filename1);
 }
 
 void NonReentrantDemo() {
-	Puts("Trying to enter Non reentrant.");
+	UppLog() << "\nTrying to enter Non reentrant. ";
 	NON_REENTRANT_V;
 	
-	Puts("Entered in Non reentrant. It has to be once.");
+	UppLog() << "Entered in Non reentrant. It has to be once.";
 	NonReentrantDemo();	
 	NonReentrantDemo();
 }
 
 void DistanceDemo() {
-	Puts(Format("Distance between 'hello' and 'hello'  is %d", DamerauLevenshteinDistance("hello", "hello")));
-	Puts(Format("Distance between 'hello' and 'helo'   is %d", DamerauLevenshteinDistance("hello", "helo")));
-	Puts(Format("Distance between 'hello' and 'heloo'  is %d", DamerauLevenshteinDistance("hello", "helloo")));
-	Puts(Format("Distance between 'hello' and 'yellow' is %d", DamerauLevenshteinDistance("hello", "yellow")));
+	UppLog() << Format("Distance between 'hello' and 'hello'  is %d", DamerauLevenshteinDistance("hello", "hello"));
+	UppLog() << Format("Distance between 'hello' and 'helo'   is %d", DamerauLevenshteinDistance("hello", "helo"));
+	UppLog() << Format("Distance between 'hello' and 'heloo'  is %d", DamerauLevenshteinDistance("hello", "helloo"));
+	UppLog() << Format("Distance between 'hello' and 'yellow' is %d", DamerauLevenshteinDistance("hello", "yellow"));
 }
 
 void MiscellaneousDemos() {
 	SetConsoleColor(CONSOLE_COLOR::LTRED);
-	Cout() << "This message is in red\n";
+	UppLog() << "This message is in red\n";
 	SetConsoleColor(CONSOLE_COLOR::LTYELLOW);
-	Cout() << "This message is in yellow\n";
+	UppLog() << "This message is in yellow\n";
 	SetConsoleColor(CONSOLE_COLOR::RESET);
-	Cout() << "This message is in normal color\n";
+	UppLog() << "This message is in normal color\n";
 	
-	Cout() << "Next text messages (printf, Cout()) will be disabled\n";
+	UppLog() << "Next text messages (printf, Cout(), UppLog()) will be disabled\n";
 	ConsoleOutputDisable(true);
 	Cout() << "Text with Cout()\n";
+	UppLog() << "Text with UppLog()\n";
 	printf("Text with printf()\n");
 	ConsoleOutputDisable(false);
-	Cout() << "Text messages are now enabled\n";
-}
-
-void Test() {
-	Puts("Miscellaneous demos");
-	MiscellaneousDemos();
-	
-	Puts("Files demo");
-	FilesDemo();
-	
-	Puts("\nPlugin demo");
-	PluginDemo();	
-
-	Puts("\nNonReentrant demo");
-	NonReentrantDemo();	
-	
-	Puts("\nDistance demo");
-	DistanceDemo();	
-	
-	Puts("\nPress enter to end...");	TestGetchar();
+	UppLog() << "Text messages are now enabled\n";
 }
 
 CONSOLE_APP_MAIN
 {	
-	FileDelete(AppendFileName(GetDesktopFolder(), "log"));
+	StdLogSetup(LOG_COUT|LOG_FILE);
 
-	Puts("Introduce number of test cycles or just type enter to run it once: ");
-	char str[50];
-	fgets(str, 49, stdin);
-	int numTests = atoi(str);
-	for (int i = 0; i < numTests; ++i)
-		Test();
-	end = true;
-	Test();
+	UppLog() << "Miscellaneous demos";
+	MiscellaneousDemos();
+	
+	UppLog() << "\n\nFiles demo";
+	FilesDemo();
+	
+	UppLog() << "\n\nPlugin demo";
+	PluginDemo();	
+
+	UppLog() << "\n\nNonReentrant demo";
+	NonReentrantDemo();	
+	
+	UppLog() << "\n\nDistance demo";
+	DistanceDemo();	
+	
+	#ifdef flagDEBUG
+	Cout() << "\nPress enter key to end";
+	ReadStdIn();
+	#endif   
 }
 
