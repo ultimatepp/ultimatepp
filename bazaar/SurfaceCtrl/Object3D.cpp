ae28b0ad9345d1cb676c653a8736dbd90fa09ea3
xemuth <xemuth@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-27 13:21:52 +0000

    SurfaceCtrl: Fixed texture bug !
    git-svn-id: svn://ultimatepp.org/upp/trunk@14915 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SurfaceCtrl/Object3D.cpp
+++ bazaar/SurfaceCtrl/Object3D.cpp
@@ -141,7 +141,8 @@ int Object3D::LoadTexture(const Image& img , const String& name, int indiceWhere
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.cy , size.cx, 0, GL_BGRA, GL_UNSIGNED_BYTE, ~image);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.cx , size.cy , 0, GL_BGRA, GL_UNSIGNED_BYTE, ~image);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.cy , size.cx, 0, GL_RGBA, GL_UNSIGNED_BYTE, ~image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 	return indice;
