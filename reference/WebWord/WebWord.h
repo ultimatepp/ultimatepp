ffe06027543398da5547502631f2513b9188ed3a
oblivion <oblivion@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-08-31 12:22:46 +0000

    reference: WebWord example is updated to reflect the new Turtle package.
    git-svn-id: svn://ultimatepp.org/upp/trunk@14945 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/WebWord/WebWord.h
+++ reference/WebWord/WebWord.h
@@ -1,22 +1,26 @@
-#ifndef _WebWord_WebWord_h_
-#define _WebWord_WebWord_h_
+#ifndef _WebWord_WebWord_h
+#define _WebWord_WebWord_h
+
+#ifdef flagTURTLEGUI
+#include <Turtle/Turtle.h>
+#else
+#include <CtrlLib/CtrlLib.h>
+#endif
 
 #include <RichEdit/RichEdit.h>
 #include <PdfDraw/PdfDraw.h>
 
-using namespace Upp;
+namespace Upp {
 
-struct WebWord : public TopWindow {
-public:
-	RichEditWithToolBar   editor;
-	StatusBar  statusbar;
-	
-	void ShowInfo();
-
+class WebWord : public TopWindow {
 public:
 	typedef WebWord CLASSNAME;
-
 	WebWord();
+	void ShowInfo();
+	
+	RichEditWithToolBar  editor;
+	StatusBar            statusbar;
 };
 
+}
 #endif
