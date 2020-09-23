21ee79081a3e8a6d5f8dbeb7d4669ffcb7971f9b
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-09-23 14:59:36 +0000

    ScatterDraw_Demo: Added spline
    git-svn-id: svn://ultimatepp.org/upp/trunk@15127 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- reference/ScatterDraw_Demo/ScatterDraw_Demo.cpp
+++ reference/ScatterDraw_Demo/ScatterDraw_Demo.cpp
@@ -3,6 +3,7 @@
 using namespace Upp;
 
 #include <plugin/png/png.h>
+#include <PdfDraw/PdfDraw.h>
 
 
 CONSOLE_APP_MAIN
@@ -10,57 +11,43 @@ CONSOLE_APP_MAIN
 	StdLogSetup(LOG_COUT|LOG_FILE);
 	
 	UppLog() << "\nScatterDraw_Demo";
-	UppLog() << "\nScatter painted to Image and saved to file";
 	
 	String fileName = GetExeDirFile("Scatter");
 	
 	ScatterDraw scatter;
-	Vector<Pointf> s1;
-	double x[] = {20, 30, 40, 50, 60};
-	double y[] = {34, 45, 49, 44, 56};
+	Vector<Pointf> s1 = {{10,14}, {20,65}, {30,29}, {40,32}, {50,12}};
+	Vector<double> x = {20, 30, 40, 50, 60};
+	Vector<double> y = {34, 85, 49, 72, 56};
 	
-	scatter.SerializeData(true);
+	UppLog() << "\nPreparing scatter";
 	
-	bool loaded = false;
-	if (FileExists(fileName + ".xml")) {
-		LoadFromXMLFile(scatter, fileName + ".xml");
-		loaded = true;
-	} else if (FileExists(fileName + ".json")) {
-		LoadFromJsonFile(scatter, fileName + ".json");
-		loaded = true;
-	} else if (FileExists(fileName + ".bin")) {
-		LoadFromFile(scatter, fileName + ".bin");
-		loaded = true;
-	}
+	scatter.AddSeries(s1).Legend("Series 1").NoPlot().Units("ºC", "seg");
 	
-	if (loaded) {
-		UppLog() << "\nRead scatter from file";
-		fileName = fileName + "+";
-	} else {
-		UppLog() << "\nPreparing scatter";
-		
-		s1 << Pointf(10,14) << Pointf(20,25) << Pointf(30,29) << Pointf(40,24) << Pointf(50,36);
-		
-		scatter.AddSeries(s1).Legend("Series 1").NoMark().Units("ºC", "seg");
-		
-		scatter.AddSeries(x, y, 5).Units("ºC", "seg").Legend("Series 2").Stroke(5, Null)
-			   .Dash(LINE_DASHED).MarkStyle<SquareMarkPlot>().MarkWidth(12);
-		
-		scatter.SetXYMin(10, 0).SetRange(50, 100).SetMinUnits(20, 0).SetMajorUnits(10, 20);		// Done by hand
-		//scatter.ZoomToFit();																	// Done automatically
-		
-		scatter.SetTitle("ScatterDraw_Demo").SetTitleFont(SansSerif(14).Bold());
-		scatter.SetLabelY("Temperature").SetLabelX("Time").SetLabelsFont(SansSerif(12));
-		scatter.SetPlotAreaLeftMargin(40).SetPlotAreaRightMargin(30).SetPlotAreaTopMargin(40).SetPlotAreaBottomMargin(50);
-		scatter.SetSize(Size(1000, 500));
-		scatter.SetMode(ScatterDraw::MD_ANTIALIASED);
-		scatter.SetLegendAnchor(ScatterDraw::RIGHT_TOP);
-	}
+	scatter.AddSeries(x, y).Units("ºC", "seg").Legend("Series 2").Stroke(5, Null)
+		   .Dash(LINE_DASHED).MarkStyle<SquareMarkPlot>().MarkWidth(12);
+	
+	SplineEquation spline;
+	VectorPointf s1data(s1);
+	spline.Fit(s1data);
+	scatter.AddSeries(spline).Legend("Series 1 Softened").NoMark().Dash(LINE_SOLID).Stroke(1.5);
+	
+	scatter.SetXYMin(10, 0).SetRange(50, 100).SetMinUnits(20, 0).SetMajorUnits(10, 20);		// Done by hand
+	
+	scatter.SetTitle("ScatterDraw_Demo").SetTitleFont(SansSerif(14).Bold());
+	scatter.SetLabelY("Temperature").SetLabelX("Time").SetLabelsFont(SansSerif(12));
+	scatter.SetPlotAreaLeftMargin(40).SetPlotAreaRightMargin(30).SetPlotAreaTopMargin(40).SetPlotAreaBottomMargin(50);
+	scatter.SetSize(Size(1000, 500));
+	scatter.SetMode(ScatterDraw::MD_ANTIALIASED);
+	scatter.SetPlotAreaLeftMargin(70);
+	scatter.SetLegendAnchor(ScatterDraw::RIGHT_TOP);
+	
 	PNGEncoder().SaveFile(fileName + ".png", scatter.GetImage());
-	StoreAsJsonFile(scatter, fileName + ".json", true);
-	StoreAsXMLFile(scatter, "Scatter", fileName + ".xml");
-	StoreToFile(scatter, fileName + ".bin");
 	
+	PdfDraw pdf(Size(1000, 500));
+	scatter.SetSize(Size(1000, 500));
+	scatter.SetDrawing(pdf, false);
+	SaveFile(fileName + ".pdf", pdf.Finish());
+		
 	UppLog() << "\nSaved '" << fileName << "'";
 	#ifdef flagDEBUG
 	Cout() << "\nPress enter key to end";
