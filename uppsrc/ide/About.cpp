e57d2dc3e425e185d89149da53d4e3771f91933b
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-10-11 08:48:05 +0000

    Ide: About box now detects latest version of c++ and show only one version.
    git-svn-id: svn://ultimatepp.org/upp/trunk@15230 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/About.cpp
+++ uppsrc/ide/About.cpp
@@ -31,11 +31,14 @@ String SplashCtrl::GenerateVersionInfo(char separator)
 	h << " (GCC)";
 #endif
 #endif
-#if __cplusplus >= 201100
-	h << " (C++11)";
-#endif
-#if __cplusplus >= 201400
+#if __cplusplus >= 202000
+	h << " (C++20)";
+#elif __cplusplus >= 201700
+	h << " (C++17)";
+#elif __cplusplus >= 201400
 	h << " (C++14)";
+#elif __cplusplus >= 201100
+	h << " (C++11)";
 #endif
 #ifdef GUI_GTK
 	h << " (Gtk)";
