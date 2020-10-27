cd0a46714fd37ac51c20caa5c77d0d88b1707cf9
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-10-27 19:02:52 +0000

    SysInfo: Removed CrashHandler
    git-svn-id: svn://ultimatepp.org/upp/trunk@15312 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- bazaar/SysInfo/SysInfo_in.h
+++ bazaar/SysInfo/SysInfo_in.h
@@ -79,40 +79,6 @@ namespace Upp {
 #define __cdecl __attribute__((cdecl))
 #endif
 	
-class CrashHandler {
-public:
-    CrashHandler();
-    virtual ~CrashHandler() {};
-
-private:
-#if defined(PLATFORM_WIN32) 
-    static LONG WINAPI UnhandledHandler(EXCEPTION_POINTERS *p);
-    static void __cdecl SEHHandler(unsigned u, EXCEPTION_POINTERS* p);
-#endif
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wattributes"
-#endif
-    static void __cdecl TerminateHandler();
-    static void __cdecl UnexpectedHandler();
-
-    static void __cdecl PureCallHandler();
-
-    static void __cdecl InvalidParameterHandler(const wchar_t* expression, 
-        const wchar_t* function, const wchar_t* file, unsigned int line, uintptr_t pReserved);
-
-    static void __cdecl NewHandler();
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
-    static void SigabrtHandler(int);
-    static void SigfpeHandler(int);
-    static void SigintHandler(int);
-    static void SigillHandler(int);
-    static void SigsegvHandler(int);
-    static void SigtermHandler(int);
-};
-
 }
 
 #endif
