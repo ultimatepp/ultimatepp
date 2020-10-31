91aff475ac36ebfd118f71b2a1ab430856480988
koldo <koldo@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-10-31 12:10:58 +0000

    .STEM4U: Fixing warning with lowercase file name
    git-svn-id: svn://ultimatepp.org/upp/trunk@15335 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ bazaar/STEM4U/_sundials.h
@@ -0,0 +1,23 @@
+#ifndef _STEM4U_Sundials_h_
+#define _STEM4U_Sundials_h_
+
+namespace Upp {
+	
+void SolveDAE(const Eigen::VectorXd &y, const Eigen::VectorXd &dy, double dt, double maxt, Upp::Array<Eigen::VectorXd> &res,  
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], double residual[])>Residual, int numZero = 0,
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], double residual[])>ResidualZero = Null);
+void SolveDAE(const Eigen::VectorXd &y, const Eigen::VectorXd &dy, double dt, double maxt, 
+		Upp::Array<Eigen::VectorXd> &res, Upp::Array<Eigen::VectorXd> &dres,  
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], double residual[])>Residual, int numZero = 0,
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], double residual[])>ResidualZero = Null);
+void SolveDAE(const double y[], const double dy[], int numEq, double dt, double maxt, 
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], double residual[])>Residual, int numZero = 0,  
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], double residual[])>ResidualZero = Null,
+		Function <bool(double t, Eigen::Index iiter, const double y[], const double dy[], bool isZero, int *whichZero)>OnIteration = Null);
+
+void SolveNonLinearEquationsSun(double y[], int numEq,  
+			Function <bool(const double b[], double residuals[])> Residual, int constraints[] = nullptr);
+
+}
+
+#endif
