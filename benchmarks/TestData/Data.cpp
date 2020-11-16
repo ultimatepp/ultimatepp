a180803db044f9ad12097e7fa0d932cf24903cc7
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-11-16 22:57:09 +0000

    .benchmarks
    git-svn-id: svn://ultimatepp.org/upp/trunk@15436 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- benchmarks/TestData/Data.cpp
+++ benchmarks/TestData/Data.cpp
@@ -2,7 +2,20 @@
 
 Vector<String> AliceWords()
 {
-	return Split(alice_text, [](int c) { return c > 32 ? 0 : c; });
+	Vector<String> w = Split(alice_text, [](int c) { return c > 32 ? 0 : c; });
+	Vector<String> r;
+	for(String s : w) {
+		s = ToLower(s);
+		int q = 0;
+		while(!IsAlNum(s[q]) && q < s.GetCount())
+			q++;
+		s = s.Mid(q);
+		while(s.GetCount() && !IsAlNum(*s.Last()))
+			s.TrimLast();
+		if(s.GetCount())
+			r.Add(s);
+	}
+	return r;
 }
 
 Vector<String> AliceLines()
