89cbaf009f6c532bff56b5ba9442cb51292d1583
klugier <klugier@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-11-21 22:32:37 +0000

    UppWeb: .cosmetics in cpp code generation.
    git-svn-id: svn://ultimatepp.org/upp/trunk@15481 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/uppweb/cpp.cpp
+++ uppbox/uppweb/cpp.cpp
@@ -4,31 +4,31 @@ String CppAsQtf(const String& str)
 {
 	static const char *cpp[] = {
 		"__asm", "__cdecl", "__declspec", "__except", "__fastcall",
-        "__finally", "__inline", "__int16", "__int32", "__int64",
-        "__int8", "__leave", "__stdcall", "__try", "__uuidof",
-        "alignas", "alignof", "and", "and_eq", "asm", "auto",
-        "bitand", "bitor", "bool", "break", "case", "catch",
-        "char", "char8_t", "char16_t", "char32_t", "class",
-        "co_await", "co_return", "co_yield", "compl", "concept",
-        "const", "const_cast", "consteval", "constexpr", "constinit",
-        "continue", "decltype", "default", "delete", "dllexport",
-        "dllimport", "do", "double", "dynamic_cast", "else", "enum",
-        "explicit", "export", "extern", "false", "final", "float",
-        "for", "force_inline", "friend", "goto", "if", "import",
-        "inline", "int", "long", "module", "mutable", "namespace",
-        "never_inline", "new", "noexcept", "not", "not_eq", "nullptr",
-        "operator", "or", "or_eq", "override", "private", "protected",
-        "public", "register", "reinterpret_cast", "requires", "return",
-        "short", "signed", "sizeof", "static", "static_assert",
-        "static_cast", "struct", "switch", "template", "this", "thread",
-        "thread_local", "throw", "true", "try", "typedef", "typeid",
-        "typename", "union", "unsigned", "using", "virtual",
-        "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+		"__finally", "__inline", "__int16", "__int32", "__int64",
+		"__int8", "__leave", "__stdcall", "__try", "__uuidof",
+		"alignas", "alignof", "and", "and_eq", "asm", "auto",
+		"bitand", "bitor", "bool", "break", "case", "catch",
+		"char", "char8_t", "char16_t", "char32_t", "class",
+		"co_await", "co_return", "co_yield", "compl", "concept",
+		"const", "const_cast", "consteval", "constexpr", "constinit",
+		"continue", "decltype", "default", "delete", "dllexport",
+		"dllimport", "do", "double", "dynamic_cast", "else", "enum",
+		"explicit", "export", "extern", "false", "final", "float",
+		"for", "force_inline", "friend", "goto", "if", "import",
+		"inline", "int", "long", "module", "mutable", "namespace",
+		"never_inline", "new", "noexcept", "not", "not_eq", "nullptr",
+		"operator", "or", "or_eq", "override", "private", "protected",
+		"public", "register", "reinterpret_cast", "requires", "return",
+		"short", "signed", "sizeof", "static", "static_assert",
+		"static_cast", "struct", "switch", "template", "this", "thread",
+		"thread_local", "throw", "true", "try", "typedef", "typeid",
+		"typename", "union", "unsigned", "using", "virtual",
+		"void", "volatile", "wchar_t", "while", "xor", "xor_eq",
 		"include", "define", "ifdef", "ifndef", "endif",
-		NULL
+		nullptr
 	};
 	static Index<String> keyword;
-	if(keyword.GetCount() == 0) 
+	if(keyword.GetCount() == 0)
 		for(const char **q = cpp; *q; q++)
 			keyword.Add(*q);
 
