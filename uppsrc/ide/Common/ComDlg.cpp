a01614cb423b0d39dcf8980ba4d3dc9163e23868
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-12-01 13:03:42 +0000

    ide: cosmetics, GridCtrl: Selection refactored to follow spreadsheet model
    git-svn-id: svn://ultimatepp.org/upp/trunk@15558 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppsrc/ide/Common/ComDlg.cpp
+++ uppsrc/ide/Common/ComDlg.cpp
@@ -4,6 +4,24 @@
 #define IMAGEFILE  <ide/Common/common.iml>
 #include <Draw/iml_source.h>
 
+void DirSelect(Ctrl& t, Button& b)
+{
+	b.SetImage(CtrlImg::Dir());
+	b << [&] {
+		String s = SelectDirectory();
+		if(s.GetCount())
+			t <<= s;
+	};
+}
+
+void DirSel(EditField& f, FrameRight<Button>& b)
+{
+	b.Width(DPI(20));
+	f.InsertFrame(0, b);
+	f.InsertFrame(1, RightGapFrame());
+	DirSelect(f, b);
+}
+
 void IdeFileIcon0(bool dir, const String& filename, Image& img)
 {
 	if(dir) return;
@@ -232,14 +250,4 @@ void ShellOpenFolder(const String& dir)
 			system("xdg-open " + tempDir + " &")
 		);
 	#endif
-}
-
-void DirSelect(Ctrl& t, Button& b)
-{
-	b.SetImage(CtrlImg::Dir());
-	b << [&] {
-		String s = SelectDirectory();
-		if(s.GetCount())
-			t <<= s;
-	};
 }
