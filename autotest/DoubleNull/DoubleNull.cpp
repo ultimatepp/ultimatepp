0870074185f2ccf74da558d4d9994f971cb4313b
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2020-12-14 09:30:32 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@15591 f0d560ea-af0d-0410-9eb7-867de7ffcac7

---
+++ autotest/DoubleNull/DoubleNull.cpp
@@ -0,0 +1,19 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+double zero;
+
+CONSOLE_APP_MAIN
+{
+	double x = Null;
+	double y = 1e-306;
+	RDUMP(IsNull(x));
+	RDUMP(IsNull(y));
+	RDUMP(IsNull(1e300));
+	RDUMP(IsNull(0));
+	RDUMP(IsNull(log(-1)));
+	RDUMP(IsNull(1 / zero));
+
+	CheckLogEtalon();
+}
