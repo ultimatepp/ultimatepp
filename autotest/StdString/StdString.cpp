beb8c781cb7051c5b45ab8980dc255837cd5bdb2
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2021-02-03 22:54:51 +0000

    .reference
    git-svn-id: svn://ultimatepp.org/upp/trunk@15734 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/StdString/StdString.cpp
+++ autotest/StdString/StdString.cpp
@@ -7,25 +7,25 @@ CONSOLE_APP_MAIN
 	StdLogSetup(LOG_COUT|LOG_FILE);
 	{
 		String h = "Hello world";
-		std::string sh = h;
+		std::string sh = h.ToStd();
 		DDUMP(sh);
 		h = sh;
 		DDUMP(h);
 		Value v = sh;
 		DDUMP(v);
-		sh = v;
+		sh = v.ToStd();
 		DDUMP(sh);
 	}
 
 	{
 		WString h = "Hello world";
-		std::wstring sh = h;
+		std::wstring sh = h.ToStd();
 		DDUMP(sh);
 		h = sh;
 		DDUMP(h);
 		Value v = sh;
 		DDUMP(v);
-		sh = v;
+		sh = v.ToWStd();
 		DDUMP(sh);
 	}
 	CheckLogEtalon();
