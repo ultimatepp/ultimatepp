3e78ee9e53b7b1a91adf447f5828f4ea0500f75e
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2021-02-22 09:51:45 +0000

    .autotest
    git-svn-id: svn://ultimatepp.org/upp/trunk@15791 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- autotest/EncodeHtml/main.cpp
+++ autotest/EncodeHtml/main.cpp
@@ -1,19 +1,23 @@
-#include <CtrlLib/CtrlLib.h>
+#include <RichText/RichText.h>
 
 #define TOPICFILE <EncodeHtml/Test.tpp/all.i>
 #include <Core/topic_group.h>
 
 using namespace Upp;
 
-GUI_APP_MAIN
+
+CONSOLE_APP_MAIN
 {
+	StdLogSetup(LOG_COUT|LOG_FILE);
+
 	String path = GetExeDirFile("test.html");
 	Index<String> css;
 	String html = EncodeHtml(ParseQTF(GetTopic("topic://EncodeHtml/Test/Test_en-us").text), css,
 	                         VectorMap<String, String>(), VectorMap<String, String>(),
 	                         GetFileFolder(path));
 	SaveFile(path, MakeHtml("Test", AsCss(css), html));
-	
+
+	DDUMP(path);
 	DDUMP(SHA1StringS(LoadFile(path)));
 	
 	CheckLogEtalon();
