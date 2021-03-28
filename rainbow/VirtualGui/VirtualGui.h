d3db697aed55c78c4e740a16c0c6850483e001ed
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2021-03-28 13:35:50 +0000

    .rainbow
    git-svn-id: svn://ultimatepp.org/upp/trunk@15878 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- rainbow/VirtualGui/VirtualGui.h
+++ rainbow/VirtualGui/VirtualGui.h
@@ -124,6 +124,25 @@ public:
 	~PrinterJob()                                       {}
 };
 
+struct NilGUI : VirtualGui {
+	virtual dword       GetOptions();
+	virtual Size        GetSize();
+	virtual dword       GetMouseButtons();
+	virtual dword       GetModKeys();
+	virtual bool        IsMouseIn();
+	virtual bool        ProcessEvent(bool *quit);
+	virtual void        WaitEvent(int ms);
+	virtual bool        IsWaitingEvent();
+	virtual void        WakeUpGuiThread();
+	virtual void        SetMouseCursor(const Image& image);
+	virtual SystemDraw& BeginDraw();
+	virtual void        CommitDraw();
+	virtual void        Quit();
+
+	NilDraw       nildraw;
+	SystemDraw    sysdraw;
+};
+
 }
 
 #define GUIPLATFORM_INCLUDE_AFTER <VirtualGui/After.h>
