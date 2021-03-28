5f93d662f7be0416e64d0aa47804004f9309a37c
cxl <cxl@f0d560ea-af0d-0410-9eb7-867de7ffcac7>
2021-03-28 13:33:23 +0000

    .uppbox: debian now installs xdotool
    git-svn-id: svn://ultimatepp.org/upp/trunk@15876 f0d560ea-af0d-0410-9eb7-867de7ffcac7

--- uppbox/MakePosixInstall/scripts.cpp
+++ uppbox/MakePosixInstall/scripts.cpp
@@ -12,7 +12,7 @@ AskContinue()
 uname=`uname`
 
 if [ -x "$(command -v apt-get)" ]; then
-  DEP="apt-get install g++ clang make libgtk-3-dev libnotify-dev libbz2-dev libssl-dev"
+  DEP="apt-get install g++ clang make libgtk-3-dev libnotify-dev libbz2-dev libssl-dev xdotool"
 elif [ -x "$(command -v yum)" ]; then
   DEP="yum install gcc-c++ clang make gtk3-devel libnotify-devel bzip2-devel freetype-devel openssl-devel"
 elif [ -x "$(command -v dnf)" ]; then
