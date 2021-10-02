d043d0996401d5068ffd74958bc5ff47141a6ed9
Mirek Fidler <mirek.fidler@gmail.com>
2021-10-02 09:40:05 +0200

    CtrlLib: Fixed issue with key_source.h and t.h (causing wrong texts)

--- uppsrc/Core/t_.h
+++ uppsrc/Core/t_.h
@@ -1,6 +1,3 @@
-#ifndef t_h
-#define t_h
-
 #ifdef t_
 #undef t_
 #endif
@@ -9,9 +6,10 @@
 #undef tt_
 #endif
 
+#ifndef t_h
+#define t_h
+
 inline const char *t_(const char *s)  { return t_GetLngString(s); }
 inline const char *tt_(const char *s) { return s; }
 
-// #define t_(x)          t_GetLngString(x)
-// #define tt_(x)         x
 #endif
