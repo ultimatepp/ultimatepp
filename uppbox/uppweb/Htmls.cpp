dbf46a0a1374eda3477d76836dc0389f337a573c
Zbigniew Rębacz <klugier@yahoo.com>
2022-01-22 04:29:06 +0100

    UppWeb: lang information is now attached to each webpage.

--- uppbox/uppweb/Htmls.cpp
+++ uppbox/uppweb/Htmls.cpp
@@ -31,15 +31,6 @@ String ToHtml(const char *s)
 	return result;
 }
 
-/*
-Htmls& Htmls::Qtf(const char *qtf) {
-	HtmlDocOut html;
-	html.Qtf(qtf);
-	Cat(html.Get());
-	return *this;
-}
-*/
-
 Htmls& Htmls::Text(const char *s)                  { Cat(ToHtml(s)); return *this; }
 Htmls& Htmls::Quote(const char *s)                 { Cat('\"' + ToHtml(s) + '\"'); return *this; }
 Htmls& Htmls::Color(class Color color)             { Quote(ColorToHtml(color)); return *this; }
@@ -311,24 +302,6 @@ HtmlTag HtmlCell() {
 	return HtmlTag("TD");
 }
 
-/*
-HtmlTag HtmlFont(Font font) {
-	HtmlTag tag;
-	if(font.IsBold())
-		tag /= HtmlTag("B");
-	if(font.IsItalic())
-		tag /= HtmlTag("I");
-	if(font.IsUnderline())
-		tag /= HtmlTag("U");
-	if(font.IsStrikeout())
-		tag /= HtmlTag("S");
-	HtmlTag fnt("FONT");
-	fnt.Face(FontFaceToHtml(font));
-	fnt.Size(FontSizeToHtml(font));
-	return tag / fnt;
-}
-*/
-
 HtmlTag HtmlLink(const char *link)
 {
 	return HtmlTag("A").Href(link);
@@ -355,7 +328,7 @@ Htmls HtmlBlock(Htmls html, double width, double left, double top, double right,
 HtmlTag HtmlHeader(const char *title, String css, const char *other)
 {
 	String h =
-		"HTML>\r\n"
+		"HTML lang=\"en-us\">\r\n"
 	    "<HEAD>\t\n"
 	    "<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=utf-8\">\t\n"
 	    "<META NAME=\"Generator\" CONTENT=\"U++ HTML Package\">\t\n"
@@ -377,23 +350,6 @@ Htmls operator+(const HtmlTag& tag1, const HtmlTag& tag2)
 	h.Cat(tag2);
 	return h;
 }
-/*
-Htmls operator+(const String& s, const HtmlTag& tag)
-{
-	Htmls h;
-	h.Cat(s);
-	h.Cat(tag);
-	return h;
-}
-
-Htmls operator+(const HtmlTag& tag, const String& s)
-{
-	Htmls h;
-	h.Cat(tag);
-	h.Cat(s);
-	return h;
-}
-*/
 
 Htmls operator+(const char *s, const HtmlTag& tag) {
 	Htmls h;
