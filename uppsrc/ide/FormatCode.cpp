e2156ded4e0fbcf906e46b702f9d549b711e9d8f
Zbigniew Rębacz <klugier@yahoo.com>
2022-12-25 23:24:38 +0100

    Working on clang-format integration.

--- uppsrc/ide/FormatCode.cpp
+++ uppsrc/ide/FormatCode.cpp
@@ -54,29 +54,165 @@ void Ide::FormatJSON_XML(bool xml)
 void Ide::FormatJSON() { FormatJSON_XML(false); }
 void Ide::FormatXML() { FormatJSON_XML(true); }
 
+class ClangFormat final {
+public:
+	struct Output {
+		struct Replacment : Moveable<Replacment> {
+			String m_data;
+			int m_offset;
+			int m_length;
+		};
+		
+		String m_raw_result;
+		int m_code;
+		
+		bool IsSuccessful() const { return m_code == 0; }
+		Vector<Replacment> FindReplacments() const;
+	};
+	
+	struct Parameters {
+		String m_file;
+		int m_offset = Null;
+		int m_length = Null;
+		bool m_output_replacments_xml = false;
+	};
+	
+public:
+	ClangFormat(Ide* ide);
+	
+	Output Execute(const Parameters& params);
+
+private:
+	Output Execute0(const String& cmd);
+	
+	static String GenerateClangFormatNotFoundErrorMsg();
+	
+private:
+	String m_file;
+	int m_offset;
+	int m_length;
+	
+	Host m_host;
+	Ide* m_ide;
+};
+
+Vector<ClangFormat::Output::Replacment> ClangFormat::Output::FindReplacments() const
+{
+	Vector<Replacment> replacmenets;
+	
+	XmlParser p(m_raw_result);
+	while(!p.IsTag()) {
+		p.Skip();
+	}
+	p.PassTag("replacements");
+	while (!p.End()) {
+		if(p.TagE("replacement")) {
+			Replacment replacment;
+			replacment.m_offset = p.Int("offset");
+			replacment.m_length = p.Int("length");
+			replacment.m_data = p.ReadText();
+			replacmenets.Add(replacment);
+		}
+			
+		p.Skip();
+	}
+	
+	return replacmenets;
+}
+
+ClangFormat::ClangFormat(Ide* ide)
+	: m_ide(ide)
+{
+	m_ide->CreateHost(m_host);
+}
+
+ClangFormat::Output ClangFormat::Execute(const Parameters& params)
+{
+	String cmd = "clang-format ";
+	if (params.m_output_replacments_xml) {
+		cmd << "--output-replacements-xml ";
+	}
+	if (!IsNull(params.m_offset) && !IsNull(params.m_length)) {
+		cmd << "--offset=" << IntStr(params.m_offset) << " --length=" << IntStr(params.m_length) << " ";
+	}
+	cmd << params.m_file;
+	m_ide->PutConsole(cmd);
+	return Execute0(cmd);
+}
+
+ClangFormat::Output ClangFormat::Execute0(const String& cmd)
+{
+	StringStream ss;
+	int code = m_host.Execute(cmd, ss);
+	if(code != 0) {
+		m_ide->ConsoleClear();
+		m_ide->ConsoleShow();
+		if(IsNull(code)) {
+			PutConsole(String() << "Error: " << GenerateClangFormatNotFoundErrorMsg());
+		} else {
+			PutConsole(String() << "Error: clang-format ended with \"" << IntStr(code) << "\" error code.");
+			if (!ss.GetResult().IsEmpty()) {
+				PutConsole(String() << "\nProgram output:\n" << ss.GetResult());
+			}
+		}
+		m_ide->BeepMuteExclamation();
+		return {"", code};
+	}
+	return {ss.GetResult(), code};
+}
+
+String ClangFormat::GenerateClangFormatNotFoundErrorMsg() {
+#ifdef PLATFORM_WIN32
+	return "Failed to find clang-format command. Make sure you have valid TheIDE installation and clang-format is available under bin folder.";
+#else
+	return "Failed to find clang-format command. Make sure it is added to the enviroment variables.";
+#endif
+}
+
 void Ide::ReformatFile()
 {
 	SaveFile();
 
-	String cmd;
-	cmd << "clang-format " << editfile;
+	int l, h;
+	bool sel = editor.GetSelection(l, h);
 
-	Host host;
-	CreateHost(host);
+	ClangFormat::Parameters params;
+	params.m_file = editfile;
+	if (sel) {
+		params.m_offset = l;
+		params.m_length = h - l;
+	}
+	params.m_output_replacments_xml = true;
 
-	StringStream ss;
-	int code = host.Execute(cmd, ss);
-	if(code < 0) {
-		ErrorOK(IntStr(code));
+	ClangFormat clang_format(this);
+	auto output = clang_format.Execute(params);
+	if (!output.IsSuccessful()) {
 		return;
 	}
-
-	PutConsole(cmd);
-	PutConsole(IntStr(code));
-
+	
+	Vector<ClangFormat::Output::Replacment> replacmenets;
+	try {
+		replacmenets = output.FindReplacments();
+	} catch(const XmlError& e) {
+		PutConsole(e); // TODO: Better error handling...
+		return;
+	}
+	PutConsole(String() << "Replacmenets: " << replacmenets.GetCount());
+	
 	editor.NextUndo();
-	editor.Remove(0, editor.GetLength());
-	editor.Insert(0, String(ss));
+	for(const auto& replacmenet : replacmenets) {
+		
+	}
+	
+	editor.NextUndo();
+	if(sel) {
+		editor.Remove(l, h - l);
+		editor.SetSelection(l, l + editor.Insert(l, output.m_raw_result));
+	}
+	else {
+		editor.Remove(0, editor.GetLength());
+		editor.Insert(0, output.m_raw_result);
+	}
 }
 
 void Ide::ReformatComment() { editor.ReformatComment(); }
