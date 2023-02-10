9a4b58a8a912c7b1b6d985e39c700d03f885a664
Mirek Fidler <mirek.fidler@gmail.com>
2023-02-10 13:05:27 +0100

    .autotest

--- autotest/SetEnvVar/SetEnvVar.cpp
+++ autotest/SetEnvVar/SetEnvVar.cpp
@@ -4,6 +4,8 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
+	StdLogSetup(LOG_COUT|LOG_FILE);
+	
 	String cmd, out;
 	
 #ifdef PLATFORM_WIN32
@@ -12,11 +14,13 @@ CONSOLE_APP_MAIN
 	SetEnv("FOOBAR","BAZ");
 	ASSERT(Sys(cmd) == "1 BAZ 2\r\n");
 #else
-	cmd = "echo \"1 $FOOBAR 2\"";
-	ASSERT(Sys(cmd) == "1  2\n");
+	cmd = "bash -c \"echo 1 $FOOBAR 2\"";
+	ASSERT(Sys(cmd) == "1 2\n");
 	SetEnv("FOOBAR","BAZ");
 	ASSERT(Sys(cmd) == "1 BAZ 2\n");
 #endif
 	
 	ASSERT(GetEnv("FOOBAR") == "BAZ");
+	
+	LOG("================= OK");
 }
