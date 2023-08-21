97da5df41e9c99e9de5988ddfb9d94a00dc46ed4
Mirek Fidler <mirek.fidler@gmail.com>
2023-08-21 10:11:13 +0200

    More UHD icons

--- uppsrc/ide/idebar.cpp
+++ uppsrc/ide/idebar.cpp
@@ -482,7 +482,7 @@ void Ide::Project(Bar& menu)
 			menu.Add(AK_MAINCONFIG, IdeImg::main_package(), THISBACK(MainConfig))
 				.Help("Configuring compiler, operating system, output application parameters, custom flags");
 		menu.Separator();
-		menu.Add(AK_SYNCT, IdeImg::Language(), THISBACK1(SyncT, 0))
+		menu.AddMenu(AK_SYNCT, IdeImg::Language(), THISBACK1(SyncT, 0))
 		    .Help("Synchronize all language translation files of current workspace");
 		menu.AddMenu(AK_TRIMPORT, IdeImg::Language(), THISBACK1(SyncT, 1))
 		    .Help("Import runtime translation file");
