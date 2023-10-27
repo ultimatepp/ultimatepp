e029af614ff3e46266d4410226a188d24d2f7583
Zbigniew Rębacz <klugier@yahoo.com>
2023-10-27 23:44:46 +0200

    CtrlLib, Doc: added tutorial about time callback (23).

--- tutorial/Gui23/main.cpp
+++ tutorial/Gui23/main.cpp
@@ -1,40 +1,47 @@
 #include <CtrlLib/CtrlLib.h>
 
-#define TFILE <Gui23/Gui23.t>
-#include <Core/t.h>
-
 using namespace Upp;
 
 struct MyAppWindow : TopWindow {
-    MyAppWindow() {
-        Title(t_("My application"));
-        Zoomable().Sizeable().SetRect(0, 0, 550, 100);
-    }
-    
-    void Paint(Draw& w) override {
-        w.DrawRect(GetSize(), SLtYellow);
-        w.DrawText(20, 20, t_("Hello translation engine!"), Arial(30), Blue);
-    }
+	Rect  rect;
+	Color color;
+	
+	MyAppWindow() {
+		Zoomable().SetRect(0, 0, 200, 200);
+		
+		RandomizeRect();
+		SetTimeCallback(-2000, [=] { OnTimer(); });
+	}
+	
+	~MyAppWindow() {
+		KillTimeCallback();
+	}
+	
+	void Paint(Draw& w) override {
+		Size sz = GetSize();
+		
+		w.DrawRect(sz, White());
+		w.DrawRect(rect, color);
+	}
+	
+	void OnTimer() {
+		RandomizeRect();
+		Refresh();
+	}
+	
+	void RandomizeRect() {
+		Size sz = GetSize();
+		
+		int length = 50;
+		int x = Random() % (sz.cx - length);
+		int y = Random() % (sz.cy - length);
+		
+		rect = Rect(x, y, x + length, y + length);
+		color = Color(Random() % 255, Random() % 255, Random() % 255);
+	}
 };
 
 GUI_APP_MAIN
 {
-	// Set system language for whole application
-	SetLanguage(GetSystemLNG());
-    
-    // Uncomment to force specific language...
-    // SetLanguage("en-us"); // English = default
-    // SetLanguage("cs-cz"); // Czech
-    // SetLanguage("de-de"); // German
-    // SetLanguage("es-es"); // Spanish
-    // SetLanguage("fr-fr"); // French
-    // SetLanguage("it-it"); // Italian
-    // SetLanguage("ja-jp"); // Japanese
-    // SetLanguage("pl-pl"); // Polish
-    // SetLanguage("pt-pt"); // Portuguese
-    // SetLanguage("ru-ru"); // Russian
-    // SetLanguage("tr-tr"); // Turkish
-    // SetLanguage("zh-cn"); // Traditional Chinese
-    
-    MyAppWindow().Run();
+	MyAppWindow().Run();
 }
