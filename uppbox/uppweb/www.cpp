80a3237686b42be690112b7233767c6e0f5a885e
Zbigniew Rębacz <klugier@yahoo.com>
2023-10-28 19:07:04 +0200

    UppWeb: update information about release in various places.

--- uppbox/uppweb/www.cpp
+++ uppbox/uppweb/www.cpp
@@ -910,7 +910,7 @@ CONSOLE_APP_MAIN
 	if (outHtml)
 		SaveFile(AppendFileName(targetdir, "sdj.gif"), LoadFile(GetRcFile("sdj.gif")));
 	
-	String release = "16847";
+	String release = "17045";
 	escape.Add("RELEASE", release);
 	escape.Add("RELEASET", release);
 	escape.Add("UPDATETIME", Format("%`", GetUtcTime()));
