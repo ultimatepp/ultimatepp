3d4911c26622beafa47fe03096e7c748b005e7ce
Mirek Fidler <mirek.fidler@gmail.com>
2023-11-09 19:32:10 +0100

    ide: Full block caret

--- uppsrc/CtrlLib/LineEdit.cpp
+++ uppsrc/CtrlLib/LineEdit.cpp
@@ -22,6 +22,7 @@ LineEdit::LineEdit() {
 	overwrite = false;
 	filter = NULL;
 	showspaces = false;
+	block_caret = false;
 	showlines = false;
 	showreadonly = true;
 	dorectsel = false;
@@ -810,7 +811,7 @@ Rect LineEdit::GetCaret() const
 	if(overwrite)
 		return RectC(caretpos.x, caretpos.y + fsz.cy - 2, fsz.cx, 2);
 	else
-		return RectC(caretpos.x, caretpos.y, 2, fsz.cy);
+		return RectC(caretpos.x, caretpos.y, block_caret? fsz.cx : 2, fsz.cy);
 }
 
 void LineEdit::PlaceCaret0(Point p) {
--- uppsrc/ide/Config.cpp
+++ uppsrc/ide/Config.cpp
@@ -187,7 +187,7 @@ void Sentinel(Stream& s, const char *txt)
 
 void Ide::Serialize(Stream& s)
 {
-	int version = 28;
+	int version = 29;
 	Sentinel(s, "before 12341234");
 	s.Magic(0x12341234);
 	Sentinel(s, "after magic");
@@ -225,6 +225,8 @@ void Ide::Serialize(Stream& s)
 	s % show_tabs;
 	if(version >= 18)
 		s % show_spaces;
+	if(version >= 29)
+		s % block_caret;
 	if(version >= 7)
 		s % warnwhitespace;
 	s % tabs_icons;
