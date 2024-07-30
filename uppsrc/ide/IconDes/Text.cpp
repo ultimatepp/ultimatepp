30bf5b5d26898b0c0c5771821e1f0d94d6f3bb1c
Mirek Fidler <mirek.fidler@gmail.com>
2024-07-30 15:20:57 +0200

    ide: IconDes Text improvements

--- uppsrc/ide/IconDes/Text.cpp
+++ uppsrc/ide/IconDes/Text.cpp
@@ -81,6 +81,7 @@ void IconDes::Text()
 			textdlg.NoCenter().SetRect(r);
 		}
 		Paste(CreateImage(Size(1, 1), RGBAZero()));
+		textdlg.nonaa = Current().flags & IML_IMAGE_FLAG_S3;
 		textdlg.WhenClose = THISBACK(CloseText);
 		textdlg <<= THISBACK(PasteText);
 		textdlg.Open();
