f74fe22f8bd4fc22fa0f297dc21759d2f312fb6b
Mirek Fidler <mirek.fidler@gmail.com>
2024-11-27 11:19:55 +0100

    reference: ThemeChangeSensitive

--- reference/ThemeChangeSensitive/main.cpp
+++ reference/ThemeChangeSensitive/main.cpp
@@ -13,22 +13,36 @@ using namespace Upp;
 struct MyApp : public WithMyAppLayout<TopWindow> {
 	virtual void Skin();
 	
+	Image MakeImage();
+	
 	MyApp();
 };
 
 #endif
 
+Image MyApp::MakeImage()
+{ // make some image in a way that reflects dark/light mode
+	Size sz = CtrlImg::HandCursor().GetSize();
+	ImageDraw iw(sz.cx, sz.cy);
+	iw.DrawRect(sz, SColorFace());
+	iw.DrawImage(0, 0, CtrlImg::HandCursor()); // HandCursor is adjusted wrt dark/light
+	iw.DrawRect(sz.cx / 2, sz.cy / 2, 5, 5, SColorHighlight()); // SColorHighlight is adjusted wrt dark/light
+	return iw;
+}
+
 void MyApp::Skin()
-{ // called on opening window or on skin change
+{ // called on opening window and on theme change
 	list4.Clear(); // just refill the list with new colors
 	list4.Add(AttrText("Blend(SRed, SYellow)").NormalInk(Blend(SRed(), SYellow())).Bold());
 	list4.Add(AttrText("SLtBlue").NormalInk(SBlue().Resolved()).Bold()); // Resolved converts SBlue to normal color, just as does Blend
 	list4.Add(AttrText("Blend(SRed, SLtBlue)").NormalInk(Blend(SRed(), SLtBlue())).Bold());
+	
+	img3.SetImage(MakeImage());
 }
 
 MyApp::MyApp()
 {
-	CtrlLayout(*this, "Window title");
+	CtrlLayout(*this, "How to dynamically react to theme changes");
 	list1.AddColumn("Ignoring skin change");
 	list1.NoCursor();
 	list1.Add(AttrText("Blend(Red, Yellow)").NormalInk(Blend(SRed(), SYellow())).Bold());
@@ -37,8 +51,8 @@ MyApp::MyApp()
 	
 	list2.AddColumn("Using AColor");
 	list2.NoCursor();
-	list2.Add(AttrText("Blend(Red, Yellow)").NormalInk(AColor(Blend(Red(), Yellow()))).Bold());
-	list2.Add(AttrText("SLtBlue").NormalInk(AColor(Blue())).Bold()); // Resolved converts SBlue to normal color, just as does Blend
+	list2.Add(AttrText("Blend(Red, Yellow)").NormalInk(AColor(Blend(Red(), Yellow()))).Bold()); // Light theme color that gets adjusted if theme is dark
+	list2.Add(AttrText("SLtBlue").NormalInk(AColor(Blue())).Bold());
 	list2.Add(AttrText("Blend(Red, LtBlue)").NormalInk(AColor(Blend(Red(), LtBlue()))).Bold());
 
 	list3.AddColumn("Using SColor");
@@ -51,10 +65,26 @@ MyApp::MyApp()
 	
 	list4.AddColumn("Reloaded with Skin");
 	list4.NoCursor();
+	
+	img1.SetImage(CtrlImg::HandCursor()); // iml image constants react to theme changes (even theirs copies)
+	img2.SetImage(MakeImage()); // the image copy is stored just once and does not reflect theme changes
+	
+	std << [=] { Ctrl::SetSkin(ChStdSkin); };
+	dark << [=] { Ctrl::SetSkin(ChDarkSkin); };
+	host << [=] { Ctrl::SetSkin(ChHostSkin); };
+	
+	toggle << [=] { Ctrl::SwapDarkLight(); };
+	
+#ifndef _DEBUG
+	info = "Predefined key for Toggle dark was set to Space";
+#endif
 }
 
 GUI_APP_MAIN
 {
+#ifndef _DEBUG
+	Ctrl::SwapDarkLightKey(K_SPACE);
+#endif
 	Ctrl::SkinChangeSensitive(); // activate changes of skin (theme) when host theme changes
 	MyApp().Run();
 }
