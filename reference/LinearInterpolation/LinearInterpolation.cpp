d29b51dfd99eaec28bdc86486e21b6fc1a8ae2f9
Mirek Fidler <mirek.fidler@gmail.com>
2024-12-09 09:06:15 +0100

    Core: Lerp(Color)

--- reference/LinearInterpolation/LinearInterpolation.cpp
+++ reference/LinearInterpolation/LinearInterpolation.cpp
@@ -6,13 +6,17 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
-	StdLogSetup(LOG_COUT);
+	StdLogSetup(LOG_COUT|LOG_FILE);
 	
     DUMP(Lerp(10, 20, 0.5));
     DUMP(Lerp(10.0, 20.0, 0.25));
     DUMP(Lerp(Point(10, 20), Point(30, 40), 0.5));
     DUMP(Lerp(Size(100, 200), Size(300, 400), 0.5));
     DUMP(Lerp(Rect(0, 0, 100, 100), Rect(100, 100, 200, 200), 0.5));
+
+    DUMP(Lerp(Color(100, 200, 150), Color(200, 100, 120), 0.5));
+    DUMP(Lerp(Color(100, 200, 150), Color(200, 100, 120), 1.5));
+    DUMP(Lerp(Color(100, 200, 150), Color(200, 100, 120), -1));
 }
 
 
