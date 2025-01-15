4098183ed5dcc184da47ab3541b82d1757ab7ccc
Mirek Fidler <mirek.fidler@gmail.com>
2025-01-15 09:37:38 +0100

    ide: Json view local menu to copy path / node

--- uppsrc/ide/Json.cpp
+++ uppsrc/ide/Json.cpp
@@ -23,8 +23,6 @@ public:
 private:
 	void   Reset();
 	String Load0(const String& json);
-	
-	int AddNode(int parent_id, const Value& id, const String& name, const Value& v);
 };
 
 JsonView::JsonView()
@@ -40,9 +38,8 @@ JsonView::JsonView()
 	errorbg.Height(25).Add(error.SizePos());
 	view.SetReadOnly();
 	view.SetColor(LineEdit::PAPER_READONLY, SColorPaper());
-	tree.SetDisplay(QTFDisplay());
 	tree.NoRoot();
-	tree.WhenLeftDouble = THISBACK(CopyPath);
+	SetupJsonTree(tree);
 }
 
 bool JsonView::Key(dword key, int count)
@@ -76,19 +73,7 @@ void JsonView::Load(const String& json)
 
 void JsonView::CopyPath()
 {
-	int id = tree.GetCursor();
-	String path;
-	while(id) {
-		Value k = tree.Get(id);
-		if(!IsNull(k)) {
-			if(IsNumber(k))
-				path = "[" + AsString(k) + "]" + path;
-			if(IsString(k))
-				path = "[" + AsCString(String(k)) + "]" + path;
-		}
-		id = tree.GetParent(id);
-	}
-	WriteClipboardText(path);
+	::CopyJsonPath(tree);
 }
 
 void JsonView::Serialize(Stream& s)
@@ -115,61 +100,7 @@ void JsonView::Reset()
 
 String JsonView::Load0(const String& json)
 {
-	String parsingError;
-	
-	CParser p(json);
-	try {
-		tree.Open(AddNode(0, Null, "JSON", ParseJSON(p)));
-	}
-	catch(const CParser::Error& e) {
-		parsingError << e;
-	}
-	
-	return parsingError;
-}
-
-int JsonView::AddNode(int parent_id, const Value& id, const String& name, const Value& v)
-{
-	if(IsError(v)) {
-		// TODO: Replace with JsonExc or something that is more accurate in this situation.
-		String errorText = GetErrorText(v);
-		errorText.Remove(0, errorText.Find(" ") + 1);
-		throw Exc(errorText);
-	}
-	else
-	if(v.Is<ValueMap>()) {
-		ValueMap m = v;
-		parent_id = tree.Add(parent_id, IdeCommonImg::JsonStruct(), id, "[G1 [* " + name);
-		for(int i = 0; i < m.GetCount(); i++)
-			AddNode(parent_id, m.GetKey(i), "[@B \1" + String(m.GetKey(i)) + "\1:]", m.GetValue(i));
-	}
-	else
-	if(v.Is<ValueArray>()) {
-		parent_id = tree.Add(parent_id, IdeCommonImg::JsonArray(), id, "[G1 [* " + name);
-		for(int i = 0; i < v.GetCount(); i++)
-			AddNode(parent_id, i, "[@c " + AsString(i) + ":]", v[i]);
-	}
-	else {
-		String qtf = "[G1 [* " + name + "]";
-		Image img = IdeCommonImg::JsonNumber();
-		if(IsString(v)) {
-			img = IdeCommonImg::JsonString();
-			if(IsNull(v))
-				qtf << "[*@g  Null";
-			else
-				qtf << "[@r \1 " + AsCString(String(v));
-		}
-		else {
-			if(v.Is<bool>())
-				img = IdeCommonImg::JsonBool();
-			if(IsNull(v))
-				qtf << "[*@g  Null";
-			else
-				qtf << "\1 " + AsString(v);
-		}
-		parent_id = tree.Add(parent_id, img, id, qtf);
-	}
-	return parent_id;
+	return LoadJson(tree, json);
 }
 
 void Ide::Json()
