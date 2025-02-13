88e5c7ff289bcb6bdbfccc4537c9b9fcdcc786e0
Mirek Fidler <mirek.fidler@gmail.com>
2025-02-13 10:45:00 +0100

    .upptst

---
+++ upptst/WindowPosWayland/main.cpp
@@ -0,0 +1,31 @@
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+struct MyApp : TopWindow {
+	TimeCallback tm;
+	virtual void Paint(Draw& w) {
+		Size sz = GetSize();
+		w.DrawRect(GetSize(), SWhite());
+		int y = 0;
+		String txt;
+		#define DMP(x)  txt << #x << " " << x << "\n";
+		DMP(GetScreenRect());
+		DMP(GetVirtualScreenArea());
+		DMP(GetVirtualWorkArea());
+		DMP(GetWorkArea());
+		DMP(GetMousePos());
+		
+		DrawSmartText(w, 0, 0, sz.cx, txt);
+	}
+	
+	MyApp() {
+		Sizeable().Zoomable();
+		tm.Set(-200, [=] { Refresh(); });
+	}
+};
+
+GUI_APP_MAIN
+{
+	MyApp().Run();
+}
