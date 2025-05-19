7d9bb8f91b606566ae2a5d30b67cdaa9a14d6fae
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2025-05-19 14:46:41 +0300

    Core/SSL: AES module further refactored (getting ready for masterkey encryption support)

--- uppsrc/Core/SSL/SSL.h
+++ uppsrc/Core/SSL/SSL.h
@@ -6,7 +6,7 @@
 #include <openssl/engine.h>
 
 namespace Upp {
-	
+    
 INITIALIZE(SSL);
 INITIALIZE(SSLSocket);
 INITIALIZE(P7S);
@@ -16,125 +16,125 @@ void SslInitThread();
 class SslBuffer
 {
 public:
-	SslBuffer(BUF_MEM *m = NULL) : buf_mem(m) {}
-	~SslBuffer()                              { Clear(); }
+    SslBuffer(BUF_MEM *m = NULL) : buf_mem(m) {}
+    ~SslBuffer()                              { Clear(); }
 
-	bool     IsEmpty() const                  { return !buf_mem; }
+    bool     IsEmpty() const                  { return !buf_mem; }
 
-	bool     Set(BUF_MEM *b)                  { Clear(); return !!(buf_mem = b); }
-	bool     Create()                         { return Set(BUF_MEM_new()); }
-	void     Clear()                          { if(buf_mem) { BUF_MEM_free(buf_mem); buf_mem = NULL; } }
-	BUF_MEM *Detach()                         { BUF_MEM *b = buf_mem; buf_mem = NULL; return b; }
+    bool     Set(BUF_MEM *b)                  { Clear(); return !!(buf_mem = b); }
+    bool     Create()                         { return Set(BUF_MEM_new()); }
+    void     Clear()                          { if(buf_mem) { BUF_MEM_free(buf_mem); buf_mem = NULL; } }
+    BUF_MEM *Detach()                         { BUF_MEM *b = buf_mem; buf_mem = NULL; return b; }
 
-	bool     Grow(int length);
+    bool     Grow(int length);
 
-	String   Get() const;
-	bool     Set(const String& d);
+    String   Get() const;
+    bool     Set(const String& d);
 
-	operator BUF_MEM * () const { return buf_mem; }
+    operator BUF_MEM * () const { return buf_mem; }
 
 private:
-	BUF_MEM *buf_mem;
+    BUF_MEM *buf_mem;
 };
 
 class SslStream
 {
 public:
-	SslStream(BIO *b = NULL) : bio(b)        {}
-	~SslStream()                             { Clear(); }
+    SslStream(BIO *b = NULL) : bio(b)        {}
+    ~SslStream()                             { Clear(); }
 
-	bool     IsEmpty() const                 { return !bio; }
+    bool     IsEmpty() const                 { return !bio; }
 
-	bool     Set(BIO *b)                     { Clear(); return !!(bio = b); }
+    bool     Set(BIO *b)                     { Clear(); return !!(bio = b); }
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
-	bool     Create(const BIO_METHOD *meth)  { return Set(BIO_new(meth)); }
+    bool     Create(const BIO_METHOD *meth)  { return Set(BIO_new(meth)); }
 #else
-	bool     Create(BIO_METHOD *meth)        { return Set(BIO_new(meth)); }
+    bool     Create(BIO_METHOD *meth)        { return Set(BIO_new(meth)); }
 #endif
-	void     Clear()                         { if(bio) { BIO_free(bio); bio = NULL; } }
+    void     Clear()                         { if(bio) { BIO_free(bio); bio = NULL; } }
 
-	bool     OpenBuffer(const char *data, int length);
-	bool     CreateBuffer();
-	String   GetResult() const;
+    bool     OpenBuffer(const char *data, int length);
+    bool     CreateBuffer();
+    String   GetResult() const;
 
-	operator BIO * () const                  { return bio; }
+    operator BIO * () const                  { return bio; }
 
 private:
-	BIO     *bio;
+    BIO     *bio;
 };
 
 class SslKey
 {
 public:
-	SslKey(EVP_PKEY *k = NULL) : key(k) {}
-	~SslKey()                           { Clear(); }
+    SslKey(EVP_PKEY *k = NULL) : key(k) {}
+    ~SslKey()                           { Clear(); }
 
-	bool      IsEmpty() const            { return !key; }
+    bool      IsEmpty() const            { return !key; }
 
-	bool      Set(EVP_PKEY *k)           { Clear(); return !!(key = k); }
-	void      Clear()                    { if(key) { EVP_PKEY_free(key); key = NULL; } }
-	EVP_PKEY *Detach()                   { EVP_PKEY *k = key; key = NULL; return k; }
+    bool      Set(EVP_PKEY *k)           { Clear(); return !!(key = k); }
+    void      Clear()                    { if(key) { EVP_PKEY_free(key); key = NULL; } }
+    EVP_PKEY *Detach()                   { EVP_PKEY *k = key; key = NULL; return k; }
 
-	operator  EVP_PKEY * () const        { return key; }
+    operator  EVP_PKEY * () const        { return key; }
 
-	bool      Load(const String& data);
+    bool      Load(const String& data);
 
 private:
-	EVP_PKEY *key;
+    EVP_PKEY *key;
 };
 
 class SslCertificate
 {
 public:
-	SslCertificate(X509 *c = NULL) : cert(c) {}
-	~SslCertificate()                        { Clear(); }
+    SslCertificate(X509 *c = NULL) : cert(c) {}
+    ~SslCertificate()                        { Clear(); }
 
-	bool     IsEmpty() const                 { return !cert; }
+    bool     IsEmpty() const                 { return !cert; }
 
-	bool     Set(X509 *c)                    { Clear(); return !!(cert = c); }
-	bool     Create()                        { return Set(X509_new()); }
-	void     Clear()                         { if(cert) { X509_free(cert); cert = NULL; } }
-	X509    *Detach()                        { X509 *c = cert; cert = NULL; return c; }
+    bool     Set(X509 *c)                    { Clear(); return !!(cert = c); }
+    bool     Create()                        { return Set(X509_new()); }
+    void     Clear()                         { if(cert) { X509_free(cert); cert = NULL; } }
+    X509    *Detach()                        { X509 *c = cert; cert = NULL; return c; }
 
-	bool     Load(const String& data, bool asn1 = false);
-	String   Save(bool asn1 = false) const;
+    bool     Load(const String& data, bool asn1 = false);
+    String   Save(bool asn1 = false) const;
 
-	String   GetSubjectName() const;
-	String   GetIssuerName() const;
-	Date     GetNotBefore() const;
-	Date     GetNotAfter() const;
-	int      GetVersion() const;
-	String   GetSerialNumber() const;
+    String   GetSubjectName() const;
+    String   GetIssuerName() const;
+    Date     GetNotBefore() const;
+    Date     GetNotAfter() const;
+    int      GetVersion() const;
+    String   GetSerialNumber() const;
 
-	operator X509 * () const                 { return cert; }
+    operator X509 * () const                 { return cert; }
 
 private:
-	X509    *cert;
+    X509    *cert;
 };
 
 class SslContext
 {
 public:
-	SslContext(SSL_CTX *c = NULL);
-	~SslContext()                              { Clear(); }
+    SslContext(SSL_CTX *c = NULL);
+    ~SslContext()                              { Clear(); }
 
-	bool     IsEmpty() const                   { return !ssl_ctx; }
+    bool     IsEmpty() const                   { return !ssl_ctx; }
 
-	bool     Set(SSL_CTX *c)                   { Clear(); return !!(ssl_ctx = c); }
-	bool     Create(SSL_METHOD *meth)          { return Set(SSL_CTX_new(meth)); }
-	void     Clear()                           { if(ssl_ctx) { SSL_CTX_free(ssl_ctx); ssl_ctx = NULL; } }
-	SSL_CTX *Detach()                          { SSL_CTX *c = ssl_ctx; ssl_ctx = NULL; return c; }
+    bool     Set(SSL_CTX *c)                   { Clear(); return !!(ssl_ctx = c); }
+    bool     Create(SSL_METHOD *meth)          { return Set(SSL_CTX_new(meth)); }
+    void     Clear()                           { if(ssl_ctx) { SSL_CTX_free(ssl_ctx); ssl_ctx = NULL; } }
+    SSL_CTX *Detach()                          { SSL_CTX *c = ssl_ctx; ssl_ctx = NULL; return c; }
 
-	operator SSL_CTX * () const                { return ssl_ctx; }
+    operator SSL_CTX * () const                { return ssl_ctx; }
 
-	bool     CipherList(const char *list);
-	bool     UseCertificate(String certificate, String private_key, bool cert_asn1 = false);
-	void     VerifyPeer(bool verify = true, int depth = 2);
-	
-	bool     UseCAcert(String ca_cert, bool cert_asn1 = false);
+    bool     CipherList(const char *list);
+    bool     UseCertificate(String certificate, String private_key, bool cert_asn1 = false);
+    void     VerifyPeer(bool verify = true, int depth = 2);
+    
+    bool     UseCAcert(String ca_cert, bool cert_asn1 = false);
 
 private:
-	SSL_CTX *ssl_ctx;
+    SSL_CTX *ssl_ctx;
 };
 
 String SslGetLastError(int& code);
@@ -146,8 +146,6 @@ String Asn1ToString(ASN1_STRING *s);
 constexpr const int AES_GCM_MIN_ITERATION     = 10000;
 constexpr const int AES_GCM_MAX_ITERATION     = 1000000;
 constexpr const int AES_GCM_DEFAULT_ITERATION = 100000;
-
-INITIALIZE(AES);
 
 class Aes256Gcm : NoCopy {
 public:
@@ -167,13 +165,30 @@ public:
     String GetErrorDesc() const                                         { return err; }
 
 private:
+    bool   Init(Stream& in);
+    bool   DeriveKey(const String& password, const String& salt, byte *key, int keylen);
+    
+    // Encryption
+    bool   GenerateSaltAndIV(String& salt, String& iv);
+    int    WriteHeader(Stream& out, const String& salt, const String& iv);
+    bool   EncryptStream(Stream& in, Stream& out, int64& processed);
+    bool   FinalizeEncryption(Stream& out, int64& processed);
+
+    // Decryption
+    bool   ReadHeader(Stream& in, String& salt, String& iv);
+    bool   DecryptStream(Stream& in, Stream& out, int64& processed);
+    bool   ReadTag(Stream& in, String& tag);
+    bool   SetGcmTag(const String& tag);
+    bool   FinalizeDecryption(Stream& out);
+    
     bool   EncDec(bool enc, const String& in, const String& pwd, String& out);
     void   SetError(const String& txt);
 
-    EVP_CIPHER_CTX* ctx;
-    int             chunksize;
-    int             iteration;
-    String          err;
+    EVP_CIPHER_CTX*   ctx;
+    const EVP_CIPHER* cipher;
+    int               chunksize;
+    int               iteration;
+    String            err;
 };
 
 String AES256Encrypt(const String& in, const String& password, Gate<int64, int64> WhenProgress = Null);
