214a6f4f0ddf117bf8cd0743c068c02660f1b723
Sergey Ortman <sergey@ortman.pro>
2025-11-18 18:56:46 +0500

    CtrlLib: Added SetFont/GetFont methods. Added font, color, percent, total properties to Layout Designer.

--- uppsrc/CtrlLib/Progress.cpp
+++ uppsrc/CtrlLib/Progress.cpp
@@ -67,14 +67,14 @@ void ProgressIndicator::Paint(Draw& w) {
 		w.DrawRect(r3, SColorPaper);
 		if(percent) {
 			String pt = Format("%d %%", (int)(100L * actual / max(total, 1)));
-			Size psz = GetTextSize(pt, StdFont());
+			Size psz = GetTextSize(pt, font);
 			int px = (sz.cx - psz.cx) / 2 + 2;
 			int py = (sz.cy - psz.cy) / 2 + 2;
 			w.Clip(r1);
-			w.DrawText(px, py, pt, StdFont(), SColorLight);
+			w.DrawText(px, py, pt,font, SColorLight);
 			w.End();
 			w.Clip(r2);
-			w.DrawText(px, py, pt, StdFont(), SColorText);
+			w.DrawText(px, py, pt, font, SColorText);
 			w.End();
 		}
 		w.End();
@@ -125,6 +125,7 @@ void ProgressIndicator::Set(int _actual, int _total) {
 
 ProgressIndicator::ProgressIndicator() {
 	style = &StyleDefault();
+	font = StdFont();
 	Transparent();
 	NoWantFocus();
 	total = actual = 0;
