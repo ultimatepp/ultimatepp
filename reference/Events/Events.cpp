e6c0fc4f8ece29d3213a87a1276fbfc1befc1a92
Zbigniew Rębacz <klugier@yahoo.com>
2026-01-08 23:04:08 +0100

    Reference: Events migrate to override plus fix HorzMouseWheel.

--- reference/Events/Events.cpp
+++ reference/Events/Events.cpp
@@ -19,18 +19,18 @@ struct App : TopWindow {
 		log.GoEnd();
 	}
 
-	virtual void   Activate()
+	void Activate() override
 	{
 		Log("Activate");
 		SetFocus();
 	}
 
-	virtual void   Deactivate()
+	void Deactivate() override
 	{
 		Log("Deactivate");
 	}
 
-	virtual Image  FrameMouseEvent(int event, Point p, int zdelta, dword keyflags)
+	Image FrameMouseEvent(int event, Point p, int zdelta, dword keyflags) override
 	{
 		if(event != CURSORIMAGE)
 			Log(Format("FrameMouseEvent(%d, Point(%d, %d), %d, %x)",
@@ -38,7 +38,7 @@ struct App : TopWindow {
 		return TopWindow::FrameMouseEvent(event, p, zdelta, keyflags);
 	}
 
-	virtual Image  MouseEvent(int event, Point p, int zdelta, dword keyflags)
+	Image MouseEvent(int event, Point p, int zdelta, dword keyflags) override
 	{
 		if(event != CURSORIMAGE)
 			Log(Format("MouseEvent(%d, Point(%d, %d), %d, %x)",
@@ -46,175 +46,176 @@ struct App : TopWindow {
 		return TopWindow::MouseEvent(event, p, zdelta, keyflags);
 	}
 
-	virtual void   MouseEnter(Point p, dword keyflags)
+	void MouseEnter(Point p, dword keyflags) override
 	{
 		Log(Format("MouseEnter(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   MouseMove(Point p, dword keyflags)
+	void MouseMove(Point p, dword keyflags) override
 	{
 		Log(Format("MouseMove(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   LeftDown(Point p, dword keyflags)
+	void LeftDown(Point p, dword keyflags) override
 	{
 		Log(Format("LeftDown(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 		SetFocus();
 	}
 
-	virtual void   LeftDouble(Point p, dword keyflags)
+	void LeftDouble(Point p, dword keyflags) override
 	{
 		Log(Format("LeftDouble(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void LeftTriple(Point p, dword keyflags)
+	void LeftTriple(Point p, dword keyflags) override
 	{
 		Log(Format("LeftTriple(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 	
-	virtual void LeftDrag(Point p, dword keyflags) {
+	void LeftDrag(Point p, dword keyflags) override {
 		Log(Format("LeftDrag(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 	
-	virtual void LeftHold(Point p, dword keyflags) {
+	void LeftHold(Point p, dword keyflags) override {
 		Log(Format("LeftHold(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   LeftRepeat(Point p, dword keyflags)
+	void LeftRepeat(Point p, dword keyflags) override
 	{
 		Log(Format("LeftRepeat(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   LeftUp(Point p, dword keyflags)
+	void LeftUp(Point p, dword keyflags) override
 	{
 		Log(Format("LeftUp(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   RightDown(Point p, dword keyflags)
+	void RightDown(Point p, dword keyflags) override
 	{
 		Log(Format("RightDown(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   RightDouble(Point p, dword keyflags)
+	void RightDouble(Point p, dword keyflags) override
 	{
 		Log(Format("RightDouble(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void RightTriple(Point p, dword keyflags)
+	void RightTriple(Point p, dword keyflags) override
 	{
 		Log(Format("RightTriple(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 	
-	virtual void RightDrag(Point p, dword keyflags) {
+	void RightDrag(Point p, dword keyflags) override {
 		Log(Format("RightDrag(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 	
-	virtual void RightHold(Point p, dword keyflags) {
+	void RightHold(Point p, dword keyflags) override {
 		Log(Format("RightHold(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   RightRepeat(Point p, dword keyflags)
+	void RightRepeat(Point p, dword keyflags) override
 	{
 		Log(Format("RightRepeat(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   RightUp(Point p, dword keyflags)
+	void RightUp(Point p, dword keyflags) override
 	{
 		Log(Format("RightUp(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   MiddleDown(Point p, dword keyflags)
+	void MiddleDown(Point p, dword keyflags) override
 	{
 		Log(Format("MiddleDown(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   MiddleDouble(Point p, dword keyflags)
+	void MiddleDouble(Point p, dword keyflags) override
 	{
 		Log(Format("MiddleDouble(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void MiddleTriple(Point p, dword keyflags)
+	void MiddleTriple(Point p, dword keyflags) override
 	{
 		Log(Format("MiddleTriple(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 	
-	virtual void MiddleDrag(Point p, dword keyflags) {
+	void MiddleDrag(Point p, dword keyflags) override {
 		Log(Format("MiddleDrag(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 	
-	virtual void MiddleHold(Point p, dword keyflags) {
+	void MiddleHold(Point p, dword keyflags) override {
 		Log(Format("MiddleHold(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   MiddleRepeat(Point p, dword keyflags)
+	void MiddleRepeat(Point p, dword keyflags) override
 	{
 		Log(Format("MiddleRepeat(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   MiddleUp(Point p, dword keyflags)
+	void MiddleUp(Point p, dword keyflags) override
 	{
 		Log(Format("MiddleUp(Point(%d, %d), %x)", p.x, p.y, (int)keyflags));
 	}
 
-	virtual void   MouseWheel(Point p, int zdelta, dword keyflags)
+	void MouseWheel(Point p, int zdelta, dword keyflags) override
 	{
 		Log(Format("MouseWheel(Point(%d, %d), %d, %x)", p.x, p.y, zdelta, (int)keyflags));
 	}
 
-	virtual void   MouseWheelH(Point p, int zdelta, dword keyflags)
+	void HorzMouseWheel(Point p, int zdelta, dword keyflags) override
 	{
-		Log(Format("MouseWheelH(Point(%d, %d), %d, %x)", p.x, p.y, zdelta, (int)keyflags));
+		Log(Format("HorzMouseWheel(Point(%d, %d), %d, %x)", p.x, p.y, zdelta, (int)keyflags));
 	}
 
-	virtual void   MouseLeave()
+	void MouseLeave() override
 	{
 		Log("MouseLeave");
 	}
 
-	virtual Image  CursorImage(Point p, dword keyflags)
+	Image CursorImage(Point p, dword keyflags) override
 	{
 		return keyflags & K_CTRL ? CtrlImg::swap_color_cursor() : Image::Arrow();
 	}
 
-	virtual bool   Key(dword key, int count)
+	bool Key(dword key, int count) override
 	{
 		Log(Format("Key(%x, %d) ", (int)key, count) + GetKeyDescEx(key));
 		return false;
 	}
 
-	virtual void   GotFocus()
+	void GotFocus() override
 	{
 		Log("GotFocus");
 	}
 
-	virtual void   LostFocus()
+	void LostFocus() override
 	{
 		Log("LostFocus");
 	}
 
-	virtual bool   HotKey(dword key)
+	bool HotKey(dword key) override
 	{
 		Log(Format("HotKey(%x) ", (int)key) + GetKeyDescEx(key));
 		return false;
 	}
 
-	virtual void   ChildGotFocus()
+	void ChildGotFocus() override
 	{
 		Log("ChildGotFocus");
 	}
 
-	virtual void   ChildLostFocus()
+	void ChildLostFocus() override
 	{
 		Log("ChildLostFocus");
 	}
 
-	virtual void   Layout()
+	void Layout() override
 	{
 		Log("Layout");
 	}
 
 	App()
 	{
+		Title("Events");
 		SetFrame(InsetFrame());
 		AddFrame(InsetFrame());
 		AddFrame(InsetFrame());
