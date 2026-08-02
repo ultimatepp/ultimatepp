881a1e533f84ccd92b0a1494b0a8035d2a741565
Zbigniew Rębacz <klugier@yahoo.com>
2026-08-02 17:59:50 +0200

    .fix to compile

--- uppsrc/ide/Methods.cpp
+++ uppsrc/ide/Methods.cpp
@@ -884,7 +884,7 @@ void Ide::IncludeAddPkgConfig(String& include_path, const String& clang_method)
 	const Workspace& wspc = GetIdeWorkspace();
 	Host host;
 	if(clang_method.GetCount())
-		CreateHost(host, clang_method, false, false, 0);
+		CreateHost(host, clang_method, false, false, 0, false);
 	else
 		CreateHost(host, false, false);
 	One<Builder> b = CreateBuilder(&host);
